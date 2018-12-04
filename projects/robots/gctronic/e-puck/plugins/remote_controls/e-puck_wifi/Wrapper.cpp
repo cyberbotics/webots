fdf63f5e6ddbf16582a763b7bbed0f51278ea7fd
Olivier Michel <Olivier.Michel@cyberbotics.com>
2018-12-04 17:37:53 +0100

    Added license headers

--- projects/robots/gctronic/e-puck/plugins/remote_controls/e-puck_wifi/Wrapper.cpp
+++ projects/robots/gctronic/e-puck/plugins/remote_controls/e-puck_wifi/Wrapper.cpp
@@ -1,3 +1,17 @@
+// Copyright 1996-2018 Cyberbotics Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include "Wrapper.hpp"
 
 #include "Camera.hpp"
@@ -173,7 +187,7 @@ int Wrapper::robotStep(int step) {
           exit(1);
         }
         log("Got image (location = %p size = %d)\n", image, IMAGE_SIZE);
-  #ifndef NDEBUG
+#ifndef NDEBUG
         // save the image as BMP file to debug
         static int i = 0;
         char filename[32];
@@ -183,7 +197,7 @@ int Wrapper::robotStep(int step) {
         save_bmp_image(filename, rgb888, 160, 120);
         free(rgb888);
         i++;
-  #endif
+#endif
         got_camera_image++;
       } else if (header == 0x02) {  // sensor data
         if (cCommunication->receive((char *)sensor_data, SENSOR_DATA_SIZE, true) == -1) {
