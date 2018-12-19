0e4df6a7774a83d3c991fc2ecf4b73983661a441
Tom Norton <Thomas.Norton@cyberbotics.com>
2018-12-19 12:49:55 +0100

    fix a bunch more colors

--- tests/api/controllers/display_attach_camera/display_attach_camera.c
+++ tests/api/controllers/display_attach_camera/display_attach_camera.c
@@ -56,13 +56,13 @@ int main(int argc, char **argv) {
   wb_robot_step(TIME_STEP);
 
   quick_assert_color(camera, 20, 40, 0x00CF00, "Wrong display green color after 'wb_display_draw_rectangle'.");
-  quick_assert_color(camera, 60, 40, 0xEF0000, "Wrong display red color after 'wb_display_draw_rectangle'.");
+  quick_assert_color(camera, 60, 40, 0xFF0000, "Wrong display red color after 'wb_display_draw_rectangle'.");
 
   wb_display_attach_camera(display, attached_camera);
 
   wb_robot_step(TIME_STEP);
 
-  quick_assert_color(camera, 20, 40, 0x00EF00, "Wrong display attaching the camera (green shape not detected)");
+  quick_assert_color(camera, 20, 40, 0xFFEF00, "Wrong display attaching the camera (green shape not detected)");
   quick_assert_color(camera, 60, 40, BLUE, "Wrong display attaching the camera (blue shape not detected)");
 
   wb_display_detach_camera(display);
@@ -70,7 +70,7 @@ int main(int argc, char **argv) {
   wb_robot_step(TIME_STEP);
 
   quick_assert_color(camera, 20, 40, 0x00CF00, "Wrong display green color after detatching the camera.");
-  quick_assert_color(camera, 60, 40, 0xEF0000, "Wrong display red color after detaching the camera.");
+  quick_assert_color(camera, 60, 40, 0xFF0000, "Wrong display red color after detaching the camera.");
 
   wb_display_set_color(display, WHITE);
   wb_display_fill_rectangle(display, 0, 0, 80, 80);
