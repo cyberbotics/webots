d9fb8c2245a28a1cace3a785fba0fa9ae082055e
Fabien Rohrer <fabien.rohrer@gmail.com>
2019-01-24 16:47:58 +0100

    Add comments

--- resources/sumo_exporter/node_extractor.py
+++ resources/sumo_exporter/node_extractor.py
@@ -2,16 +2,18 @@ import re
 
 
 class NodeExtractor(object):
+    """Helper class to extract basic root nodes string."""
     def __init__(self, wbtFilename):
         with open(wbtFilename, 'r') as f:
             self.wbtContent = f.read()
 
     def extractRootNodes(self, nodeName):
         nodes = []
+        # Iter on "nodeName {" and then count the curly brackets until the node is closed.
         for m in re.finditer(r'[^a-zA-Z]%s\s*\{' % (nodeName), self.wbtContent):
             nodeString = nodeName + ' {'
             curlyBracketCounter = 1
-            index = m.end()
+            index = m.end()  # => character after the first open curly bracket.
             while curlyBracketCounter != 0:
                 c = self.wbtContent[index]
                 nodeString += c
