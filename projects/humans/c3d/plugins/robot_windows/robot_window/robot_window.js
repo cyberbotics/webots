efed3a60c21b0dfa7f204f106701d4f91f9deb34
DavidMansolino <David.Mansolino@cyberbotics.com>
2019-02-01 09:45:33 +0100

    Feature C3D

---
+++ projects/humans/c3d/plugins/robot_windows/robot_window/robot_window.js
@@ -0,0 +1,17 @@
+var robotWindow = null;
+
+function checkboxCallback(checkbox) {
+  if (checkbox.checked)
+    robotWindow.send(checkbox.getAttribute('marker') + ':enable');
+  else
+    robotWindow.send(checkbox.getAttribute('marker') + ':disable');
+}
+
+webots.window('robot_window').receive = function(message, robot) {
+  robotWindow = this;
+  names = message.split(" ");
+  for (var i = 0; i < names.length; i++) {
+    document.body.innerHTML +='<input type="checkbox" title="Show/hide this marker." marker="' + names[i] + '" onclick="checkboxCallback(this)" checked/>';
+    document.body.innerHTML += ' ' + names[i] + '</br>';
+  }
+}
