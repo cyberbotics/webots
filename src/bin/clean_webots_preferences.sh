cadfc977098fface61dccc4f89156e96be429f6c
David Mansolino <David.Mansolino@cyberbotics.com>
2019-02-07 11:52:54 +0100

    Update clean_webots_preferences.sh

--- src/bin/clean_webots_preferences.sh
+++ src/bin/clean_webots_preferences.sh
@@ -7,6 +7,11 @@ if [ "$(uname)" == "Darwin" ]; then
     defaults remove $n;
     rm $HOME/Library/Preferences/$n.plist;
   done
+elif [[ "$OSTYPE" == "linux-gnu" ]]; then
+  for n in `find $HOME/.config/Cyberbotics -name Webots-*.conf`; do
+    echo Remove "$n";
+    rm $n;
+  done
 else
   echo "Unsupported OS"
   exit 1
