dea226d1ad859e9da961415b857506cde19d02a6
Fabien Rohrer <fabien.rohrer@gmail.com>
2019-02-12 15:27:01 +0100

    Obvious minor optimizations

--- src/webots/nodes/WbImageTexture.cpp
+++ src/webots/nodes/WbImageTexture.cpp
@@ -103,50 +103,47 @@ static void blurred(QImage &result, int radius) {
   int rgba[4];
   unsigned char *p;
 
-  int i1 = 0;
-  int i2 = 3;
-
-  for (int col = c1; col <= c2; col++) {
+  for (int col = c1; col <= c2; ++col) {
     p = result.scanLine(r1) + col * 4;
-    for (int i = i1; i <= i2; i++)
+    for (int i = 0; i < 4; ++i)
       rgba[i] = p[i] << 4;
 
     p += bpl;
-    for (int j = r1; j < r2; j++, p += bpl)
-      for (int i = i1; i <= i2; i++)
+    for (int j = r1; j < r2; ++j, p += bpl)
+      for (int i = 0; i < 4; ++i)
         p[i] = (rgba[i] += ((p[i] << 4) - rgba[i]) * alpha / 16) >> 4;
   }
 
-  for (int row = r1; row <= r2; row++) {
+  for (int row = r1; row <= r2; ++row) {
     p = result.scanLine(row) + c1 * 4;
-    for (int i = i1; i <= i2; i++)
+    for (int i = 0; i < 4; ++i)
       rgba[i] = p[i] << 4;
 
     p += 4;
-    for (int j = c1; j < c2; j++, p += 4)
-      for (int i = i1; i <= i2; i++)
+    for (int j = c1; j < c2; ++j, p += 4)
+      for (int i = 0; i < 4; ++i)
         p[i] = (rgba[i] += ((p[i] << 4) - rgba[i]) * alpha / 16) >> 4;
   }
 
-  for (int col = c1; col <= c2; col++) {
+  for (int col = c1; col <= c2; ++col) {
     p = result.scanLine(r2) + col * 4;
-    for (int i = i1; i <= i2; i++)
+    for (int i = 0; i < 4; ++i)
       rgba[i] = p[i] << 4;
 
     p -= bpl;
-    for (int j = r1; j < r2; j++, p -= bpl)
-      for (int i = i1; i <= i2; i++)
+    for (int j = r1; j < r2; ++j, p -= bpl)
+      for (int i = 0; i < 4; ++i)
         p[i] = (rgba[i] += ((p[i] << 4) - rgba[i]) * alpha / 16) >> 4;
   }
 
-  for (int row = r1; row <= r2; row++) {
+  for (int row = r1; row <= r2; ++row) {
     p = result.scanLine(row) + c2 * 4;
-    for (int i = i1; i <= i2; i++)
+    for (int i = 0; i < 4; ++i)
       rgba[i] = p[i] << 4;
 
     p -= 4;
-    for (int j = c1; j < c2; j++, p -= 4)
-      for (int i = i1; i <= i2; i++)
+    for (int j = c1; j < c2; ++j, p -= 4)
+      for (int i = 0; i < 4; ++i)
         p[i] = (rgba[i] += ((p[i] << 4) - rgba[i]) * alpha / 16) >> 4;
   }
 }
