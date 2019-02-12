93e1cb1ea39bc48627866e8372c8abc0c9c99eb1
Fabien Rohrer <fabien.rohrer@gmail.com>
2019-02-12 14:24:29 +0100

    Fix the texture downscaling by blurring the image before fast scaling

--- src/webots/nodes/WbImageTexture.cpp
+++ src/webots/nodes/WbImageTexture.cpp
@@ -90,6 +90,67 @@ void WbImageTexture::postFinalize() {
     emit changed();
 }
 
+static void blurred(QImage &result, int radius) {
+  // blur filter source: https://stackoverflow.com/a/7936933/2210777
+  int tab[] = {14, 10, 8, 6, 5, 5, 4, 3, 3, 3, 3, 2, 2, 2, 2, 2, 2};
+  int alpha = (radius < 1) ? 16 : (radius > 17) ? 1 : tab[radius - 1];
+
+  const int r1 = 0;
+  const int r2 = result.height() - 1;
+  const int c1 = 0;
+  const int c2 = result.width() - 1;
+  const int bpl = result.bytesPerLine();
+  int rgba[4];
+  unsigned char *p;
+
+  int i1 = 0;
+  int i2 = 3;
+
+  for (int col = c1; col <= c2; col++) {
+    p = result.scanLine(r1) + col * 4;
+    for (int i = i1; i <= i2; i++)
+      rgba[i] = p[i] << 4;
+
+    p += bpl;
+    for (int j = r1; j < r2; j++, p += bpl)
+      for (int i = i1; i <= i2; i++)
+        p[i] = (rgba[i] += ((p[i] << 4) - rgba[i]) * alpha / 16) >> 4;
+  }
+
+  for (int row = r1; row <= r2; row++) {
+    p = result.scanLine(row) + c1 * 4;
+    for (int i = i1; i <= i2; i++)
+      rgba[i] = p[i] << 4;
+
+    p += 4;
+    for (int j = c1; j < c2; j++, p += 4)
+      for (int i = i1; i <= i2; i++)
+        p[i] = (rgba[i] += ((p[i] << 4) - rgba[i]) * alpha / 16) >> 4;
+  }
+
+  for (int col = c1; col <= c2; col++) {
+    p = result.scanLine(r2) + col * 4;
+    for (int i = i1; i <= i2; i++)
+      rgba[i] = p[i] << 4;
+
+    p -= bpl;
+    for (int j = r1; j < r2; j++, p -= bpl)
+      for (int i = i1; i <= i2; i++)
+        p[i] = (rgba[i] += ((p[i] << 4) - rgba[i]) * alpha / 16) >> 4;
+  }
+
+  for (int row = r1; row <= r2; row++) {
+    p = result.scanLine(row) + c2 * 4;
+    for (int i = i1; i <= i2; i++)
+      rgba[i] = p[i] << 4;
+
+    p -= 4;
+    for (int j = c1; j < c2; j++, p -= 4)
+      for (int i = i1; i <= i2; i++)
+        p[i] = (rgba[i] += ((p[i] << 4) - rgba[i]) * alpha / 16) >> 4;
+  }
+}
+
 void WbImageTexture::updateWrenTexture() {
   destroyWrenTexture();
 
@@ -135,7 +196,13 @@ void WbImageTexture::updateWrenTexture() {
       }
 
       if (mImage->width() != width || mImage->height() != height) {
-        QImage tmp = mImage->scaled(width, height, Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
+        // Qt::SmoothTransformation alterates the alpha channel.
+        // Qt::FastTransformation creates alias effects.
+        // Blurring the image before scaling it without filter (Qt::FastTransformation) is the solution given in litterature, for example here:
+        // - https://stackoverflow.com/questions/49879466/downscaling-images-using-bilinear-and-bicubic-algorithms
+        const int blurRatio = qMax(0, qMax(mImage->width() / width, mImage->height() / height) - 1);
+        blurred(*mImage, blurRatio);
+        QImage tmp = mImage->scaled(width, height, Qt::IgnoreAspectRatio, Qt::FastTransformation);
         mImage->swap(tmp);
       }
 
