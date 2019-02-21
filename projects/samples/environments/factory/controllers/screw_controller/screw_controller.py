05363b720d6526b9147b05dc857d15fc0b96d089
David Mansolino <David.Mansolino@cyberbotics.com>
2019-02-21 10:48:59 +0100

    Cleanup controllers.

--- projects/samples/environments/factory/controllers/screw_controller/screw_controller.py
+++ projects/samples/environments/factory/controllers/screw_controller/screw_controller.py
@@ -1,17 +1,28 @@
+# Copyright 1996-2018 Cyberbotics Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 """screw_controller controller."""
 
-# You may need to import some classes of the controller module. Ex:
-#  from controller import Robot, LED, DistanceSensor
 from controller import Robot
 
-# create the Robot instance.
 robot = Robot()
 
-# get the time step of the current world.
 timestep = int(robot.getBasicTimeStep())
 
 motor = robot.getMotor('linear motor')
- 
+
 positionSensor = robot.getPositionSensor('position sensor')
 positionSensor.enable(timestep)
 
@@ -21,4 +32,3 @@ while robot.step(timestep) != -1:
     minPosition = motor.getMinPosition()
     targetPosition = max(min(targetPosition, maxPosition), minPosition)
     motor.setPosition(targetPosition)
-    
