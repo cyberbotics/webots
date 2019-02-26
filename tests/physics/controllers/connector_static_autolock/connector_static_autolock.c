1234953796c9fc507462665254ab6e4a105b2ab1
David Mansolino <David.Mansolino@cyberbotics.com>
2019-02-26 10:16:34 +0100

    Added static connector test.

---
+++ tests/physics/controllers/connector_static_autolock/connector_static_autolock.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+#include <webots/robot.h>
+#include <webots/supervisor.h>
+
+#include "../../../lib/ts_assertion.h"
+#include "../../../lib/ts_utils.h"
+
+#define TIME_STEP 32
+
+int main(int argc, char **argv) {
+  double previous_distance = 0;
+  ts_setup(argv[0]);
+
+  WbNodeRef solid = wb_supervisor_node_get_from_def("SOLID");
+
+  WbDeviceTag connector = wb_robot_get_device("connector");
+  wb_connector_enable_presence(connector, TIME_STEP);
+
+  ts_assert_boolean_equal(wb_connector_get_presence(connector) == 1,
+                          "Connectors are expected to be in contact at the beginning.");
+
+  wb_connector_lock(connector);
+
+  wb_robot_step(TIME_STEP);
+
+  for (int i = 0; i < 100; ++i)
+    wb_robot_step(TIME_STEP);
+
+  const double *position = wb_supervisor_node_get_position(solid);
+  const double expected_position[3] = {0.25, 0.2, 0.0};
+  ts_assert_vec3_in_delta(position[0], position[1 position[2], 0.25, 0.2, 0.0, 0.001,
+                          "Solid not at teh expected position.")
+
+  ts_send_success();
+  return EXIT_SUCCESS;
+}
