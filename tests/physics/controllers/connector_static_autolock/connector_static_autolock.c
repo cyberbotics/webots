c546e3e4eefa918680f499d8b182afb6f680a00f
David Mansolino <David.Mansolino@cyberbotics.com>
2019-02-26 11:34:04 +0100

    Improved test.

--- tests/physics/controllers/connector_static_autolock/connector_static_autolock.c
+++ tests/physics/controllers/connector_static_autolock/connector_static_autolock.c
@@ -16,6 +16,8 @@ int main(int argc, char **argv) {
 
   WbDeviceTag connector = wb_robot_get_device("connector");
   wb_connector_enable_presence(connector, TIME_STEP);
+  
+  wb_robot_step(TIME_STEP);
 
   ts_assert_boolean_equal(wb_connector_get_presence(connector) == 1,
                           "Connectors are expected to be in contact at the beginning.");
@@ -28,7 +30,7 @@ int main(int argc, char **argv) {
     wb_robot_step(TIME_STEP);
 
   const double *position = wb_supervisor_node_get_position(solid);
-  ts_assert_vec3_in_delta(position[0], position[1], position[2], 0.25, 0.2, 0.0, 0.001, "Solid not at teh expected position.");
+  ts_assert_vec3_in_delta(position[0], position[1], position[2], 0.25, 0.2, 0.0, 0.005, "Solid not at the expected position.");
 
   ts_send_success();
   return EXIT_SUCCESS;
