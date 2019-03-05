360c6d5b190762ee94223ce28d4cea28148f5324
DavidMansolino <David.Mansolino@cyberbotics.com>
2019-03-05 17:05:24 +0100

    Finalized integration of GRF.

--- projects/humans/c3d/controllers/c3d_viewer/c3d_viewer.py
+++ projects/humans/c3d/controllers/c3d_viewer/c3d_viewer.py
@@ -150,10 +150,10 @@ for i in range(markerField.getCount()):
 # ground reaction forces (GRF)
 grfList = []
 names = ['LGroundReaction', 'RGroundReaction']
-for name in names:
-    if name + 'Force' in labels and name + 'Moment' in labels:
-        grfList.append({'name': name})
-        markerField.importMFNodeFromString(-1, 'C3dGrf {}')
+for i in range(len(names)):
+    if names[i] + 'Force' in labels and names[i] + 'Moment' in labels:
+        grfList.append({'name': names[i]})
+        markerField.importMFNodeFromString(-1, 'C3dGrf { translation %s %s %s}' % (sys.argv[3 + 3 * i], sys.argv[4 + 3 * i], sys.argv[5 + 3 * i]))
         grfList[-1]['node'] = markerField.getMFNode(-1)
         grfList[-1]['translation'] = grfList[-1]['node'].getField('translation')
         grfList[-1]['point'] = grfList[-1]['node'].getField('point')
