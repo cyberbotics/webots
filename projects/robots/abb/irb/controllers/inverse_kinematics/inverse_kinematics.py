b5da1be8db0878e7b2963ede7f8c45f264571981
Fabien Rohrer <fabien.rohrer@gmail.com>
2019-03-12 10:43:10 +0100

    Proofread

--- projects/robots/abb/irb/controllers/inverse_kinematics/inverse_kinematics.py
+++ projects/robots/abb/irb/controllers/inverse_kinematics/inverse_kinematics.py
@@ -1,17 +1,18 @@
-"""Let the arm hand points to the "TARGET" solid using inverse kinematics (ikpy)."""
+"""Demonstration of inverse kinematics using the "ikpy" Python module."""
 
 try:
     from ikpy.chain import Chain
     from ikpy.link import OriginLink, URDFLink
 except ImportError:
     import sys
-    sys.exit('The Python module "ikpy" is not installed. Please install it with "pip" to run this example. Typically: "pip install ikpy"')
+    sys.exit('The "ikpy" Python module is not installed. To run this sample, please upgrade "pip" and install ikpy with this command: "pip install ikpy"')
 
+import math
 from controller import Supervisor
 
 # Create the arm chain.
-# The values below are taken from the Irb4600-40.proto file looking at the HingeJoint node fields.
-# Note: it's necessary to give up to the "E motor" bone to ikpy, because this bone defines the hand position.
+# The constants below have been manually extracted from the Irb4600-40.proto file, looking at the HingeJoint node fields.
+# The chain should contain the "E motor" bone, because this bone defines the hand position.
 armChain = Chain(name='arm', links=[
     OriginLink(),
     URDFLink(
@@ -19,41 +20,44 @@ armChain = Chain(name='arm', links=[
         bounds=[-3.1415, 3.1415],
         translation_vector=[0, 0, 0.159498],
         orientation=[0, 0, 0],
-        rotation=[0, 0, -1],
+        rotation=[0, 0, -1]
     ),
     URDFLink(
         name="B motor",
         bounds=[-1.5708, 2.61799],
         translation_vector=[0.178445, -0.122498, 0.334888],
         orientation=[0, 0, 0],
-        rotation=[0, -1, 0],
+        rotation=[0, -1, 0]
     ),
     URDFLink(
         name="C motor",
         bounds=[-3.1415, 1.309],
         translation_vector=[-0.003447, -0.0267, 1.095594],
         orientation=[0, 0, 0],
-        rotation=[0, -1, 0],
+        rotation=[0, -1, 0]
     ),
     URDFLink(
         name="D motor",
         bounds=[-6.98132, 6.98132],
         translation_vector=[0.340095, 0.149198, 0.174998],
         orientation=[0, 0, 0],
-        rotation=[1, 0, 0],
+        rotation=[1, 0, 0]
     ),
     URDFLink(
         name="E motor",
         bounds=[-2.18166, 2.0944],
         translation_vector=[0.929888, 0, 0],
         orientation=[0, 0, 0],
-        rotation=[0, 1, 0],
+        rotation=[0, 1, 0]
     )
 ])
 
 # Initialize the Webots Supervisor.
 supervisor = Supervisor()
-print('Move the green sphere to move the arm...')
+timeStep = int(4 * supervisor.getBasicTimeStep())
+
+# Initialize the Pen device.
+supervisor.getPen('pen').write(False)
 
 # Initialize the arm motors.
 motors = []
@@ -65,19 +69,54 @@ for motorName in ['A motor', 'B motor', 'C motor', 'D motor', 'E motor', 'F moto
 target = supervisor.getFromDef('TARGET')
 arm = supervisor.getFromDef('ARM')
 
-# Main loop.
-while supervisor.step(32) != -1:
+# Loop 1: Draw a circle on the paper sheet.
+print('Draw a circle on the paper sheet...')
+while supervisor.step(timeStep) != -1:
+    t = supervisor.getTime()
+
+    # Use the circle equation relatively to the arm base as an input of the IK algorithm.
+    x = 0.25 * math.cos(t) + 1.1
+    y = 0.25 * math.sin(t) - 0.95
+    z = 0.23
+
+    # Call "ikpy" to compute the inverse kinematics of the arm.
+    ikResults = armChain.inverse_kinematics([
+        [1, 0, 0, x],
+        [0, 1, 0, y],
+        [0, 0, 1, z],
+        [0, 0, 0, 1]
+    ])
+
+    # Actuate the 3 first arm motors with the IK results.
+    for i in range(3):
+        motors[i].setPosition(ikResults[i + 1])
+    # Keep the hand orientation down.
+    motors[4].setPosition(ikResults[2] + ikResults[3] + math.pi / 2)
+    # Keep the hand orientation perpendicular.
+    motors[5].setPosition(-ikResults[1])
+
+    # Conditions to start/stop drawing and leave this loop.
+    if supervisor.getTime() > 2 * math.pi + 1:
+        supervisor.getPen('pen').write(False)
+        break
+    elif supervisor.getTime() > 1:
+        # Note: start to draw at 1 second to be sure the arm is well located.
+        supervisor.getPen('pen').write(True)
+
+# Loop 2: Move the arm hand to the target.
+print('Move the green sphere to move the arm...')
+while supervisor.step(timeStep) != -1:
     # Get the absolute postion of the target and the arm base.
     targetPosition = target.getPosition()
     armPosition = arm.getPosition()
 
     # Compute the position of the target relatively to the arm.
-    # Note: x and y axis are inverted because the arm is not aligned with the Webots global axes.
+    # x and y axis are inverted because the arm is not aligned with the Webots global axes.
     x = targetPosition[0] - armPosition[0]
     y = - (targetPosition[2] - armPosition[2])
     z = targetPosition[1] - armPosition[1]
 
-    # Call ikpy to inverse the arm.
+    # Call "ikpy" to compute the inverse kinematics of the arm.
     ikResults = armChain.inverse_kinematics([
         [1, 0, 0, x],
         [0, 1, 0, y],
