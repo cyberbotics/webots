3fc83f9bdd5025eee528e614dce481cb6e4bb300
Stefania Pedrazzi <stefania.pedrazzi@cyberbotics.com>
2019-03-12 11:44:36 +0100

    Add touch events (not tested)

--- resources/web/wwi/mouse_events.js
+++ resources/web/wwi/mouse_events.js
@@ -6,12 +6,6 @@ function MouseEvents(sceneManager, contextMenu, domElement) {
   this.contextMenu = contextMenu;
   this.domElement = domElement;
   this.enabledContextMenu = true;
-  // this.controls = new THREE.OrbitControls(this.sceneManager.camera, this.sceneManager.domElement);
-
-  this.MOUSE_BUTTONS = {LEFT: THREE.MOUSE.LEFT, MIDDLE: THREE.MOUSE.MIDDLE, RIGHT: THREE.MOUSE.RIGHT};
-  this.ACTION = { NONE: -1, ROTATE: 0, DOLLY: 1, PAN: 2, TOUCH_ROTATE: 3, TOUCH_DOLLY_PAN: 4 };
-  this.EPS = 0.000001;
-  this.action = this.ACTION.NONE;
 
   this.state = {
     'initialized': false,
@@ -24,13 +18,22 @@ function MouseEvents(sceneManager, contextMenu, domElement) {
   this.enableNavigation = true;
 
   var that = this;
+  this.onmousemove = function(event) { that.onMouseMove(event); };
+  this.onmouseup = function(event) { that.onMouseUp(event); };
+  this.ontouchmove = function(event) { that.onTouchMove(event); };
+  this.ontouchend = function(event) {
+    that._clearMouseMove();
+    that.domElement.removeEventListener('touchend', that.ontouchend, true);
+    that.domElement.removeEventListener('touchmove', that.ontouchmove, true);
+
+    if (that.ontouchend)
+      that.ontouchend(event);
+  };
   domElement.addEventListener('mousedown', function(event) { that.onMouseDown(event); }, false);
   domElement.addEventListener('mouseover', function(event) { that.onMouseOver(event); }, false);
   domElement.addEventListener('mouseleave', function(event) { that.onMouseLeave(event); }, false);
   domElement.addEventListener('wheel', function(event) { that.onMouseWheel(event); }, false);
-  // domElement.addEventListener('touchstart', this.onTouchStart, false);
-  // domElement.addEventListener('touchend', this.onTouchEnd, false);
-  // domElement.addEventListener('touchmove', this.onTouchMove, false);
+  domElement.addEventListener('touchstart', function(event) { that.onTouchStart(event); }, true);
   domElement.addEventListener('contextmenu', function(event) { event.preventDefault(); }, false);
 };
 
@@ -43,35 +46,23 @@ MouseEvents.prototype = {
 
     switch (event.button) {
       case this.MOUSE_BUTTONS.LEFT:
-        if (event.ctrlKey || event.metaKey || event.shiftKey) {
-          // this.handleMouseDownPan(event);
-          this.action = this.ACTION.PAN;
-        } else {
-          // this.handleMouseDownRotate(event);
-          this.action = this.ACTION.ROTATE;
-        }
         this.state.mouseDown |= 1;
         break;
       case this.MOUSE_BUTTONS.MIDDLE:
-        // this.handleMouseDownDolly(event);
-        this.action = this.ACTION.DOLLY;
         this.state.mouseDown |= 4;
         break;
       case this.MOUSE_BUTTONS.RIGHT:
-        // this.handleMouseDownPan(event);
-        this.action = this.ACTION.PAN;
         this.state.mouseDown |= 2;
         break;
     }
 
-    var relativePosition = MouseEvents.convertMouseEventPositionToRelativePosition(this.sceneManager.renderer, event.clientX, event.clientY);
-    var screenPosition = MouseEvents.convertMouseEventPositionToScreenPosition(this.sceneManager.renderer, event.clientX, event.clientY);
-    this.intersection = this.sceneManager.pick(relativePosition, screenPosition);
+    if (this.state.mouseDown !== 0) {
+      var relativePosition = MouseEvents.convertMouseEventPositionToRelativePosition(this.sceneManager.renderer, event.clientX, event.clientY);
+      var screenPosition = MouseEvents.convertMouseEventPositionToScreenPosition(this.sceneManager.renderer, event.clientX, event.clientY);
+      this.intersection = this.sceneManager.pick(relativePosition, screenPosition);
 
-    if (this.action !== this.ACTION.NONE) {
-      var that = this;
-      document.addEventListener('mousemove', function(event) { that.onMouseMove(event); }, false);
-      document.addEventListener('mouseup', function(event) { that.onMouseUp(event); }, false);
+      document.addEventListener('mousemove', this.onmousemove, false);
+      document.addEventListener('mouseup', this.onmouseup, false);
     }
   },
 
@@ -93,25 +84,8 @@ MouseEvents.prototype = {
         default: this.state.pressedButton = 0; break;
       }
     }
-    if (this.state.mouseDown === 0) {
-      if (this.animation && this.animation.playSlider && this.animation.sliding) {
-        // TODO is this needed?
-        /* var w = event.target.clientWidth - 66; // size of the borders of the slider
-        var x = event.clientX - event.target.getBoundingClientRect().left - 48; // size of the left border (including play button) of the slider
-        var value = 100 * x / w;
-        if (value < 0)
-          value = 0;
-        else if (value >= 100)
-          value = 99.999;
-        that.animation.playSlider.slider('value', value);
-        // setting the value should trigger the change event, unfortunately, doesn't seem to work reliably,
-        // therefore, we need to trigger this event manually:
-        var ui = {};
-        ui.value = value;
-        that.animation.playSlider.slider('option', 'change').call(that.animation.playSlider, event, ui); */
-      }
+    if (this.state.mouseDown === 0)
       return;
-    }
 
     if (this.state.initialTimeStamp === null)
       // prevent applying mouse move action before drag initialization in mousedrag event
@@ -126,7 +100,6 @@ MouseEvents.prototype = {
       params.distanceToPickPosition = this.sceneManager.viewpoint.camera.position.length();
     else
       params.distanceToPickPosition = this.intersection.distance;
-
     if (params.distanceToPickPosition < 0.001) // 1 mm
       params.distanceToPickPosition = 0.001;
 
@@ -168,6 +141,9 @@ MouseEvents.prototype = {
       if (this.sceneManager.viewpoint.onCameraPositionChanged)
         this.sceneManager.viewpoint.onCameraPositionChanged();
     }
+
+    document.removeEventListener('mousemove', this.onmousemove, false);
+    document.removeEventListener('mouseup', this.onmouseup, false);
   },
 
   onMouseWheel: function(event) {
@@ -186,6 +162,8 @@ MouseEvents.prototype = {
       return;
     }
     this.sceneManager.viewpoint.zoom(this.intersection.distance, event.deltaY);
+    if (this.sceneManager.viewpoint.onCameraPositionChanged)
+      this.sceneManager.viewpoint.onCameraPositionChanged();
   },
 
   wheelTimeoutCallback: function(event) {
@@ -202,10 +180,119 @@ MouseEvents.prototype = {
     if (this.state.wheelTimeout != null) {
       clearTimeout(this.state.wheelTimeout);
       this.state.wheelTimeout = null;
-      if (this.sceneManager.viewpoint.onCameraPositionChanged)
-        this.sceneManager.viewpoint.onCameraPositionChanged();
     }
     this.state.wheelFocus = false;
+  },
+
+  onTouchMove: function(event) {
+    if (!this.enableNavigation || event.targetTouches.length === 0 || event.targetTouches.length > 2)
+      return;
+    if (this.state.initialTimeStamp === null)
+      // prevent applying mouse move action before drag initialization in mousedrag event
+      return;
+    if ((this.state.mouseDown !== 2) !== (event.targetTouches.length > 1))
+      // gesture single/multi touch changed after initialization
+      return;
+
+    var touch = event.targetTouches['0'];
+
+    var params = {};
+    params.scaleFactor = params.distanceToPickPosition * 3.35 * Math.tan(this.viewpointFieldOfView / 2);
+    var viewHeight = parseFloat($(this.sceneManager.domElement).css('height').slice(0, -2));
+    var viewWidth = parseFloat($(this.sceneManager.domElement).css('width').slice(0, -2));
+    params.scaleFactor /= Math.max(viewHeight, viewWidth);
+
+    if (this.intersection == null)
+      params.distanceToPickPosition = this.sceneManager.viewpoint.camera.position.length();
+    else
+      params.distanceToPickPosition = this.intersection.distance;
+    if (params.distanceToPickPosition < 0.001) // 1 mm
+      params.distanceToPickPosition = 0.001;
+    var x = Math.round(touch.clientX); // discard decimal values returned on android
+    var y = Math.round(touch.clientY);
+
+    if (this.state.mouseDown === 2) { // translation
+      params.dx = x - this.state.x;
+      params.dy = y - this.state.y;
+      this.sceneManager.viewpoint.translate(params);
+
+      // on small phone screens (Android) this is needed to correctly detect clicks and longClicks
+      if (this.state.initialX == null && this.state.initialY == null) {
+        this.state.initialX = Math.round(this.state.x);
+        this.state.initialY = Math.round(this.state.y);
+      }
+      if (Math.abs(params.dx) < 2 && Math.abs(params.dy) < 2 &&
+          Math.abs(this.state.initialX - x) < 5 && Math.abs(this.state.initialY - y) < 5)
+        this.state.moved = false;
+      else
+        this.state.moved = true;
+    } else {
+      var touch1 = event.targetTouches['1'];
+      var x1 = Math.round(touch1.clientX);
+      var y1 = Math.round(touch1.clientY);
+      var distanceX = x - x1;
+      var distanceY = y - y1;
+      var newTouchDistance = distanceX * distanceX + distanceY * distanceY;
+      var pinchSize = this.state.touchDistance - newTouchDistance;
+
+      var moveX1 = x - this.state.x;
+      var moveX2 = x1 - this.state.x1;
+      var moveY1 = y - this.state.y;
+      var moveY2 = y1 - this.state.y1;
+      var ratio = window.devicePixelRatio || 1;
+
+      if (Math.abs(pinchSize) > 500 * ratio) { // zoom and tilt
+        var d;
+        if (Math.abs(moveX2) < Math.abs(moveX1))
+          d = moveX1;
+        else
+          d = moveX2;
+        params.tiltAngle = 0.0004 * d;
+        params.zoomScale = params.scaleFactor * 0.015 * pinchSize;
+        this.sceneManager.viewpoint.zoomAndTilt(params);
+      } else if (Math.abs(moveY2 - moveY1) < 3 * ratio && Math.abs(moveX2 - moveX1) < 3 * ratio) { // rotation (pitch and yaw)
+        params.dx = moveX1 * 0.8;
+        params.dy = moveY1 * 0.5;
+        this.sceneManager.viewpoint.rotate(params);
+      }
+
+      this.state.touchDistance = newTouchDistance;
+      this.state.moved = true;
+    }
+
+    this.state.x = x;
+    this.state.y = y;
+    this.state.x1 = x1;
+    this.state.y1 = y1;
+    if (this.ontouchmove)
+      this.ontouchmove(event);
+  },
+
+  onTouchStart: function(event) {
+    this._initMouseMove(event.targetTouches['0']);
+    if (event.targetTouches.length === 2) {
+      var touch1 = event.targetTouches['1'];
+      this.state.x1 = touch1.clientX;
+      this.state.y1 = touch1.clientY;
+      var distanceX = this.state.x - this.state.x1;
+      var distanceY = this.state.y - this.state.y1;
+      this.state.touchDistance = distanceX * distanceX + distanceY * distanceY;
+      this.state.touchOrientation = Math.atan2(this.state.y1 - this.state.y, this.state.x1 - this.state.x);
+      this.state.mouseDown = 3; // two fingers: rotation, tilt, zoom
+    } else
+      this.state.mouseDown = 2; // 1 finger: translation or single click
+
+    this.domElement.addEventListener('touchend', this.ontouchend, true);
+    this.domElement.addEventListener('touchmove', this.ontouchmove, true);
+  },
+
+  onTouchEnd: function(event) {
+    this._clearMouseMove();
+    this.domElement.removeEventListener('touchend', this.ontouchend, true);
+    this.domElement.removeEventListener('touchmove', this.ontouchend, true);
+
+    if (this.ontouchend)
+      this.ontouchend(event);
   },
 
   _initMouseMove: function(event) {
@@ -232,121 +319,7 @@ MouseEvents.prototype = {
     this.state.initialTimeStamp = null;
     this.state.initialX = null;
     this.state.initialY = null;
-    this.action = this.ACTION.NONE;
-
-    // TODO fix listener function
-    document.removeEventListener('mousemove', this.onMouseMove, false);
-    document.removeEventListener('mouseup', this.onMouseUp, false);
   }
-
-  /* if (that.mobileDevice) {
-    that.x3dNode.addEventListener('touchmove', function(event) {
-      if (!that.enableNavigation || event.targetTouches.length === 0 || event.targetTouches.length > 2)
-        return;
-      if (that.state.initialTimeStamp === null)
-        // prevent applying mouse move action before drag initialization in mousedrag event
-        return;
-      if ((that.state.mouseDown !== 2) !== (event.targetTouches.length > 1))
-        // gesture single/multi touch changed after initialization
-        return;
-
-      var touch = event.targetTouches['0'];
-
-      var params = {};
-      var viewpoint = that.x3dSceneManager.getElementsByTagName('Viewpoint')[0];
-      params.vp = x3dom.fields.SFVec3f.parse(viewpoint.getAttribute('position'));
-      params.vo = x3dom.fields.SFVec4f.parse(viewpoint.getAttribute('orientation'));
-      params.c = Math.cos(params.vo.w);
-      params.s = Math.sin(params.vo.w);
-      params.scaleFactor = 1.90 * Math.tan(that.viewpointFieldOfView / 2);
-      var viewHeight = parseFloat($(that.x3dNode).css('height').slice(0, -2));
-      var viewWidth = parseFloat($(that.x3dNode).css('width').slice(0, -2));
-      params.scaleFactor /= Math.max(viewHeight, viewWidth);
-
-      if (that.state.pickPosition == null)
-        params.distanceToPickPosition = params.vp.length();
-      else
-        params.distanceToPickPosition = params.vp.subtract(that.state.pickPosition).length() - 0.05; // FIXME this is different from webots.
-      if (params.distanceToPickPosition < 0.001) // 1 mm
-        params.distanceToPickPosition = 0.001;
-      var x = Math.round(touch.clientX); // discard decimal values returned on android
-      var y = Math.round(touch.clientY);
-
-      if (that.state.mouseDown === 2) { // translation
-        params.dx = x - that.state.x;
-        params.dy = y - that.state.y;
-        translateViewpoint(viewpoint, params);
-
-        // on small phone screens (Android) this is needed to correctly detect clicks and longClicks
-        if (that.state.initialX == null && that.state.initialY == null) {
-          that.state.initialX = Math.round(that.state.x);
-          that.state.initialY = Math.round(that.state.y);
-        }
-        if (Math.abs(params.dx) < 2 && Math.abs(params.dy) < 2 &&
-            Math.abs(that.state.initialX - x) < 5 && Math.abs(that.state.initialY - y) < 5)
-          that.state.moved = false;
-        else
-          that.state.moved = true;
-      } else {
-        var touch1 = event.targetTouches['1'];
-        var x1 = Math.round(touch1.clientX);
-        var y1 = Math.round(touch1.clientY);
-        var distanceX = x - x1;
-        var distanceY = y - y1;
-        var newTouchDistance = distanceX * distanceX + distanceY * distanceY;
-        var pinchSize = that.state.touchDistance - newTouchDistance;
-
-        var moveX1 = x - that.state.x;
-        var moveX2 = x1 - that.state.x1;
-        var moveY1 = y - that.state.y;
-        var moveY2 = y1 - that.state.y1;
-        var ratio = window.devicePixelRatio || 1;
-
-        if (Math.abs(pinchSize) > 500 * ratio) { // zoom and tilt
-          var d;
-          if (Math.abs(moveX2) < Math.abs(moveX1))
-            d = moveX1;
-          else
-            d = moveX2;
-          params.tiltAngle = 0.0004 * d;
-          params.zoomScale = params.scaleFactor * 0.015 * pinchSize;
-          zoomAndTiltViewpoint(viewpoint, params);
-        } else if (Math.abs(moveY2 - moveY1) < 3 * ratio && Math.abs(moveX2 - moveX1) < 3 * ratio) { // rotation (pitch and yaw)
-          params.dx = moveX1 * 0.8;
-          params.dy = moveY1 * 0.5;
-          rotateViewpoint(viewpoint, params);
-        }
-
-        that.state.touchDistance = newTouchDistance;
-        that.state.moved = true;
-      }
-
-      that.state.x = x;
-      that.state.y = y;
-      that.state.x1 = x1;
-      that.state.y1 = y1;
-      if (that.ontouchmove)
-        that.ontouchmove(event);
-    }, true);
-    that.x3dNode.addEventListener('touchstart', function(event) {
-      initMouseMove(event.targetTouches['0']);
-      if (event.targetTouches.length === 2) {
-        var touch1 = event.targetTouches['1'];
-        that.state.x1 = touch1.clientX;
-        that.state.y1 = touch1.clientY;
-        var distanceX = that.state.x - that.state.x1;
-        var distanceY = that.state.y - that.state.y1;
-        that.state.touchDistance = distanceX * distanceX + distanceY * distanceY;
-        that.state.touchOrientation = Math.atan2(that.state.y1 - that.state.y, that.state.x1 - that.state.x);
-        that.state.mouseDown = 3; // two fingers: rotation, tilt, zoom
-      } else
-        that.state.mouseDown = 2; // 1 finger: translation or single click
-    }, true);
-    that.x3dNode.addEventListener('touchend', function(event) {
-      clearMouseMove();
-      if (that.ontouchend)
-        that.ontouchend(event);
-    }, true); */
 };
 
 MouseEvents.convertMouseEventPositionToScreenPosition = function(renderer, eventX, eventY) {
