42a3ecc2e6716fac70e316659df0505562473ad5
David Mansolino <David.Mansolino@cyberbotics.com>
2019-03-20 11:45:09 +0100

    Improved controller.

--- projects/robots/universal_robots/ur5e/controllers/ur5e_can_grasper/ur5e_can_grasper.c
+++ projects/robots/universal_robots/ur5e/controllers/ur5e_can_grasper/ur5e_can_grasper.c
@@ -1,38 +1,41 @@
-#include <webots/robot.h>
-#include <webots/motor.h>
 #include <webots/distance_sensor.h>
+#include <webots/motor.h>
 #include <webots/position_sensor.h>
+#include <webots/robot.h>
 
 #include <stdio.h>
 
-
 #define TIME_STEP 32
 
-enum State { WAITING, GRASPING, ROTATING, RELEASING, ROTATING_BACK }; 
-
+enum State { WAITING, GRASPING, ROTATING, RELEASING, ROTATING_BACK };
 
 int main(int argc, char **argv) {
   wb_robot_init();
   int counter = 0, i = 0;
   int state = WAITING;
-  
+  const double target_positions[] = {-1.88, -2.14, -2.38, -1.51};
+
   WbDeviceTag hand_motors[3];
   hand_motors[0] = wb_robot_get_device("finger_1_joint_1");
   hand_motors[1] = wb_robot_get_device("finger_2_joint_1");
   hand_motors[2] = wb_robot_get_device("finger_middle_joint_1");
-  WbDeviceTag ur_motor_0 = wb_robot_get_device("shoulder_lift_joint");
-  WbDeviceTag ur_motor_1 = wb_robot_get_device("wrist_1_joint");
-  
+  WbDeviceTag ur_motors[4];
+  ur_motors[0] = wb_robot_get_device("shoulder_lift_joint");
+  ur_motors[1] = wb_robot_get_device("elbow_joint");
+  ur_motors[2] = wb_robot_get_device("wrist_1_joint");
+  ur_motors[3] = wb_robot_get_device("wrist_2_joint");
+  for (i = 0; i < 4; ++i)
+    wb_motor_set_velocity(ur_motors[i], 1.0);
+
   WbDeviceTag distance_sensor = wb_robot_get_device("distance sensor");
   wb_distance_sensor_enable(distance_sensor, TIME_STEP);
-  
-  WbDeviceTag position_sensor = wb_robot_get_device("shoulder_lift_joint_sensor");
+
+  WbDeviceTag position_sensor = wb_robot_get_device("wrist_1_joint_sensor");
   wb_position_sensor_enable(position_sensor, TIME_STEP);
 
   while (wb_robot_step(TIME_STEP) != -1) {
-
     if (counter <= 0) {
-      switch(state) {
+      switch (state) {
         case WAITING:
           if (wb_distance_sensor_get_value(distance_sensor) < 500) {
             state = GRASPING;
@@ -43,23 +46,23 @@ int main(int argc, char **argv) {
           }
           break;
         case GRASPING:
-          wb_motor_set_position(ur_motor_0, -3.14159);
-          wb_motor_set_position(ur_motor_1, 3.14159);
+          for (i = 0; i < 4; ++i)
+            wb_motor_set_position(ur_motors[i], target_positions[i]);
           printf("Rotating arm\n");
           state = ROTATING;
           break;
         case ROTATING:
-          if (wb_position_sensor_get_value(position_sensor) < -3.1) {
+          if (wb_position_sensor_get_value(position_sensor) < -2.3) {
             counter = 8;
             printf("Releasing can\n");
             state = RELEASING;
             for (i = 0; i < 3; ++i)
-               wb_motor_set_position(hand_motors[i], 0);
+              wb_motor_set_position(hand_motors[i], 0);
           }
           break;
         case RELEASING:
-          wb_motor_set_position(ur_motor_0, 0);
-          wb_motor_set_position(ur_motor_1, 0);
+          for (i = 0; i < 4; ++i)
+            wb_motor_set_position(ur_motors[i], 0.0);
           printf("Rotating arm back\n");
           state = ROTATING_BACK;
           break;
@@ -71,11 +74,9 @@ int main(int argc, char **argv) {
           break;
       }
     }
-    
     counter--;
   };
 
   wb_robot_cleanup();
-
   return 0;
 }
