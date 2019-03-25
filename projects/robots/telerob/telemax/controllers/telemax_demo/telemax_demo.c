ff43b078f8ed950bcd9e38567c788c2567a8d87c
David Mansolino <David.Mansolino@cyberbotics.com>
2019-03-25 11:33:20 +0100

    Improved demo.

--- projects/robots/telerob/telemax/controllers/telemax_demo/telemax_demo.c
+++ projects/robots/telerob/telemax/controllers/telemax_demo/telemax_demo.c
@@ -23,6 +23,17 @@
 
 #define TIME_STEP 32
 
+void wait(int time) {
+  int counter = 0;
+  while (counter < time) {
+    counter += TIME_STEP;
+    if (wb_robot_step(TIME_STEP) == -1) {
+      wb_robot_cleanup();
+      return 0;
+    }
+  }
+}
+
 int main(int argc, char **argv) {
   wb_robot_init();
 
@@ -42,6 +53,15 @@ int main(int argc, char **argv) {
     arm_motors[i] = wb_robot_get_device(buffer);
   }
 
+  WbDeviceTag left_gripper = wb_robot_get_device("left gripper motor");
+  WbDeviceTag right_gripper = wb_robot_get_device("right gripper motor");
+  WbDeviceTag gripper_camera = wb_robot_get_device("gripper camera");
+
+  WbDeviceTag camera_pan = wb_robot_get_device("camera 0 pan motor");
+  WbDeviceTag camera_tilt = wb_robot_get_device("camera 0 tilt motor");
+  WbDeviceTag camera = wb_robot_get_device("camera 0");
+
+  // go out of the box
   wb_motor_set_position(front_left_track, INFINITY);
   wb_motor_set_position(front_right_track, INFINITY);
   wb_motor_set_position(rear_left_track, INFINITY);
@@ -51,33 +71,52 @@ int main(int argc, char **argv) {
   wb_motor_set_velocity(rear_left_track, 0.1);
   wb_motor_set_velocity(rear_right_track, 0.1);
 
-  wb_robot_step(20000);
+  wait(20000);
 
+  // look around with camera
   wb_motor_set_velocity(front_left_track, 0.0);
   wb_motor_set_velocity(front_right_track, 0.0);
   wb_motor_set_velocity(rear_left_track, 0.0);
   wb_motor_set_velocity(rear_right_track, 0.0);
+  wb_camera_enable(camera, TIME_STEP);
+  wb_motor_set_position(camera_pan, 1.0);
+  wait(2000);
+  wb_motor_set_position(camera_pan, -1.0);
+  wait(4000);
+  wb_motor_set_position(camera_pan, 0.0);
+  wait(2000);
+  wb_motor_set_position(camera_tilt, -0.5);
+  wait(2000);
+  wb_motor_set_position(camera_tilt, 0.5);
+  wait(4000);
+  wb_motor_set_position(camera_tilt, -0.1);
+
+  // prepare tracks for stairs
   wb_motor_set_position(front_motor, 1.0);
   wb_motor_set_position(rear_motor, -0.2);
 
-  wb_robot_step(7000);
+  wait(7000);
 
+  // climb stairs
   wb_motor_set_velocity(front_left_track, -0.1);
   wb_motor_set_velocity(front_right_track, -0.1);
   wb_motor_set_velocity(rear_left_track, 0.1);
   wb_motor_set_velocity(rear_right_track, 0.1);
 
-  wb_robot_step(13000);
+  wait(13000);
 
+  // align fron tracks with stairs
   wb_motor_set_position(front_motor, 0.25);
 
-  wb_robot_step(60000);
+  wait(60000);
 
+  // put tracks in 'flat' position
   wb_motor_set_position(front_motor, 0.0);
   wb_motor_set_position(rear_motor, 0.0);
 
-  wb_robot_step(5000);
+  wait(5000);
 
+  // go in 'high' position
   wb_motor_set_velocity(front_left_track, 0.1);
   wb_motor_set_velocity(front_right_track, 0.1);
   wb_motor_set_velocity(rear_left_track, 0.1);
@@ -85,23 +124,50 @@ int main(int argc, char **argv) {
   wb_motor_set_position(front_motor, -1.2);
   wb_motor_set_position(rear_motor, -1.2);
 
-  wb_robot_step(4500);
+  wait(4500);
 
-
+  // stop tracks and move arm
   wb_motor_set_velocity(front_left_track, 0.0);
   wb_motor_set_velocity(front_right_track, 0.0);
   wb_motor_set_velocity(rear_left_track, 0.0);
   wb_motor_set_velocity(rear_right_track, 0.0);
-  wb_motor_set_position(arm_motors[0], -1.51);
-  wb_motor_set_position(arm_motors[1], 0.69);
-  wb_motor_set_position(arm_motors[2], -0.38);
-  wb_motor_set_position(arm_motors[3], -1.63);
-  wb_motor_set_position(arm_motors[4], -0.75);
-  wb_motor_set_position(arm_motors[5], 0.57);
-  wb_motor_set_position(arm_motors[6], 0.69);
+  wb_motor_set_position(arm_motors[0], 0.0);
+  wb_motor_set_position(arm_motors[1], 0.7);
+  wb_motor_set_position(arm_motors[2], 0.0);
+  wb_motor_set_position(arm_motors[3], -1.45);
+  wb_motor_set_position(arm_motors[4], -0.0);
+  wb_motor_set_position(arm_motors[5], -0.75);
+  wb_motor_set_position(arm_motors[6], 0.0);
 
-  while (true)
-    wb_robot_step(TIME_STEP);
+  wait(5000);
+
+  // actuate gripper
+  wb_camera_enable(gripper_camera, TIME_STEP);
+  for (i = 0; i < 3; ++i) {
+    wb_motor_set_position(left_gripper, 0.012);
+    wb_motor_set_position(right_gripper, 0.012);
+    wait(1000);
+    wb_motor_set_position(left_gripper, -0.005);
+    wb_motor_set_position(right_gripper, -0.005);
+    wait(1000);
+  }
+
+  // push the door
+  wb_motor_set_velocity(front_left_track, -0.1);
+  wb_motor_set_velocity(front_right_track, -0.1);
+  wb_motor_set_velocity(rear_left_track, 0.1);
+  wb_motor_set_velocity(rear_right_track, 0.1);
+
+  wait(10000);
+
+  // rotate gripper camera to see in the other room
+  wb_motor_set_velocity(front_left_track, 0.0);
+  wb_motor_set_velocity(front_right_track, 0.0);
+  wb_motor_set_velocity(rear_left_track, 0.0);
+  wb_motor_set_velocity(rear_right_track, 0.0);
+  wb_motor_set_position(arm_motors[4], 1.38);
+  wb_motor_set_position(arm_motors[5], 0.31);
+  wb_motor_set_position(arm_motors[6], -0.75);
 
   wb_robot_cleanup();
 
