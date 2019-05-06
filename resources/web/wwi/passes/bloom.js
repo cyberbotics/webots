1315015aeb32a6c64cff63eb36c5709e3c89e931
Fabien Rohrer <fabien.rohrer@gmail.com>
2019-05-06 15:08:42 +0200

    Encapsulate gaussian blur shader

--- resources/web/wwi/passes/bloom.js
+++ resources/web/wwi/passes/bloom.js
@@ -46,9 +46,11 @@ THREE.Bloom = class Bloom extends THREE.Pass {
       console.error('Bloom relies on THREE.brightPassShader');
 
     var brightPassShader = THREE.brightPassShader;
+    console.log(brightPassShader);
     this.brightPassUniforms = THREE.UniformsUtils.clone(brightPassShader.uniforms);
-    this.brightPassUniforms[ 'threshold' ].value = threshold;
-    this.brightPassUniforms[ 'textureSize' ].value = resolution;
+    console.log(this.brightPassUniforms);
+    this.brightPassUniforms[ 'threshold' ].value = this.threshold;
+    this.brightPassUniforms[ 'textureSize' ].value = this.resolution;
 
     this.materialBrightPass = new THREE.ShaderMaterial({
       uniforms: this.brightPassUniforms,
@@ -58,14 +60,15 @@ THREE.Bloom = class Bloom extends THREE.Pass {
     });
 
     // Gaussian Blur Materials
+    if (THREE.gaussianBlur13Tap === undefined)
+      console.error('Bloom relies on THREE.gaussianBlur13Tap');
+
     this.separableBlurMaterials = [];
-    var kernelSizeArray = [ 3, 5, 7, 9, 11, 13 ];
     resx = Math.round(this.resolution.x / 2);
     resy = Math.round(this.resolution.y / 2);
 
     for (let i = 0; i < 6; i++) {
-      this.separableBlurMaterials.push(this.getSeperableBlurMaterial(kernelSizeArray[ i ]));
-
+      this.separableBlurMaterials.push(new THREE.ShaderMaterial(THREE.gaussianBlur13Tap));
       this.separableBlurMaterials[ i ].uniforms[ 'texSize' ].value = new THREE.Vector2(resx, resy);
 
       resx = Math.round(resx / 2);
@@ -221,57 +224,5 @@ THREE.Bloom = class Bloom extends THREE.Pass {
 
     renderer.setClearColor(this.oldClearColor, this.oldClearAlpha);
     renderer.autoClear = oldAutoClear;
-  }
-
-  getSeperableBlurMaterial(kernelRadius) {
-    return new THREE.ShaderMaterial({
-
-      defines: {
-        'KERNEL_RADIUS': kernelRadius,
-        'SIGMA': kernelRadius
-      },
-
-      uniforms: {
-        'colorTexture': { value: null },
-        'texSize': { value: new THREE.Vector2(0.5, 0.5) },
-        'direction': { value: new THREE.Vector2(0.5, 0.5) }
-      },
-
-      vertexShader: [
-        'varying vec2 vUv;',
-        'void main() {',
-        '  vUv = uv;',
-        '  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);',
-        '}'
-      ].join('\n'),
-      fragmentShader: [
-        '#include <common>',
-        'varying vec2 vUv;',
-        'uniform sampler2D colorTexture;',
-        'uniform vec2 texSize;',
-        'uniform vec2 direction;',
-
-        'float gaussianPdf(in float x, in float sigma) {',
-        '  return 0.39894 * exp(-0.5 * x * x/(sigma * sigma))/sigma;',
-        '}',
-
-        'void main() {',
-        '  vec2 invSize = 1.0 / texSize;',
-        '  float fSigma = float(SIGMA);',
-        '  float weightSum = gaussianPdf(0.0, fSigma);',
-        '  vec3 diffuseSum = texture2D(colorTexture, vUv).rgb * weightSum;',
-        '  for(int i = 1; i < KERNEL_RADIUS; i ++) {',
-        '    float x = float(i);',
-        '    float w = gaussianPdf(x, fSigma);',
-        '    vec2 uvOffset = direction * invSize * x;',
-        '    vec3 sample1 = texture2D(colorTexture, vUv + uvOffset).rgb;',
-        '    vec3 sample2 = texture2D(colorTexture, vUv - uvOffset).rgb;',
-        '    diffuseSum += (sample1 + sample2) * w;',
-        '    weightSum += 2.0 * w;',
-        '  }',
-        '  gl_FragColor = vec4(diffuseSum/weightSum, 1.0);',
-        '}'
-      ].join('\n')
-    });
   }
 };
