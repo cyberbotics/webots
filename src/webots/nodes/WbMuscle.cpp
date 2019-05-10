4feeb2815e9b8f7584df85e16b281ba7a543a968
David Mansolino <David.Mansolino@cyberbotics.com>
2019-05-10 11:14:19 +0200

    Fix compilation.

--- src/webots/nodes/WbMuscle.cpp
+++ src/webots/nodes/WbMuscle.cpp
@@ -55,8 +55,8 @@ void WbMuscle::init() {
 
   // deprected field
   mMaxRadius = findSFDouble("maxRadius");
-  if (mVolume->isDefault() && !mMaxRadius->isDefault())
-    warn(tr("'maxRadius' field replaced by the 'volume' field, use this new field instead.")));
+  if (mVolume->value() == 0.01 && mMaxRadius->value() != 0.0)
+    warn(tr("'maxRadius' field replaced by the 'volume' field, use this new field instead."));
 
   mEndPoint = NULL;
   mHeight = 0.1;
