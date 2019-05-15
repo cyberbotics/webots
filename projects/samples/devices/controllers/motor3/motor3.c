41b2f53f8482f8c8dc62adf1c89e5b6d130a62e6
David Mansolino <David.Mansolino@cyberbotics.com>
2019-05-15 09:45:00 +0200

    Balljoint example.

--- projects/samples/devices/controllers/motor3/motor3.c
+++ projects/samples/devices/controllers/motor3/motor3.c
@@ -15,7 +15,7 @@
  */
 
 /*
- * Description: simple example of motor position control for Hinge2Joint
+ * Description: simple example of motor position control for BallJoint
  */
 
 #include <math.h>
@@ -28,6 +28,7 @@ int main(int argc, char **argv) {
   double time_step = wb_robot_get_basic_time_step();
   WbDeviceTag motor1 = wb_robot_get_device("motor 1");
   WbDeviceTag motor2 = wb_robot_get_device("motor 2");
+  WbDeviceTag motor3 = wb_robot_get_device("motor 3");
 
   int counter = 0;
   while (wb_robot_step(time_step) != -1) {
@@ -42,9 +43,18 @@ int main(int argc, char **argv) {
       wb_motor_set_position(motor2, 1);
     else if (counter == 400)
       wb_motor_set_position(motor2, -1);
-    else if (counter > 500) {
-      wb_motor_set_position(motor1, 0.5 * cos(0.01 * counter));
-      wb_motor_set_position(motor2, 0.5 * sin(0.003 * counter));
+    else if (counter == 500)
+      wb_motor_set_position(motor2, 0);
+    else if (counter == 600)
+      wb_motor_set_position(motor3, 1);
+    else if (counter == 700)
+      wb_motor_set_position(motor3, -1);
+    else if (counter == 800)
+      wb_motor_set_position(motor3, 0);
+    else if (counter > 900) {
+      wb_motor_set_position(motor1, cos(0.01 * counter));
+      wb_motor_set_position(motor2, sin(0.012 * counter));
+      wb_motor_set_position(motor3, cos(0.013 * counter));
     }
   }
 
