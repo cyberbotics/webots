e6f695dd54ee9b837c2b04cbf2f0162fa388c33c
Fabien Rohrer <fabien.rohrer@gmail.com>
2019-05-29 15:09:12 +0200

    Fix Access-Control-Allow-Origin Chrome issue

--- src/webots/core/WbHttpReply.cpp
+++ src/webots/core/WbHttpReply.cpp
@@ -3,16 +3,23 @@
 #include <QtCore/QFile>
 #include <QtCore/QFileInfo>
 
+// Remarks:
+// - "Access-Control-Allow-Origin" is to solve this error appearing at least on Chrome:
+//    `Error: No 'Access-Control-Allow-Origin' header is present on the requested resource`
+
 QByteArray WbHttpReply::forge404Reply() {
   static QByteArray reply;
-  if (reply.isEmpty())
+  if (reply.isEmpty()) {
     reply.append("HTTP/1.1 404 Not Found\r\n");
+    reply.append("Access-Control-Allow-Origin: *\r\n");
+  }
   return reply;
 }
 
 QByteArray WbHttpReply::forgeHTMLReply(const QString &htmlContent) {
   QByteArray reply;
   reply.append("HTTP/1.1 200 OK\r\n");
+  reply.append("Access-Control-Allow-Origin: *\r\n");
   reply.append("Content-Type: text/html\r\n");
   reply.append(QString("Content-Length: %1\r\n").arg(htmlContent.length()));
   reply.append("\r\n");
@@ -33,6 +40,7 @@ QByteArray WbHttpReply::forgeImageReply(const QString &imageFileName) {
   QString imageExtension = fi.suffix().toLower();
 
   reply.append("HTTP/1.1 200 OK\r\n");
+  reply.append("Access-Control-Allow-Origin: *\r\n");
   reply.append(QString("Content-Type: image/%1\r\n").arg(imageExtension));
   reply.append(QString("Content-Length: %1\r\n").arg(imageSize));
   reply.append("\r\n");
