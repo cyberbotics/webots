af1c05a18be49d1081fa25d187a88945f72d93c7
David Mansolino <David.Mansolino@cyberbotics.com>
2019-06-11 14:26:50 +0200

    Update test_clang_format.py

--- tests/sources/test_clang_format.py
+++ tests/sources/test_clang_format.py
@@ -116,7 +116,9 @@ class TestClangFormat(unittest.TestCase):
                         if extension in extensions:
                             sources.append(os.path.join(rootPath, fileName).replace('/', os.sep))
         curdir = os.getcwd()
+        print(curdir)
         os.chdir(self.WEBOTS_HOME)
+        print(os.getcwd())
         for source in sources:
             diff = ''
             with open(source, encoding='utf8') as file:
