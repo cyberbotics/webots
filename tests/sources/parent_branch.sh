ad31384d2cc30875e6847f2fe769379ab9474b78
Olivier Michel <Olivier.Michel@cyberbotics.com>
2019-06-11 23:34:58 +0200

    Update parent_branch.sh

--- tests/sources/parent_branch.sh
+++ tests/sources/parent_branch.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-
+# This script prints the parent branch name of the current branch
 git show-branch | grep '\*' | grep -v "$(git rev-parse --abbrev-ref HEAD)" | head -n1 | sed 's/.*\[\(.*\)\].*/\1/' | sed 's/[\^~].*//'
