ca5b26161f9c1b545bced7f68d3ce891c9481707
David Mansolino <David.Mansolino@cyberbotics.com>
2019-06-14 18:00:24 +0200

    Update set_commit_in_version.py

--- tests/set_commit_in_version.py
+++ tests/set_commit_in_version.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Test suite for ROS."""
+"""This script should be called when creating the nightly builds to add the commit id to the Webots version."""
 
 import os
 
