6b2c80d8d9bc8ac04787638b31ce42deb731dab9
David Mansolino <David.Mansolino@cyberbotics.com>
2019-06-14 17:56:19 +0200

    Update webots_distro.c

--- src/packaging/webots_distro.c
+++ src/packaging/webots_distro.c
@@ -945,8 +945,8 @@ static void create_file(const char *name, int m) {
       fprintf(fd, "chmod -Rf go-w \"/Volumes/%s\"\n", application_name);
       fprintf(fd, "sync\n");
       fprintf(fd, "sync\n");
-      //fprintf(fd, "hdiutil detach \"/Volumes/%s\"\n", application_name);
-      fprintf(fd, "hdiutil convert -format UDBZ %s.dmg -o %s-%s.dmg\n", application_name_lowercase_and_dashes,
+      fprintf(fd, "hdiutil detach \"/Volumes/%s\" -debug\n", application_name);
+      fprintf(fd, "hdiutil convert -format UDBZ %s.dmg -o %s-%s.dmg -debug\n", application_name_lowercase_and_dashes,
               application_name_lowercase_and_dashes, package_version);  // BZIP2 compression
       //fprintf(fd, "rm %s.dmg\n", application_name_lowercase_and_dashes);
       break;
