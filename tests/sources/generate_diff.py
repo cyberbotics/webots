e87b0b0ac357584464ee578d303712e1ab6d4fc8
Olivier Michel <Olivier.Michel@cyberbotics.com>
2019-06-22 22:01:57 +0200

    Cleanup

--- tests/sources/generate_diff.py
+++ tests/sources/generate_diff.py
@@ -26,10 +26,13 @@ import urllib.request
 from urllib.error import HTTPError
 
 
-def json_wget(url):
-    key = os.getenv('TRAVIS_GITHUB_API_KEY')
+def github_api(url):
+    d = time.time() - github_api.last_time
+    if d < 1:
+         time.sleep(1 - d)  # wait at least one second between GitHub API calls
+    key = os.getenv('GITHUB_API_KEY')
     req = urllib.request.Request(url)
-    req.add_header('User-Agent', 'omichel')
+    req.add_header('User-Agent', 'omichel/webots')
     if key is not None:
         req.add_header('Authorization', 'token %s' % key)
     try:
@@ -38,19 +41,19 @@ def json_wget(url):
         print(e.reason)
         print(e.info())
     content = response.read()
+    github_api.last_time = time.time()
     return json.loads(content)
 
 
+github_api.last_time = 0
 commit = os.getenv("TRAVIS_COMMIT")
 if commit is None:
     commit = subprocess.check_output(['git', 'rev-parse', 'head']).decode('utf-8').strip()
-j = json_wget('https://api.github.com/search/issues?q=' + commit)
+j = github_api('https://api.github.com/search/issues?q=' + commit)
 url = j["items"][0]["pull_request"]["url"]
-time.sleep(1)
-j = json_wget(url)
+j = github_api(url)
 branch = j["base"]["ref"]
 with open(os.path.join(os.getenv('WEBOTS_HOME'), 'tests', 'sources', 'modified_files.txt'), 'w') as file:
-    time.sleep(1)
-    j = json_wget('https://api.github.com/repos/omichel/webots/compare/' + branch + '...' + commit)
+    j = github_api('https://api.github.com/repos/omichel/webots/compare/' + branch + '...' + commit)
     for f in j['files']:
         file.write(f['filename'] + '\n')
