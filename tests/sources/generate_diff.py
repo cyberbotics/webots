7dac4734bdf7c6f835cf6a2cfbc5091fb918ccf0
Olivier Michel <Olivier.Michel@cyberbotics.com>
2019-06-22 21:18:26 +0200

    Update generate_diff.py

--- tests/sources/generate_diff.py
+++ tests/sources/generate_diff.py
@@ -14,9 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Generate a list of modified files with respect to the parent branch. If not passed as an argument, the
-parent branch is automatically computed. This list of modified files is used by Travis for testing only the
-files modified by the current pull request (during the sources tests) and hence run the CI tests
+"""Generate a list of modified files with respect to the parent branch. This list of modified files is used by Travis for
+testing only the files modified by the current pull request (during the sources tests) and hence run the CI tests
 significantly faster."""
 
 import git
@@ -43,25 +42,16 @@ def json_wget(url):
     return json.loads(content)
 
 
-curdir = os.getcwd()
-os.chdir(os.getenv('WEBOTS_HOME'))
-if len(sys.argv) != 2:  # no parent branch passed as an argument, computing it from GitHub API
-    commit = os.getenv("TRAVIS_COMMIT")
-    if commit is None:
-        commit = subprocess.check_output(['git', 'rev-parse', 'head']).decode('utf-8').strip()
-    print(commit)
-    j = json_wget('https://api.github.com/search/issues?q=' + commit)
-    url = j["items"][0]["pull_request"]["url"]
-    print(url)
-    j = json_wget(url)
-    branch = j["base"]["ref"]
-else:
-    branch = sys.argv[1]
-# branch = 'origin/' + branch
+commit = os.getenv("TRAVIS_COMMIT")
+if commit is None:
+    commit = subprocess.check_output(['git', 'rev-parse', 'head']).decode('utf-8').strip()
+print(commit)
+j = json_wget('https://api.github.com/search/issues?q=' + commit)
+url = j["items"][0]["pull_request"]["url"]
+print(url)
+j = json_wget(url)
+branch = j["base"]["ref"]
 with open(os.path.join(os.getenv('WEBOTS_HOME'), 'tests', 'sources', 'modified_files.txt'), 'w') as file:
-    #  current_branch = subprocess.check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD']).decode()
-    #  subprocess.check_output(['git', 'fetch'])
-    #  subprocess.check_output(['git', 'checkout', branch])
-    #  subprocess.check_output(['git', 'checkout', current_branch])
-    file.write(subprocess.check_output("git diff --name-only " + branch, shell=True).decode())
-os.chdir(curdir)
+    j = json_wget('https://api.github.com/repos/omichel/webots/compare/' + branch + '...' + commit)
+    for f in j['files']:
+        file.write(f['filename'] + '\n')
