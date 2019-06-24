d57d907d85aef47a91bcbb42f259dacd8bd537e3
Stefania Pedrazzi <stefaniapedrazzi@users.noreply.github.com>
2019-06-24 16:03:51 +0200

    Add comment

--- resources/web/wwi/animation.js
+++ resources/web/wwi/animation.js
@@ -24,6 +24,8 @@ class Animation { // eslint-disable-line no-unused-vars
     xmlhttp.send();
   }
 
+  // Return the animation status: play or pause.
+  // This should be used to store the current animation status and restore it later when calling webots.View.setAnimation.
   getStatus() {
     return this.gui === 'real_time' ? 'play' : 'pause';
   }
