bc98b336ee788bf9af39e2f936d39160306659a3
Fabien Rohrer <fabien.rohrer@gmail.com>
2019-06-27 09:50:33 +0200

    Support anistropy

--- resources/web/wwi/x3d.js
+++ resources/web/wwi/x3d.js
@@ -383,10 +383,17 @@ THREE.X3DLoader = class X3DLoader {
       'url': filename[0]
     };
 
+    // wrap / repeat
     var wrapS = getNodeAttribute(imageTexture, 'repeatS', 'true').toLowerCase();
     var wrapT = getNodeAttribute(imageTexture, 'repeatT', 'true').toLowerCase();
     texture.wrapS = wrapS === 'true' ? THREE.RepeatWrapping : THREE.ClampToEdgeWrapping;
     texture.wrapT = wrapT === 'true' ? THREE.RepeatWrapping : THREE.ClampToEdgeWrapping;
+
+    // anisotropy / filtering
+    texture.anisotropy = 8; // matches with the default value: `ImageTexture.filtering = 4`
+    var textureProperties = imageTexture.getElementsByTagName('TextureProperties');
+    if (textureProperties.length > 0)
+      texture.anisotropy = parseFloat(getNodeAttribute(textureProperties[0], 'anisotropicDegree', '8'));
 
     // This is the encoding used in Webots.
     texture.encoding = THREE.sRGBEncoding;
