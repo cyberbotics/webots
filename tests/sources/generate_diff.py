24ebaea99b7a4a8b031a3f7e574cd916a6217f56
Olivier Michel <Olivier.Michel@cyberbotics.com>
2019-06-27 12:35:45 +0200

    Force test

--- tests/sources/generate_diff.py
+++ tests/sources/generate_diff.py
@@ -63,7 +63,7 @@ github_api.last_time = 0
 github_api.user_agent = repo
 j = github_api('search/issues?q=' + commit)
 filename = os.path.join(os.getenv('WEBOTS_HOME'), 'tests', 'sources', 'modified_files.txt')
-if j['total_count'] == 0:  # if no PR is associated with this commit, create an empty modified_files.txt to disable tests
+if j['total_count'] == 0:  # if no PR is associated with this commit, create an empty modified_files.txt to disable the tests
     open(filename, 'w').close()
 else:
     url = j['items'][0]['pull_request']['url']
