2d55b3f256ed3309fa1d99879ac000e8011b7e81
Olivier Michel <Olivier.Michel@cyberbotics.com>
2019-07-02 08:07:32 +0200

    Update msys.sh

--- src/install_scripts/msys.sh
+++ src/install_scripts/msys.sh
@@ -7,7 +7,6 @@ cd ~
 else
 WINDOWS_DRIVE=${1:1:1}:\\${1:3}
 export WEBOTS_HOME=${WINDOWS_DRIVE////\\}
-export WEBOTS_HOME_PATH=`echo "$WEBOTS_HOME" | sed 's/\\\\/\\//g' | sed 's/://'`
 export PATH="$PYTHON37_HOME:$PYTHON37_HOME/Scripts:$1/msys64/mingw64/bin":/mingw64/bin:/usr/bin:$JAVA_HOME/bin:/c/WINDOWS/system32:/c/WINDOWS:$MATLAB_HOME/bin
 cd "$1"
 fi
