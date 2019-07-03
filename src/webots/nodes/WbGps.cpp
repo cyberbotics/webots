6fcd97bbbc511366deaaa421ffe542eabc932bb5
Olivier Michel <Olivier.Michel@cyberbotics.com>
2019-07-03 15:33:37 +0200

    Fixed clang format

--- src/webots/nodes/WbGps.cpp
+++ src/webots/nodes/WbGps.cpp
@@ -12,8 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "WbFieldChecker.hpp"
 #include "WbGps.hpp"
+
+#include "WbFieldChecker.hpp"
 #include "WbMathsUtilities.hpp"
 #include "WbRandom.hpp"
 #include "WbSFDouble.hpp"
