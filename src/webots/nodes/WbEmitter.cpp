7e7003049d81844dbec393809df165a72d56d872
Olivier Michel <Olivier.Michel@cyberbotics.com>
2019-07-03 15:18:26 +0200

    Fixed clang format

--- src/webots/nodes/WbEmitter.cpp
+++ src/webots/nodes/WbEmitter.cpp
@@ -12,8 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "WbDataPacket.hpp"
 #include "WbEmitter.hpp"
+
+#include "WbDataPacket.hpp"
 #include "WbFieldChecker.hpp"
 #include "WbReceiver.hpp"
 
