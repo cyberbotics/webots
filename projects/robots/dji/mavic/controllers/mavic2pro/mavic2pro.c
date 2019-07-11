4e8d83fb641cdda01cb29f65b7a386a12de85554
Fabien Rohrer <fabien.rohrer@gmail.com>
2019-07-11 10:07:13 +0200

    Document controller

--- projects/robots/dji/mavic/controllers/mavic2pro/mavic2pro.c
+++ projects/robots/dji/mavic/controllers/mavic2pro/mavic2pro.c
@@ -1,9 +1,36 @@
+/*
+ * Copyright 1996-2019 Cyberbotics Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Description:  Simplistic drone control:
+ * - Stabilize the robot using the embedded sensors.
+ * - Use PID technique to stabilize the vertical input (the altitude differences are integrated.)
+ * - Stabilize the camera.
+ * - Control the robot using the computer keyboard.
+ */
+
+#include <stdio.h>
 #include <stdlib.h>
 
 #include <webots/robot.h>
 
 #include <webots/camera.h>
+#include <webots/compass.h>
 #include <webots/gps.h>
+#include <webots/gyro.h>
 #include <webots/inertial_unit.h>
 #include <webots/keyboard.h>
 #include <webots/led.h>
@@ -25,7 +52,14 @@ int main(int argc, char **argv) {
   wb_inertial_unit_enable(imu, timestep);
   WbDeviceTag gps = wb_robot_get_device("gps");
   wb_gps_enable(gps, timestep);
+  WbDeviceTag compass = wb_robot_get_device("compass");
+  wb_compass_enable(compass, timestep);
+  WbDeviceTag gyro = wb_robot_get_device("gyro");
+  wb_gyro_enable(gyro, timestep);
   wb_keyboard_enable(timestep);
+  WbDeviceTag camera_roll_motor = wb_robot_get_device("camera roll");
+  WbDeviceTag camera_pitch_motor = wb_robot_get_device("camera pitch");
+  // WbDeviceTag camera_yaw_motor = wb_robot_get_device("camera yaw");  // Not used in this example.
 
   // Get propeller motors and set them to velocity mode.
   WbDeviceTag front_left_motor = wb_robot_get_device("front left propeller");
@@ -39,80 +73,115 @@ int main(int argc, char **argv) {
     wb_motor_set_velocity(motors[m], 1.0);
   }
 
+  // Display the welcome message.
+  printf("Start the drone...\n");
+
   // Wait one second.
   while (wb_robot_step(timestep) != -1) {
     if (wb_robot_get_time() > 1.0)
       break;
   }
 
-  // Constants.
-  const double k = 28.7;
-  const double kv1 = 2.5;
-  const double kv2 = 0.0002;
-  const double kr = 5.0;
-  const double kp = 5.0;
-  double tA = 1.0;
-  double sum = 0.0;
+  // Display manual control message.
+  printf("You can control the drone with your computer keyboard:\n");
+  printf("- 'up': move forward.\n");
+  printf("- 'down': move backward.\n");
+  printf("- 'right': turn right.\n");
+  printf("- 'left': turn left.\n");
+  printf("- 'shift + up': increase the target altitude.\n");
+  printf("- 'shift + down': decrease the target altitude.\n");
+  printf("- 'shift + right': strafe right.\n");
+  printf("- 'shift + left': strafe left.\n");
 
+  // Constants, empirically found.
+  const double k_vertical_thrust = 28.7;  // with this thrust, the drone lifts.
+  const double k_vertical_p = 2.5; // P constant of the vertical PID.
+  const double k_vertical_i = 0.0002; // I constant of the vertical PID.
+  const double k_roll_p = 5.0; // P constant of the roll PID.
+  const double k_pitch_p = 5.0; // P constant of the pitch PID.
+
+  // Variables.
+  double target_altitude = 1.0;  // The target altitude. Can be changed by the user.
+  double sum_altitude = 0.0;     // Store the altitude difference sum.
+
+  // Main loop
   while (wb_robot_step(timestep) != -1) {
-    bool led_state = ((int)(wb_robot_get_time())) % 2;
+    const double time = wb_robot_get_time();  // in seconds.
+
+    // Retrieve robot position using the sensors.
+    const double roll = wb_inertial_unit_get_roll_pitch_yaw(imu)[0] + M_PI / 2.0;
+    const double pitch = wb_inertial_unit_get_roll_pitch_yaw(imu)[1];
+    const double altitude = wb_gps_get_values(gps)[1];
+    const double roll_acceleration = wb_gyro_get_values(gyro)[0];
+    const double pitch_acceleration = wb_gyro_get_values(gyro)[1];
+
+    // Blink the front LEDs alternatively with a 1 second rate.
+    const bool led_state = ((int)time) % 2;
     wb_led_set(front_left_led, led_state);
     wb_led_set(front_right_led, !led_state);
 
-    double roll = wb_inertial_unit_get_roll_pitch_yaw(imu)[0];
-    double pitch = wb_inertial_unit_get_roll_pitch_yaw(imu)[1];
-    double y = wb_gps_get_values(gps)[1];
+    // Stabilize the Camera by actuating the camera motors according to the gyro feedback.
+    wb_motor_set_position(camera_roll_motor, -0.115 * roll_acceleration);
+    wb_motor_set_position(camera_pitch_motor, -0.1 * pitch_acceleration);
 
-    double a = 0.0;
-    double b = 0.0;
-    double c = 0.0;
-
+    // Transform the keyboard input to disturbances on the stabilization algorithm.
+    double roll_disturbance = 0.0;
+    double pitch_disturbance = 0.0;
+    double yaw_disturbance = 0.0;
     int key = wb_keyboard_get_key();
     while (key > 0) {
       switch (key) {
         case WB_KEYBOARD_UP:
-          a = 0.3;
+          pitch_disturbance = 0.3;
           break;
         case WB_KEYBOARD_DOWN:
-          a = -0.3;
+          pitch_disturbance = -0.3;
           break;
         case WB_KEYBOARD_RIGHT:
-          b = 0.5;
+          yaw_disturbance = 0.5;
           break;
         case WB_KEYBOARD_LEFT:
-          b = -0.5;
+          yaw_disturbance = -0.5;
           break;
         case (WB_KEYBOARD_SHIFT + WB_KEYBOARD_RIGHT):
-          c = -0.3;
+          roll_disturbance = -0.3;
           break;
         case (WB_KEYBOARD_SHIFT + WB_KEYBOARD_LEFT):
-          c = 0.3;
+          roll_disturbance = 0.3;
           break;
         case (WB_KEYBOARD_SHIFT + WB_KEYBOARD_UP):
-          tA += 0.05;
+          target_altitude += 0.01;
+          printf("target altitude: %f [m]\n", target_altitude);
           break;
         case (WB_KEYBOARD_SHIFT + WB_KEYBOARD_DOWN):
-          tA -= 0.05;
+          target_altitude -= 0.01;
+          printf("target altitude: %f [m]\n", target_altitude);
           break;
       }
       key = wb_keyboard_get_key();
     }
 
-    double dA = tA - y;
-    if (SIGN(dA) != SIGN(sum))
-      sum = 0.0;
-    sum += dA;
+    // Update `sum_altitude`.
+    const double difference_altitude = target_altitude - altitude;
+    if (SIGN(difference_altitude) != SIGN(sum_altitude))
+      sum_altitude = 0.0;  // Reset windup.
+    sum_altitude += difference_altitude;
 
-    roll += 1.5708;
-    double r = kr * CLAMP(roll, -0.5, 0.5) + c;
-    double p = kp * CLAMP(pitch, -0.5, 0.5) + a;
-    double yF = b;
-    double v = k + kv1 * CLAMP(dA, -0.05, 0.05) + kv2 * sum;
+    // Compute the roll, pitch, yaw and vertical inputs.
+    const double roll_input = k_roll_p * CLAMP(roll, -0.5, 0.5) + roll_disturbance;
+    const double pitch_input = k_pitch_p * CLAMP(pitch, -0.5, 0.5) + pitch_disturbance;
+    const double yaw_input = yaw_disturbance;
+    const double vertical_input = k_vertical_p * CLAMP(difference_altitude, -0.05, 0.05) + k_vertical_i * sum_altitude;
 
-    wb_motor_set_velocity(front_left_motor, v - r - p + yF);
-    wb_motor_set_velocity(front_right_motor, -(v + r - p - yF));
-    wb_motor_set_velocity(rear_left_motor, -(v - r + p - yF));
-    wb_motor_set_velocity(rear_right_motor, v + r + p + yF);
+    // Actuate the motors taking into consideration all the computed inputs.
+    const double front_left_motor_input = k_vertical_thrust + vertical_input - roll_input - pitch_input + yaw_input;
+    const double front_right_motor_input = k_vertical_thrust + vertical_input + roll_input - pitch_input - yaw_input;
+    const double rear_left_motor_input = k_vertical_thrust + vertical_input - roll_input + pitch_input - yaw_input;
+    const double rear_right_motor_input = k_vertical_thrust + vertical_input + roll_input + pitch_input + yaw_input;
+    wb_motor_set_velocity(front_left_motor, front_left_motor_input);
+    wb_motor_set_velocity(front_right_motor, -front_right_motor_input);
+    wb_motor_set_velocity(rear_left_motor, -rear_left_motor_input);
+    wb_motor_set_velocity(rear_right_motor, rear_right_motor_input);
   };
 
   wb_robot_cleanup();
