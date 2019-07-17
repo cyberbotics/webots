331a9a1cdd0102d52211cec78597a929412a95ae
Fabien Rohrer <fabien.rohrer@gmail.com>
2019-07-17 11:35:23 +0200

    Improve control

--- projects/samples/devices/controllers/brake/brake.c
+++ projects/samples/devices/controllers/brake/brake.c
@@ -11,16 +11,26 @@ int main(int argc, char **argv) {
 
   WbDeviceTag motor = wb_robot_get_device("motor");
   wb_motor_set_position(motor, INFINITY);
-  wb_motor_set_velocity(motor, 1.0);
+  wb_motor_set_torque(motor, 1.0);
+
+  WbDeviceTag linear_motor_a = wb_robot_get_device("linear motor a");
+  WbDeviceTag linear_motor_b = wb_robot_get_device("linear motor b");
 
   WbDeviceTag brake = wb_robot_get_device("brake");
 
   while (wb_robot_step(time_step) != -1) {
     if (wb_robot_get_time() > 4.0) {
-      printf("BRAKKKKE!!\n");
+      printf("BRAKE!\n");
+      wb_motor_set_position(linear_motor_a, wb_motor_get_max_position(linear_motor_a));
+      wb_motor_set_position(linear_motor_b, wb_motor_get_max_position(linear_motor_b));
       wb_brake_set_damping_constant(brake, 100000.0);
+      wb_motor_set_torque(motor, 0.0);
+      break;
     }
   };
+
+  while (wb_robot_step(time_step) != -1) {
+  }
 
   wb_robot_cleanup();
 
