80e8529d6c358d5aa33db27b0addc07317ffd1bc
Fabien Rohrer <fabien.rohrer@gmail.com>
2019-07-17 14:07:58 +0200

    Add comments

--- projects/samples/devices/controllers/accelerometer/accelerometer.c
+++ projects/samples/devices/controllers/accelerometer/accelerometer.c
@@ -15,7 +15,9 @@
  */
 
 /*
- * Description: TODO
+ * Description:
+ *   Turn the robot around itself.
+ *   Switch on the most bottom LED using the gravity force given by the accelerometer feedback.
  */
 
 #include <webots/accelerometer.h>
@@ -25,17 +27,19 @@
 
 int main(int argc, char **argv) {
   wb_robot_init();
-
   int time_step = (int)wb_robot_get_basic_time_step();
 
+  // Get the accelerometer and enable it.
   WbDeviceTag accelerometer = wb_robot_get_device("accelerometer");
   wb_accelerometer_enable(accelerometer, time_step);
 
+  // Get the LEDs.
   WbDeviceTag front_led = wb_robot_get_device("front led");
   WbDeviceTag back_led = wb_robot_get_device("back led");
   WbDeviceTag left_led = wb_robot_get_device("left led");
   WbDeviceTag right_led = wb_robot_get_device("right led");
 
+  // Get the motors, and actuate them in velocity mode to make the robot turn around itself.
   WbDeviceTag left_motor = wb_robot_get_device("left wheel motor");
   WbDeviceTag right_motor = wb_robot_get_device("right wheel motor");
   wb_motor_set_position(left_motor, INFINITY);
@@ -44,8 +48,10 @@ int main(int argc, char **argv) {
   wb_motor_set_velocity(right_motor, -0.5);
 
   while (wb_robot_step(time_step) != -1) {
+    // Get the acceleration vector, which is close the gravity vector.
     const double *acceleration = wb_accelerometer_get_values(accelerometer);
 
+    // Actuate the LEDs according to the acceleration vector.
     if (fabs(acceleration[0]) > fabs(acceleration[2])) {
       wb_led_set(front_led, false);
       wb_led_set(back_led, false);
