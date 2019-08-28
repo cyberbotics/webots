d343e83ccd3a7e22a73f8dcc099cb8506b1fcd18
Fabien Rohrer <fabien.rohrer@gmail.com>
2019-08-28 14:20:02 +0200

    Minor refactor

--- scripts/background/equirectangular_to_cubemap.py
+++ scripts/background/equirectangular_to_cubemap.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python
 
-"""Convert equirectanglar image to cubemap images."""
+"""Convert a equirectanglar HDR image to cubemap HDR images."""
 
 import optparse
-
-# from PIL import Image
-
 from hdr import HDR
 
 optParser = optparse.OptionParser(usage="usage: %prog --input=image.hdr [options]")
@@ -13,6 +10,9 @@ optParser.add_option("--input", dest="input", default="image.hdr", help="specifi
 options, args = optParser.parse_args()
 
 hdr = HDR.load_from_file(options.input)
-im = hdr.to_pil()
-im.show()
 hdr.save(options.input.replace('.hdr', '.test.hdr'))
+
+# Verification script
+# from PIL import Image
+# im = hdr.to_pil()
+# im.show()
