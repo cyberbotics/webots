6c5d15ab5b3d0abb7db38a16b0228f04e5cfeaf7
Fabien Rohrer <fabien.rohrer@gmail.com>
2019-09-11 17:38:57 +0200

    Create the diffuse irradiance map

--- scripts/image_tools/hdr_to_irradiance_maps.py
+++ scripts/image_tools/hdr_to_irradiance_maps.py
@@ -15,11 +15,13 @@
 """Convert an HDR image to HDR irradiance maps."""
 # The irradiance maps are generated in the same directory as the input HDR image.
 
+import math
 import optparse
 import os
 
 from clamp import clamp_int
 from hdr import HDR
+from vec3 import Vec3
 
 optParser = optparse.OptionParser(usage='usage: %prog --input=image.hdr')
 optParser.add_option(
@@ -39,15 +41,37 @@ print('Load the HDR image...')
 hdr = HDR.load_from_file(hdr_path)
 assert hdr.is_valid(), 'Invalid input HDR file.'
 
-print('Create the irradiance map.')
-irradiance_map = HDR.create_black_image(hdr.width, hdr.height)
-for y in range(hdr.height):
-    for x in range(hdr.width):
-        pixel = hdr.get_pixel(x, y)
-        pixel = (
-            clamp_int(255.0 * pixel[0], 0, 255),
-            clamp_int(255.0 * pixel[1], 0, 255),
-            clamp_int(255.0 * pixel[2], 0, 255)
-        )
+
+def drange(start, stop, step):
+    r = start
+    while r < stop:
+        yield r
+        r += step
+
+
+print('Create the diffuse irradiance map...')
+irradiance_map = HDR.create_black_image(32, 32)
+for y in range(32):
+    for x in range(32):
+        N = Vec3(0.0, 0.0, 0.0)  # hu?
+        irradiance = Vec3(0.0, 0.0, 0.0)
+        up = Vec3(0.0, 1.0, 0.0)
+        right = up.cross(N)
+        up = N.cross(right)
+
+        sampleDelta = 0.025
+        nrSamples = 0.0
+        for phi in drange(0.0, 2.0 * math.pi, sampleDelta):
+            for theta in drange(0.0, 0.5 * math.pi, sampleDelta):
+                tangentSample = Vec3(math.sin(theta) * math.cos(phi), math.sin(theta) * math.sin(phi), math.cos(theta))
+                sampleVec = right * tangentSample.x + up * tangentSample.y + N * tangentSample.z
+
+                p1 = hdr.get_pixel(int(sampleVec.x), int(sampleVec.y))
+                p1 = Vec3(p1[0], p1[1], p1[2])
+                irradiance += p1 * math.cos(theta) * math.sin(theta)
+                nrSamples += 1.0
+        irradiance = irradiance * math.pi * (1.0 / float(nrSamples))
+
+        pixel = (irradiance.x, irradiance.y, irradiance.z)
         irradiance_map.set_pixel(x, y, pixel)
 irradiance_map.save(irradiance_map_path)
