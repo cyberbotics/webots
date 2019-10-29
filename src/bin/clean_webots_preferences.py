194e47a5e5ebe808f2f0474dc778b3e972b75ee7
Fabien Rohrer <fabien.rohrer@gmail.com>
2019-10-29 09:19:51 +0100

    Better code layout

--- src/bin/clean_webots_preferences.py
+++ src/bin/clean_webots_preferences.py
@@ -1,24 +1,41 @@
 #!/usr/bin/env python
 '''Cleanup all Webots preferences.'''
 
-import platform
 
-osName = platform.system()
-if osName == 'Linux':
+def cleanupLinuxPreferences():
+    print('Cleanup Linux preferences')
     #  for n in `find $HOME/.config/Cyberbotics -name Webots-*.conf`; do
     #    echo Remove "$n";
     #    rm $n;
     #  done
     pass
-elif osName == 'Darwin':
+
+
+def cleanupMacOSPreferences():
+    print('Cleanup macOS preferences')
     #  for n in `find $HOME/Library/Preferences -name com.cyberbotics.* | sed 's:.*/::;s:.plist::'`; do
     #    echo Remove "$n";
     #    defaults remove $n;
     #    rm $HOME/Library/Preferences/$n.plist;
     #  done
     pass
-elif osName == 'Windows':
+
+
+def cleanupWindowsPreferences():
+    print('Cleanup Windows preferences')
     # TODO: Something with regex
     pass
-else:
-    sys.exit('Unsupported OS: ' + osName)
+
+
+if __name__ == "__main__":
+    import platform
+    import sys
+    osName = platform.system()
+    if osName == 'Linux':
+        cleanupLinuxPreferences()
+    elif osName == 'Darwin':
+        cleanupMacOSPreferences()
+    elif osName == 'Windows':
+        cleanupWindowsPreferences()
+    else:
+        sys.exit('Unsupported OS: ' + osName)
