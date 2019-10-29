120edfe9b0177b86462c67efa233bbd6bf4aef7a
Fabien Rohrer <fabien.rohrer@gmail.com>
2019-10-29 09:12:00 +0100

    Add windows case

--- src/bin/clean_webots_preferences.py
+++ src/bin/clean_webots_preferences.py
@@ -4,18 +4,21 @@
 import platform
 
 osName = platform.system()
-if (osName == 'Linux'):
+if osName == 'Linux':
     #  for n in `find $HOME/.config/Cyberbotics -name Webots-*.conf`; do
     #    echo Remove "$n";
     #    rm $n;
     #  done
     pass
-elif (osName == 'Darwin'):
+elif osName == 'Darwin':
     #  for n in `find $HOME/Library/Preferences -name com.cyberbotics.* | sed 's:.*/::;s:.plist::'`; do
     #    echo Remove "$n";
     #    defaults remove $n;
     #    rm $HOME/Library/Preferences/$n.plist;
     #  done
+    pass
+elif osName == 'Windows':
+    # TODO: Something with regex
     pass
 else:
     sys.exit('Unsupported OS: ' + osName)
