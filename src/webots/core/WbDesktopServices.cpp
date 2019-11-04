a08a4d532ebc41c55d89cb4e0e79d23fc7cc72be
Olivier Michel <Olivier.Michel@cyberbotics.com>
2019-11-04 16:06:08 +0100

    Fixed confusion of Visual Studio

--- src/webots/core/WbDesktopServices.cpp
+++ src/webots/core/WbDesktopServices.cpp
@@ -1,3 +1,4 @@
+#include <QtCore/QDebug>
 // Copyright 1996-2019 Cyberbotics Ltd.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -30,6 +31,19 @@ bool WbDesktopServices::openUrl(const QString &url) {
   process.setStandardOutputFile(QProcess::nullDevice());
   return process.startDetached();
 #else
-  return QDesktopServices::openUrl(QUrl(url));
+#ifdef _WIN32
+  // The TEMP/TMP environment variables set my the MSYS2 console are confusing Visual C++ (among possibly other apps)
+  // as they refer to "/tmp" which is not a valid Windows path. It is therefore safer to remove them
+  const QByteArray TEMP = qgetenv("TEMP");
+  const QByteArray TMP = qgetenv("TMP");
+  qunsetenv("TMP");
+  qunsetenv("TEMP");
+#endif
+  bool result = QDesktopServices::openUrl(QUrl(url));
+#ifdef _WIN32
+  qputenv("TEMP", TEMP);
+  qputenv("TMP", TMP);
+#endif
+  return result;
 #endif
 }
