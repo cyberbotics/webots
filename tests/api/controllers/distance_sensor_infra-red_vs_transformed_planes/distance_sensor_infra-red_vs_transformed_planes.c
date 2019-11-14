38cc2eaeb250a94caa5291134c10093c2dabb975
Fabien Rohrer <fabien.rohrer@gmail.com>
2019-11-14 17:13:13 +0100

    Show distance values

--- tests/api/controllers/distance_sensor_infra-red_vs_transformed_planes/distance_sensor_infra-red_vs_transformed_planes.c
+++ tests/api/controllers/distance_sensor_infra-red_vs_transformed_planes/distance_sensor_infra-red_vs_transformed_planes.c
@@ -10,7 +10,25 @@
 int main(int argc, char **argv) {
   ts_setup(argv[0]);
 
-  // TODO: add stuff here :-)
+  WbDeviceTag ds[5];
+  int i;
+  char name[2] = "A";
+  for (i = 0; i < 5; ++i) {
+    ds[i] = wb_robot_get_device(name);
+    wb_distance_sensor_enable(ds[i], TIME_STEP);
+    name[0] += 1;
+  }
+
+  for (i = 0; i < 10; ++i) {
+    printf("Values:\n");
+    for (i = 0; i < 5; ++i) {
+      double value = wb_distance_sensor_get_value(ds[i]);
+      printf("%.1f - ", value);
+    }
+    printf("\n\n");
+
+    wb_robot_step(TIME_STEP);
+  }
 
   ts_send_success();
   return EXIT_SUCCESS;
