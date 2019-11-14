e5aa3f23eb1bcc838fe009e2050aacf792f099b1
Fabien Rohrer <fabien.rohrer@gmail.com>
2019-11-14 16:09:53 +0100

    Test suite stuff

---
+++ tests/api/controllers/distance_sensor_infra-red_vs_transformed_planes/distance_sensor_infra-red_vs_transformed_planes.c
@@ -0,0 +1,17 @@
+#include <webots/camera.h>
+#include <webots/distance_sensor.h>
+#include <webots/robot.h>
+
+#include "../../../lib/ts_assertion.h"
+#include "../../../lib/ts_utils.h"
+
+#define TIME_STEP 32
+
+int main(int argc, char **argv) {
+  ts_setup(argv[0]);
+
+  // TODO: add stuff here :-)
+
+  ts_send_success();
+  return EXIT_SUCCESS;
+}
