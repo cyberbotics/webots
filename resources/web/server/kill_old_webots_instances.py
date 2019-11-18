fcbb909304cd1de060270e5af18d705ddcb1c7a9
Stefania Pedrazzi <stefaniapedrazzi@users.noreply.github.com>
2019-11-18 16:45:01 +0100

    Update resources/web/server/kill_old_webots_instances.py
    Co-Authored-By: Olivier Michel <Olivier.Michel@cyberbotics.com>

--- resources/web/server/kill_old_webots_instances.py
+++ resources/web/server/kill_old_webots_instances.py
@@ -23,7 +23,7 @@ def checkIfRecentlyModified(path, timeout):
 
 
 currentTime = time.time()
-creationTimeLimit = currentTime - 86400 * 3  # days in seconds
+creationTimeLimit = currentTime - 86400 * 3  # 3 days in seconds
 modificationTimeLimit = currentTime - 86400 * 1  # 1 day in seconds
 processName = 'webots'
 streamPattern = re.compile('^--stream.*')
