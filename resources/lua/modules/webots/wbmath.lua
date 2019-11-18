f8a0ef5eedfb7b87eaccaff78a18647190543c40
Fabien Rohrer <fabien.rohrer@gmail.com>
2019-11-18 14:21:18 +0100

    Cleaner code

--- resources/lua/modules/webots/wbmath.lua
+++ resources/lua/modules/webots/wbmath.lua
@@ -4,5 +4,12 @@ function M.round(num)
   return math.floor(num + 0.5)
 end
 
-return M
+function M.degrees2radians(angle)
+  return angle * math.pi / 180.0
+end
 
+function M.radians2degrees(angle)
+  return angle * 180.0 / math.pi
+end
+
+return M
