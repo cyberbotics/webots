7ba5bad4e1ba8916fe1971b3344bbcb53e926873
Stefania Pedrazzi <stefaniapedrazzi@users.noreply.github.com>
2019-11-18 16:48:38 +0100

    Update description

--- resources/web/server/kill_old_webots_instances.py
+++ resources/web/server/kill_old_webots_instances.py
@@ -6,7 +6,7 @@ import time
 import os
 
 '''
-Kill all instances of streaming Webots processes fulfilling the following conditions:
+Utility script for Linux server machines that kills all instances of streaming Webots processes fulfilling the following conditions:
 - created 3 days ago or earlier
 - controller files not modified in the last 24 hours
 Only the batch script is terminated but this script will also check and notify if Webots binary is still running.
