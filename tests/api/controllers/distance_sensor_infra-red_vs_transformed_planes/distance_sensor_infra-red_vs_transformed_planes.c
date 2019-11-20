152ab1929a9e753afeed46e8c58b2908fed287d7
Fabien Rohrer <fabien.rohrer@gmail.com>
2019-11-20 15:29:04 +0100

    More interesting test

--- tests/api/controllers/distance_sensor_infra-red_vs_transformed_planes/distance_sensor_infra-red_vs_transformed_planes.c
+++ tests/api/controllers/distance_sensor_infra-red_vs_transformed_planes/distance_sensor_infra-red_vs_transformed_planes.c
@@ -6,29 +6,37 @@
 #include "../../../lib/ts_utils.h"
 
 #define TIME_STEP 32
+#define N_DISTANCE_SENSORS 10
+
+#define OBSTACLE 833.3333
+#define NO_OBSTACLE 1000.0
+
+const double expected_values[N_DISTANCE_SENSORS] = {NO_OBSTACLE, OBSTACLE, OBSTACLE,    OBSTACLE,    NO_OBSTACLE,
+                                                    OBSTACLE,    OBSTACLE, NO_OBSTACLE, NO_OBSTACLE, NO_OBSTACLE};
 
 int main(int argc, char **argv) {
   ts_setup(argv[0]);
 
-  WbDeviceTag ds[5];
+  WbDeviceTag ds[N_DISTANCE_SENSORS];
   int i;
   char name[2] = "A";
-  for (i = 0; i < 5; ++i) {
+  for (i = 0; i < N_DISTANCE_SENSORS; ++i) {
     ds[i] = wb_robot_get_device(name);
     wb_distance_sensor_enable(ds[i], TIME_STEP);
     name[0] += 1;
   }
 
-  for (i = 0; i < 10; ++i) {
-    printf("Values:\n");
-    for (i = 0; i < 5; ++i) {
-      double value = wb_distance_sensor_get_value(ds[i]);
-      printf("%.1f - ", value);
-    }
-    printf("\n\n");
+  wb_robot_step(TIME_STEP);
 
-    wb_robot_step(TIME_STEP);
+  for (i = 0; i < N_DISTANCE_SENSORS; ++i) {
+    double value = wb_distance_sensor_get_value(ds[i]);
+    ts_assert_double_in_delta(value, expected_values[i], 0.01,
+                              "Distance sensor '%c' doesn't return the right distance when hitting an object "
+                              "(expected = %f, received = %f)",
+                              'A' + i, expected_values[i], value);
   }
+
+  wb_robot_step(TIME_STEP);
 
   ts_send_success();
   return EXIT_SUCCESS;
