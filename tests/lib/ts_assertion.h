7537c347a8ad1058af098a5491787f4b8d4c07a6
Fabien Rohrer <fabien.rohrer@gmail.com>
2019-12-02 15:43:37 +0100

    Simpler solution

--- tests/lib/ts_assertion.h
+++ tests/lib/ts_assertion.h
@@ -111,16 +111,28 @@ void ts_assert_doubles_equal(int size, const double *value, const double *expect
 
 // An euler and axis rotation equals to another.
 void ts_assert_rotation_equals(const double *rot_a, const double *rot_b, double tolerance, const char *error_message, ...) {
+  // Inspired from WbRotation::normalizeAngle()
+  double angle_a = rot_a[3];
+  while (angle_a < -M_PI)
+    angle_a += 2.0 * M_PI;
+  while (angle_a > M_PI)
+    angle_a -= 2.0 * M_PI;
+  double angle_b = rot_b[3];
+  while (angle_b < -M_PI)
+    angle_b += 2.0 * M_PI;
+  while (angle_b > M_PI)
+    angle_b -= 2.0 * M_PI;
+
   // Inspired from WbRotation::almostEquals()
   bool correct = false;
-  if (fabs(rot_a[3] - rot_b[3]) < tolerance) {
-    if (fabs(rot_a[3]) < tolerance)
+  if (fabs(angle_a - angle_b) < tolerance) {
+    if (fabs(angle_a) < tolerance)
       correct = true;  // axis can be different but rotation is the same
     else
       correct =
         fabs(rot_a[0] - rot_b[0]) < tolerance && fabs(rot_a[1] - rot_b[1]) < tolerance && fabs(rot_a[2] - rot_b[2]) < tolerance;
-  } else if (fabs(rot_a[3] + rot_b[3]) < tolerance) {
-    if (fabs(rot_a[3]) < tolerance)
+  } else if (fabs(angle_a + angle_b) < tolerance) {
+    if (fabs(angle_a) < tolerance)
       correct = true;  // axis can be different but rotation is the same
     else
       correct =
