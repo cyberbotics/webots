7990a47c4bde367fb668610e1b2e81849cbbb286
David Mansolino <David.Mansolino@cyberbotics.com>
2019-12-04 08:34:42 +0100

    Clang format

--- tests/api/controllers/supervisor_force_torque/supervisor_force_torque.c
+++ tests/api/controllers/supervisor_force_torque/supervisor_force_torque.c
@@ -1,12 +1,11 @@
+#include <webots/position_sensor.h>
 #include <webots/robot.h>
 #include <webots/supervisor.h>
-#include <webots/position_sensor.h>
 
 #include "../../../lib/ts_assertion.h"
 #include "../../../lib/ts_utils.h"
 
 #define TIME_STEP 32
-
 
 int main(int argc, char **argv) {
   ts_setup(argv[0]);
@@ -24,7 +23,7 @@ int main(int argc, char **argv) {
   ps[1] = wb_robot_get_device("ps1");
   ps[2] = wb_robot_get_device("ps2");
   ps[3] = wb_robot_get_device("ps3");
-  
+
   wb_position_sensor_enable(ps[0], TIME_STEP);
   wb_position_sensor_enable(ps[1], TIME_STEP);
   wb_position_sensor_enable(ps[2], TIME_STEP);
@@ -32,9 +31,10 @@ int main(int argc, char **argv) {
 
   for (i = 0; i < 20; ++i)
     wb_robot_step(TIME_STEP);
-    
+
   for (i = 0; i < 4; ++i)
-    ts_assert_double_in_delta(wb_position_sensor_get_value(ps[i]), 0.0, 0.01, "All position angle should be ~0.0 before applying any force/torque");
+    ts_assert_double_in_delta(wb_position_sensor_get_value(ps[i]), 0.0, 0.01,
+                              "All position angle should be ~0.0 before applying any force/torque");
 
   const double force[] = {0.0, 0.1, 0.0};
   const double torque[] = {0.1, 0.0, 0.0};
@@ -49,11 +49,11 @@ int main(int argc, char **argv) {
     wb_supervisor_node_add_relative_force(node[2], force, offset2);
     wb_supervisor_node_add_torque(node[3], torque);
   }
-  
+
   double positions[4];
   for (i = 0; i < 4; ++i)
     positions[i] = wb_position_sensor_get_value(ps[i]);
-    
+
   ts_assert_double_in_delta(positions[0], -positions[1], 0.01, "Angle 0 should be equal to -angle 1.");
   ts_assert_double_in_delta(positions[2], 0.0, 0.01, "Angle 2 should be zero");
   ts_assert_double_is_bigger(-0.2, positions[0], "Angle 0 should be smaller than -0.2");
