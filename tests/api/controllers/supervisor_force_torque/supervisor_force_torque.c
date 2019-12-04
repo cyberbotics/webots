b16fead5b2a558bb7b32b9c449a09ab9fb2af9f3
David Mansolino <David.Mansolino@cyberbotics.com>
2019-12-04 11:09:33 +0100

    extend test suite.

--- tests/api/controllers/supervisor_force_torque/supervisor_force_torque.c
+++ tests/api/controllers/supervisor_force_torque/supervisor_force_torque.c
@@ -11,28 +11,34 @@ int main(int argc, char **argv) {
   ts_setup(argv[0]);
 
   int i = 0;
-  WbNodeRef node[4];
-  WbDeviceTag ps[4];
+  WbNodeRef node[6];
+  WbDeviceTag ps[6];
 
   node[0] = wb_supervisor_node_get_from_def("BALANCE_0");
   node[1] = wb_supervisor_node_get_from_def("BALANCE_1");
   node[2] = wb_supervisor_node_get_from_def("BALANCE_2");
   node[3] = wb_supervisor_node_get_from_def("BALANCE_3");
+  node[4] = wb_supervisor_node_get_from_def("BALANCE_4");
+  node[5] = wb_supervisor_node_get_from_def("BALANCE_5");
 
   ps[0] = wb_robot_get_device("ps0");
   ps[1] = wb_robot_get_device("ps1");
   ps[2] = wb_robot_get_device("ps2");
   ps[3] = wb_robot_get_device("ps3");
+  ps[4] = wb_robot_get_device("ps4");
+  ps[5] = wb_robot_get_device("ps5");
 
   wb_position_sensor_enable(ps[0], TIME_STEP);
   wb_position_sensor_enable(ps[1], TIME_STEP);
   wb_position_sensor_enable(ps[2], TIME_STEP);
   wb_position_sensor_enable(ps[3], TIME_STEP);
+  wb_position_sensor_enable(ps[4], TIME_STEP);
+  wb_position_sensor_enable(ps[5], TIME_STEP);
 
   for (i = 0; i < 20; ++i)
     wb_robot_step(TIME_STEP);
 
-  for (i = 0; i < 4; ++i)
+  for (i = 0; i < 6; ++i)
     ts_assert_double_in_delta(wb_position_sensor_get_value(ps[i]), 0.0, 0.01,
                               "All position angle should be ~0.0 before applying any force/torque");
 
@@ -48,17 +54,22 @@ int main(int argc, char **argv) {
     wb_supervisor_node_add_relative_force(node[1], force, offset1);
     wb_supervisor_node_add_relative_force(node[2], force, offset2);
     wb_supervisor_node_add_torque(node[3], torque);
+    wb_supervisor_node_add_force(node[4], force);
+    wb_supervisor_node_add_relative_force(node[5], force, offset2);
   }
 
-  double positions[4];
-  for (i = 0; i < 4; ++i)
+  double positions[6];
+  for (i = 0; i < 6; ++i)
     positions[i] = wb_position_sensor_get_value(ps[i]);
 
   ts_assert_double_in_delta(positions[0], -positions[1], 0.01, "Angle 0 should be equal to -angle 1.");
+  ts_assert_double_in_delta(positions[4], positions[5], 0.01, "Angle 4 should be equal to angle 5.");
   ts_assert_double_in_delta(positions[2], 0.0, 0.01, "Angle 2 should be zero");
   ts_assert_double_is_bigger(-0.2, positions[0], "Angle 0 should be smaller than -0.2");
   ts_assert_double_is_bigger(positions[1], 0.2, "Angle 1 should be bigger than 0.2");
   ts_assert_double_is_bigger(positions[3], 0.2, "Angle 3 should be bigger than 0.2");
+  ts_assert_double_is_bigger(positions[4], -0.2, "Angle 4 should be smaller than -0.2");
+  ts_assert_double_is_bigger(positions[5], -0.2, "Angle 5 should be smaller than -0.2");
 
   ts_send_success();
   return EXIT_SUCCESS;
