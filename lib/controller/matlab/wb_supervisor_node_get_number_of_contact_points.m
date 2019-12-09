600a774cf498368db6d6962d78140b57b8410003
Stefania Pedrazzi <stefania.pedrazzi@cyberbotics.com>
2019-12-09 09:20:39 +0100

    Fix wb_supervisor_node_get_number_of_contact_points arguments for MATLAB API

--- lib/controller/matlab/wb_supervisor_node_get_number_of_contact_points.m
+++ lib/controller/matlab/wb_supervisor_node_get_number_of_contact_points.m
@@ -1,6 +1,6 @@
-function result = wb_supervisor_node_get_number_of_contact_points(noderef, index)
-% Usage: wb_supervisor_node_get_number_of_contact_points(noderef, index)
+function result = wb_supervisor_node_get_number_of_contact_points(noderef)
+% Usage: wb_supervisor_node_get_number_of_contact_points(noderef)
 % Matlab API for Webots
 % Online documentation is available <a href="https://www.cyberbotics.com/doc/reference/supervisor">here</a>
 
-result = calllib('libController', 'wb_supervisor_node_get_number_of_contact_points', noderef, index);
+result = calllib('libController', 'wb_supervisor_node_get_number_of_contact_points', noderef);
