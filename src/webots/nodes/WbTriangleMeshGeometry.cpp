07ce1ec2450a8ae8cded632eab7040683ea3a5d6
David Mansolino <David.Mansolino@cyberbotics.com>
2020-03-11 16:35:46 +0100

    fix streaming.

--- src/webots/nodes/WbTriangleMeshGeometry.cpp
+++ src/webots/nodes/WbTriangleMeshGeometry.cpp
@@ -601,7 +601,9 @@ void WbTriangleMeshGeometry::exportNodeContents(WbVrmlWriter &writer) const {
     }
   }
 
-  findField("solid", true)->write(writer);
+  const WbField *solidField = findField("solid", true);
+  if (solidField)
+    solidField->write(writer);
 
   if (mTriangleMesh && !mTriangleMesh->areTextureCoordinatesValid())
     // notify three.js if a default mapping is used to prevent issue https://github.com/cyberbotics/webots/issues/752
