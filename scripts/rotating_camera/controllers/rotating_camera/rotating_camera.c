2330dcce871c99b786fe42f7cc86d61ed93b9061
Michael <36080014+michou214@users.noreply.github.com>
2020-03-16 15:40:07 +0100

    Update scripts/rotating_camera/controllers/rotating_camera/rotating_camera.c
    Co-Authored-By: Olivier Michel <Olivier.Michel@cyberbotics.com>

--- scripts/rotating_camera/controllers/rotating_camera/rotating_camera.c
+++ scripts/rotating_camera/controllers/rotating_camera/rotating_camera.c
@@ -38,7 +38,7 @@ WbFieldRef robot_translation;
 // The viewpoint revolves around the robot following a circle trajectory
 //  - robot_position: position of the robot.
 //  - rotation_direction: CW (clockwise, default) or CCW (counterclockwise).
-//  o n_turn: Make the number of complete revolution defined.
+//  - n_turn: number of complete revolution.
 //  o radius: Set the distance between the robot and the camera.
 //  o height_step: Height achieved during a complete revolution.
 //  o angle_end: Make an arc of a circle. Set to 6.28 for complete revolution.
