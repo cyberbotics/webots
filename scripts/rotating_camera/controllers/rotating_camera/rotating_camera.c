6dccd750042ff3528aafc263574338075d7b451a
Michael <36080014+michou214@users.noreply.github.com>
2020-03-17 08:26:39 +0100

    Update scripts/rotating_camera/controllers/rotating_camera/rotating_camera.c
    Co-Authored-By: Olivier Michel <Olivier.Michel@cyberbotics.com>

--- scripts/rotating_camera/controllers/rotating_camera/rotating_camera.c
+++ scripts/rotating_camera/controllers/rotating_camera/rotating_camera.c
@@ -119,7 +119,7 @@ int main(int argc, char *argv[]) {
 
   printf("\n");
   printf("Robot.position = %g %g %g\n", robot_position[0], robot_position[1], robot_position[2]);
-  printf("Number of revolutions: %d\n", N);
+  printf("Number of revolutions: %d\n", n_turn);
   printf("Radius: %f\n", R);
   printf("Height step per revolution: %f\n", height);
   printf("Angle stop at %f rad\n", angle_end);
