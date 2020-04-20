d644ff96a3e5e0cbae5339b8664299ed40fa6d35
Stefania Pedrazzi <stefania.pedrazzi@cyberbotics.com>
2020-04-20 11:23:51 +0200

    Extend test

--- tests/rendering/controllers/modify_nested_def_node/modify_nested_def_node.c
+++ tests/rendering/controllers/modify_nested_def_node/modify_nested_def_node.c
@@ -12,10 +12,10 @@ int main(int argc, char **argv) {
 
   int time_step = wb_robot_get_basic_time_step();
 
-  WbDeviceTag ds_def1 = wb_robot_get_device("ds1");
-  WbDeviceTag ds_use1 = wb_robot_get_device("ds2");
-  WbDeviceTag ds_def2 = wb_robot_get_device("ds3");
-  WbDeviceTag ds_use2 = wb_robot_get_device("ds4");
+  WbDeviceTag ds_def1 = wb_robot_get_device("ds1"); // generic
+  WbDeviceTag ds_use1 = wb_robot_get_device("ds2"); // generic
+  WbDeviceTag ds_def2 = wb_robot_get_device("ds3"); // generic
+  WbDeviceTag ds_use2 = wb_robot_get_device("ds4"); // infra-red (no bounding object)
   wb_distance_sensor_enable(ds_def1, time_step);
   wb_distance_sensor_enable(ds_use1, time_step);
   wb_distance_sensor_enable(ds_def2, time_step);
@@ -29,7 +29,7 @@ int main(int argc, char **argv) {
   double use1 = wb_distance_sensor_get_value(ds_use2);
   double def2 = wb_distance_sensor_get_value(ds_def2);
   double use2 = wb_distance_sensor_get_value(ds_use2);
-  // values = 1000.000
+  // nothing detected: values = 1000.000
 
   ts_assert_double_in_delta(def1, 1000.0, 1.0, "The initial size of box B1 is wrong.");
   ts_assert_double_in_delta(use1, 1000.0, 1.0, "The initial size of USE node containing box B1 is wrong.");
@@ -55,11 +55,11 @@ int main(int argc, char **argv) {
   def2 = wb_distance_sensor_get_value(ds_def2);
   use2 = wb_distance_sensor_get_value(ds_use2);
 
-  // values <= 500.000
+  // object detected: values <= 500.000
   ts_assert_double_in_delta(def1, 500.0, 1.0, "The size of box B1 is wrong after modifying it.");
   ts_assert_double_in_delta(use1, 500.0, 1.0, "The size of USE node containing box B1 after modifying it.");
   ts_assert_double_in_delta(def2, 500.0, 1.0, "The size of box B2 is wrong after modifying it.");
-  ts_assert_double_in_delta(use2, 500.0, 1.0, "The size of of USE node containing box B2 is wrong after modifying it.");
+  ts_assert_double_in_delta(use2, 526.0, 10.0, "The size of of USE node containing box B2 is wrong after modifying it.");
 
   ts_send_success();
   return EXIT_SUCCESS;
