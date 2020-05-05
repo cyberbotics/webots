7299c343c1b33f7980f2937d87ff0b1625bde3cd
Darko Lukic <lukicdarkoo@gmail.com>
2020-05-05 15:49:09 +0200

    Add matlab support for lookup table getter

---
+++ lib/controller/matlab/wb_distance_sensor_get_lookup_table.m
@@ -0,0 +1,6 @@
+function result = wb_distance_sensor_get_lookup_table(tag)
+% Usage: wb_distance_sensor_get_lookup_table(tag)
+% Matlab API for Webots
+% Online documentation is available <a href="https://www.cyberbotics.com/doc/reference/distancesensor">here</a>
+
+result = calllib('libController', 'wb_distance_sensor_get_lookup_table', tag);
