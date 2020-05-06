10b96ec42c8642cd53f7b56ee23c4d3dd7b749e1
Darko Lukic <lukicdarkoo@gmail.com>
2020-05-06 17:37:39 +0200

    Simplify lookup table API for Matlab

--- lib/controller/matlab/wb_touch_sensor_get_lookup_table.m
+++ lib/controller/matlab/wb_touch_sensor_get_lookup_table.m
@@ -3,4 +3,7 @@ function wb_touch_sensor_get_lookup_table(tag, sampling_period)
 % Matlab API for Webots
 % Online documentation is available <a href="https://www.cyberbotics.com/doc/reference/touchsensor">here</a>
 
-calllib('libController', 'wb_touch_sensor_get_lookup_table', tag, sampling_period);
+size = calllib('libController', 'wb_touch_sensor_get_lookup_table_size', tag);
+pointer = calllib('libController', 'wb_touch_sensor_get_lookup_table', tag);
+setdatatype(pointer, 'doublePtr', 3 * size, 1);
+result = get(pointer, 'Value');
