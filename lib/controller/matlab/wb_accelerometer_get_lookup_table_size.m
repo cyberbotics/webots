b4fd5dd6327806f09e406a977908644f3e59cdfc
Darko Lukic <lukicdarkoo@gmail.com>
2020-05-07 09:32:17 +0200

    Generate files Matlab files with `mgenerate.py`

--- lib/controller/matlab/wb_accelerometer_get_lookup_table_size.m
+++
@@ -1,6 +0,0 @@
-function wb_accelerometer_get_lookup_table_size(tag)
-% Usage: wb_accelerometer_get_lookup_table_size(tag)
-% Matlab API for Webots
-% Online documentation is available <a href="https://www.cyberbotics.com/doc/reference/accelerometer">here</a>
-
-calllib('libController', 'wb_accelerometer_get_lookup_table_size', tag);
