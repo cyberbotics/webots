0047597b7c3d18f5b9e5714954f6b84deb1d118e
Olivier Michel <Olivier.Michel@cyberbotics.com>
2020-05-07 17:52:23 +0200

    return the first GET paramter found, not the last one

--- resources/web/wwi/webots.js
+++ resources/web/wwi/webots.js
@@ -164,15 +164,14 @@ webots.View = class View {
 
     var initWorld = () => {
       function findGetParameter(parameterName) {
-        let result = null;
         let tmp = [];
         let items = window.location.search.substr(1).split('&');
         for (let index = 0; index < items.length; index++) {
           tmp = items[index].split('=');
           if (tmp[0] === parameterName)
-            result = decodeURIComponent(tmp[1]);
+            return decodeURIComponent(tmp[1]);
         }
-        return result;
+        return null;
       }
       if (this.isWebSocketProtocol) {
         this.progress = document.createElement('div');
