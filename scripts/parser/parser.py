a9c9aeb847304420853fd07ab665c8e9f8f3870f
Olivier Michel <Olivier.Michel@cyberbotics.com>
2020-05-11 16:15:15 +0200

    working parser

--- scripts/parser/parser.py
+++ scripts/parser/parser.py
@@ -16,20 +16,14 @@
 
 '''Parse Webots world files.'''
 
+import json
 import sys
 
+'''This assumes the world file was saved with Webots and the indentation written by Webots was not changed.'''
 
-def read_node(file):
+
+def read_node(file, line):
     node = {'fields': []}
-    line = file.readline()
-    indent = 0
-    for character in line:
-        if character == ' ':
-            indent += 1
-        else:
-            break
-    node['depth'] = indent / 2
-    line = line.trim()
     words = line.split(' ')
     if words[0] != 'DEF':
         node['name'] = words[0]
@@ -37,24 +31,25 @@ def read_node(file):
         node['DEF'] = words[1]
         node['name'] = words[2]
     for line in file:
-        node['field'].append(read_field(file))
+        line = line.strip()
+        if line == '}':
+            break
+        node['fields'].append(read_field(file, line))
+    return node
 
 
-def read_field(file):
+def read_field(file, line):
     field = {}
-    field['name']
-    line = file.readline()
-    line = line.trim()
     words = line.split(' ', 1)
     field['name'] = words[0]
     character = words[1][0]
     if character == '[':
-        field['value'] = read_mffield(file)
+        field['value'] = read_mffield(file)  # MF*
     elif character == '"':
-        field['value'] = words[1][1:-1]
-    elif words[1] == 'TRUE':
+        field['value'] = words[1][1:-1]  # SFString
+    elif words[1] == 'TRUE':  # SFBool
         field['value'] = True
-    elif words[1] == 'FALSE':
+    elif words[1] == 'FALSE':  # SFBool
         field['value'] = False
     elif character.isdigit() or character == '-':
         words = words[1].split(' ')
@@ -64,13 +59,47 @@ def read_field(file):
             field['value'] = []
             for number in words:
                 field['value'].append(float(number))  # SFVec2f / SFVec3f / SFRotation / SFColor
+    else:
+        field['value'] = read_node(file, line)  # SFNode
     return field
+
+
+def read_mffield(file):
+    mffield = []
+    while True:
+        line = file.readline().strip()
+        character = line[0]
+        if character == ']':
+            break
+        if character == '"':
+            mffield.append(line[1:-1])  # MFString
+        elif line == 'TRUE':
+            mffield.append(True)  # MFBool
+        elif line == 'FALSE':
+            mffield.append(False)  # MFBool
+        elif character.isdigit() or character == '-':
+            words = line.split(' ')
+            if len(words) == 1:
+                mffield.append(float(words[0]))  # MFInt32 / MFFloat
+            else:
+                array = []
+                for number in words:
+                    array.append(float(number))  # MFVec2f / MFVec3f / MFRotation / MFColor
+                mffield.append(array)
+        else:
+            node = read_node(file, line)
+            mffield.append(node)
+        words = line.split(' ')
+    return mffield
 
 
 filename = sys.argv[1]
 world = {}
 with open(filename, 'r') as file:
-    world['header'] = file.readline()
+    world['header'] = file.readline().strip()
     world['root'] = []
-    count = 2
-    read_node(file)
+    for line in file:
+        line = line.strip()
+        if line:
+            world['root'].append(read_node(file, line))
+    print(json.dumps(world, indent=2, sort_keys=True))
