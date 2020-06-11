da1a493c0f9d16414d0c83d9d4857e2da046179b
DavidMansolino <David.Mansolino@cyberbotics.com>
2020-06-11 16:21:24 +0200

    Show console in new world case.

--- src/webots/gui/WbMainWindow.cpp
+++ src/webots/gui/WbMainWindow.cpp
@@ -1234,6 +1234,10 @@ void WbMainWindow::restorePerspective(bool reloading, bool firstLoad, bool loadi
     mConsoles.last()->setEnabledLevels(consoleList.at(i).enabledLevels);
   }
 
+  // display at least one console
+  if (world->isUnnamed() && mConsoles.size() == 0)
+    openNewConsole();
+
   if (meansOfLoading) {
     if (!perspective->enabledRobotWindowNodeNames().isEmpty()) {
       const QList<WbRobot *> &robots = world->robots();
