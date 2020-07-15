ef43772f693865a8e22a9abdf905e9cdfd279395
Stefania Pedrazzi <stefania.pedrazzi@cyberbotics.com>
2020-07-15 11:22:48 +0200

    Improve test to detect issues with node ids after PROTO generation

--- tests/protos/controllers/template_node_id/template_node_id.c
+++ tests/protos/controllers/template_node_id/template_node_id.c
@@ -7,7 +7,7 @@
 #define TIME_STEP 32
 
 int main(int argc, char **argv) {
-  ts_setup(argv[1]);  // give the controller args
+  ts_setup(argv[0]);  // give the controller args
 
   // let the time for the robot controller to copy the args in customData field
   wb_robot_step(5 * TIME_STEP);
@@ -15,6 +15,14 @@ int main(int argc, char **argv) {
   WbNodeRef robot = wb_supervisor_node_get_from_def("ROBOT");
   WbFieldRef custom_data_field = wb_supervisor_node_get_field(robot, "customData");
   WbFieldRef procedural_field = wb_supervisor_node_get_field(robot, "proceduralField");
+
+  WbNodeRef texture = wb_supervisor_node_get_from_def("TEXTURE");
+  ts_assert_pointer_not_null(texture, "Can'tB find 'appearance' field of SHAPE node.");
+  WbFieldRef repeats_field = wb_supervisor_node_get_field(texture, "repeatS");
+  ts_assert_pointer_not_null(repeats_field, "Can't find 'repeatS' field of TEXTURE node.");
+
+  WbNodeRef shape = wb_supervisor_node_get_from_def("SHAPE");
+  ts_assert_pointer_not_null(shape, "Can'tB find 'appearance' field of SHAPE node.");
 
   const char *field_content = wb_supervisor_field_get_sf_string(custom_data_field);
   int id;
@@ -41,8 +49,29 @@ int main(int argc, char **argv) {
   WbNodeRef root_node = wb_supervisor_node_get_root();
   ts_assert_int_equal(0, wb_supervisor_node_get_id(root_node), "The 'id' of the root node should be '0'.");
 
+  // test that fields can be retrieved (for the first time) even after PROTO regeneration
+
   WbFieldRef root_children_field = wb_supervisor_node_get_field(root_node, "children");
   ts_assert_pointer_not_null(root_children_field, "Can't find 'children' field of root node.");
+
+  WbFieldRef controller_field = wb_supervisor_node_get_field(robot, "controller");
+  ts_assert_pointer_not_null(controller_field, "Can't find 'controller' field of ROBOT node.");
+
+  WbFieldRef appearance_field = wb_supervisor_node_get_field(shape, "appearance");
+  ts_assert_pointer_not_null(appearance_field, "Can't find 'appearance' field of SHAPE node.");
+
+  WbFieldRef repeatt_field = wb_supervisor_node_get_field(texture, "repeatT");
+  ts_assert_pointer_not_null(repeatt_field, "Can't find 'repeatS' field of TEXTURE node.");
+
+  // test that fields can still be modified after PROTO regeneration
+
+  wb_supervisor_field_set_sf_bool(repeats_field, false);
+  wb_supervisor_field_set_sf_bool(repeatt_field, false);
+
+  wb_robot_step(TIME_STEP);
+
+  ts_assert_boolean_not_equal(wb_supervisor_field_get_sf_bool(repeats_field), "'repeatS' field has not been set.");  
+  ts_assert_boolean_not_equal(wb_supervisor_field_get_sf_bool(repeatt_field), "'repeatT' field has not been set.");  
 
   ts_send_success();
   return EXIT_SUCCESS;
