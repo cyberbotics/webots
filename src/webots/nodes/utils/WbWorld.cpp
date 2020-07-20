c3fdc4ca7774683654221ea7c1e2763c38c0bff3
David Mansolino <David.Mansolino@cyberbotics.com>
2020-07-20 11:13:22 +0200

    Handle controlled balljoint.

--- src/webots/nodes/utils/WbWorld.cpp
+++ src/webots/nodes/utils/WbWorld.cpp
@@ -436,10 +436,14 @@ void WbWorld::createX3DMetaFile(const QString &filename) const {
           deviceObject.insert("minPosition", motor->minPosition());
           deviceObject.insert("maxPosition", motor->maxPosition());
           deviceObject.insert("position", motor->position());
-          if (motor->positionIndex() == 2)
+          if (motor->positionIndex() == 3)
+            deviceObject.insert("axis", motor->joint()->parameters3()->axis().toString(WbPrecision::FLOAT_MAX));
+          else if (motor->positionIndex() == 2)
             deviceObject.insert("axis", motor->joint()->parameters2()->axis().toString(WbPrecision::FLOAT_MAX));
-          else
+          else {
+            assert(motor->positionIndex() == 1);
             deviceObject.insert("axis", motor->joint()->parameters()->axis().toString(WbPrecision::FLOAT_MAX));
+          }
         }
       } else if (jointDevice && jointDevice->propeller() && motor) {  // case: propeller.
         WbSolid *helix = jointDevice->propeller()->helix(WbPropeller::SLOW_HELIX);
