cf51f2280d818a497e98a0b6fe62578d8b62b0ea
Olivier Michel <Olivier.Michel@cyberbotics.com>
2020-07-22 12:51:02 +0200

    It's not supported to change the coordinate system from a simulation and expect correct results (warning)

--- tests/api/controllers/gps_coordinates_north_direction/gps_coordinates_north_direction.c
+++ tests/api/controllers/gps_coordinates_north_direction/gps_coordinates_north_direction.c
@@ -13,25 +13,14 @@ int main(int argc, char **argv) {
   WbDeviceTag gps = wb_robot_get_device("gps");
   wb_gps_enable(gps, TIME_STEP);
 
-  WbNodeRef worldinfo_node = wb_supervisor_node_get_from_def("WORLDINFO");
-  WbFieldRef coordinate_system_field = wb_supervisor_node_get_field(worldinfo_node, "coordinateSystem");
-
   wb_robot_step(TIME_STEP);
 
   const double expected_position[] = {0.0451744, 0.0179479, 0};
-  const double expected_enu_position[] = {0, 0.0451744, 0.0179479};
   const double *position = wb_gps_get_values(gps);
   for (int i = 0; i < 3; i++)
     ts_assert_double_in_delta(position[i], expected_position[i], 0.0000001,
-                              "The %c value measured by the GPS (NUE) should be %g and not %g",
-                              i == 0 ? 'X' : i == 1 ? 'Y' : 'Z', expected_position[i], position[i]);
-  wb_supervisor_field_set_sf_string(coordinate_system_field, "ENU");
-  wb_robot_step(TIME_STEP);
-  position = wb_gps_get_values(gps);
-  for (int i = 0; i < 3; i++)
-    ts_assert_double_in_delta(position[i], expected_enu_position[i], 0.0000001,
-                              "The %c value measured by the GPS (ENU) should be %g and not %g",
-                              i == 0 ? 'X' : i == 1 ? 'Y' : 'Z', expected_enu_position[i], position[i]);
+                              "The %c value measured by the GPS should be %g and not %g", i == 0 ? 'X' : i == 1 ? 'Y' : 'Z',
+                              expected_position[i], position[i]);
 
   ts_send_success();
   return EXIT_SUCCESS;
