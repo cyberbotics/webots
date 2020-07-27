eeac749370a9577818d69dec9460b36e4308aa7f
David Mansolino <David.Mansolino@cyberbotics.com>
2020-07-27 14:25:48 +0200

    Renamming.

--- src/install_scripts/linux_compilation_dependencies.sh
+++ src/install_scripts/linux_compilation_dependencies.sh
@@ -4,11 +4,11 @@ sudo apt update
 sudo apt install git cmake swig python2.7-dev libglu1-mesa-dev libglib2.0-dev libfreeimage-dev libfreetype6-dev libxml2-dev libzzip-0-13 libboost-dev libgd3 libssh-gcrypt-dev libzip-dev libreadline-dev pbzip2 libpci-dev
 
 UBUNTU_VERSION=$(lsb_release -rs)
-if [[ $(UBUNTU_VERSION) == "16.04" ]]; then
+if [[ $UBUNTU_VERSION == "16.04" ]]; then
        sudo apt install libssl-dev python-pip
-elif [[ $(UBUNTU_VERSION) == "18.04" ]]; then
+elif [[ $UBUNTU_VERSION == "18.04" ]]; then
        sudo apt install libssl1.0-dev python-pip
-elif [[ $(UBUNTU_VERSION) == "20.04" ]]; then
+elif [[ $UBUNTU_VERSION == "20.04" ]]; then
        sudo apt install libssl-dev libzip5 python-pip-whl
 else
        echo "Unsupported Linux version."
--- src/install_scripts/linux_optional_compilation_dependencies.sh
+++ src/install_scripts/linux_optional_compilation_dependencies.sh
@@ -5,15 +5,15 @@ sudo apt update
 sudo apt install python3.5-dev python3.6-dev python3.7-dev python3.8-dev
 
 UBUNTU_VERSION=$(lsb_release -rs)
-if [[ $(UBUNTU_VERSION) == "16.04" ]]; then
+if [[ $UBUNTU_VERSION == "16.04" ]]; then
        sudo apt install openjdk-8-jdk
        curl -sL https://deb.nodesource.com/setup_12.x -o nodesource_setup.sh
        sudo bash nodesource_setup.sh
        sudo apt-get install nodejs
        rm nodesource_setup.sh
-elif [[ $(UBUNTU_VERSION) == "18.04" ]]; then
+elif [[ $UBUNTU_VERSION == "18.04" ]]; then
        sudo apt install openjdk-11-jdk npm
-elif [[ $(UBUNTU_VERSION) == "20.04" ]]; then
+elif [[ $UBUNTU_VERSION == "20.04" ]]; then
        sudo apt install openjdk-14-jdk npm
 else
        echo "Unsupported Linux version."
