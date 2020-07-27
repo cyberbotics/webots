b761792a52b4f76263895773bf62e0549511475c
DavidMansolino <David.Mansolino@cyberbotics.com>
2020-07-27 10:10:11 +0200

    Fix compilation.

--- src/webots/control/WbLanguageTools.cpp
+++ src/webots/control/WbLanguageTools.cpp
@@ -80,7 +80,7 @@ QString WbLanguageTools::pythonCommand(QString &shortVersion, const QString &com
   if (!command.endsWith(".exe", Qt::CaseInsensitive))
     pythonCommand += ".exe";
   QProcess process;
-  process->setProcessEnvironment(env);
+  process.setProcessEnvironment(env);
   process.start(pythonCommand, QStringList() << "-u"
                                              << "-c"
                                              << "import sys;print(sys.version);print(sys.maxsize > 2**32)");
@@ -98,6 +98,7 @@ QString WbLanguageTools::pythonCommand(QString &shortVersion, const QString &com
     shortVersion = QString(version[0][0]) + version[0][2];
 #else  // macOS and Linux
   QProcess process;
+  process.setProcessEnvironment(env);
   process.start(pythonCommand, QStringList() << "-c"
                                              << "import sys;print(sys.version);");
   process.waitForFinished();
