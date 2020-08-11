622faeda8cb569423bad5a34af60e97deb36a0b9
Olivier Michel <Olivier.Michel@cyberbotics.com>
2020-08-11 18:06:29 +0200

    cleanup

--- src/webots/core/WbHttpReply.cpp
+++ src/webots/core/WbHttpReply.cpp
@@ -40,30 +40,7 @@ QByteArray WbHttpReply::forgeHTMLReply(const QString &htmlContent) {
   reply.append(htmlContent);
   return reply;
 }
-/*
-QByteArray WbHttpReply::forgeImageReply(const QString &imageFileName) {
-  QByteArray reply;
 
-  QFile imageFile(imageFileName);
-  if (!imageFile.open(QIODevice::ReadOnly))
-    return forge404Reply();
-
-  QByteArray imageData = imageFile.readAll();
-  int imageSize = imageData.length();
-  QFileInfo fi(imageFile);
-  QString imageExtension = fi.suffix().toLower();
-
-  reply.append("HTTP/1.1 200 OK\r\n");
-  reply.append("Access-Control-Allow-Origin: *\r\n");
-  reply.append("Cache-Control: public, max-age=3600\r\n");  // Help the browsers to cache the images for 1 hour.
-  reply.append(QString("Content-Type: image/%1\r\n").arg(imageExtension));
-  reply.append(QString("Content-Length: %1\r\n").arg(imageSize));
-  reply.append("\r\n");
-  reply.append(imageData);
-
-  return reply;
-}
-*/
 QByteArray WbHttpReply::forgeFileReply(const QString &fileName) {
   QByteArray reply;
 
--- src/webots/core/WbHttpReply.hpp
+++ src/webots/core/WbHttpReply.hpp
@@ -20,7 +20,6 @@
 namespace WbHttpReply {
   QByteArray forge404Reply();
   QByteArray forgeHTMLReply(const QString &htmlContent);
-  // QByteArray forgeImageReply(const QString &imageFileName);
   QByteArray forgeFileReply(const QString &fileName);
 };  // namespace WbHttpReply
 
