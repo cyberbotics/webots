5aefbef02d240ddc835ca44690b1d014da920125
David Mansolino <David.Mansolino@cyberbotics.com>
2020-08-17 14:36:57 +0200

    Improve conversion.

--- scripts/converter/convert_to_nue.py
+++ scripts/converter/convert_to_nue.py
@@ -16,6 +16,7 @@
 
 """Convert world file from R2020a to R2020b keeping the NUE coordinate system."""
 
+import math
 import sys
 
 from transforms3d import quaternions
@@ -24,10 +25,10 @@ from webots_parser import WebotsParser
 from converted_protos import converted_protos
 
 
-def rotation(value, r):
+def rotation(value, r, invert=False):
     q0 = quaternions.axangle2quat([float(value[0]), float(value[1]), float(value[2])], float(value[3]))
     q1 = quaternions.axangle2quat([r[0], r[1], r[2]], r[3])
-    qr = quaternions.qmult(q0, q1)
+    qr = quaternions.qmult(q1, q0) if invert else quaternions.qmult(q0, q1)
     v, theta = quaternions.quat2axangle(qr)
     return [WebotsParser.str(v[0]), WebotsParser.str(v[1]), WebotsParser.str(v[2]), WebotsParser.str(theta)]
 
@@ -36,6 +37,7 @@ def convert_to_nue(filename):
     world = WebotsParser()
     world.load(filename)
 
+    isNUE = False
     for node in world.content['root']:
         if node['name'] == 'WorldInfo':
             for field in node['fields']:
@@ -46,9 +48,11 @@ def convert_to_nue(filename):
                     field['value'] = WebotsParser.str(gravity)
                     field['type'] = 'SFFloat'
                 if field['name'] == 'coordinateSystem':  # world file already updated
-                    return
-            node['fields'].append({'name': 'coordinateSystem', 'value': 'NUE', 'type': 'SFString'})
-        elif node['name'] in converted_protos:
+                    del node['fields'][node['fields'].index(field)]
+                    isNUE = True
+            if not isNUE:
+                node['fields'].append({'name': 'coordinateSystem', 'value': 'NUE', 'type': 'SFString'})
+        elif not isNUE and node['name'] in converted_protos:
             print('Rotating', node['name'])
             rotation_found = False
             for field in node['fields']:
@@ -58,6 +62,19 @@ def convert_to_nue(filename):
             if not rotation_found:
                 node['fields'].append({'name': 'rotation',
                                        'value': rotation(['0', '1', '0', '0'], converted_protos[node['name']]),
+                                       'type': 'SFRotation'})
+        elif isNUE and node['name'] not in ['Viewpoint', 'TexturedBackground', 'TexturedBackgroundLight']:
+            print('Rotating', node['name'])
+            rotation_found = False
+            for field in node['fields']:
+                if field['name'] in ['rotation']:
+                    rotation_found = True
+                    field['value'] = rotation(field['value'], [1, 0, 0, 0.5 * math.pi], True)
+                elif field['name'] in ['translation']:
+                    field['value'] = [field['value'][0], str(-float(field['value'][2])), field['value'][1]]
+            if not rotation_found:
+                node['fields'].append({'name': 'rotation',
+                                       'value': ['1', '0', '0', str(0.5 * math.pi)],
                                        'type': 'SFRotation'})
     world.save(filename)
 
