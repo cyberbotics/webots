92ddd2d05111582f622a22d2788ec0118d727ce4
Stefania Pedrazzi <stefania.pedrazzi@cyberbotics.com>
2020-08-17 16:41:52 +0200

    Split long comment line

--- tests/rendering/controllers/mirror_supervisor/mirror_supervisor.c
+++ tests/rendering/controllers/mirror_supervisor/mirror_supervisor.c
@@ -15,7 +15,8 @@
 // even if the green object is rendered behind the overlays, the top area of the overlays' image should be white (199, 199,
 // 199).
 
-// The mirror camera should be open in the external rendering device window to test that the rendering in the external window doesn't affect the main rendering.
+// The mirror camera should be open in the external rendering device window to test that the rendering in the external window
+// doesn't affect the main rendering.
 
 int main(int argc, char **argv) {
   ts_setup(argv[0]);
@@ -53,7 +54,7 @@ int main(int argc, char **argv) {
   // test Webots doesn't crash when deleting a camera when the external window is open
   WbNodeRef robot_camera = wb_supervisor_node_get_from_def("ROBOT_CAMERA");
   wb_supervisor_node_remove(robot_camera);
-  
+
   wb_robot_step(TIME_STEP);
 
   ts_send_success();
