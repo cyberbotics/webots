f1d2c0d9e0601a03f41376b564cc37c3a7670d97
Stefania Pedrazzi <stefania.pedrazzi@cyberbotics.com>
2020-08-17 17:40:18 +0200

    Fix returned value in case no answer is received from Webots due to invalid node

--- src/Controller/api/supervisor.c
+++ src/Controller/api/supervisor.c
@@ -1960,6 +1960,8 @@ const double *wb_supervisor_node_get_velocity(WbNodeRef node) {
   }
 
   robot_mutex_lock_step();
+  free(node->solid_velocity);
+  node->solid_velocity = NULL;
   get_velocity_node_ref = node;
   wb_robot_flush_unlocked();
   get_velocity_node_ref = NULL;
