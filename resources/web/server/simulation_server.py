cd07c93b3c8835d8c454d6c8738612a18baf97e5
Olivier Michel <Olivier.Michel@cyberbotics.com>
2020-08-19 17:16:06 +0200

    Update resources/web/server/simulation_server.py
    Co-authored-by: Stefania Pedrazzi <stefaniapedrazzi@users.noreply.github.com>

--- resources/web/server/simulation_server.py
+++ resources/web/server/simulation_server.py
@@ -250,7 +250,7 @@ class Client:
             world = self.project_instance_path + '/worlds/' + self.world
             port = client.streaming_server_port
             command = config['webots'] + ' --batch --mode=pause '
-            # on Windows, the MJPEG stream won't work if the Webots window is minimized
+            # the MJPEG stream won't work if the Webots window is minimized
             if not hasattr(self, 'mode') or self.mode == 'x3d':
                 command += '--minimize '
             command += '--stream="port=' + str(port) + ';monitorActivity'
