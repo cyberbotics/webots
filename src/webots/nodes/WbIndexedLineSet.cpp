ba9d6ca6562fe30d46d24ce63bae5fd3238e4bfb
Olivier Michel <Olivier.Michel@cyberbotics.com>
2020-08-26 10:05:40 +0200

    Update WbIndexedLineSet.cpp

--- src/webots/nodes/WbIndexedLineSet.cpp
+++ src/webots/nodes/WbIndexedLineSet.cpp
@@ -69,15 +69,11 @@ WbCoordinate *WbIndexedLineSet::coord() const {
 
 void WbIndexedLineSet::createWrenObjects() {
   WbGeometry::createWrenObjects();
-
   updateCoord();
-
   if (isInBoundingObject())
     connect(WbWrenRenderingContext::instance(), &WbWrenRenderingContext::lineScaleChanged, this,
             &WbIndexedLineSet::updateLineScale);
-
   buildWrenMesh();
-
   emit wrenObjectsCreated();
 }
 
