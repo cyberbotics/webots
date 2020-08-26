8f4ddf9a26a192fe0c4c4ee444d4e870833163d5
Gautier Arcin <gaarci@pc079l.ad.phedre.fr>
2020-08-26 16:43:17 +0200

    minor change to connector

--- tests/api/controllers/connector/connector.c
+++ tests/api/controllers/connector/connector.c
@@ -22,12 +22,18 @@ int main(int argc, char **argv) {
       ts_assert_int_equal(wb_connector_get_presence(rear_connector), 1, "connector presence should be 1");
       wb_connector_unlock(front_connector);
       wb_connector_unlock(rear_connector);
-      ts_assert_boolean_equal(wb_connector_is_locked(rear_connector), false, "connector presence should be unlocked");
- 
-    } else if (t == 38 * TIME_STEP) {
+
+    } else if (t == 35 * TIME_STEP) {
+      ts_assert_boolean_equal(wb_connector_is_locked(rear_connector), true, "connector presence should be locked");
+      break;
+
+    }  else if (t == 38 * TIME_STEP) {
       ts_assert_int_equal(wb_connector_get_presence(rear_connector), 0,
                           "connector presence should be 0, as the other module should have fallen.");
       wb_connector_unlock(rear_connector);
+      break;
+
+    }  else if (t == 39 * TIME_STEP) {
       ts_assert_boolean_equal(wb_connector_is_locked(rear_connector), true, "connector presence should be locked");
       break;
     }
