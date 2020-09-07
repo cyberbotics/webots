98d2071665dd5d0a3f68e9fdac77f9541311e445
David Mansolino <David.Mansolino@cyberbotics.com>
2020-09-07 15:35:02 +0200

    remove useless include.

--- src/webots/nodes/WbHingeJointParameters.cpp
+++ src/webots/nodes/WbHingeJointParameters.cpp
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "WbHingeJointParameters.hpp"
-
 void WbHingeJointParameters::init(bool fromDeprecatedHinge2JointParameters) {
   mAnchor = findSFVector3("anchor");
   mSuspensionSpringConstant = findSFDouble("suspensionSpringConstant");
