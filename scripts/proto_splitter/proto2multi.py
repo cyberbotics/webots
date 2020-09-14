31a25631117b795d96bff11c8c020e1793c27afe
Simon Steinmann <52213164+Simon-Steinmann@users.noreply.github.com>
2020-09-14 14:42:12 +0200

    Update proto2multi.py

--- scripts/proto_splitter/proto2multi.py
+++ scripts/proto_splitter/proto2multi.py
@@ -1,9 +1,51 @@
+#!/usr/bin/env python3
 
+# Copyright 1996-2020 Cyberbotics Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Author Simon Steinmann (https://github.com/Simon-Steinmann)
+
+
+
+
+# Automatic PROTO to multi-file PROTO trimesh extractor
+#
+# INSTRUCTIONS:
+# Call the script with the --input=<path> argument
+#
+# 1. if <path> ends in <filename.proto>:
+#   - creates a new folder in the same directory named "multifile_<filename>"
+#   - creates a new <filename.proto> inside this folder with all trimeshes replaced
+#      by proto files, placed in a subfolder
+#
+# 2. If <path> does not end in ".proto" -> assumes directory
+#   - creates a new folder inside of <path> called <multi_file_conversion>
+#   - searches for every .proto file in <path> recursively and:
+#       - creates a conversion as described in 1.
+#       - places conversion into the <multi_file_conversion> folder, preserving
+#         the same folder structure as in <path>. This allows <path> to be replaced 
+#         with <path>/<multi_file_conversion> when validated, as names structures 
+#         are preserved
+#
+
+
+
+import xml.etree.ElementTree as ET
 import os
 import optparse
+import xml.dom.minidom
 import errno
-from tkinter.filedialog import askopenfilename, askdirectory
-
 
 def mkdirSafe(directory):
     """Create a dir safely."""
@@ -15,10 +57,10 @@ def mkdirSafe(directory):
         else:
             print('Directory "' + directory + '" already exists!')
 
-
 class proto2multi():
     def __init__(self):
-        print('Proto 2 multi-file proto converter by Simon Steinmann')
+        print('Proto 2 multi-file proto converter by Simon Steinmann')     
+
 
     def header(self, proto):
         """Specify VRML file header."""
@@ -29,7 +71,7 @@ class proto2multi():
         proto.write('# This is a proto file for Webots for the ' + self.robotName + '\n\n')
 
     def createProto(self, string):
-        name = self.robotName + '_' + str(self.shapeIndex)
+        name = self.robotName + '_' + str(self.shapeIndex)           
         print('Create meshFile: %sMesh.proto' % name)
         filepath = '%s/%sMesh.proto' % (self.meshFilesPath, name)
         meshProtoFile = open(filepath, 'w')
@@ -59,28 +101,28 @@ class proto2multi():
         self.pf = open(outFile, 'w')
         self.shapeIndex = 0
 
-        indent = '  '
-        level = 0
+        indent = '  ' 
+        level = 0 
         while True:
             line = self.f.readline()
-            ln = line.split()
+            ln = line.split()  
             # termination condition:
             eof = 0
             while ln == []:
                 line = self.f.readline()
-                ln = line.split()
+                ln = line.split() 
                 eof += 1
                 if eof > 10:
                     print('done parsing')
                     self.pf.close()
-                    return
-            if 'IndexedFaceSet' in ln:
+                    return 
+            if 'IndexedFaceSet' in ln:      
                 shapeLevel = 1
                 newProtoString = shapeLevel * indent + ' '.join(ln[-2:]) + '\n'
                 defString = ''
                 if 'DEF' in ln:
                     defString = 'DEF ' + ln[ln.index('DEF') + 1]
-                shapeLevel = 2
+                shapeLevel = 2             
                 while shapeLevel > 1:
                     line = self.f.readline()
                     ln = line.split()
@@ -98,7 +140,7 @@ class proto2multi():
                 elif '{' in ln or '[' in ln:
                     level += 1
                 self.pf.write(line)
-
+                
     def convert_all(self, sourcePath):
         outPath = sourcePath + '/multi_file_conversioin'
         mkdirSafe(outPath)
@@ -117,20 +159,22 @@ class proto2multi():
             inFile = sourcePath + proto
             outFile = outPath + proto
             self.convert(inFile, outFile)
+                        
+        
 
 
 if __name__ == "__main__":
     optParser = optparse.OptionParser(usage='usage: %prog  [options]')
-    optParser.add_option('--file', dest='inFile', default=None, help='Specifies the self.robotName.')
-    optParser.add_option('--all', dest='all', action='store_true', default=False,
-                         help='If set, converts all .proto files in the chosen folder')
+    optParser.add_option('--input', dest='inPath', default=None, 
+      help='Specifies the proto file, or a directory. Converts all .proto files, if it is a directory.')
     options, args = optParser.parse_args()
-    inFile = options.inFile
+
+    inPath = options.inPath
     p2m = proto2multi()
-    if options.all:
-        path = askdirectory(title='Select Folder')  # Shows dialog box and return the path
-        p2m.convert_all(path)
+    if os.path.basename(inPath).split('.')[-1] == 'proto':        
+        p2m.convert(inPath)
     else:
-        if inFile is None:
-            inFile = askopenfilename(title='Select .proto file')  # Shows dialog box and return the path
-        p2m.convert(inFile)
+        inPath = os.path.abspath(inPath)
+        p2m.convert_all(inPath)          
+        
+    
