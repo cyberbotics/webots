4ff435e97b79b8d18975f0c279cb4a6874498586
Olivier Michel <Olivier.Michel@cyberbotics.com>
2020-09-23 10:25:44 +0200

    Update src/Controller/api/system.c
    Co-authored-by: Stefania Pedrazzi <stefaniapedrazzi@users.noreply.github.com>

--- src/Controller/api/system.c
+++ src/Controller/api/system.c
@@ -177,6 +177,7 @@ const char *wbu_system_webots_tmp_path() {
       struct dirent *entry;
       char folder_start[32];
       sprintf(folder_start, "webots-%d", webots_pid);
+      // reset webots_pid containing the webots script process id before retrieving the webots binary id
       webots_pid = 0;
       while ((entry = readdir(dir))) {
         if (strncmp(entry->d_name, folder_start, strlen(folder_start)) == 0) {
