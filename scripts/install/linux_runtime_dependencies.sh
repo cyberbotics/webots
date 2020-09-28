c4af2136ac7a550796587f65bcb7b74636ea7033
Stefania Pedrazzi <stefania.pedrazzi@cyberbotics.com>
2020-09-28 11:37:12 +0200

    Fix variable name and missing dependency

--- scripts/install/linux_runtime_dependencies.sh
+++ scripts/install/linux_runtime_dependencies.sh
@@ -9,7 +9,7 @@ apt update
 apt install --yes lsb-release g++ make libavcodec-extra libglu1-mesa libxkbcommon-x11-dev execstack libusb-dev libxcb-keysyms1 libxcb-image0 libxcb-icccm4 libxcb-randr0 libxcb-render-util0 libxcb-xinerama0 libxcomposite-dev libxtst6 libnss3
 
 UBUNTU_VERSION=$(lsb_release -rs)
-if [[ $UBUNTU_VERSIOM == "16.04" ]]; then
+if [[ $UBUNTU_VERSION == "16.04" ]]; then
        apt install --yes libav-tools
 elif [[ $UBUNTU_VERSION == "18.04" ]]; then
        apt install --yes ffmpeg
