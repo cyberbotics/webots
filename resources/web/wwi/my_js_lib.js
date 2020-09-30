54d4aa72e015cafe368f93bcbbd5710ece3e64f4
Benjamin Délèze <benjamin.deleze@epfl.ch>
2020-09-30 10:15:51 +0200

    boxbuffergeometry

--- resources/web/wwi/my_js_lib.js
+++ resources/web/wwi/my_js_lib.js
@@ -155,9 +155,8 @@ class Cam extends Obj3d {
 }
 
 
-class BoxBufferGeo extends THREE.BufferGeometry {
+class BoxBufferGeo {
   constructor( width = 1, height = 1, depth = 1, widthSegments = 1, heightSegments = 1, depthSegments = 1 ) {
-    super(width,height,depth,widthSegments,heightSegments,depthSegments );
     this.type = 'BoxBufferGeometry';
 		this.width = width;
 		this.height = height;
@@ -165,29 +164,23 @@ class BoxBufferGeo extends THREE.BufferGeometry {
 		this.widthSegments = widthSegments;
 		this.heightSegments = heightSegments;
 		this.depthSegments = depthSegments;
-  
-		const scope = this;
+    this.attributes = {};
+    this.isBufferGeometry = true
+    this.drawRange = { start: 0, count: Infinity };
 
 		// segments
-
 		widthSegments = Math.floor( widthSegments );
 		heightSegments = Math.floor( heightSegments );
 		depthSegments = Math.floor( depthSegments );
 
 		// buffers
-
 		const indices = [];
 		const vertices = [];
-		const normals = [];
-		const uvs = [];
 
 		// helper variables
-
 		let numberOfVertices = 0;
-		let groupStart = 0;
 
 		// build each side of the box geometry
-
 		buildPlane( 'z', 'y', 'x', - 1, - 1, depth, height, width, depthSegments, heightSegments, 0 ); // px
 		buildPlane( 'z', 'y', 'x', 1, - 1, depth, height, - width, depthSegments, heightSegments, 1 ); // nx
 		buildPlane( 'x', 'z', 'y', 1, 1, width, depth, height, widthSegments, depthSegments, 2 ); // py
@@ -196,14 +189,10 @@ class BoxBufferGeo extends THREE.BufferGeometry {
 		buildPlane( 'x', 'y', 'z', - 1, - 1, width, height, - depth, widthSegments, heightSegments, 5 ); // nz
 
 		// build geometry
-
 		this.setIndex( indices );
 		this.attributes['position'] = new THREE.Float32BufferAttribute( vertices, 3 );
-		this.attributes['normal'] = new THREE.Float32BufferAttribute( normals, 3 );
-		this.attributes ['uv'] = new THREE.Float32BufferAttribute( uvs, 2 );
 
 		function buildPlane( u, v, w, udir, vdir, width, height, depth, gridX, gridY, materialIndex ) {
-
 			const segmentWidth = width / gridX;
 			const segmentHeight = height / gridY;
 
@@ -215,96 +204,73 @@ class BoxBufferGeo extends THREE.BufferGeometry {
 			const gridY1 = gridY + 1;
 
 			let vertexCounter = 0;
-			let groupCount = 0;
 
-			const vector = new THREE.Vector3();
-
+			//const vector = new THREE.Vector3();
+      const vector = new glm.vec3();
 			// generate vertices, normals and uvs
-
 			for ( let iy = 0; iy < gridY1; iy ++ ) {
-
 				const y = iy * segmentHeight - heightHalf;
-
 				for ( let ix = 0; ix < gridX1; ix ++ ) {
-
 					const x = ix * segmentWidth - widthHalf;
 
 					// set values to correct vector component
-
 					vector[ u ] = x * udir;
 					vector[ v ] = y * vdir;
 					vector[ w ] = depthHalf;
 
 					// now apply vector to vertex buffer
-
 					vertices.push( vector.x, vector.y, vector.z );
 
 					// set values to correct vector component
-
 					vector[ u ] = 0;
 					vector[ v ] = 0;
 					vector[ w ] = depth > 0 ? 1 : - 1;
 
-					// now apply vector to normal buffer
-
-					normals.push( vector.x, vector.y, vector.z );
-
-					// uvs
-
-					uvs.push( ix / gridX );
-					uvs.push( 1 - ( iy / gridY ) );
-
-					// counters
-
 					vertexCounter += 1;
-
 				}
-
 			}
 
 			// indices
-
 			// 1. you need three indices to draw a single face
 			// 2. a single segment consists of two faces
 			// 3. so we need to generate six (2*3) indices per segment
-
 			for ( let iy = 0; iy < gridY; iy ++ ) {
-
 				for ( let ix = 0; ix < gridX; ix ++ ) {
-
 					const a = numberOfVertices + ix + gridX1 * iy;
 					const b = numberOfVertices + ix + gridX1 * ( iy + 1 );
 					const c = numberOfVertices + ( ix + 1 ) + gridX1 * ( iy + 1 );
 					const d = numberOfVertices + ( ix + 1 ) + gridX1 * iy;
 
 					// faces
-
 					indices.push( a, b, d );
 					indices.push( b, c, d );
 
-					// increase counter
-
-					groupCount += 6;
-
 				}
-
 			}
 
-			// add a group to the geometry. this will ensure multi material support
-
-			scope.addGroup( groupStart, groupCount, materialIndex );
-
-			// calculate new start value for groups
-
-			groupStart += groupCount;
-
 			// update total number of vertices
-
 			numberOfVertices += vertexCounter;
-
 		}
-
 	}
+  arrayMax( array ) {
+    if ( array.length === 0 ) return - Infinity;
+    let max = array[ 0 ];
+    for ( let i = 1, l = array.length; i < l; ++ i ) {
+      if ( array[ i ] > max ) max = array[ i ];
+    }
+    return max;
+  }
+  setIndex(index) {
+    if ( Array.isArray( index ) ) {
+      this.index = new ( this.arrayMax( index ) > 65535 ? THREE.Uint32BufferAttribute : THREE.Uint16BufferAttribute )( index, 1 );
+    } else {
+      this.index = index;
+    }
+    return this;
+  }
+  addEventListener(){
+
+  }
 }
 
 class MeshBasicMat {
