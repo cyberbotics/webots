599dba1f86b75fca2e695089e90c2fc3475fb79a
David Mansolino <mansolino.david@gmail.com>
2020-10-14 16:42:22 +0200

    Update RosLidar.cpp

--- projects/default/controllers/ros/RosLidar.cpp
+++ projects/default/controllers/ros/RosLidar.cpp
@@ -170,6 +170,7 @@ bool RosLidar::setFrequencyCallback(webots_ros::set_float::Request &req, webots_
   return true;
 }
 
+// cppcheck-suppress constParameter
 // cppcheck-suppress constParameterCallback
 bool RosLidar::enablePointCloudCallback(webots_ros::set_bool::Request &req, webots_ros::set_bool::Response &res) {
   if (!mIsPointCloudEnabled && req.value) {
