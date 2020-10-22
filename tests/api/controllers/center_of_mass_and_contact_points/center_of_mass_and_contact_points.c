2f886de661e3f5d81789938ca8adc9814519368b
David Mansolino <David.Mansolino@cyberbotics.com>
2020-10-22 13:02:11 +0200

    Improve test.

--- tests/api/controllers/center_of_mass_and_contact_points/center_of_mass_and_contact_points.c
+++ tests/api/controllers/center_of_mass_and_contact_points/center_of_mass_and_contact_points.c
@@ -61,7 +61,8 @@ int main(int argc, char **argv) {
                           "First contact point should belong to the main node.");
 
   ts_assert_boolean_equal(wb_supervisor_node_get_contact_point_node(
-                            node, REFERENCE_NUMBER_OF_CONTACT_POINTS + ADDITIONAL_CONTACT_POINTS_NUMBER - 1) != node,
+                            node, REFERENCE_NUMBER_OF_CONTACT_POINTS + ADDITIONAL_CONTACT_POINTS_NUMBER - 1) ==
+                            wb_supervisor_node_get_from_def("SECONDARY_SOLID"),
                           "Last contact point should not belong to the main node.");
 
   // static balance checks
