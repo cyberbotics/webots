6eb504e5c4b70c3f5bf3615003b66291cdc62d5e
Benjamin Délèze <benjamin.deleze@epfl.ch>
2020-10-26 17:35:34 +0100

    wrenjs.js empty

---
+++ resources/web/wwi/wrenjs.js
@@ -0,0 +1,15 @@
+class Scene {
+//initialize the scene as in the main loop of default.cpp
+}
+
+class Viewpoint {
+//camera
+}
+
+class Shape {
+//compile and link the shaders
+}
+
+class Box {
+
+}
