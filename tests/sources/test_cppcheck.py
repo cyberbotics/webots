8f186e023c3ed553ade5468466c87d0032ff58ff
David Mansolino <David.Mansolino@cyberbotics.com>
2020-10-28 17:18:26 +0100

    debug 2

--- tests/sources/test_cppcheck.py
+++ tests/sources/test_cppcheck.py
@@ -170,7 +170,7 @@ class TestCppCheck(unittest.TestCase):
         command = self.cppcheck + ' --enable=warning,style,performance,portability --inconclusive --force -q '
         command += '--inline-suppr --suppress=invalidPointerCast --suppress=useStlAlgorithm -UKROS_COMPILATION '
         command += '--suppress=strdupCalled '
-        # command += '--xml '  # Uncomment this line to get more information on the errors
+        command += '--xml '  # Uncomment this line to get more information on the errors
         command += '--std=c++03 --output-file=\"' + self.reportFilename + '\"'
         sources = self.add_source_files(sourceDirs, skippedDirs, skippedfiles)
         if not sources:
