90008fc4d48986718c9d70271ef2fd691aed853e
Stefania Pedrazzi <stefaniapedrazzi@users.noreply.github.com>
2020-11-16 16:53:10 +0100

    Remove unneeded sudo

--- scripts/install/linux_optional_compilation_dependencies.sh
+++ scripts/install/linux_optional_compilation_dependencies.sh
@@ -10,8 +10,8 @@ apt install --yes software-properties-common
 add-apt-repository -y ppa:deadsnakes/ppa
 apt update
 apt install --yes lsb-release curl python3.6-dev python3.7-dev python3.8-dev python3.9-dev dirmngr
-curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash -
-sudo apt-get install -y nodejs
+curl -sL https://deb.nodesource.com/setup_15.x | bash -
+apt-get install --yes nodejs
 
 UBUNTU_VERSION=$(lsb_release -rs)
 if [[ $UBUNTU_VERSION == "16.04" ]]; then
