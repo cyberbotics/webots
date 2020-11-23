9186927ce69b7a213f167634bc68c17c9ce09be6
Darko Lukic <lukicdarkoo@gmail.com>
2020-11-23 18:40:49 +0100

    Add mimic method

--- resources/languages/python/tests/arraymimic.c
+++ resources/languages/python/tests/arraymimic.c
@@ -1,29 +1,24 @@
 #include <Python.h>
 
-typedef struct
-{
+typedef struct {
   PyObject_HEAD char *arr;
   int len;
 } ArrayMimic;
 
-static int ArrayMimic_init(ArrayMimic *self, PyObject *args, PyObject *kwds)
-{
+static int ArrayMimic_init(ArrayMimic *self, PyObject *args, PyObject *kwds) {
   self->len = 2;
   self->arr = (char *)malloc(self->len * sizeof(char));
-  for (int i = 0; i < self->len; i++)
-  {
+  for (int i = 0; i < self->len; i++) {
     self->arr[i] = i;
   }
 
   return 0;
 }
 
-static PyObject *ArrayMimic_str(ArrayMimic *self)
-{
+static PyObject *ArrayMimic_str(ArrayMimic *self) {
   int pos = 0;
   char *s = malloc(self->len * 10);
-  for (int i = 0; i < self->len; i++)
-  {
+  for (int i = 0; i < self->len; i++) {
     pos += sprintf(s + pos, "%d, ", self->arr[i]);
   }
 
@@ -32,10 +27,8 @@ static PyObject *ArrayMimic_str(ArrayMimic *self)
   return ret;
 }
 
-static int ArrayMimic_getbuffer(PyObject *obj, Py_buffer *view, int flags)
-{
-  if (view == NULL)
-  {
+static int ArrayMimic_getbuffer(PyObject *obj, Py_buffer *view, int flags) {
+  if (view == NULL) {
     PyErr_SetString(PyExc_ValueError, "NULL view in getbuffer");
     return -1;
   }
@@ -58,60 +51,72 @@ static int ArrayMimic_getbuffer(PyObject *obj, Py_buffer *view, int flags)
 }
 
 static PyBufferProcs ArrayMimic_as_buffer = {
-    (getbufferproc)ArrayMimic_getbuffer,
-    (releasebufferproc)0,
+  (getbufferproc)ArrayMimic_getbuffer,
+  (releasebufferproc)0,
 };
+
+static PyObject *ArrayMimic_setpointer(ArrayMimic *self, PyObject *original) {
+  // This method copies reference (not data) from the passed Python object which supports a Python buffer protocol.
+  // In that way this object mimics the original one, except this one exposed as `array.array`.
+
+  Py_buffer buffer;
+
+  if (PyObject_GetBuffer(original, &buffer, PyBUF_WRITABLE) == -1) {
+    return NULL;
+  }
+
+  self->arr = buffer.buf;
+  self->len = buffer.len;
+
+  PyBuffer_Release(&buffer);
+  return Py_BuildValue("d", 1);
+}
+
+static PyMethodDef ArrayMimic_methods[] = {{"setpointer", (PyCFunction)ArrayMimic_setpointer, METH_O, "Set memory pointer"},
+                                           {NULL, NULL, 0, NULL}};
 
 static PyTypeObject ArrayMimicType = {
-    PyVarObject_HEAD_INIT(NULL, 0) "array.array", /* tp_name */
-    sizeof(ArrayMimic),                           /* tp_basicsize */
-    0,                                            /* tp_itemsize */
-    0,                                            /* tp_dealloc */
-    0,                                            /* tp_print */
-    0,                                            /* tp_getattr */
-    0,                                            /* tp_setattr */
-    0,                                            /* tp_reserved */
-    (reprfunc)ArrayMimic_str,                     /* tp_repr */
-    0,                                            /* tp_as_number */
-    0,                                            /* tp_as_sequence */
-    0,                                            /* tp_as_mapping */
-    0,                                            /* tp_hash  */
-    0,                                            /* tp_call */
-    (reprfunc)ArrayMimic_str,                     /* tp_str */
-    0,                                            /* tp_getattro */
-    0,                                            /* tp_setattro */
-    &ArrayMimic_as_buffer,                        /* tp_as_buffer */
-    Py_TPFLAGS_DEFAULT,                           /* tp_flags */
-    "ArrayMimic object",                          /* tp_doc */
-    0,                                            /* tp_traverse */
-    0,                                            /* tp_clear */
-    0,                                            /* tp_richcompare */
-    0,                                            /* tp_weaklistoffset */
-    0,                                            /* tp_iter */
-    0,                                            /* tp_iternext */
-    0,                                            /* tp_methods */
-    0,                                            /* tp_members */
-    0,                                            /* tp_getset */
-    0,                                            /* tp_base */
-    0,                                            /* tp_dict */
-    0,                                            /* tp_descr_get */
-    0,                                            /* tp_descr_set */
-    0,                                            /* tp_dictoffset */
-    (initproc)ArrayMimic_init,                    /* tp_init */
+  PyVarObject_HEAD_INIT(NULL, 0) "array.array", /* tp_name */
+  sizeof(ArrayMimic),                           /* tp_basicsize */
+  0,                                            /* tp_itemsize */
+  0,                                            /* tp_dealloc */
+  0,                                            /* tp_print */
+  0,                                            /* tp_getattr */
+  0,                                            /* tp_setattr */
+  0,                                            /* tp_reserved */
+  (reprfunc)ArrayMimic_str,                     /* tp_repr */
+  0,                                            /* tp_as_number */
+  0,                                            /* tp_as_sequence */
+  0,                                            /* tp_as_mapping */
+  0,                                            /* tp_hash  */
+  0,                                            /* tp_call */
+  (reprfunc)ArrayMimic_str,                     /* tp_str */
+  0,                                            /* tp_getattro */
+  0,                                            /* tp_setattro */
+  &ArrayMimic_as_buffer,                        /* tp_as_buffer */
+  Py_TPFLAGS_DEFAULT,                           /* tp_flags */
+  "ArrayMimic object",                          /* tp_doc */
+  0,                                            /* tp_traverse */
+  0,                                            /* tp_clear */
+  0,                                            /* tp_richcompare */
+  0,                                            /* tp_weaklistoffset */
+  0,                                            /* tp_iter */
+  0,                                            /* tp_iternext */
+  ArrayMimic_methods,                           /* tp_methods */
+  0,                                            /* tp_members */
+  0,                                            /* tp_getset */
+  0,                                            /* tp_base */
+  0,                                            /* tp_dict */
+  0,                                            /* tp_descr_get */
+  0,                                            /* tp_descr_set */
+  0,                                            /* tp_dictoffset */
+  (initproc)ArrayMimic_init,                    /* tp_init */
 };
 
-static PyMethodDef ArrayMimic_methods[] = {
-    {NULL, NULL, 0, NULL}};
+static struct PyModuleDef ArrayMimic_module = {PyModuleDef_HEAD_INIT, "arraymimic", "Module mytest description", -1,
+                                               ArrayMimic_methods};
 
-static struct PyModuleDef ArrayMimic_module = {
-    PyModuleDef_HEAD_INIT,
-    "arraymimic",
-    "Module mytest description",
-    -1,
-    ArrayMimic_methods};
-
-PyMODINIT_FUNC PyInit_arraymimic(void)
-{
+PyMODINIT_FUNC PyInit_arraymimic(void) {
   PyObject *module = PyModule_Create(&ArrayMimic_module);
 
   ArrayMimicType.tp_new = PyType_GenericNew;
--- resources/languages/python/tests/test.py
+++ resources/languages/python/tests/test.py
@@ -7,6 +7,11 @@ arr = array.array('B', [8])
 print(arr)
 len(arr)
 
+# Test byte array
+barr = bytearray(b'123')
+
 # Test built our module
 arr = arraymimic.array()
+print(arr)
+arr.setpointer(barr)
 print(arr)
