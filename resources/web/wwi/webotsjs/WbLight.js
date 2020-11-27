4ad645969f1e367ab7a4bb8e68da4727fb9fb80e
Benjamin Délèze <benjamin.deleze@epfl.ch>
2020-11-27 10:04:22 +0100

    remove ambient color as it is buggy and not need until we actually export ambientIntensity

--- resources/web/wwi/webotsjs/WbLight.js
+++ resources/web/wwi/webotsjs/WbLight.js
@@ -28,21 +28,8 @@ class WbLight extends WbBaseNode {
   applyLightShadowsToWren(){}
 
   applySceneAmbientColorToWren() {
-    this.computeAmbientLight();
-  }
-
-  computeAmbientLight() {
-    let rgb = glm.vec3(0,0,0);
-
-    //foreach (const WbLight *light, cLights) {
-      if (this.on) {
-        rgb[0] += this.ambientIntensity * this.color.x;
-        rgb[1] += this.ambientIntensity * this.color.y;
-        rgb[2] += this.ambientIntensity * this.color.z;
-      }
-    //}
-    let pointer = _wrjs_color_array(rgb.x, rgb.y, rgb.z);
-    _wr_scene_set_ambient_light(rgb);
+    //TODO: not needed until we export ambientIntensity
+    //this.computeAmbientLight();
   }
 }
 export {WbLight}
