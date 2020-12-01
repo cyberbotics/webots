9b90fdff70f96b45bf9b3b14ba43b6a950789900
Simon Steinmann <simon.steinmann91@gmail.com>
2020-12-01 19:10:15 +0100

    repeating header fix

--- scripts/proto_splitter/proto2multi.py
+++ scripts/proto_splitter/proto2multi.py
@@ -47,27 +47,30 @@ import optparse
 import shutil
 
 
-class proto2multi():
+class proto2multi:
     def __init__(self):
-        print('Proto 2 multi-file proto converter by Simon Steinmann')
+        print("Proto 2 multi-file proto converter by Simon Steinmann")
 
     def header(self, proto):
         """Specify VRML file header."""
-        self.headerString += '# tags: hidden\n'
-        self.headerString += '# This is a proto file for Webots for the %s\n\n' % (self.robotName)
-        proto.write(self.headerString)
+        newHeaderString = self.headerString
+        newHeaderString += "# tags: hidden\n"
+        newHeaderString += "# This is a proto file for Webots for the {}\n\n".format(
+            self.robotName
+        )
+        proto.write(newHeaderString)
 
     def createProto(self, string):
         """turn mesh to proto file and stores it in the _meshes subfolder"""
-        name = self.robotName + 'Mesh' + str(self.shapeIndex)
-        filepath = '%s/%s.proto' % (self.meshFilesPath, name)
-        meshProtoFile = open(filepath, 'w')
+        name = self.robotName + "Mesh" + str(self.shapeIndex)
+        filepath = "{}/{}.proto".format(self.meshFilesPath, name)
+        meshProtoFile = open(filepath, "w")
         self.header(meshProtoFile)
-        meshProtoFile.write('PROTO %sMesh [\n]\n{\n' % name)
+        meshProtoFile.write("PROTO {}Mesh [\n]\n{}\n".format(name, "{"))
         meshProtoFile.write(string)
-        meshProtoFile.write('}\n')
+        meshProtoFile.write("}\n")
         meshProtoFile.close()
-        replaceString = '%sMesh {\n' % name
+        replaceString = "{}Mesh {}\n".format(name, "{")
         self.shapeIndex += 1
         return replaceString
 
@@ -75,19 +78,19 @@ class proto2multi():
         path = os.path.dirname(inFile)
         self.robotName = os.path.splitext(os.path.basename(inFile))[0]
         if outFile is None:
-            newPath = '{}/multifile_{}'.format(path, self.robotName)
-            outFile = '{}/{}.proto'.format(newPath, self.robotName)
+            newPath = "{}/multifile_{}".format(path, self.robotName)
+            outFile = "{}/{}.proto".format(newPath, self.robotName)
         else:
             newPath = os.path.dirname(outFile)
         os.makedirs(newPath, exist_ok=True)
         # make a dir called 'x_meshes'
-        os.makedirs(outFile.replace('.proto', '') + '_meshes', exist_ok=True)
-        self.meshFilesPath = outFile.replace('.proto', '') + '_meshes'
+        os.makedirs(outFile.replace(".proto", "") + "_meshes", exist_ok=True)
+        self.meshFilesPath = outFile.replace(".proto", "") + "_meshes"
         self.f = open(inFile)
-        self.pf = open(outFile, 'w')
+        self.pf = open(outFile, "w")
         self.shapeIndex = 0
-        self.headerString = ''
-        indent = '  '
+        self.headerString = ""
+        indent = "  "
         level = 0
         headerExtract = True
         n = 1
@@ -95,14 +98,14 @@ class proto2multi():
             line = self.f.readline()
             ln = line.split()
             if headerExtract:
-                if not line.startswith('#'):
-                    print('skipping - proto file has no header ' + outFile)
+                if not line.startswith("#"):
+                    print("skipping - proto file has no header " + outFile)
                     self.pf.close()
                     self.cleanup(inFile, outFile)
                     return
-                while line.startswith('#'):
+                while line.startswith("#"):
                     self.pf.write(line)
-                    if line[:5] in ['#VRML', '# lic', '# doc']:
+                    if line[:5] in ["#VRML", "# lic", "# doc"]:
                         self.headerString += line
                     line = self.f.readline()
                     ln = line.split()
@@ -119,34 +122,33 @@ class proto2multi():
                     self.cleanup(inFile)
                     self.pf.close()
                     return
-            if 'IndexedFaceSet' in ln:
+            if "IndexedFaceSet" in ln:
                 shapeLevel = 1
-                newProtoString = shapeLevel * indent + ' '.join(ln[-2:]) + '\n'
-                defString = ''
-                if 'DEF' in ln:
-                    defString = 'DEF ' + ln[ln.index('DEF') + 1]
+                newProtoString = shapeLevel * indent + " ".join(ln[-2:]) + "\n"
+                defString = ""
+                if "DEF" in ln:
+                    defString = "DEF " + ln[ln.index("DEF") + 1]
                 shapeLevel = 2
                 while shapeLevel > 1:
                     line = self.f.readline()
                     ln = line.split()
-                    if '}' in ln or ']' in ln:
+                    if "}" in ln or "]" in ln:
                         shapeLevel -= 1
-                    newProtoString += shapeLevel * indent + ' '.join(ln) + '\n'
-                    if '{' in ln or '[' in ln:
+                    newProtoString += shapeLevel * indent + " ".join(ln) + "\n"
+                    if "{" in ln or "[" in ln:
                         shapeLevel += 1
                 replaceString = self.createProto(newProtoString)
-                self.pf.write(indent * level + defString +
-                              'geometry ' + replaceString)
-                self.pf.write(indent * level + '}\n')
+                self.pf.write(indent * level + defString + "geometry " + replaceString)
+                self.pf.write(indent * level + "}\n")
             else:
-                if '}' in ln or ']' in ln:
+                if "}" in ln or "]" in ln:
                     level -= 1
-                elif '{' in ln or '[' in ln:
+                elif "{" in ln or "[" in ln:
                     level += 1
                 self.pf.write(line)
 
     def cleanup(self, inFile, outFile=None):
-        if inFile.endswith('_temp'):
+        if inFile.endswith("_temp"):
             os.remove(inFile)
         if outFile is not None:
             os.remove(outFile)
@@ -158,50 +160,56 @@ class proto2multi():
         # Find all the proto files, and store their filePaths
         os.chdir(sourcePath)
         # Walk the tree.
-        proto_files = []  # List of the full filepaths.
-        for root, directories, files in os.walk('./'):
+        protoFiles = []  # List of the full filepaths.
+        for root, directories, files in os.walk("./"):
             for filename in files:
                 # Join the two strings in order to form the full filepath.
                 if filename.endswith(".proto"):
                     filepath = os.path.join(root, filename)
                     filepath = filepath[1:]
-                    proto_files.append(filepath)
-        for proto in proto_files:
+                    protoFiles.append(filepath)
+        for proto in protoFiles:
             inFile = sourcePath + proto
             outFile = outPath + proto
-            print('converting ' + outFile)
+            print("converting " + outFile)
             # make a copy of our inFile, which will be read and later deleted
-            shutil.copy(inFile, inFile + '_temp')
-            inFile = inFile + '_temp'
+            shutil.copy(inFile, inFile + "_temp")
+            inFile = inFile + "_temp"
             self.convert(inFile, outFile)
 
     def create_backup(self, sourcePath):
         # Create a backup of the folder we are converting
-        backupName = os.path.basename(sourcePath) + '_backup_0'
-        backupPath = os.path.dirname(sourcePath) + '/' + backupName
+        backupName = os.path.basename(sourcePath) + "_backup_0"
+        backupPath = os.path.dirname(sourcePath) + "/" + backupName
         n = 0
         while os.path.isdir(backupPath):
             n += 1
             backupPath = backupPath[:-1] + str(n)
-        shutil.copytree(sourcePath,  backupPath)
+        shutil.copytree(sourcePath, backupPath)
 
 
 if __name__ == "__main__":
-    optParser = optparse.OptionParser(usage='usage: %prog  [options]')
-    optParser.add_option('--input', dest='inPath', default=None,
-                         help='Specifies the proto file, or a directory. Converts all .proto files, if it is a directory.')
+    optParser = optparse.OptionParser(usage="usage: %prog  [options]")
+    optParser.add_option(
+        "--input",
+        dest="inPath",
+        default=None,
+        help="Specifies the proto file, or a directory. Converts all .proto files, if it is a directory.",
+    )
     options, args = optParser.parse_args()
     inPath = options.inPath
     if inPath is not None:
         p2m = proto2multi()
-        if os.path.splitext(inPath)[1] == '.proto':
+        if os.path.splitext(inPath)[1] == ".proto":
             p2m.convert(inPath)
-            print('Multi-file extraction done')
+            print("Multi-file extraction done")
         elif os.path.isdir(inPath):
             inPath = os.path.abspath(inPath)
             p2m.convert_all(inPath)
-            print('Multi-file extraction done')
+            print("Multi-file extraction done")
         else:
-            print('ERROR: --input has to be a .proto file or directory!')
+            print("ERROR: --input has to be a .proto file or directory!")
     else:
-        print('Mandatory argument --input=<path> missing!\nSpecify a .proto file or directory path.')
+        print(
+            "Mandatory argument --input=<path> missing!\nSpecify a .proto file or directory path."
+        )
