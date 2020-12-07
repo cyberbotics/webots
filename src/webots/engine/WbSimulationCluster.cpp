dfeacd337ab0ebb5ce788f3bf5e5d873bc62e810
Darko Lukic <lukicdarkoo@gmail.com>
2020-12-07 10:29:00 +0100

    Better fix

--- src/webots/engine/WbSimulationCluster.cpp
+++ src/webots/engine/WbSimulationCluster.cpp
@@ -474,6 +474,9 @@ void WbSimulationCluster::odeNearCallback(void *data, dGeomID o1, dGeomID o2) {
       return;
   }
 
+  if (dGeomIsSpace(o1) || dGeomIsSpace(o2))
+    return;
+
   // yvan: we are never interested in the collision between 2 ray geoms
   // (rays geoms can be either distance sensor, emitter-receiver or light sensor rays)
   const bool isRayGeom1 = dGeomGetClass(o1) == dRayClass;
