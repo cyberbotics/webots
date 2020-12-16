7d12eedba8892cf6879680bde89dbb4ed3389b2d
Benjamin Délèze <benjamin.deleze@epfl.ch>
2020-12-16 17:25:33 +0100

    add copyright to webotjs

--- resources/web/wwi/webotsjs/WbConstants.js
+++ resources/web/wwi/webotsjs/WbConstants.js
@@ -1,3 +1,17 @@
+// Copyright 1996-2020 Cyberbotics Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 export const M_PI = 3.14159265358979323846;
 export const M_PI_4 = 0.78539816339744830962;
 export const TAN_M_PI_8 = Math.tan(M_PI_4 / 2.0);
