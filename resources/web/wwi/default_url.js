f1ed6024958f2ca462f0caf27afa270c853839c5
Benjamin <>
2021-01-07 08:31:45 +0100

    correct typo

--- resources/web/wwi/default_url.js
+++ resources/web/wwi/default_url.js
@@ -10,7 +10,7 @@ var DefaultUrl = {
         var src = scripts[i].src;
         if (src.indexOf('?') > 0)
           src = src.substring(0, src.indexOf('?'));
-        if (src.endsWith('setup_viewer.js') || src.endsWith('init_animation,js')) {
+        if (src.endsWith('setup_viewer.js') || src.endsWith('init_animation.js')) {
           console.log(src);
           src = src.substring(0, src.lastIndexOf('/')); // remove "webots.js"
           this._wwiUrl = src.substring(0, src.lastIndexOf('/') + 1); // remove "streaming_viewer"
