ae928a71022e3a423dbba2ae0e5498ad4fb7ae2a
Benjamin <>
2021-02-02 17:42:34 +0100

    fix lights selection

--- resources/web/wwi/webotsjs/WbGroup.js
+++ resources/web/wwi/webotsjs/WbGroup.js
@@ -14,6 +14,7 @@
 
 import {WbBaseNode} from "./WbBaseNode.js"
 import {World} from "./World.js";
+import {WbLight} from "./WbLight.js"
 
 class WbGroup extends WbBaseNode{
   constructor(id,isPropeller){
@@ -57,7 +58,9 @@ class WbGroup extends WbBaseNode{
 
   updateBoundingObjectVisibility() {
     this.children.forEach(child => {
-      child.updateBoundingObjectVisibility();
+      if (!(child instanceof WbLight)){
+        child.updateBoundingObjectVisibility();
+      }
     });
   }
 
