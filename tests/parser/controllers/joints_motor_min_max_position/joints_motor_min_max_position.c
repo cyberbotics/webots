59df0824bcd0aba1b9eb146f98c7bd5bf713ccd4
ad-daniel <daniel.dias@epfl.ch>
2021-03-09 17:28:04 +0100

    consistency and wrong messages

--- tests/parser/controllers/joints_motor_min_max_position/joints_motor_min_max_position.c
+++ tests/parser/controllers/joints_motor_min_max_position/joints_motor_min_max_position.c
@@ -10,130 +10,126 @@
 int main(int argc, char **argv) {
   ts_setup(argv[0]);
   ts_disable_output_log();
+
   // This test suite verifies that if the position field, as set in the jointParameters, is outside of the bounds of the
   // minPosition and maxPosition of RotationalMotor, these bounds must adadpt accordingly.
   // This suite also checks that the correct pairing is done (device <-> jointParameters, device2 <-> jointParameters2)
 
   // SliderJoint Tests -------------------------------------------------------------------------------------------------
-  WbDeviceTag slider_joint_motor_lower_limit = wb_robot_get_device("slider_joint_lower_limit_motor");
+  WbDeviceTag slider_joint_motor_lower_limit = wb_robot_get_device("slider_joint_motor_lower_limit");
 
   // for motor: [minPosition, maxPosition] was [-0.1, 0.1] in world, should be [-0.2, 0.1] after loading
-  ts_assert_double_equal(wb_motor_get_min_position(slider_joint_motor_lower_limit), -0.2,
-                         "'minPosition' of SliderJoint (for motor) not adjusted correctly when position below lower limit.");
   ts_assert_double_equal(
-    wb_motor_get_max_position(slider_joint_motor_lower_limit), 0.1,
-    "'maxPosition' of SliderJoint (for motor) changed but shouldn't have (testing slider_joint_motor_lower).");
+    wb_motor_get_min_position(slider_joint_motor_lower_limit), -0.2,
+    "'minPosition' of SliderJoint (for motor) didn't adjust correctly when position is below lower limit.");
+  ts_assert_double_equal(wb_motor_get_max_position(slider_joint_motor_lower_limit), 0.1,
+                         "'maxPosition' of SliderJoint (for motor) changed but shouldn't, lower limit being tested.");
 
-  WbDeviceTag slider_joint_motor_upper_limit = wb_robot_get_device("slider_joint_upper_limit_motor");
+  WbDeviceTag slider_joint_motor_upper_limit = wb_robot_get_device("slider_joint_motor_upper_limit");
 
   // for motor: [minPosition, maxPosition] was [-0.1, 0.1] in world, should be [-0.1, 0.2] after loading
   ts_assert_double_equal(wb_motor_get_min_position(slider_joint_motor_upper_limit), -0.1,
-                         "'minPosition' of SliderJoint (for motor) not adjusted correctly when position beyond upper limit.");
+                         "'minPosition' of SliderJoint (for motor) changed but shouldn't, upper limit being tested.");
   ts_assert_double_equal(
     wb_motor_get_max_position(slider_joint_motor_upper_limit), 0.2,
-    "'maxPosition' of SliderJoint (for motor) changed but shouldn't have (testing slider_joint_motor_upper).");
+    "'maxPosition' of SliderJoint (for motor) didn't adjust correctly when position is above upper limit.");
 
   // HingeJoint Tests --------------------------------------------------------------------------------------------------
-  WbDeviceTag hinge_joint_motor_lower_limit = wb_robot_get_device("hinge_joint_lower_limit_motor");
+  WbDeviceTag hinge_joint_motor_lower_limit = wb_robot_get_device("hinge_joint_motor_lower_limit");
 
   // for motor: [minPosition, maxPosition] was [-0.1, 0.1] in world, should be [-0.2, 0.1] after loading
   ts_assert_double_equal(wb_motor_get_min_position(hinge_joint_motor_lower_limit), -0.2,
-                         "'minPosition' of HingeJoint (for motor) not adjusted correctly when position below lower limit.");
-  ts_assert_double_equal(
-    wb_motor_get_max_position(hinge_joint_motor_lower_limit), 0.1,
-    "'maxPosition' of HingeJoint (for motor) changed but shouldn't have (testing hinge_joint_motor_lower).");
+                         "'minPosition' of HingeJoint (for motor) didn't adjust correctly when position is below lower limit.");
+  ts_assert_double_equal(wb_motor_get_max_position(hinge_joint_motor_lower_limit), 0.1,
+                         "'maxPosition' of HingeJoint (for motor) changed but shouldn't, lower limit being tested.");
 
-  WbDeviceTag hinge_joint_motor_upper_limit = wb_robot_get_device("hinge_joint_upper_limit_motor");
+  WbDeviceTag hinge_joint_motor_upper_limit = wb_robot_get_device("hinge_joint_motor_upper_limit");
 
   // for motor: [minPosition, maxPosition] was [-0.1, 0.1] in world, should be [-0.1, 0.2] after loading
   ts_assert_double_equal(wb_motor_get_min_position(hinge_joint_motor_upper_limit), -0.1,
-                         "'minPosition' of HingeJoint (for motor) not adjusted correctly when position beyond upper limit.");
-  ts_assert_double_equal(
-    wb_motor_get_max_position(hinge_joint_motor_upper_limit), 0.2,
-    "'maxPosition' of HingeJoint (for motor) changed but shouldn't have (testing hinge_joint_motor_upper).");
+                         "'minPosition' of HingeJoint (for motor) changed but shouldn't, upper limit being tested.");
+  ts_assert_double_equal(wb_motor_get_max_position(hinge_joint_motor_upper_limit), 0.2,
+                         "'maxPosition' of HingeJoint (for motor) didn't adjust correctly when position is above upper limit.");
 
   // Hinge2Joint Tests -------------------------------------------------------------------------------------------------
-  WbDeviceTag hinge_2_joint_motor_lower_limit = wb_robot_get_device("hinge_2_joint_lower_limit_motor");
-  WbDeviceTag hinge_2_joint_motor_lower_limit2 = wb_robot_get_device("hinge_2_joint_lower_limit_motor2");
+  WbDeviceTag hinge_2_joint_motor_lower_limit = wb_robot_get_device("hinge_2_joint_motor_lower_limit");
+  WbDeviceTag hinge_2_joint_motor2_lower_limit = wb_robot_get_device("hinge_2_joint_motor2_lower_limit");
 
   // for motor: [minPosition, maxPosition] was [-0.1, 0.1] in world, should be [-0.2, 0.1] after loading
-  ts_assert_double_equal(wb_motor_get_min_position(hinge_2_joint_motor_lower_limit), -0.2,
-                         "'minPosition' of Hinge2Joint (for motor) not adjusted correctly when position below lower limit.");
   ts_assert_double_equal(
-    wb_motor_get_max_position(hinge_2_joint_motor_lower_limit), 0.1,
-    "'maxPosition' of Hinge2Joint (for motor) changed but shouldn't have (testing hinge_2_joint_motor_lower).");
+    wb_motor_get_min_position(hinge_2_joint_motor_lower_limit), -0.2,
+    "'minPosition' of Hinge2Joint (for motor) didn't adjust correctly when position is below lower limit.");
+  ts_assert_double_equal(wb_motor_get_max_position(hinge_2_joint_motor_lower_limit), 0.1,
+                         "'maxPosition' of Hinge2Joint (for motor) changed but shouldn't, lower limit being tested.");
 
   // for motor2: [minPosition, maxPosition] was [-0.1, 0.1] in world, should be [-0.3, 0.1] after loading
-  ts_assert_double_equal(wb_motor_get_min_position(hinge_2_joint_motor_lower_limit2), -0.3,
-                         "'minPosition' of Hinge2Joint (for motor2) not adjusted correctly when position below lower limit.");
   ts_assert_double_equal(
-    wb_motor_get_max_position(hinge_2_joint_motor_lower_limit2), 0.1,
-    "'maxPosition' of Hinge2Joint (for motor2) changed but shouldn't have (testing hinge_2_joint_motor_lower).");
+    wb_motor_get_min_position(hinge_2_joint_motor2_lower_limit), -0.3,
+    "'minPosition' of Hinge2Joint (for motor2) didn't adjust correctly when position is below lower limit.");
+  ts_assert_double_equal(wb_motor_get_max_position(hinge_2_joint_motor2_lower_limit), 0.1,
+                         "'maxPosition' of Hinge2Joint (for motor2) changed but shouldn't, lower limit being tested.");
 
-  WbDeviceTag hinge_2_joint_motor_upper_limit = wb_robot_get_device("hinge_2_joint_upper_limit_motor");
-  WbDeviceTag hinge_2_joint_motor_upper_limit2 = wb_robot_get_device("hinge_2_joint_upper_limit_motor2");
+  WbDeviceTag hinge_2_joint_motor_upper_limit = wb_robot_get_device("hinge_2_joint_motor_upper_limit");
+  WbDeviceTag hinge_2_joint_motor2_upper_limit = wb_robot_get_device("hinge_2_joint_motor2_upper_limit");
 
   // for motor: [minPosition, maxPosition] was [-0.1, 0.1] in world, should be [-0.1, 0.2] after loading
   ts_assert_double_equal(wb_motor_get_min_position(hinge_2_joint_motor_upper_limit), -0.1,
-                         "'minPosition' of Hinge2Joint (for motor) not adjusted correctly when position beyond upper limit.");
+                         "'minPosition' of Hinge2Joint (for motor) changed but shouldn't, upper limit being tested.");
   ts_assert_double_equal(
     wb_motor_get_max_position(hinge_2_joint_motor_upper_limit), 0.2,
-    "'maxPosition' of Hinge2Joint (for motor) changed but shouldn't have (testing hinge_2_joint_motor_upper).");
+    "'maxPosition' of Hinge2Joint (for motor) didn't adjust correctly when position is above lower limit.");
 
   // for motor2: [minPosition, maxPosition] was [-0.1, 0.1] in world, should be [-0.1, 0.3] after loading
-  ts_assert_double_equal(wb_motor_get_min_position(hinge_2_joint_motor_upper_limit2), -0.1,
-                         "'minPosition' of Hinge2Joint (for motor2) not adjusted correctly when position beyond upper limit.");
+  ts_assert_double_equal(wb_motor_get_min_position(hinge_2_joint_motor2_upper_limit), -0.1,
+                         "'minPosition' of Hinge2Joint (for motor2) changed but shouldn't, upper limit being tested.");
   ts_assert_double_equal(
-    wb_motor_get_max_position(hinge_2_joint_motor_upper_limit2), 0.3,
-    "'maxPosition' of Hinge2Joint (for motor2) changed but shouldn't have (testing hinge_2_joint_motor_upper).");
+    wb_motor_get_max_position(hinge_2_joint_motor2_upper_limit), 0.3,
+    "'maxPosition' of Hinge2Joint (for motor2) didn't adjust correctly when position is above lower limit.");
 
   // BallJoint Tests ---------------------------------------------------------------------------------------------------
-  WbDeviceTag ball_joint_motor_lower_limit = wb_robot_get_device("ball_joint_lower_limit_motor");
-  WbDeviceTag ball_joint_motor_lower_limit2 = wb_robot_get_device("ball_joint_lower_limit_motor2");
-  WbDeviceTag ball_joint_motor_lower_limit3 = wb_robot_get_device("ball_joint_lower_limit_motor3");
+  WbDeviceTag ball_joint_motor_lower_limit = wb_robot_get_device("ball_joint_motor_lower_limit");
+  WbDeviceTag ball_joint_motor2_lower_limit = wb_robot_get_device("ball_joint_motor2_lower_limit");
+  WbDeviceTag ball_joint_motor3_lower_limit = wb_robot_get_device("ball_joint_motor3_lower_limit");
 
   // for motor: [minPosition, maxPosition] was [-0.1, 0.1] in world, should be [-0.2, 0.1] after loading
   ts_assert_double_equal(wb_motor_get_min_position(ball_joint_motor_lower_limit), -0.2,
-                         "'minPosition' of BallJoint (for motor) not adjusted correctly when position below lower limit.");
+                         "'minPosition' of BallJoint (for motor) didn't adjust correctly when position is below lower limit.");
   ts_assert_double_equal(wb_motor_get_max_position(ball_joint_motor_lower_limit), 0.1,
-                         "'maxPosition' of BallJoint (for motor) changed but shouldn't have (testing ball_joint_motor_lower).");
+                         "'maxPosition' of BallJoint (for motor) changed but shouldn't, lower limit being tested.");
 
   // for motor2: [minPosition, maxPosition] was [-0.1, 0.1] in world, should be [-0.3, 0.1] after loading
-  ts_assert_double_equal(wb_motor_get_min_position(ball_joint_motor_lower_limit2), -0.3,
-                         "'minPosition' of BallJoint (for motor2) not adjusted correctly when position below lower limit.");
-  ts_assert_double_equal(
-    wb_motor_get_max_position(ball_joint_motor_lower_limit2), 0.1,
-    "'maxPosition' of BallJoint (for motor2) changed but shouldn't have (testing ball_joint_motor_lower).");
+  ts_assert_double_equal(wb_motor_get_min_position(ball_joint_motor2_lower_limit), -0.3,
+                         "'minPosition' of BallJoint (for motor2) didn't adjust correctly when position is below lower limit.");
+  ts_assert_double_equal(wb_motor_get_max_position(ball_joint_motor2_lower_limit), 0.1,
+                         "'maxPosition' of BallJoint (for motor2) changed but shouldn't, lower limit being tested.");
 
   // for motor3: [minPosition, maxPosition] was [-0.1, 0.1] in world, should be [-0.4, 0.1] after loading
-  ts_assert_double_equal(wb_motor_get_min_position(ball_joint_motor_lower_limit3), -0.4,
-                         "'minPosition' of BallJoint (for motor3) not adjusted correctly when position below lower limit.");
-  ts_assert_double_equal(wb_motor_get_max_position(ball_joint_motor_lower_limit3), 0.1,
-                         "'maxPosition' of BallJoint (for motor3) but shouldn't have (testing ball_joint_motor_lower).");
+  ts_assert_double_equal(wb_motor_get_min_position(ball_joint_motor3_lower_limit), -0.4,
+                         "'minPosition' of BallJoint (for motor3) didn't adjust correctly when position is below lower limit.");
+  ts_assert_double_equal(wb_motor_get_max_position(ball_joint_motor3_lower_limit), 0.1,
+                         "'maxPosition' of BallJoint (for motor3) changed but shouldn't, lower limit being tested.");
 
-  WbDeviceTag ball_joint_motor_upper_limit = wb_robot_get_device("ball_joint_upper_limit_motor");
-  WbDeviceTag ball_joint_motor_upper_limit2 = wb_robot_get_device("ball_joint_upper_limit_motor2");
-  WbDeviceTag ball_joint_motor_upper_limit3 = wb_robot_get_device("ball_joint_upper_limit_motor3");
+  WbDeviceTag ball_joint_motor_upper_limit = wb_robot_get_device("ball_joint_motor_upper_limit");
+  WbDeviceTag ball_joint_motor2_upper_limit = wb_robot_get_device("ball_joint_motor2_upper_limit");
+  WbDeviceTag ball_joint_motor3_upper_limit = wb_robot_get_device("ball_joint_motor3_upper_limit");
 
   // for motor: [minPosition, maxPosition] was [-0.1, 0.1] in world, should be [-0.1, 0.2] after loading
   ts_assert_double_equal(wb_motor_get_min_position(ball_joint_motor_upper_limit), -0.1,
-                         "'minPosition' of BallJoint (for motor) not adjusted correctly when position beyond upper limit.");
+                         "'minPosition' of BallJoint (for motor) changed but shouldn't, upper limit being tested.");
   ts_assert_double_equal(wb_motor_get_max_position(ball_joint_motor_upper_limit), 0.2,
-                         "'maxPosition' of BallJoint (for motor) changed but shouldn't have (testing ball_joint_motor_upper).");
+                         "'maxPosition' of BallJoint (for motor) didn't adjust correctly when position is above upper limit.");
 
   // for motor2: [minPosition, maxPosition] was [-0.1, 0.1] in world, should be [-0.1, 0.3] after loading
-  ts_assert_double_equal(wb_motor_get_min_position(ball_joint_motor_upper_limit2), -0.1,
-                         "'minPosition' of BallJoint (for motor2) not adjusted correctly when position beyond upper limit.");
-  ts_assert_double_equal(
-    wb_motor_get_max_position(ball_joint_motor_upper_limit2), 0.3,
-    "'maxPosition' of BallJoint (for motor2) changed but shouldn't have (testing ball_joint_motor_upper).");
+  ts_assert_double_equal(wb_motor_get_min_position(ball_joint_motor2_upper_limit), -0.1,
+                         "'minPosition' of BallJoint (for motor2) changed but shouldn't, upper limit being tested.");
+  ts_assert_double_equal(wb_motor_get_max_position(ball_joint_motor2_upper_limit), 0.3,
+                         "'maxPosition' of BallJoint (for motor2) didn't adjust correctly when position is above upper limit.");
 
   // for motor3: [minPosition, maxPosition] was [-0.1, 0.1] in world, should be [-0.1, 0.4] after loading
-  ts_assert_double_equal(wb_motor_get_min_position(ball_joint_motor_upper_limit3), -0.1,
-                         "'minPosition' of BallJoint (for motor3) not adjusted correctly when position beyond upper limit.");
-  ts_assert_double_equal(
-    wb_motor_get_max_position(ball_joint_motor_upper_limit3), 0.4,
-    "'maxPosition' of BallJoint (for motor3) changed but shouldn't have (testing ball_joint_motor_upper).");
+  ts_assert_double_equal(wb_motor_get_min_position(ball_joint_motor3_upper_limit), -0.1,
+                         "'minPosition' of BallJoint (for motor3) changed but shouldn't, upper limit being tested.");
+  ts_assert_double_equal(wb_motor_get_max_position(ball_joint_motor3_upper_limit), 0.4,
+                         "'maxPosition' of BallJoint (for motor3) didn't adjust correctly when position is above upper limit.");
 
   ts_send_success();
   return EXIT_SUCCESS;
