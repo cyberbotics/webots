eafc7b1bae27c628617092cc288724492d829b99
Sis Hamilton <69378550+sishamilton@users.noreply.github.com>
2021-03-15 22:00:31 -0700

    Update WbAltimeter member functions.

---
+++ src/webots/nodes/WbAltimeter.cpp
@@ -0,0 +1,114 @@
+// Copyright 1996-2021 Cyberbotics Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "WbAltimeter.hpp"
+
+#include "WbFieldChecker.hpp"
+#include "WbMathsUtilities.hpp"
+#include "WbRandom.hpp"
+#include "WbSFDouble.hpp"
+#include "WbSensor.hpp"
+#include "WbWorld.hpp"
+
+#include "../../controller/c/messages.h"
+
+#include <QtCore/QDataStream>
+
+#include <cassert>
+
+void WbAltimeter::init() {
+  mType = findSFString("type");
+  mAccuracy = findSFDouble("accuracy");
+  mNoiseCorrelation = findSFDouble("noiseCorrelation");
+  mResolution = findSFDouble("resolution");
+
+  mSensor = NULL;
+}
+
+WbAltimeter::WbAltimeter(WbTokenizer *tokenizer) : WbSolidDevice("Altimeter", tokenizer) {
+  init();
+}
+
+WbAltimeter::WbAltimeter(const WbAltimeter &other) : WbSolidDevice(other) {
+  init();
+}
+
+WbAltimeter::WbAltimeter(const WbNode &other) : WbSolidDevice(other) {
+  init();
+}
+
+WbAltimeter::~WbAltimeter() {
+  delete mSensor;
+}
+
+void WbAltimeter::preFinalize() {
+  WbSolidDevice::preFinalize();
+  mSensor = new WbSensor();
+}
+
+void WbAltimeter::postFinalize() {
+  WbSolidDevice::postFinalize();
+
+  connect(mNoiseCorrelation, &WbSFDouble::changed, this, &WbAltimeter::updateCorrelation);
+  connect(mResolution, &WbSFDouble::changed, this, &WbAltimeter::updateResolution);
+}
+
+void WbAltimeter::updateResolution() {
+  WbFieldChecker::resetDoubleIfNonPositiveAndNotDisabled(this, mResolution, -1.0, -1.0);
+}
+
+void WbAltimeter::updateCorrelation() {
+  WbFieldChecker::resetDoubleIfNotInRangeWithIncludedBounds(this, mNoiseCorrelation, 0.0, 1.0, 0.0);
+}
+
+bool WbAltimeter::refreshSensorIfNeeded() {
+  if (!isPowerOn() || !mSensor->needToRefresh())
+    return false;
+    
+  const WbVector3 &t = matrix().translation();
+
+  // compute current altitude
+  double accuracy = mAccuracy->value();
+  double correlation = mNoiseCorrelation->value();
+  static double noise = 0;
+  double ratio = 0;
+  if (accuracy != 0.0 && correlation != 0.0) {
+    ratio = pow(correlation, mSensor->elapsedTime() / 1000.0);
+  }
+  
+  WbVector3 reference = WbWorld::instance()->worldInfo()->gpsReference();
+  const QString &coordinateSystem = WbWorld::instance()->worldInfo()->coordinateSystem();
+  int upIndex = coordinateSystem.indexOf('U');
+  if (WbWorld::instance()->worldInfo()->gpsCoordinateSystem() == "WGS84") {
+    double altitude = reference[2];
+    reference[upIndex] = altitude;
+  }
+
+  mAltitude = t[upIndex]; //get exact altitude
+  mAltitude += reference[upIndex];
+  // add noise if necessary
+  if (accuracy != 0.0) {
+    // generate correlated gaussian number from previous one
+    // https://www.cmu.edu/biolphys/deserno/pdf/corr_gaussian_random.pdf
+    noise = ratio * noise + sqrt(1 - pow(ratio, 2)) * WbRandom::nextGaussian();
+    mAltitude += accuracy * noise;
+  }
+  // apply resolution if necessary
+  if (mResolution->value() != -1.0) {
+    mAltitude = WbMathsUtilities::discretize(mAltitude, mResolution->value());
+  }
+
+  mSensor->updateTimer();
+  return true;
+}
