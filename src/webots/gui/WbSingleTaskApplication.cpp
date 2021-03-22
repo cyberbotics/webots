a36d33612b65b91c80fd28bfaa5abb3adf0f090c
Darko Lukić <lukicdarkoo@gmail.com>
2021-03-22 17:28:26 +0100

    Update src/webots/gui/WbSingleTaskApplication.cpp
    Co-authored-by: Olivier Michel <Olivier.Michel@cyberbotics.com>

--- src/webots/gui/WbSingleTaskApplication.cpp
+++ src/webots/gui/WbSingleTaskApplication.cpp
@@ -81,7 +81,7 @@ void WbSingleTaskApplication::convertProto() const {
   // Get user parameters strings
   QMap<QString, QString> userParameters;
   for (QString param : cliParser.values("p")) {
-    QStringList pair = param.split("=");
+    const QStringList pair = param.split("=");
     if (pair.size() != 2) {
       cout << tr("A parameter is not properly formated!\n").toUtf8().constData();
       cliParser.showHelp(1);
