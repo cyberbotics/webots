be16caf30d1704b8b4d00a44eaa4f9a70a0d04d5
Olivier Michel <Olivier.Michel@cyberbotics.com>
2021-03-23 11:33:49 +0100

    sample TCP/IP client

---
+++ projects/samples/contests/robocup/controllers/player/client.c
@@ -0,0 +1,121 @@
+/*
+ * Copyright 1996-2021 Cyberbotics Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Description:  A simple client program to connect to the TCP/IP server thanks to Darren Smith
+ */
+
+/*
+ * Linux:   compile with gcc -Wall client.c -o client
+ * Windows: compile with gcc -Wall -mno-cygwin client.c -o client -lws2_32
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#ifdef _WIN32
+#include <winsock.h>
+#else
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#endif
+
+static void close_socket(int fd) {
+#ifdef _WIN32
+  closesocket(fd);
+#else
+  close(fd);
+#endif
+}
+
+int main(int argc, char *argv[]) {
+  struct sockaddr_in address;
+  struct hostent *server;
+  int fd, rc;
+  char buffer[256];
+  int port = 10003;
+  char host[256];  // localhost
+
+  sprintf(host, "127.0.0.1");
+  if (argc > 1) {
+    if (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0) {
+      printf("Usage: client <IP:port> or <IP> or <port>\n");
+      return 0;
+    }
+    char *n = strchr(argv[1], ':');
+    if (n > 0) {
+      port = atoi(n + 1);
+      strncpy(host, argv[1], 255);
+      host[n - argv[1]] = '\0';
+    } else if (strchr(argv[1], '.') || !isdigit(argv[1][0]))
+      strncpy(host, argv[1], 255);
+    else
+      port = atoi(argv[1]);
+  }
+#ifdef _WIN32
+  WSADATA info;
+  rc = WSAStartup(MAKEWORD(1, 1), &info);  // Winsock 1.1
+  if (rc != 0) {
+    fprintf(stderr, "Cannot initialize Winsock\n");
+    return 1;
+  }
+#endif
+  fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (fd == -1) {
+    fprintf(stderr, "Cannot create socket\n");
+    return 1;
+  }
+  memset(&address, 0, sizeof(struct sockaddr_in));
+  address.sin_family = AF_INET;
+  address.sin_port = htons(port);
+  server = gethostbyname(host);
+
+  if (server)
+    memcpy((char *)&address.sin_addr.s_addr, (char *)server->h_addr, server->h_length);
+  else {
+    fprintf(stderr, "Cannot resolve server name: %s\n", host);
+    close_socket(fd);
+    return 1;
+  }
+  rc = connect(fd, (struct sockaddr *)&address, sizeof(struct sockaddr));
+  if (rc == -1) {
+    fprintf(stderr, "Cannot connect to %s:%d\n", host, port);
+    close_socket(fd);
+    return 1;
+  }
+  printf("Connected %s:%d\n", host, port);
+  for (;;) {
+    printf("Enter command: ");
+    fflush(stdout);
+    fgets(buffer, sizeof(buffer), stdin);
+    int n = strlen(buffer);
+    printf("length = %d\n", n);
+    buffer[n++] = '\n';
+    buffer[n] = '\0';
+    n = send(fd, buffer, n, 0);
+    if (strncmp(buffer, "exit", 4) == 0)
+      break;
+    n = recv(fd, buffer, 256, 0);
+    buffer[n] = '\0';
+    printf("Answer is: %s", buffer);
+  }
+  close_socket(fd);
+  return 0;
+}
--- projects/samples/contests/robocup/controllers/player/player.cpp
+++ projects/samples/contests/robocup/controllers/player/player.cpp
@@ -8,7 +8,14 @@
 #include <sys/time.h>
 #include <unistd.h> /* definition of close */
 #endif
+// #define TURBOJPEG 1
+// It turns out that the libjpeg interface to turbojpeg runs faster than the native turbojpeg interface
+// Alternatives to be considered: NVIDIA CUDA nvJPEG Encoder, Intel IPP JPEG encoder
+#ifdef TURBOJPEG
+#include <turbojpeg.h>
+#else
 #include <jpeglib.h>
+#endif
 #include <webots/Accelerometer.hpp>
 #include <webots/Camera.hpp>
 #include <webots/Gyro.hpp>
@@ -18,34 +25,46 @@
 #include <webots/TouchSensor.hpp>
 #include "messages.pb.h"
 
-static int fd;
+static int server_fd = -1;
 static fd_set rfds;
+
+static bool set_blocking(int fd, bool blocking) {
+#ifdef _WIN32
+  unsigned long mode = blocking ? 0 : 1;
+  return (ioctlsocket(fd, FIONBIO, &mode) == 0) ? true : false;
+#else
+  int flags = fcntl(fd, F_GETFL, 0);
+  if (flags == -1)
+    return false;
+  flags = blocking ? (flags & ~O_NONBLOCK) : (flags | O_NONBLOCK);
+  return (fcntl(fd, F_SETFL, flags) == 0) ? true : false;
+#endif
+}
 
 static int accept_client(int server_fd) {
   int cfd;
   struct sockaddr_in client;
-#ifndef _WIN32
-  socklen_t asize;
-#else
-  int asize;
-#endif
-  struct hostent *client_info;
-
-  asize = sizeof(struct sockaddr_in);
-
-  cfd = accept(server_fd, (struct sockaddr *)&client, &asize);
-  if (cfd == -1) {
-    printf("cannot accept client\n");
-    return -1;
+  int size;
+  size = sizeof(struct sockaddr_in);
+  cfd = accept(server_fd, (struct sockaddr *)&client, &size);
+  if (cfd != -1) {
+    struct hostent *client_info = gethostbyname((char *)inet_ntoa(client.sin_addr));
+    printf("Accepted connection from: %s \n", client_info->h_name);
   }
-  client_info = gethostbyname((char *)inet_ntoa(client.sin_addr));
-  printf("Accepted connection from: %s \n", client_info->h_name);
-
   return cfd;
 }
 
+static void close_socket(int fd) {
+#ifdef _WIN32
+  closesocket(fd);
+#else
+  close(fd);
+#endif
+}
+
 static int create_socket_server(int port) {
-  int sfd, rc;
+  int rc;
+  int server_fd;
   struct sockaddr_in address;
 
 #ifdef _WIN32
@@ -59,8 +78,8 @@ static int create_socket_server(int port) {
   }
 #endif
   /* create the socket */
-  sfd = socket(AF_INET, SOCK_STREAM, 0);
-  if (sfd == -1) {
+  server_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (server_fd == -1) {
     printf("cannot create socket\n");
     return -1;
   }
@@ -72,34 +91,29 @@ static int create_socket_server(int port) {
   address.sin_addr.s_addr = INADDR_ANY;
 
   /* bind to port */
-  rc = bind(sfd, (struct sockaddr *)&address, sizeof(struct sockaddr));
+  rc = bind(server_fd, (struct sockaddr *)&address, sizeof(struct sockaddr));
   if (rc == -1) {
     printf("cannot bind port %d\n", port);
-#ifdef _WIN32
-    closesocket(sfd);
-#else
-    close(sfd);
-#endif
+    close_socket(server_fd);
     return -1;
   }
 
   /* listen for connections */
-  if (listen(sfd, 1) == -1) {
+  if (listen(server_fd, 1) == -1) {
     printf("cannot listen for connections\n");
-#ifdef _WIN32
-    closesocket(sfd);
-#else
-    close(sfd);
-#endif
+    close_socket(server_fd);
     return -1;
   }
-  printf("Waiting for a connection on port %d...\n", port);
-
-  return accept_client(sfd);
+  return server_fd;
 }
 
 static void encode_jpeg(const unsigned char *image, int width, int height, int quality, unsigned long *size,
                         unsigned char **buffer) {
+#ifdef TURBOJPEG
+  tjhandle compressor = tjInitCompress();
+  tjCompress2(compressor, image, width, 0, height, TJPF_RGB, buffer, size, TJSAMP_444, quality, TJFLAG_FASTDCT);
+  tjDestroy(compressor);
+#else
   struct jpeg_compress_struct cinfo;
   struct jpeg_error_mgr jerr;
   JSAMPROW row_pointer[1];
@@ -108,7 +122,7 @@ static void encode_jpeg(const unsigned char *image, int width, int height, int q
   cinfo.image_width = width;
   cinfo.image_height = height;
   cinfo.input_components = 3;
-  cinfo.in_color_space = JCS_GRAYSCALE;
+  cinfo.in_color_space = JCS_RGB;
   jpeg_set_defaults(&cinfo);
   jpeg_set_quality(&cinfo, quality, TRUE);
   jpeg_mem_dest(&cinfo, buffer, size);
@@ -119,6 +133,15 @@ static void encode_jpeg(const unsigned char *image, int width, int height, int q
   }
   jpeg_finish_compress(&cinfo);
   jpeg_destroy_compress(&cinfo);
+#endif
+}
+
+static void free_jpeg(unsigned char *buffer) {
+#ifdef TURBOJPEG
+  tjFree(buffer);
+#else
+  free(buffer);
+#endif
 }
 
 int main(int argc, char *argv[]) {
@@ -131,6 +154,7 @@ int main(int argc, char *argv[]) {
   const double timeStep = robot->getBasicTimeStep();
   const size_t size = sizeof(player_names) / sizeof(player_names[0]);
   const std::string name = robot->getName();
+  int client_fd = -1;
   int player_id = -1;
   int port = -1;
   for (unsigned int i = 0; i < size; i++)
@@ -140,9 +164,8 @@ int main(int argc, char *argv[]) {
       break;
     }
   std::cout << name << ": player ID = " << player_id << " running on port " << port << std::endl;
-  fd = create_socket_server(port);
-  FD_ZERO(&rfds);
-  FD_SET(fd, &rfds);
+  server_fd = create_socket_server(port);
+  set_blocking(server_fd, false);
 
   webots::Camera *camera = robot->getCamera("Camera");
   camera->enable(timeStep);
@@ -153,6 +176,29 @@ int main(int argc, char *argv[]) {
   while (robot->step(timeStep) != -1) {
     const unsigned char *image = camera->getImage();
     encode_jpeg(image, width, height, 95, &bufferSize, &buffer);
+    free_jpeg(buffer);
+    buffer = NULL;
+    if (client_fd == -1)
+      client_fd = accept_client(server_fd);
+    else {
+      FD_ZERO(&rfds);
+      FD_SET(client_fd, &rfds);
+      struct timeval tv = {0, 0};
+      int number = select(client_fd + 1, &rfds, NULL, NULL, &tv);
+      if (number) {  // some data is available from the socket
+        char data[256];
+        int n = recv(client_fd, data, 256, 0);
+        if (n < 0) {
+          printf("Closed connection\n");
+          close_socket(client_fd);
+          client_fd = -1;
+        } else {
+          data[n] = '\0';
+          printf("Received %d bytes: %s\n", n, data);
+          send(client_fd, "OK\r\n", 3, 0);
+        }
+      }
+    }
   }
   delete robot;
   return 0;
