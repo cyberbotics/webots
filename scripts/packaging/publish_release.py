220d779a5d8c2a0aaa4f501a1dc2b8cd2248391f
Stefania Pedrazzi <stefaniapedrazzi@users.noreply.github.com>
2021-03-24 08:45:54 +0100

    Fix use of exception

--- scripts/packaging/publish_release.py
+++ scripts/packaging/publish_release.py
@@ -129,7 +129,7 @@ for release in repo.get_releases():
                             print('Release upload failed due to connection error (remaining trials: %d)' % (4 - retryCount))
                         except requests.exceptions.HTTPError:
                             print('Release upload failed due to server error (remaining trials: %d)' % (4 - retryCount))
-                        except GithubException:
+                        except github.GithubException:
                             print('Release upload failed due to GitHub error (remaining trials: %d): %s' % (4 - retryCount))
                         except:
                             print('Release upload failed due to unexpected error (remaining trials: %d)' % (4 - retryCount))
