ffe14629fc160227cc54b70214e2f5091efb325c
Olivier Michel <Olivier.Michel@cyberbotics.com>
2021-03-27 14:12:41 +0100

    Licenses

--- projects/samples/contests/robocup/controllers/player/player.cpp
+++ projects/samples/contests/robocup/controllers/player/player.cpp
@@ -1,16 +1,31 @@
+// Copyright 1996-2021 Cyberbotics Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include <assert.h>
 #include <stdio.h>
 
 #ifdef _WIN32
 #include <winsock.h>
+typedef int socklen_t;
 #else
-#include <arpa/inet.h>  /* definition of inet_ntoa */
-#include <netdb.h>      /* definition of gethostbyname */
+#include <arpa/inet.h> /* definition of inet_ntoa */
+#include <fcntl.h>
+#include <netdb.h> /* definition of gethostbyname */
 #include <netinet/in.h> /* definition of struct sockaddr_in */
 #include <sys/socket.h>
 #include <sys/time.h>
 #include <unistd.h> /* definition of close */
-#include <fcntl.h>
 #endif
 // #define TURBOJPEG 1
 // It turns out that the libjpeg interface to turbojpeg runs faster than the native turbojpeg interface
