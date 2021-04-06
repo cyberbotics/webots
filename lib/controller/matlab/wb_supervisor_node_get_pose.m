12b0ed34f115c285fce0995279c425783434eb52
Darko Lukic <lukicdarkoo@gmail.com>
2021-04-06 16:36:02 +0200

    Fix matlab

--- lib/controller/matlab/wb_supervisor_node_get_pose.m
+++ lib/controller/matlab/wb_supervisor_node_get_pose.m
@@ -1,11 +1,7 @@
-function result = wb_supervisor_node_get_pose(noderef)
-% Usage: wb_supervisor_node_get_pose(noderef)
+function result = wb_supervisor_node_get_pose(noderef, noderef_from)
+% Usage: wb_supervisor_node_get_pose(noderef, noderef_from)
 % Matlab API for Webots
 % Online documentation is available <a href="https://www.cyberbotics.com/doc/reference/supervisor">here</a>
-
-if nargin < 2
-    noderef_from = 0
-end
 
 obj = calllib('libController', 'wb_supervisor_node_get_pose', noderef, noderef_from);
 setdatatype(obj,'doublePtr', 4, 4);
