05affe4dd8a7e2ed98f4112906cdb3fa79c84ea4
Olivier Michel <Olivier.Michel@cyberbotics.com>
2021-04-06 18:16:15 +0200

    progress with game management

--- projects/samples/contests/robocup/controllers/referee/referee.py
+++ projects/samples/contests/robocup/controllers/referee/referee.py
@@ -13,6 +13,8 @@
 # limitations under the License.
 
 from gamestate import GameState, ReturnData, GAME_CONTROLLER_RESPONSE_VERSION
+from construct import Container
+
 from controller import Supervisor, AnsiCodes
 
 import json
@@ -26,7 +28,7 @@ import sys
 import time
 from types import SimpleNamespace
 
-global supervisor, game, red_team, blue_team, game_controller
+global supervisor, game, red_team, blue_team
 
 
 def spawn_team(team, color, red_on_right, children):
@@ -51,21 +53,60 @@ def spawn_team(team, color, red_on_right, children):
         children.importMFNodeFromString(-1, string)
 
 
+def format_time(s):
+    seconds = str(s % 60)
+    minutes = str(int(s / 60))
+    if len(minutes) == 1:
+        minutes = '0' + minutes
+    if len(seconds) == 1:
+        seconds = '0' + seconds
+    return minutes + ':' + seconds
+
+
 def update_time_display():
-    if game.state == 'INIT':
-        value = '00:00'
+    if game.state:
+        if game.state.seconds_remaining == 600 and game.state.game_state == 'STATE_PLAYING':
+            print('Error: GameController sent "600 seconds remaining"!', file=sys.stderr)
+            return
+        s = game.state.seconds_remaining
+        if s < 0:
+            s = -s
+            sign = '-'
+        else:
+            sign = ' '
+        value = format_time(s)
     else:
-        value = '00.00'
-    supervisor.setLabel(5, ' ' + value, game.overlay_x, game.overlay_y, game.font_size, 0x000000, 0.2, game.font)
+        sign = ' '
+        value = '--:--'
+    supervisor.setLabel(5, sign + value, game.overlay_x, game.overlay_y, game.font_size, 0x000000, 0.2, game.font)
 
 
 def update_state_display():
-    supervisor.setLabel(6, ' ' * 41 + game.state, game.overlay_x, game.overlay_y, game.font_size, 0x000000, 0.2, game.font)
+    if game.state:
+        state = game.state.game_state[6:]
+        sr = game.state.secondary_seconds_remaining
+        if sr > 65000:  # bug in GameController sometimes sending value like 65534
+            sr = 0
+        if sr > 0:
+            if state == 'PLAYING':
+                state = 'PLAY'
+            state += ': ' + format_time(sr)
+        elif state == 'PLAYING' and game.state.seconds_remaining <= 0:
+            state = 'EXTRA: ' + format_time(game.extra_seconds)
+    else:
+        state = ''
+    supervisor.setLabel(6, ' ' * 41 + state, game.overlay_x, game.overlay_y, game.font_size, 0x000000, 0.2, game.font)
 
 
 def update_score_display():
-    red_score = str(red_team['score'])
-    blue_score = str(blue_team['score'])
+    if game.state:
+        red = 0 if game.state.teams[0].team_color == 'RED' else 1
+        blue = 1 if red == 0 else 0
+        red_score = str(game.state.teams[red].score)
+        blue_score = str(game.state.teams[blue].score)
+    else:
+        red_score = '0'
+        blue_score = '0'
     if game.side_left == game.blue.id:
         red_score = ' ' * 24 + red_score
         offset = 21 if len(blue_score) == 2 else 22
@@ -105,22 +146,75 @@ def setup_display():
     update_score_display()
 
 
+def game_controller_heartbeat():
+    heartbeat = Container(
+        header=b"RGrt",
+        version=GAME_CONTROLLER_RESPONSE_VERSION,
+        team=0,
+        player=1,
+        message=2)
+    try:
+        game.udp_out.sendto(ReturnData.build(heartbeat), ('127.0.0.1', 3939))
+    except Exception as e:
+        print(f'Error: UDP out failure: {e}', file=sys.stderr)
+        return
+    data = None
+    try:
+        data, peer = game.udp_in.recvfrom(GameState.sizeof())
+    except BlockingIOError:
+        return
+    except Exception as e:
+        print(f'Error: UDP input failure: {e}', file=sys.stderr)
+        pass
+    if not data:
+        print(f'No UDP data received', file=sys.stderr)
+        return
+    previous_seconds_remaining = game.state.seconds_remaining if game.state else None
+    previous_secondary_seconds_remaining = game.state.secondary_seconds_remaining if game.state else None
+    previous_state = game.state.game_state if game.state else None
+    if game.state:
+        red = 0 if game.state.teams[0].team_color == 'RED' else 1
+        blue = 1 if red == 0 else 0
+        previous_red_score = game.state.teams[red].score
+        previous_blue_score = game.state.teams[blue].score
+    else:
+        previous_red_score = 0
+        previous_blue_score = 0
+
+    game.state = GameState.parse(data)
+    # print(f'Score: {game.state.teams[0].score}-{game.state.teams[1].score}')
+    if previous_state != game.state.game_state or \
+       previous_secondary_seconds_remaining != game.state.secondary_seconds_remaining or \
+       game.state.seconds_remaining == 0:
+        update_state_display()
+    if previous_seconds_remaining != game.state.seconds_remaining:
+        update_time_display()
+    red = 0 if game.state.teams[0].team_color == 'RED' else 1
+    blue = 1 if red == 0 else 0
+    if previous_red_score != game.state.teams[red].score or \
+       previous_blue_score != game.state.teams[blue].score:
+        update_score_display()
+    print(game.state)
+    info = str(game.state.secondary_state_info)
+    secondary_state = game.state.secondary_state
+    if secondary_state != 'STATE_NORMAL':
+        print(f'GameController {secondary_state}: {info}')
+
+
 def game_controller_send(message):
-    if game_controller:
+    if game.controller:
         game_controller_send.id += 1
         message = f'{game_controller_send.id}:{message}\n'
-        game_controller.sendall(message.encode('ascii'))
-        # print(f'Sent {message}')
+        game.controller.sendall(message.encode('ascii'))
         game_controller_send.unanswered[game_controller_send.id] = message.strip()
         while True:
             try:
-                answers = game_controller.recv(1024).decode('ascii').split('\n')
+                answers = game.controller.recv(1024).decode('ascii').split('\n')
                 for answer in answers:
                     try:
                         id, result = answer.split(':')
                     except ValueError:
                         print(f'Cannot split {answer}', file=sys.stderr)
-                    # print(f'Received {id}:{result}')
                     try:
                         message = game_controller_send.unanswered[int(id)]
                         del game_controller_send.unanswered[int(id)]
@@ -172,7 +266,7 @@ try:
         GAME_CONTROLLER_HOME = os.environ['GAME_CONTROLLER_HOME']
         if not os.path.exists(GAME_CONTROLLER_HOME):
             print(f'Error: {GAME_CONTROLLER_HOME} (GAME_CONTROLLER_HOME) folder not found.', file=sys.stderr)
-            game_controller_process = None
+            game.controller_process = None
         else:
             copyfile('game.json', os.path.join(GAME_CONTROLLER_HOME, 'resources', 'config', 'sim', 'game.json'))
             path = os.path.join(GAME_CONTROLLER_HOME, 'build', 'jar', 'config', f'hl_sim_{field_size}', 'teams.cfg')
@@ -180,17 +274,17 @@ try:
             blue_line = f'{game.blue.id}={blue_team["name"]}\n'
             with open(path, 'w') as file:
                 file.write((red_line + blue_line) if game.red.id < game.blue.id else (blue_line + red_line))
-            game_controller_process = subprocess.Popen(
+            game.controller_process = subprocess.Popen(
               [os.path.join(JAVA_HOME, 'bin', 'java'), '-jar', 'GameControllerSimulator.jar'],
               cwd=os.path.join(GAME_CONTROLLER_HOME, 'build', 'jar'))
     except KeyError:
         GAME_CONTROLLER_HOME = None
-        game_controller_process = None
+        game.controller_process = None
         print('Warning: GAME_CONTROLLER_HOME environment variable not set, unable to launch GameController.', file=sys.stderr)
 except KeyError:
     JAVA_HOME = None
     GAME_CONTROLLER_HOME = None
-    game_controller_process = None
+    game.controller_process = None
     print('Warning: JAVA_HOME environment variable not set, unable to launch GameController.', file=sys.stderr)
 
 # start the webots supervisor
@@ -208,8 +302,8 @@ ball_size = 1 if field_size == 'kid' else 5
 children.importMFNodeFromString(-1, f'DEF BALL RobocupSoccerBall {{ translation 0 0 {game.ball_kickoff_translation[2]} ' +
                                 f'size {ball_size} }}')
 game.side_left = game.red.id if bool(random.getrandbits(1)) else game.blue.id  # toss a coin to determine field side
-game.kickoff = game.red.id if bool(random.getrandbits(1)) else game.blue.id  # toss a coin to determine which team has kickoff
-game.state = 'INIT'
+game.state = None
+game.extra_seconds = 0
 game.font_size = 0.1
 game.font = 'Lucida Console'
 game.overlay_x = 0.02
@@ -222,13 +316,13 @@ setup_display()
 
 time_step = int(supervisor.getBasicTimeStep())
 
-if game_controller_process:
-    game_controller = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+if game.controller_process:
+    game.controller = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     retry = 0
     while True:
         try:
-            game_controller.connect(('localhost', 8750))
-            game_controller.setblocking(False)
+            game.controller.connect(('localhost', 8750))
+            game.controller.setblocking(False)
             break
         except socket.error as msg:
             retry += 1
@@ -240,72 +334,53 @@ if game_controller_process:
                 supervisor.step(time_step)
             else:
                 print('Warning: could not connect to GameController at localhost:8750: Giving up.', file=sys.stderr)
-                game_controller = None
+                game.controller = None
                 break
     print('Connected to GameControllerSimulator at localhost:8750')
-    game_controller_udp_in = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-    game_controller_udp_in.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    game_controller_udp_in.bind(('0.0.0.0', 3838))
-    game_controller_udp_in.setblocking(False)
-    game_controller_udp_out = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-    game_controller_udp_out.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    game.udp_in = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    game.udp_in.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    game.udp_in.bind(('0.0.0.0', 3838))
+    game.udp_in.setblocking(False)
+    game.udp_out = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    game.udp_out.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 else:
-    game_controller = None
+    game.controller = None
 
-game.state = 'READY'
 update_state_display()
 game_controller_send(f'SIDE_LEFT:{game.side_left}')
-game_controller_send(f'KICKOFF:{game.kickoff}')
-game_controller_send(f'STATE:{game.state}')
+game_controller_send(f'KICKOFF:{random.randint(1, 2)}')  # toss a coin to determine which team has kickoff
+game_controller_send(f'STATE:READY')
 
 game.ball = supervisor.getFromDef('BALL')
 game.ball_translation = supervisor.getFromDef('BALL').getField('translation')
-game.ball_exited = 0
+game.ball_exited_countdown = 0
+game.play_countdown = 0
+game.finish_countdown = 0
 time_count = 0
-time_offset = 0
-previous_seconds = -1
-scoring_team = None
+previous_seconds_remaining = 0
 while supervisor.step(time_step) != -1:
     game_controller_send(f'CLOCK:{time_count}')
-    if game.state == 'READY':
-        seconds = 45 - (int)((time_count - time_offset) / 1000)
-        if seconds != previous_seconds:
-            previous_seconds = seconds
-            supervisor.setLabel(5, f' 00:{seconds:02d}', game.overlay_x, game.overlay_y, game.font_size,
-                                0x000000, 0.2, game.font)
-        if time_count >= 45000 + time_offset:
-            game.state = 'SET'
-            previous_seconds = -1
-            time_offset = time_count
-            game_controller_send(f'STATE:{game.state}')
+    game_controller_heartbeat()
+    if game.state is None:
+        continue
+    if game.state.game_state == 'STATE_PLAYING':
+        if previous_seconds_remaining != game.state.seconds_remaining:
             update_state_display()
-    elif game.state == 'SET':
-        seconds = 5 - (int)((time_count - time_offset) / 1000)
-        if seconds != previous_seconds:
-            previous_seconds = seconds
-            supervisor.setLabel(5, f' 00:{seconds:02d}', game.overlay_x, game.overlay_y, game.font_size,
-                                0x000000, 0.2, game.font)
-        if time_count >= 5000 + time_offset:
-            game.state = 'PLAY'
-            previous_seconds = -1
-            time_offset = time_count
-            game_controller_send(f'STATE:{game.state}')
-            update_state_display()
-    elif game.state == 'PLAY':
-        countdown = 600000 - time_count + time_offset
-        seconds = (int)(countdown / 1000) % 60
-        if seconds != previous_seconds:
-            previous_seconds = seconds
-            minutes = (int)(countdown / 60000)
-            supervisor.setLabel(5, f' {minutes:02d}:{seconds:02d}', game.overlay_x, game.overlay_y, game.font_size,
-                                0x000000, 0.2, game.font)
+            print(f'{game.state.seconds_remaining}:{game.extra_seconds}')
+            previous_seconds_remaining = game.state.seconds_remaining
+            if game.state.seconds_remaining + game.extra_seconds <= 0:
+                game.extra_seconds = 0
+                if game.state.first_half:
+                    print('End of first half')
+                    game.finish_countdown = int(2000 / time_step)  # two seconds half time break
+                game_controller_send(f'STATE:FINISH')
         ball_translation = game.ball_translation.getSFVec3f()
-        if game.ball_exited == 0 and \
+        if game.ball_exited_countdown == 0 and \
             (ball_translation[1] > game.field_size_y or
              ball_translation[1] < -game.field_size_y or
              ball_translation[0] > game.field_size_x or
              ball_translation[0] < -game.field_size_x):
-            game.ball_exited = 2000  # wait 2 seconds after ball exited to replace it
+            game.ball_exited_countdown = int(2000 / time_step)  # wait 2 seconds after ball exited to replace it
             game.ball_exit_translation = ball_translation
             scoring_team = None
             if game.ball_exit_translation[1] > game.field_size_y:
@@ -327,23 +402,37 @@ while supervisor.step(time_step) != -1:
             if scoring_team:
                 game.ball_exit_translation = game.ball_kickoff_translation
                 game_controller_send(f'SCORE:{scoring_team}')
-                if scoring_team == game.red.id:
-                    red_team['score'] += 1
-                else:
-                    blue_team['score'] += 1
-                game.state = 'READY'
-                time_offset = time_count
-                update_score_display()
-                update_state_display()
+                game.extra_seconds += 50  # 45 seconds for READY state, plus 5 seconds for SET state.
 
-        if game.ball_exited > 0:
-            game.ball_exited -= time_step
-            if game.ball_exited <= 0:
-                game.ball_translation.setSFVec3f(game.ball_exit_translation)
-                game.ball.resetPhysics()
+    elif game.state.game_state == 'STATE_READY':
+        # the GameController will automatically change to the SET state once the state READY is over
+        # the referee should wait about 5 seconds since the state SET started before sending the PLAY state
+        game.play_countdown = int(5000 / time_step)
+    elif game.state.game_state == 'STATE_SET' and game.play_countdown > 0:
+        game.play_countdown -= 1
+        if game.play_countdown == 0:
+            game_controller_send(f'STATE:PLAY')
+    elif game.state.game_state == 'STATE_FINISHED':
+        if game.state.first_half:
+            if game.finish_countdown == 0:
+                print('End of the game: the winner is...')
+            else:
+                game.finish_countdown -= 1
+                if game.finish_countdown == 0:
+                    game_controller_send('STATE:READY')  # begining of second half
+
+    elif game.state.game_state == 'STATE_INITIAL':
+        pass
+
+    if game.ball_exited_countdown > 0:
+        game.ball_exited_countdown -= 1
+        if game.ball_exited_countdown == 0:
+            game.ball_translation.setSFVec3f(game.ball_exit_translation)
+            game.ball.resetPhysics()
+
     time_count += time_step
 
-if game_controller:
-    game_controller.close()
-if game_controller_process:
-    game_controller_process.terminate()
+if game.controller:
+    game.controller.close()
+if game.controller_process:
+    game.controller_process.terminate()
