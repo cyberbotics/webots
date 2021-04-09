073333b96f2ee941508eedc3881e305c11555156
Olivier Michel <Olivier.Michel@cyberbotics.com>
2021-04-09 14:38:44 +0200

    Removed unneeded imports

--- projects/samples/contests/robocup/controllers/referee/referee.py
+++ projects/samples/contests/robocup/controllers/referee/referee.py
@@ -12,8 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from gamestate import GameState, ReturnData, GAME_CONTROLLER_RESPONSE_VERSION
-from construct import Container
+from gamestate import GameState
 
 from controller import Supervisor, AnsiCodes, Node
 
