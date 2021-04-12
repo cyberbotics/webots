1532cc959aa05dfb23eebd7b88950d0ebbbe4a42
BenjaminDeleze <benjamin.deleze@epfl.ch>
2021-04-12 12:01:45 +0200

    fullscreen_handler

--- resources/web/wwi/fullscreen_handler.js
+++ resources/web/wwi/fullscreen_handler.js
@@ -14,30 +14,30 @@
 
 function requestFullscreen(view) {
   const elem = view.view3D;
-  if (elem.requestFullscreen)
+  if (typeof elem.requestFullscreen !== 'undefined')
     elem.requestFullscreen();
-  else if (elem.msRequestFullscreen)
+  else if (typeof elem.msRequestFullscreen !== 'undefined')
     elem.msRequestFullscreen();
-  else if (elem.mozRequestFullScreen)
+  else if (typeof elem.mozRequestFullScreen !== 'undefined')
     elem.mozRequestFullScreen();
-  else if (elem.webkitRequestFullscreen)
+  else if (typeof elem.webkitRequestFullscreen !== 'undefined')
     elem.webkitRequestFullscreen();
 }
 
 function exitFullscreen() {
-  if (document.exitFullscreen)
+  if (typeof document.exitFullscreen !== 'undefined')
     document.exitFullscreen();
-  else if (document.msExitFullscreen)
+  else if (typeof document.msExitFullscreen !== 'undefined')
     document.msExitFullscreen();
-  else if (document.mozCancelFullScreen)
+  else if (typeof document.mozCancelFullScreen !== 'undefined')
     document.mozCancelFullScreen();
-  else if (document.webkitExitFullscreen)
+  else if (typeof document.webkitExitFullscreen !== 'undefined')
     document.webkitExitFullscreen();
 }
 
 function onFullscreenChange(fullscreenButton, exitFullscreenButton) {
   const element = document.fullScreenElement || document.mozFullScreenElement || document.webkitFullScreenElement || document.msFullScreenElement || document.webkitCurrentFullScreenElement;
-  if (element != null) {
+  if (typeof element !== 'undefined') {
     fullscreenButton.style.display = 'none';
     exitFullscreenButton.style.display = 'inline';
   } else {
