18c257e44db2e3756767c8569617b32f047daa55
Ludovic Hofer <medrimonia@gmail.com>
2021-04-24 12:53:19 +0200

    test_supervisor: Improving error handling
    Now if an exception is raised when reading the test scenario or during a step,
    the simulation is stopped with an explicit error message.

--- projects/samples/contests/robocup/controllers/test_supervisor/test_supervisor.py
+++ projects/samples/contests/robocup/controllers/test_supervisor/test_supervisor.py
@@ -7,6 +7,8 @@ import os
 import rc_testing
 import socket
 import time
+import traceback
+import sys
 
 class GCListener:
     """A simple UDP socket listening to the GameController broadcast messages"""
@@ -37,40 +39,51 @@ class GCListener:
 scenario_config_file = os.environ['WEBOTS_ROBOCUP_TEST_SCENARIO']
 
 supervisor = Supervisor()
+time_step = int(supervisor.getBasicTimeStep())
 system_start = time.time()
 
-with open(scenario_config_file) as json_file:
-    test_scenario = rc_testing.Scenario.buildFromList(json.load(json_file))
-
-time_step = int(supervisor.getBasicTimeStep())
+try:
+    with open(scenario_config_file) as json_file:
+        test_scenario = rc_testing.Scenario.buildFromList(json.load(json_file))
+except:
+    print(f"FAILED TO LOAD SCENARIO at {scenario_config_file}", file=sys.stderr)
+    traceback.print_exc()
+    # Ticking once the supervisor to flush waiting and then exit simulation
+    # This ensures that referee is not too busy to exit properly
+    supervisor.step(time_step)
+    time.sleep(4.0)
+    supervisor.simulationQuit(0)
 
 status = rc_testing.StatusInformation(0.0, 0.0, None)
 
 gc_listener = GCListener()
 
 finished = False
+critical_failure = False
 simulated_time = 0
 
-while supervisor.step(time_step) != -1 and not finished:
-    gc_listener.receive()
-    simulated_time += time_step/1000
-    status.update(time.time() - system_start, simulated_time,
-                  gc_listener.getState())
-    if (status.gc_status is not None):
-        test_scenario.step(status, supervisor)
-
-    finished = test_scenario.isFinished()
-    critical_failure = test_scenario.hasCriticalFailure()
+while supervisor.step(time_step) != -1:
     if finished or critical_failure:
         if finished:
             print(f"{status.getFormattedTime()} END OF TESTING")
         else:
             print(f"{status.getFormattedTime()} PREMATURELY EXITING TESTING")
         test_scenario.printResults()
-        finished = True
+        break
+    try:
+        gc_listener.receive()
+        simulated_time += time_step/1000
+        status.update(time.time() - system_start, simulated_time,
+                    gc_listener.getState())
+        if (status.gc_status is not None):
+            test_scenario.step(status, supervisor)
 
-# TODO make exit status different if one of the tests did not pass
+        finished = test_scenario.isFinished()
+        critical_failure = test_scenario.hasCriticalFailure()
+    except:
+        traceback.print_exc()
+        critical_failure = True
 
+# TODO make exit status different if one of the tests did not pass or critical failure
+supervisor.step(time_step)
 supervisor.simulationQuit(0)
-
-# Enter here exit cleanup code.
