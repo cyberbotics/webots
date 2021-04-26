dd73e0935a0be975fff53c4e34217689cf10d472
BenjaminDeleze <benjamin.deleze@epfl.ch>
2021-04-26 17:31:19 +0200

    install script

--- scripts/install/linux_install_web_viewer.sh
+++ scripts/install/linux_install_web_viewer.sh
@@ -9,10 +9,14 @@ apt install python3-pip -y
 pip3 install --upgrade pip
 pip3 install pyclibrary
 
+
 git clone https://github.com/emscripten-core/emsdk.git dependencies/emsdk
-cd emsdk
+
+user=$(env | grep SUDO_USER | cut -d '=' -f 2-) 
+chown $user dependencies/emsdk
+
 git pull
 ./dependencies/emsdk/emsdk install latest
 ./dependencies/emsdk/emsdk activate latest
-source ./dependencies/emsdk/emsdk_env.sh
-echo 'source "/home/benjamin/webots/dependencies/emsdk/emsdk_env.sh"' >> $HOME/.bashrc
+
+echo 'source "/home/benjamin/webots/dependencies/emsdk/emsdk_env.sh"' >> /home/$user/.bashrc
