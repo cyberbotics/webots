a9d4f4bfe691ce6c07905604da03b2c0a2ec4a60
Olivier Michel <Olivier.Michel@cyberbotics.com>
2021-04-27 22:10:40 +0200

    Use --no-rendering mode to make Webots run faster for tests

--- projects/samples/contests/robocup/controllers/referee/tests/launch_test.sh
+++ projects/samples/contests/robocup/controllers/referee/tests/launch_test.sh
@@ -17,4 +17,4 @@ TEST_FOLDER="$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
 
 WEBOTS_ROBOCUP_TEST_SCENARIO="${TEST_FOLDER}/test_scenario.json" \
                             WEBOTS_ROBOCUP_GAME="${TEST_FOLDER}/game.json" \
-                            $WEBOTS --stdout --stderr --mode=fast
+                            $WEBOTS --stdout --stderr --mode=fast --no-rendering
