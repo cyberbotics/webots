66bc100b96f247eae604611a954d4efd6883de50
Ludovic Hofer <medrimonia@gmail.com>
2021-04-28 11:17:59 +0200

    Automated test: adding a '--render' option to the launch_test.sh
    This helps while developing test scenarios and does not impact default usage
    with a single argument

--- projects/samples/contests/robocup/controllers/referee/tests/launch_test.sh
+++ projects/samples/contests/robocup/controllers/referee/tests/launch_test.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]
+
+if [ $# -lt 1 ]
 then
-    echo "Usage $0 <test_path>"
+    echo "Usage $0 <test_path> [--render]"
     exit -1
 fi
+
+WEBOTS_OPTIONS="--stdout --stderr --mode=fast"
+
+if [ $# == 2 ]
+then
+    if [ $2 != "--render" ]
+    then
+        echo "Unknown option $2"
+        exit -1
+    fi
+else
+    WEBOTS_OPTIONS="${WEBOTS_OPTIONS} --mode=fast --minimize"
+fi
+
 
 export PYTHONPATH="${WEBOTS_HOME}/projects/samples/contests/robocup/controllers/referee"  # this should be removed once https://github.com/cyberbotics/webots/issues/3011 is fixed
 if [ "$(expr substr $(uname -s) 1 10)" == "MSYS_NT-10" ]; then
@@ -17,4 +32,4 @@ TEST_FOLDER="$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
 
 WEBOTS_ROBOCUP_TEST_SCENARIO="${TEST_FOLDER}/test_scenario.json" \
                             WEBOTS_ROBOCUP_GAME="${TEST_FOLDER}/game.json" \
-                            $WEBOTS --stdout --stderr --mode=fast --no-rendering --minimize
+                            $WEBOTS $WEBOTS_OPTIONS
