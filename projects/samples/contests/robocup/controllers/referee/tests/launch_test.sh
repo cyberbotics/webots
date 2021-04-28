e2ad8fcceeed157864849f22d290ba0a67ed8dda
Olivier Michel <Olivier.Michel@cyberbotics.com>
2021-04-28 08:41:33 +0200

    minimize Webots window during tests

--- projects/samples/contests/robocup/controllers/referee/tests/launch_test.sh
+++ projects/samples/contests/robocup/controllers/referee/tests/launch_test.sh
@@ -6,10 +6,10 @@ then
     exit -1
 fi
 
+export PYTHONPATH="${WEBOTS_HOME}/projects/samples/contests/robocup/controllers/referee"  # this should be removed once https://github.com/cyberbotics/webots/issues/3011 is fixed
 if [ "$(expr substr $(uname -s) 1 10)" == "MSYS_NT-10" ]; then
   WEBOTS="webots"
 else
-  export PYTHONPATH="${WEBOTS_HOME}/projects/samples/contests/robocup/controllers/referee"  # this should be removed once https://github.com/cyberbotics/webots/issues/3011 is fixed
   WEBOTS="${WEBOTS_HOME}/webots"
 fi
 
@@ -17,4 +17,4 @@ TEST_FOLDER="$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
 
 WEBOTS_ROBOCUP_TEST_SCENARIO="${TEST_FOLDER}/test_scenario.json" \
                             WEBOTS_ROBOCUP_GAME="${TEST_FOLDER}/game.json" \
-                            $WEBOTS --stdout --stderr --mode=fast --no-rendering
+                            $WEBOTS --stdout --stderr --mode=fast --no-rendering --minimize
