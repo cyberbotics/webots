c690d5883f7797a9693b6f4b4b66edc472708205
BenjaminDeleze <benjamin.deleze@epfl.ch>
2021-04-30 10:25:19 +0200

    update scripts and files for online compatibility

--- scripts/install/linux_web_viewer_dependencies.sh
+++ scripts/install/linux_web_viewer_dependencies.sh
@@ -12,10 +12,12 @@ pip3 install pyclibrary
 
 git clone https://github.com/emscripten-core/emsdk.git dependencies/emsdk
 
-user=$(env | grep SUDO_USER | cut -d '=' -f 2-) 
+USER=$(env | grep SUDO_USER | cut -d '=' -f 2-) 
 
 git pull
 ./dependencies/emsdk/emsdk install latest
 ./dependencies/emsdk/emsdk activate latest
-chown -R $user dependencies/emsdk
-echo 'source "/home/benjamin/webots/dependencies/emsdk/emsdk_env.sh" >/dev/null 2>&1' >> /home/$user/.bashrc
+chown -R $USER dependencies/emsdk
+
+WEBOTS_HOME=$(pwd)
+echo 'source "'$WEBOTS_HOME'/dependencies/emsdk/emsdk_env.sh" >/dev/null 2>&1' >> /home/$USER/.bashrc
