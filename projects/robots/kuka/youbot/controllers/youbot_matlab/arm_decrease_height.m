bee014fc24bb798f59e7e6c021ffe195bc251a5f
Your Name <you@example.com>
2021-05-04 14:55:30 +0200

    rotation

--- projects/robots/kuka/youbot/controllers/youbot_matlab/arm_decrease_height.m
+++ projects/robots/kuka/youbot/controllers/youbot_matlab/arm_decrease_height.m
@@ -2,9 +2,14 @@ function arm_decrease_height()
   global HEIGHTS_ID;
   global HEIGHTS;
   global current_height;
+  
   new_height_id = HEIGHTS_ID(current_height) -1;
+  
+  % Prevents from going beyond index
   if (new_height_id < 1)
     new_height_id = 1;
   end
-  arm_set_height(HEIGHTS(new_height_id));
+  
+  new_height = HEIGHTS(new_height_id);  
+  arm_set_height(new_height);
 end
--- projects/robots/kuka/youbot/controllers/youbot_matlab/base_move.m
+++ projects/robots/kuka/youbot/controllers/youbot_matlab/base_move.m
@@ -2,10 +2,10 @@ function base_move(vx, vy, omega)
   global WHEEL_RADIUS;
   global LX;
   global LY;
-  speeds = [ 1 / WHEEL_RADIUS * (vx + vy - (LX + LY) * omega),
-             1 / WHEEL_RADIUS * (vx - vy + (LX + LY) * omega),
+  speeds = [ 1 / WHEEL_RADIUS * (vx + vy + (LX + LY) * omega),
              1 / WHEEL_RADIUS * (vx - vy - (LX + LY) * omega),
-             1 / WHEEL_RADIUS * (vx + vy + (LX + LY) * omega)];
+             1 / WHEEL_RADIUS * (vx - vy + (LX + LY) * omega),
+             1 / WHEEL_RADIUS * (vx + vy - (LX + LY) * omega)];
   base_set_wheel_speeds_helper(speeds);
   wb_console_print(sprintf('Speeds [m/s]: vx=%.2f vy=%.2f ω=%.2f', vx, vy, omega), WB_STDOUT)
 end
