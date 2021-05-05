5e3e569d2faf62f402cf5edaa2e189e4fdc7b4df
mael25 <>
2021-05-05 16:05:29 +0200

    recleaned controller

--- projects/objects/factory/conveyors/controllers/keyboard_conveyor_platform/keyboard_conveyor_platform.c
+++ projects/objects/factory/conveyors/controllers/keyboard_conveyor_platform/keyboard_conveyor_platform.c
@@ -44,7 +44,6 @@ int main() {
   double target_belt_speed = 0.0;  // in [m/s].
   int sign;                        // sign of the increment (decrement if -1).
 
-  int key;
   int previous_key = 0;
   bool is_key_valid;
   wb_keyboard_enable(TIME_STEP);
@@ -54,7 +53,7 @@ int main() {
   Reset: Space bar \n");
 
   while (wb_robot_step(TIME_STEP) != -1) {
-    key = wb_keyboard_get_key();
+    int key = wb_keyboard_get_key();
     if (key >= 0 && key != previous_key) {
       is_key_valid = 1;
       switch (key) {
