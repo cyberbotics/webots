c2c058b77d58e1a04c22dd8cdfb6666699a60398
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2021-05-08 19:04:44 +0200

    Fix using static_cast for casting dynamic objects
    Replaces C-Cast with their proper dynamic_cast calls.

--- projects/samples/contests/robocup/controllers/player/player.cpp
+++ projects/samples/contests/robocup/controllers/player/player.cpp
@@ -415,27 +415,27 @@ public:
         else
           switch (device->getNodeType()) {
             case webots::Node::ACCELEROMETER: {
-              webots::Accelerometer *accelerometer = (webots::Accelerometer *)device;
+              webots::Accelerometer *accelerometer = static_cast<webots::Accelerometer *>(device);
               accelerometer->enable(sensor_time_step);
               break;
             }
             case webots::Node::CAMERA: {
-              webots::Camera *camera = (webots::Camera *)device;
+              webots::Camera *camera = static_cast<webots::Camera *>(device);
               camera->enable(sensor_time_step);
               break;
             }
             case webots::Node::GYRO: {
-              webots::Gyro *gyro = (webots::Gyro *)device;
+              webots::Gyro *gyro = static_cast<webots::Gyro *>(device);
               gyro->enable(sensor_time_step);
               break;
             }
             case webots::Node::POSITION_SENSOR: {
-              webots::PositionSensor *positionSensor = (webots::PositionSensor *)device;
+              webots::PositionSensor *positionSensor = static_cast<webots::PositionSensor *>(device);
               positionSensor->enable(sensor_time_step);
               break;
             }
             case webots::Node::TOUCH_SENSOR: {
-              webots::TouchSensor *touchSensor = (webots::TouchSensor *)device;
+              webots::TouchSensor *touchSensor = static_cast<webots::TouchSensor *>(device);
               touchSensor->enable(sensor_time_step);
               break;
             }
