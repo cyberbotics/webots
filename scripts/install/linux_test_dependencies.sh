4f99079ac88f68d04a9937cb40319d10a7c2b5a0
Darko Lukić <lukicdarkoo@gmail.com>
2021-05-10 12:14:04 +0200

    Update linux_test_dependencies.sh

--- scripts/install/linux_test_dependencies.sh
+++ scripts/install/linux_test_dependencies.sh
@@ -5,11 +5,6 @@ if [[ $EUID -ne 0 ]]; then
        exit 1
 fi
 
-if [[ $1 != "--norecurse" ]]; then
-	script_full_path=$(dirname "$0")
-	$script_full_path/linux_runtime_dependencies.sh
-fi
-
 UBUNTU_VERSION=$(lsb_release -rs)
 if [[ $UBUNTU_VERSION == "16.04" ]]; then
        export ROS_DISTRO=kinetic
@@ -25,3 +20,8 @@ sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /
 apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 apt update -qq
 apt-get install -y ros-$ROS_DISTRO-ros-base ros-$ROS_DISTRO-sensor-msgs ros-$ROS_DISTRO-tf ros-$ROS_DISTRO-ros-controllers ros-$ROS_DISTRO-controller-manager ros-$ROS_DISTRO-ros-control ros-$ROS_DISTRO-class-loader ros-$ROS_DISTRO-roslib liburdfdom-tools
+
+if [[ $1 != "--norecurse" ]]; then
+	script_full_path=$(dirname "$0")
+	$script_full_path/linux_runtime_dependencies.sh
+fi
