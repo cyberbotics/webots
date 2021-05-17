f9d4ef238fd773961aefe95642f53f95c70003e7
Benjamin Délèze <benjamin.deleze@epfl.ch>
2021-05-17 12:44:42 +0200

    Update resources/web/wwi/nodes/WbWorld.js
    Co-authored-by: Darko Lukić <lukicdarkoo@gmail.com>

--- resources/web/wwi/nodes/WbWorld.js
+++ resources/web/wwi/nodes/WbWorld.js
@@ -2,7 +2,7 @@ export default class WbWorld {
   constructor() {
     this.hasFog = false;
     this.coordinateSystem = 'ENU';
-    this.upVector = glm.vec3(0, 1, 0);
+    this.upVector = glm.vec3(0, 0, 1);
     // Only the top level nodes are represented here
     this.sceneTree = [];
     // All the nodes are included here so it is easier to retrieve them for updates
