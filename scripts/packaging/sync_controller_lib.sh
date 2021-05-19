555e5aee42bba08102ac7c3d5599bbb8496635ee
Darko Lukić <lukicdarkoo@gmail.com>
2021-05-19 11:41:57 +0200

    Update sync_controller_lib.sh

--- scripts/packaging/sync_controller_lib.sh
+++ scripts/packaging/sync_controller_lib.sh
@@ -10,26 +10,19 @@ VERSION=$(cat ${WEBOTS_HOME}/scripts/packaging/webots_version.txt)
 # Dynamic libraries to be copied
 DYNAMIC_LIBS="Controller CppController car CppCar driver CppDriver"
 
-# GitHub authorization if not running locally
-GITHUB_AUTH=''
-if [ ! -z "${GITHUB_ACTOR}" ]; then
-    GITHUB_AUTH="${GITHUB_ACTOR}:${GITHUB_TOKEN}@"
-fi
-
 # Get the repo
 rm -rf /tmp/webots-controller || true
 if [ ! -z "${GITHUB_ACTOR}" ]; then
     git config --global user.name ${GITHUB_ACTOR}
     git config --global user.email ${GITHUB_ACTOR}@github.com
 fi
-git clone --depth=1 git@github.com:cyberbotics/webots-controller.git /tmp/webots-controller
+git clone git@github.com:cyberbotics/webots-controller.git /tmp/webots-controller
 if [ ! -d /tmp/webots-controller ]; then
     echo 'The repository is not properly cloned'
     exit 1
 fi
 cd /tmp/webots-controller
 git checkout -b ${VERSION}
-git pull
 
 # Prepare the structure
 rm -rf lib/${OSTYPE}
@@ -48,4 +41,4 @@ done
 # Push
 git add -A
 git commit -m "Automatic update"
-git push origin ${VERSION}
+git push -f origin ${VERSION}
