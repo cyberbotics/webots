40b3038c98b2341b13691ec309c9a5ae2cb61c18
Your Name <you@example.com>
2021-05-20 08:32:45 +0200

    controller

--- projects/robots/robotnik/summit_xl_steel/controllers/keyboard_control/keyboard_control.c
+++ projects/robots/robotnik/summit_xl_steel/controllers/keyboard_control/keyboard_control.c
@@ -34,8 +34,8 @@
 #define WHEEL_RADIUS 0.127
 #define LX 0.2045  // lateral distance from robot's COM to wheel [m].
 #define LY 0.2225  // longitudinal distance from robot's COM to wheel [m].
-#define SPEED_INCREMENT 0.05
-#define MAX_SPEED 2.0
+#define SPEED_INCREMENT 0.1
+#define MAX_SPEED 1.5
 
 int main() {
   wb_robot_init();
