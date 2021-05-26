d5c5beb0e3963e0a1c51759397c890955ccc2883
ad-daniel <44834743+ad-daniel@users.noreply.github.com>
2021-05-26 08:09:07 +0200

    Update src/webots/nodes/WbBaseNode.cpp
    Co-authored-by: Stefania Pedrazzi <stefaniapedrazzi@users.noreply.github.com>

--- src/webots/nodes/WbBaseNode.cpp
+++ src/webots/nodes/WbBaseNode.cpp
@@ -335,7 +335,7 @@ void WbBaseNode::removeInvisibleProtoNodes() {
     invisibleProtoParameterNodes[i]->clearProtoParameterNodeInstances();  // clear downward references
 
     // clear internal field references (for protoParameterNodes the reference is kept in its fields)
-    QVector<WbField *> fields = invisibleProtoParameterNodes[i]->fields();
+    const QVector<WbField *> fields = invisibleProtoParameterNodes[i]->fields();
     for (int j = 0; j < fields.size(); ++j)
       fields[j]->clearInternalFields();
   }
