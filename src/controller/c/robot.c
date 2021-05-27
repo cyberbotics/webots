cada5af85fda13f3af40cb3697ef82ecf18088c7
Yann LEROY <yann.leroy@manurob.com>
2021-05-27 11:43:46 +0200

    Remove non-existing commented function

--- src/controller/c/robot.c
+++ src/controller/c/robot.c
@@ -279,7 +279,6 @@ static void robot_send_request(unsigned int step_duration) {
     remote_control_step(step_duration);
   }
 
-  // scheduler_print_request(req);
   if (scheduler_is_local())
     scheduler_send_request(req);
   else if (request_get_size(req) != 8)
