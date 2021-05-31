0a1c860ba5edba5d64c790800a778bd2d82feca9
Stefania Pedrazzi <stefania.pedrazzi@cyberbotics.com>
2021-05-31 15:43:31 +0200

    Add regression test

--- tests/protos/controllers/nested_parameter_transform_node/nested_parameter_transform_node.c
+++ tests/protos/controllers/nested_parameter_transform_node/nested_parameter_transform_node.c
@@ -11,7 +11,14 @@ int main(int argc, char **argv) {
 
   WbNodeRef parent_node = wb_supervisor_node_get_from_def("PARENT");
   WbFieldRef translation = wb_supervisor_node_get_field(parent_node, "translation");
+  
+  // Test position update of node defined in nested PROTO parameter
+  WbNodeRef parameter_node = wb_supervisor_node_get_from_def("PARAMETER");
+  const double *parameter_position = wb_supervisor_node_get_position(parameter_node);
+  ts_assert_vec3_in_delta(parameter_position[0], parameter_position[1], parameter_position[2], 0.45, 0.5, 0.03, 0.0001,
+                          "Position of visible nested PROTO parameter is wrong.");
 
+  // Test position update of node defined in a PROTO definition
   WbNodeRef test_node = wb_supervisor_node_get_from_proto_def(parent_node, "TEST_PROTO");
   wb_robot_step(TIME_STEP);
   const double *position_before = wb_supervisor_node_get_position(test_node);
@@ -26,7 +33,7 @@ int main(int argc, char **argv) {
   const double *position_after = wb_supervisor_node_get_position(test_node);
   ts_assert_vec3_in_delta(position_after[0], position_after[1], position_after[2], 2.0, 1.0, 1.0, 0.0001,
                           "Position of deeply nested PROTO parameter is wrong after top node position change.");
-
+  
   ts_send_success();
   return EXIT_SUCCESS;
 }
