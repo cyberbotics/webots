6f45bc70e667b3b28df650182300791874947fbb
Stefania Pedrazzi <stefania.pedrazzi@cyberbotics.com>
2021-06-07 13:39:30 +0200

    Fix python basic Jetbot motion control

--- projects/robots/nvidia/jetbot/libraries/jetbot_python_control/jetbot_python_control.py
+++ projects/robots/nvidia/jetbot/libraries/jetbot_python_control/jetbot_python_control.py
@@ -14,12 +14,12 @@
 
 """JetBot Robot class with basic motion control."""
 
-from controller import Robot, Camera, Display, Motor
+from controller import Robot
+
 
 class JetBot(Robot):
-
     def __init__(self):
-        Robot.__init__(self) 
+        Robot.__init__(self)
         self.left_motor = self.getDevice('left_wheel_hinge')
         self.right_motor = self.getDevice('right_wheel_hinge')
         self.camera = self.getDevice('camera')
@@ -29,43 +29,44 @@ class JetBot(Robot):
         self.right_motor.setVelocity(0)
         self.left_motor.setVelocity(0)
         self.speed_factor = 10
-        
+
     def left(self, speed):
         s = self.__convert_speed(speed)
         self.right_motor.setVelocity(-s)
         self.left_motor.setVelocity(s)
-        
+
     def right(self, speed):
         s = self.__convert_speed(speed)
         self.right_motor.setVelocity(s)
         self.left_motor.setVelocity(-s)
-        
+
     def forward(self, speed):
         s = self.__convert_speed(speed)
         self.right_motor.setVelocity(s)
         self.left_motor.setVelocity(s)
-        
+
     def backward(self, speed):
         s = self.__convert_speed(speed)
         self.right_motor.setVelocity(-s)
         self.left_motor.setVelocity(-s)
-        
+
     def set_motors(self, speed_left, speed_right):
         self.right_motor.setVelocity(self.__convert_speed(speed_left))
         self.left_motor.setVelocity(self.__convert_speed(speed_right))
-        
+
     def stop(self):
         self.right_motor.setVelocity(0)
         self.left_motor.setVelocity(0)
 
     def __convert_speed(self, speed):
         if speed < 0.2:
-           return 0.0
-        
+            return 0.0
+
         converted_values = [0, 0.12, 0.2, 0.3, 0.4, 0.45, 0.55, 0.6, 0.65]
         max_speed = 0.3
         for i in range(1, 9):
             if speed <= max_speed:
-                return self.speed_factor * 0.2
+                return self.speed_factor * ((converted_values[i] - converted_values[i - 1]) / 0.1 * (speed - max_speed + 0.1) +
+                                            converted_values[i - 1])
             max_speed += 0.1
-        return 0.65 * self.speed_factor
+        return converted_values[8] * self.speed_factor
