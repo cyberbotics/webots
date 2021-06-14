934a603151568e660c0b64be46985fb0428c0885
Ludovic Hofer <medrimonia@gmail.com>
2021-06-14 19:41:08 +0200

    Automatically starts the game with game controller minimized now
    Takes leverage on https://github.com/RoboCup-Humanoid-TC/GameController/pull/159
    to make usage easier for both tests and teams (Autoref is not really expecting
    to have user interaction from the GC).

--- projects/samples/contests/robocup/controllers/referee/referee.py
+++ projects/samples/contests/robocup/controllers/referee/referee.py
@@ -2173,6 +2173,7 @@ try:
             command_line = [os.path.join(JAVA_HOME, 'bin', 'java'), '-jar', 'GameControllerSimulator.jar']
             if game.minimum_real_time_factor < 1:
                 command_line.append('--fast')
+            command_line.append('--minimized')
             command_line.append('--config')
             command_line.append(game_config_file)
             if hasattr(game, 'game_controller_extra_args'):
