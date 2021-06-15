259ce8bcbedaf1a4171f4d45af5d2afab11c06b4
ad-daniel <daniel.dias@epfl.ch>
2021-06-15 12:16:55 +0200

    fix sources and cleanup

--- src/webots/core/WbQjsFile.cpp
+++ src/webots/core/WbQjsFile.cpp
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "WbLog.hpp"
 #include "WbQjsFile.hpp"
+#include "WbLog.hpp"
 #include "WbStandardPaths.hpp"
 
 #include <QtCore/QFile>
