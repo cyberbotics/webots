cdc6f19131da2f6be724a0a71d380a02974dd7f7
ad-daniel <daniel.dias@epfl.ch>
2021-06-15 17:48:04 +0200

    fix

--- scripts/install/linux_test_dependencies.sh
+++ scripts/install/linux_test_dependencies.sh
@@ -17,12 +17,12 @@ else
 fi
 
 if [[ $@ != *"--exclude-ros"* ]]; then
-  echo "Excluding ROS dependencies"
+  echo "Adding ROS dependencies"
   sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
   curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
   apt update -qq
 
-  apt-get install -y ros-$ROS_DISTRO-ros-base ros-$ROS_DISTRO-sensor-msgs ros-$ROS_DISTRO-tf ros-$ROS_DISTRO-ros-controllers ros-$ROS_DISTRO-controller-manager ros-$ROS_DISTRO-ros-control ros-$ROS_DISTRO-class-loader ros-$ROS_DISTRO-roslib ros-$ROS_DISTRO-moveit liburdfdom-tools
+  apt-get install -y ros-$ROS_DISTRO-ros-base ros-$ROS_DISTRO-sensor-msgs ros-$ROS_DISTRO-tf ros-$ROS_DISTRO-ros-controllers ros-$ROS_DISTRO-controller-manager ros-$ROS_DISTRO-ros-control ros-$ROS_DISTRO-class-loader ros-$ROS_DISTRO-roslib ros-$ROS_DISTRO-moveit-ros liburdfdom-tools
 
   if [[ $UBUNTU_VERSION == "16.04"  || $UBUNTU_VERSION == "18.04" ]]; then
          apt install -y python-rosdep
@@ -32,7 +32,7 @@ if [[ $@ != *"--exclude-ros"* ]]; then
 fi
 
 if [[ $@ != *"--norecurse"* ]]; then
-  echo "Exclude runtime dependencies"
+  echo "Installing runtime dependencies"
   script_full_path=$(dirname "$0")
   $script_full_path/linux_runtime_dependencies.sh
 fi
