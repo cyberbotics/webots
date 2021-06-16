1d36f0baa9b093ade7312a70bc2f3638566eae26
Darko Lukic <lukicdarkoo@gmail.com>
2021-06-16 12:51:49 +0200

    Commented

--- src/controller/matlab/mgenerate.py
+++ src/controller/matlab/mgenerate.py
@@ -462,6 +462,7 @@ def main(args=None):
     gen(FUNC, "wb_supervisor_field_disable_sf_tracking(field)", "supervisor")
     gen(FUNC, "wb_supervisor_node_enable_contact_point_tracking(noderef, sampling_period, include_descendants)", "supervisor")
     gen(FUNC, "wb_supervisor_node_disable_contact_point_tracking(noderef, include_descendants)", "supervisor")
+    # gen(PROC, "wb_supervisor_node_get_contact_points(noderef,include_descendants)", "supervisor")
     gen(FUNC, "wb_supervisor_node_get_proto_field(noderef, fieldname)", "supervisor")
     # gen(FUNC, "wb_supervisor_node_get_center_of_mass(noderef)", "supervisor")
     gen(FUNC, "wb_supervisor_node_get_number_of_contact_points(noderef, include_descendants)", "supervisor")
