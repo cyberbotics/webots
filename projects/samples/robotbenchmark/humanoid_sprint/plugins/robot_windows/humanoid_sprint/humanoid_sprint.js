23409cf07609555e042699f8dc6d7f95b79f07ae
BenjaminDeleze <benjamin.deleze@epfl.ch>
2021-06-16 17:32:42 +0200

    fix humanoid_sprint

--- projects/samples/robotbenchmark/humanoid_sprint/plugins/robot_windows/humanoid_sprint/humanoid_sprint.js
+++ projects/samples/robotbenchmark/humanoid_sprint/plugins/robot_windows/humanoid_sprint/humanoid_sprint.js
@@ -1,6 +1,4 @@
 /* global webots, sendBenchmarkRecord, showBenchmarkRecord, showBenchmarkError */
-$('#infotabs').tabs();
-
 var benchmarkName = 'Humanoid Sprint';
 var humanoidSprintTimeString;
 var humanoidSprintTime;
@@ -9,15 +7,15 @@ webots.window('humanoid_sprint').receive = function(message, robot) {
   if (message.startsWith('time:')) {
     humanoidSprintTime = parseFloat(message.substr(5));
     humanoidSprintTimeString = parseSecondsIntoReadableTime(humanoidSprintTime);
-    $('#time-display').html(humanoidSprintTimeString);
+    document.getElementById('time-display').innerHTML = humanoidSprintTimeString;
   } else if (message === 'stop') {
     if (typeof sendBenchmarkRecord === 'undefined' || !sendBenchmarkRecord(robot, this, benchmarkName, -humanoidSprintTime, metricToString))
-      $('#time-display').css('color', 'red');
+      document.getElementById('time-display').style.color = 'red';
   } else if (message.startsWith('record:OK:')) {
-    $('#time-display').css('font-weight', 'bold');
+    document.getElementById('time-display').style.fontWeight = 'bold';
     showBenchmarkRecord(message, benchmarkName, metricToString);
   } else if (message.startsWith('record:Error:')) {
-    $('#time-display').css('color', 'red');
+    document.getElementById('time-display').style.color = 'red';
     showBenchmarkError(message, benchmarkName);
   } else
     console.log("Received unknown message for robot '" + robot + "': '" + message + "'");
