a6ee746b3c7964f632d69f5b13ca8ec93e34c297
ad-daniel <daniel.dias@epfl.ch>
2021-06-16 12:16:28 +0200

    fix again

--- scripts/install/linux_test_dependencies.sh
+++ scripts/install/linux_test_dependencies.sh
@@ -5,18 +5,18 @@ if [[ $EUID -ne 0 ]]; then
        exit 1
 fi
 
-UBUNTU_VERSION=$(lsb_release -rs)
-if [[ $UBUNTU_VERSION == "16.04" ]]; then
-       export ROS_DISTRO=kinetic
-elif [[ $UBUNTU_VERSION == "18.04" ]]; then
-       export ROS_DISTRO=melodic
-elif [[ $UBUNTU_VERSION == "20.04" ]]; then
-       export ROS_DISTRO=noetic
-else
-       echo "Unsupported Linux version: dependencies may not be completely installed. Only the two latest Ubuntu LTS are supported."
-fi
-
 if [[ $@ != *"--exclude-ros"* ]]; then
+  UBUNTU_VERSION=$(lsb_release -rs)
+  if [[ $UBUNTU_VERSION == "16.04" ]]; then
+         export ROS_DISTRO=kinetic
+  elif [[ $UBUNTU_VERSION == "18.04" ]]; then
+         export ROS_DISTRO=melodic
+  elif [[ $UBUNTU_VERSION == "20.04" ]]; then
+         export ROS_DISTRO=noetic
+  else
+         echo "Unsupported Linux version: dependencies may not be completely installed. Only the two latest Ubuntu LTS are supported."
+  fi
+
   echo "Adding ROS dependencies"
   sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
   curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | apt-key add -
