954f739518e846fd4ddae25a11dca0ae3c11b08f
Yann LEROY <yann.leroy@manurob.com>
2021-06-17 15:20:41 +0200

    a missing break that don't hurt, but still, better use it

--- src/webots/nodes/WbSolid.cpp
+++ src/webots/nodes/WbSolid.cpp
@@ -2350,6 +2350,7 @@ void WbSolid::resetSingleSolidPhysics() {
         dJointSetBallParam(mJoint, dParamVel2, 0.0);
         dJointSetBallParam(mJoint, dParamFMax3, 0.0);
         dJointSetBallParam(mJoint, dParamVel3, 0.0);
+        break;
       default:  // only the above joint types are currently implemented in Webots
         break;
     }
