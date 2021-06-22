e84fdd5a20cbde0612ea1e6372cc42561d081ddd
sishamilton <69378550+sishamilton@users.noreply.github.com>
2021-06-21 21:02:12 -0700

    Create a better demo

--- projects/samples/devices/controllers/altimeter/altimeter.c
+++ projects/samples/devices/controllers/altimeter/altimeter.c
@@ -16,8 +16,8 @@
 
 /*
  * Description:
- *   Allow robot to roll freely down a ramp.
- *   Print altitude to console.
+ *   Use altimeter to control robot's
+ *   ascent and descent of ramp.
  */
 
 #include <webots/altimeter.h>
@@ -40,10 +40,22 @@ int main(int argc, char **argv) {
   WbDeviceTag right_motor = wb_robot_get_device("right wheel motor");
   wb_motor_set_position(left_motor, INFINITY);
   wb_motor_set_position(right_motor, INFINITY);
-
+  
+  bool direction_switch = false;
+  
   while (wb_robot_step(time_step) != -1) {
     double altitude = wb_altimeter_get_value(altimeter);
-    printf("Altitude = %f\n", altitude);
+    if (!direction_switch) {
+      wb_motor_set_velocity(left_motor, 2.0);
+      wb_motor_set_velocity(right_motor, 2.0);
+      if (altitude <= 0.05)
+        direction_switch = true;
+    } else {
+      wb_motor_set_velocity(left_motor, -2.0);
+      wb_motor_set_velocity(right_motor, -2.0);
+      if (altitude >= 0.25)
+        direction_switch = false;
+    }
   };
 
   wb_robot_cleanup();
