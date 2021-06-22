d1b0895ca9d28ad6d084ed5336f8815f63043b1f
ShuffleWire <83014015+ShuffleWire@users.noreply.github.com>
2021-06-22 14:20:14 +0200

    Update src/webots/scene_tree/WbTreeItem.cpp
    Co-authored-by: Olivier Michel <Olivier.Michel@cyberbotics.com>

--- src/webots/scene_tree/WbTreeItem.cpp
+++ src/webots/scene_tree/WbTreeItem.cpp
@@ -129,7 +129,7 @@ QString WbTreeItem::data() const {
     case NODE: {
       QString fullName = mNode->fullName();
       if (!fullName.startsWith("DEF ") && !fullName.startsWith("USE ")) {
-        WbSFString *name = mNode->findSFString("name");
+        const WbSFString *name = mNode->findSFString("name");
         if (name)
           fullName += " \"" + name->value() + "\"";
       }
