ecbbc1642d80f3e702bfbbf619d544f85e4ceb06
ad-daniel <44834743+ad-daniel@users.noreply.github.com>
2021-06-24 12:11:24 +0200

    Fix `test ros` on master (#3221)
    * apply same workflows
    
    * update agent version
    
    * revert restriction
    
    * missed some
    
    * again ..
    
    * Revert "update agent version"
    
    This reverts commit 2c7d9ebbdf3bd57849e54ff694b73b549584deb4.
    
    * restore
    
    * revert it back again..

--- scripts/install/linux_test_dependencies.sh
+++ scripts/install/linux_test_dependencies.sh
@@ -5,23 +5,35 @@ if [[ $EUID -ne 0 ]]; then
        exit 1
 fi
 
-UBUNTU_VERSION=$(lsb_release -rs)
-if [[ $UBUNTU_VERSION == "16.04" ]]; then
-       export ROS_DISTRO=kinetic
-elif [[ $UBUNTU_VERSION == "18.04" ]]; then
-       export ROS_DISTRO=melodic
-elif [[ $UBUNTU_VERSION == "20.04" ]]; then
-       export ROS_DISTRO=noetic
-else
-       echo "Unsupported Linux version: dependencies may not be completely installed. Only the two latest Ubuntu LTS are supported."
+apt install liburdfdom-tools -y
+
+if [[ $@ != *"--exclude-ros"* ]]; then
+  UBUNTU_VERSION=$(lsb_release -rs)
+  if [[ $UBUNTU_VERSION == "16.04" ]]; then
+         export ROS_DISTRO=kinetic
+  elif [[ $UBUNTU_VERSION == "18.04" ]]; then
+         export ROS_DISTRO=melodic
+  elif [[ $UBUNTU_VERSION == "20.04" ]]; then
+         export ROS_DISTRO=noetic
+  else
+         echo "Unsupported Linux version: dependencies may not be completely installed. Only the two latest Ubuntu LTS are supported."
+  fi
+
+  echo "Adding ROS dependencies"
+  sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+  curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | apt-key add -
+  apt update -qq
+  apt install -y ros-$ROS_DISTRO-ros-base ros-$ROS_DISTRO-sensor-msgs ros-$ROS_DISTRO-tf ros-$ROS_DISTRO-ros-controllers ros-$ROS_DISTRO-controller-manager ros-$ROS_DISTRO-ros-control ros-$ROS_DISTRO-class-loader ros-$ROS_DISTRO-roslib liburdfdom-tools
+
+  if [[ $UBUNTU_VERSION == "16.04"  || $UBUNTU_VERSION == "18.04" ]]; then
+         apt install -y python-rosdep
+  elif [[ $UBUNTU_VERSION == "20.04" ]]; then
+         apt install -y python3-rosdep
+  fi
 fi
 
-sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
-apt update -qq
-apt-get install -y ros-$ROS_DISTRO-ros-base ros-$ROS_DISTRO-sensor-msgs ros-$ROS_DISTRO-tf liburdfdom-tools
-
-if [[ $1 != "--norecurse" ]]; then
-	script_full_path=$(dirname "$0")
-	$script_full_path/linux_runtime_dependencies.sh
+if [[ $@ != *"--norecurse"* ]]; then
+  echo "Installing runtime dependencies"
+  script_full_path=$(dirname "$0")
+  $script_full_path/linux_runtime_dependencies.sh
 fi
