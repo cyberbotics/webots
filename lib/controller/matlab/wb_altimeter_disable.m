012b7db9aeb90fa0c52e5ada9a006ad3f9b83946
sishamilton <69378550+sishamilton@users.noreply.github.com>
2021-06-25 13:13:20 -0700

    Remove Altimeter MATLAB API files

--- lib/controller/matlab/wb_altimeter_disable.m
+++
@@ -1,6 +0,0 @@
-function wb_altimeter_disable(tag)
-% Usage: wb_altimeter_disable(tag)
-% Matlab API for Webots
-% Online documentation is available <a href="https://www.cyberbotics.com/doc/reference/altimeter">here</a>
-
-calllib('libController', 'wb_altimeter_disable', tag);
