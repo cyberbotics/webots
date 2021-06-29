7d6f2c9511e0b0fe339ee674d4b5869676ecde17
Maël Wildi <mael18@windowslive.com>
2021-06-29 17:36:53 +0200

    Update range_finder_spherical_horizontal_vertical.c

--- tests/api/controllers/range_finder_spherical_horizontal_vertical/range_finder_spherical_horizontal_vertical.c
+++ tests/api/controllers/range_finder_spherical_horizontal_vertical/range_finder_spherical_horizontal_vertical.c
@@ -40,9 +40,9 @@ int main(int argc, char **argv) {
     }
     else if (!isinf(image_horizontal[i]) && isinf(image_vertical[i])) {
       if (i > 0 && isinf(image_horizontal[i - 1]))
-        neighbour_index = i - 1;
+        neighbour_index = - 1;
       else if (isinf(image_horizontal[i + 1]))
-        neighbour_index = i + 1;
+        neighbour_index = 1;
       ts_assert_double_in_delta(image_horizontal[i + neighbour_index], image_vertical[i], 0.05,
                                 "Horizontal and vertical range-finder do not return the same value (%lf (0,%d) != %lf (%d,0)).",
                                 image_horizontal[i + neighbour_index], i + neighbour_index, image_vertical[i], i);
