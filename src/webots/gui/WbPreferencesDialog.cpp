c39ebcff77f692baf1b386d1a1c51f2781d4e8a6
Olivier Michel <Olivier.Michel@cyberbotics.com>
2021-06-30 10:45:01 +0200

    Fixed typo

--- src/webots/gui/WbPreferencesDialog.cpp
+++ src/webots/gui/WbPreferencesDialog.cpp
@@ -429,7 +429,7 @@ QWidget *WbPreferencesDialog::createNetworkTab() {
   mCacheSize = new WbLineEdit(this);
   mCacheSize->setValidator(new QIntValidator(0, 65535));
   mCacheSize->setText(WbPreferences::instance()->value("Network/cacheSize", 1024).toString());
-  layout->addWidget(new QLabel(tr("Set the of the cache (in MB):"), this), 0, 0);
+  layout->addWidget(new QLabel(tr("Set the size of the cache (in MB):"), this), 0, 0);
   layout->addWidget(mCacheSize, 0, 1);
 
   // row 1
