841ad1c1416fcbbead1342fa28bdeea49e957c6d
Darko Lukic <lukicdarkoo@gmail.com>
2021-06-30 12:55:14 +0200

    Fix

--- src/webots/control/WbControlledWorld.cpp
+++ src/webots/control/WbControlledWorld.cpp
@@ -445,13 +445,10 @@ void WbControlledWorld::updateRobotController(WbRobot *robot) {
 
   mRobotsWaitingExternController.removeAll(robot);
 
-  // There should not be any controller for `robot` in `mWaitingControllers` or `mNewControllers`
+  // There should not be any controller for `robot` in `mWaitingControllers`
   for (WbController *controller : mWaitingControllers)
     if (controller->robotId() == robotID && !mControllers.contains(controller))
       mWaitingControllers.removeOne(controller);
-  for (WbController *controller : mNewControllers)
-    if (controller->robotId() == robotID && !mControllers.contains(controller))
-      mNewControllers.removeOne(controller);
 
   // restart the controller if needed
   for (int i = 0; i < size; ++i) {
@@ -460,6 +457,8 @@ void WbControlledWorld::updateRobotController(WbRobot *robot) {
       controller->flushBuffers();
       disconnect(controller, &WbController::hasTerminatedByItself, this,
                  &WbControlledWorld::deleteController);  // avoids double delete
+      mNewControllers.removeOne(controller);
+      mWaitingControllers.removeOne(controller);
       mControllers.removeOne(controller);
       if (newControllerName.isEmpty() || newControllerName == "<extern>") {
         if (controller->name() == "<extern>") {
