25114aaa56e061e2df4f14a74e44c1d57bfe5459
sean85914 <sean19960914@gmail.com>
2021-07-09 18:19:28 +0800

    Fix to follow PEP8

--- projects/robots/universal_robots/resources/ros_package/ur_e_webots/src/ur_e_webots/trajectory_follower.py
+++ projects/robots/universal_robots/resources/ros_package/ur_e_webots/src/ur_e_webots/trajectory_follower.py
@@ -199,8 +199,8 @@ class TrajectoryFollower(object):
     def update(self):
         if self.robot and self.trajectory:
             now = self.robot.getTime()
-            if (now - self.trajectory_t0) <= self.trajectory.points[-1].time_from_start.to_sec() and
-                self.goal_handle: # check if goal is still accepted
+            if (now - self.trajectory_t0) <= self.trajectory.points[-1].time_from_start.to_sec() and \
+               self.goal_handle:  # check if goal is still accepted
                 # Sending intermediate points
                 setpoint = sample_trajectory(self.trajectory, now - self.trajectory_t0)
                 for i in range(len(setpoint.positions)):
