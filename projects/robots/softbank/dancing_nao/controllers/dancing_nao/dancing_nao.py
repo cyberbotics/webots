806d36669f2ef8ca02c63a3b68baf2402f9e7fea
Milindi-Kodikara <milindikodikara@gmail.com>
2021-08-28 14:35:24 +1000

    Add dancing nao code from demo

---
+++ projects/robots/softbank/dancing_nao/controllers/dancing_nao/dancing_nao.py
@@ -0,0 +1,227 @@
+# Copyright 1996-2021 Cyberbotics Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Example of Python controller for Nao robot.
+   This demonstrates how to access sensors and actuators"""
+
+from controller import Robot, Keyboard, Motion
+
+
+class Nao (Robot):
+    PHALANX_MAX = 8
+
+    # load motion files
+    def loadMotionFiles(self):
+        self.handWave = Motion('../../motions/HandWave.motion')
+        self.forwards = Motion('../../motions/Forwards50.motion')
+        self.backwards = Motion('../../motions/Backwards.motion')
+        self.sideStepLeft = Motion('../../motions/SideStepLeft.motion')
+        self.sideStepRight = Motion('../../motions/SideStepRight.motion')
+        self.turnLeft60 = Motion('../../motions/TurnLeft60.motion')
+        self.turnRight60 = Motion('../../motions/TurnRight60.motion')
+
+    def startMotion(self, motion):
+        # interrupt current motion
+        if self.currentlyPlaying:
+            self.currentlyPlaying.stop()
+
+        # start new motion
+        motion.play()
+        self.currentlyPlaying = motion
+
+
+    def setAllLedsColor(self, rgb):
+        # these leds take RGB values
+        for i in range(0, len(self.leds)):
+            self.leds[i].set(rgb)
+
+        # ear leds are single color (blue)
+        # and take values between 0 - 255
+        self.leds[5].set(rgb & 0xFF)
+        self.leds[6].set(rgb & 0xFF)
+
+    def setHandsAngle(self, angle):
+        for i in range(0, self.PHALANX_MAX):
+            clampedAngle = angle
+            if clampedAngle > self.maxPhalanxMotorPosition[i]:
+                clampedAngle = self.maxPhalanxMotorPosition[i]
+            elif clampedAngle < self.minPhalanxMotorPosition[i]:
+                clampedAngle = self.minPhalanxMotorPosition[i]
+
+            if len(self.rphalanx) > i and self.rphalanx[i] is not None:
+                self.rphalanx[i].setPosition(clampedAngle)
+            if len(self.lphalanx) > i and self.lphalanx[i] is not None:
+                self.lphalanx[i].setPosition(clampedAngle)
+
+    
+    def findAndEnableDevices(self):
+        # get the time step of the current world.
+        self.timeStep = int(self.getBasicTimeStep())
+
+        # camera
+        self.cameraTop = self.getDevice("CameraTop")
+        self.cameraBottom = self.getDevice("CameraBottom")
+        self.cameraTop.enable(4 * self.timeStep)
+        self.cameraBottom.enable(4 * self.timeStep)
+
+        # accelerometer
+        self.accelerometer = self.getDevice('accelerometer')
+        self.accelerometer.enable(4 * self.timeStep)
+
+        # gyro
+        self.gyro = self.getDevice('gyro')
+        self.gyro.enable(4 * self.timeStep)
+
+        # gps
+        self.gps = self.getDevice('gps')
+        self.gps.enable(4 * self.timeStep)
+
+        # inertial unit
+        self.inertialUnit = self.getDevice('inertial unit')
+        self.inertialUnit.enable(self.timeStep)
+
+        # ultrasound sensors
+        self.us = []
+        usNames = ['Sonar/Left', 'Sonar/Right']
+        for i in range(0, len(usNames)):
+            self.us.append(self.getDevice(usNames[i]))
+            self.us[i].enable(self.timeStep)
+
+        # foot sensors
+        self.fsr = []
+        fsrNames = ['LFsr', 'RFsr']
+        for i in range(0, len(fsrNames)):
+            self.fsr.append(self.getDevice(fsrNames[i]))
+            self.fsr[i].enable(self.timeStep)
+
+        # foot bumpers
+        self.lfootlbumper = self.getDevice('LFoot/Bumper/Left')
+        self.lfootrbumper = self.getDevice('LFoot/Bumper/Right')
+        self.rfootlbumper = self.getDevice('RFoot/Bumper/Left')
+        self.rfootrbumper = self.getDevice('RFoot/Bumper/Right')
+        self.lfootlbumper.enable(self.timeStep)
+        self.lfootrbumper.enable(self.timeStep)
+        self.rfootlbumper.enable(self.timeStep)
+        self.rfootrbumper.enable(self.timeStep)
+
+        # there are 7 controlable LED groups in Webots
+        self.leds = []
+        self.leds.append(self.getDevice('ChestBoard/Led'))
+        self.leds.append(self.getDevice('RFoot/Led'))
+        self.leds.append(self.getDevice('LFoot/Led'))
+        self.leds.append(self.getDevice('Face/Led/Right'))
+        self.leds.append(self.getDevice('Face/Led/Left'))
+        self.leds.append(self.getDevice('Ears/Led/Right'))
+        self.leds.append(self.getDevice('Ears/Led/Left'))
+
+        # get phalanx motor tags
+        # the real Nao has only 2 motors for RHand/LHand
+        # but in Webots we must implement RHand/LHand with 2x8 motors
+        self.lphalanx = []
+        self.rphalanx = []
+        self.maxPhalanxMotorPosition = []
+        self.minPhalanxMotorPosition = []
+        for i in range(0, self.PHALANX_MAX):
+            self.lphalanx.append(self.getDevice("LPhalanx%d" % (i + 1)))
+            self.rphalanx.append(self.getDevice("RPhalanx%d" % (i + 1)))
+
+            # assume right and left hands have the same motor position bounds
+            self.maxPhalanxMotorPosition.append(self.rphalanx[i].getMaxPosition())
+            self.minPhalanxMotorPosition.append(self.rphalanx[i].getMinPosition())
+
+        # shoulder pitch motors
+        self.RShoulderPitch = self.getDevice("RShoulderPitch")
+        self.LShoulderPitch = self.getDevice("LShoulderPitch")
+
+        # keyboard
+        self.keyboard = self.getKeyboard()
+        self.keyboard.enable(10 * self.timeStep)
+
+    def __init__(self):
+        Robot.__init__(self)
+        self.currentlyPlaying = False
+
+        # initialize stuff
+        self.findAndEnableDevices()
+        self.loadMotionFiles()
+
+    def run(self):
+        self.handWave.setLoop(True)
+        self.handWave.play()
+
+        # until a key is pressed
+        key = -1
+        while robot.step(self.timeStep) != -1:
+            key = self.keyboard.getKey()
+            if key > 0:
+                break
+
+        self.handWave.setLoop(False)
+
+        while True:
+            key = self.keyboard.getKey()
+
+            if key == Keyboard.LEFT:
+                self.startMotion(self.sideStepLeft)
+            elif key == Keyboard.RIGHT:
+                self.startMotion(self.sideStepRight)
+            elif key == Keyboard.UP:
+                self.startMotion(self.forwards)
+            elif key == Keyboard.DOWN:
+                self.startMotion(self.backwards)
+            elif key == Keyboard.LEFT | Keyboard.SHIFT:
+                self.startMotion(self.turnLeft60)
+            elif key == Keyboard.RIGHT | Keyboard.SHIFT:
+                self.startMotion(self.turnRight60)
+            elif key == ord('A'):
+                self.printAcceleration()
+            elif key == ord('G'):
+                self.printGyro()
+            elif key == ord('S'):
+                self.printGps()
+            elif key == ord('I'):
+                self.printInertialUnit()
+            elif key == ord('F'):
+                self.printFootSensors()
+            elif key == ord('B'):
+                self.printFootBumpers()
+            elif key == ord('U'):
+                self.printUltrasoundSensors()
+            elif key == Keyboard.HOME:
+                self.printCameraImage(self.cameraTop)
+            elif key == Keyboard.END:
+                self.printCameraImage(self.cameraBottom)
+            elif key == Keyboard.PAGEUP:
+                self.setHandsAngle(0.96)
+            elif key == Keyboard.PAGEDOWN:
+                self.setHandsAngle(0.0)
+            elif key == ord('7'):
+                self.setAllLedsColor(0xff0000)  # red
+            elif key == ord('8'):
+                self.setAllLedsColor(0x00ff00)  # green
+            elif key == ord('9'):
+                self.setAllLedsColor(0x0000ff)  # blue
+            elif key == ord('0'):
+                self.setAllLedsColor(0x000000)  # off
+            elif key == ord('H'):
+                self.printHelp()
+
+            if robot.step(self.timeStep) == -1:
+                break
+
+
+# create the Robot instance and run main loop
+robot = Nao()
+robot.run()
+
