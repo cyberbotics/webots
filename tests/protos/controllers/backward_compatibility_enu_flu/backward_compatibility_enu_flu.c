f688cf16efc766b131082f15deb5b3d40965f4d3
Darko Lukic <lukicdarkoo@gmail.com>
2021-09-14 18:22:16 +0200

    Tests

--- tests/protos/controllers/backward_compatibility_enu_flu/backward_compatibility_enu_flu.c
+++ tests/protos/controllers/backward_compatibility_enu_flu/backward_compatibility_enu_flu.c
@@ -10,9 +10,14 @@ int main(int argc, char **argv) {
   ts_setup(argv[0]);  // give the controller args
 
   WbNodeRef old_proto_node = wb_supervisor_node_get_from_def("OLD_PROTO");
-  WbFieldRef old_proto_rotation_field = wb_supervisor_node_get_field(old_proto_node, "rotation");
+  WbNodeRef camera_node = wb_supervisor_node_get_from_proto_def(old_proto_node, "CAMERA_DEVICE");
+  WbFieldRef camera_rotation_field = wb_supervisor_node_get_field(camera_node, "rotation");
 
   wb_robot_step(TIME_STEP);
+
+  const double *rotation = wb_supervisor_field_get_sf_rotation(camera_rotation_field);
+  double rotation_expected[] = {0.0, 0.0, 0.0, 0.0};
+  ts_assert_doubles_in_delta(4, rotation, rotation_expected, 0.1, "The Camera node should be rotated.");
 
   ts_send_success();
   return EXIT_SUCCESS;
