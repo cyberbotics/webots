9cf9154c6d8f361a88105c5dc56000dc64f1acd3
Stefania Pedrazzi <stefania.pedrazzi@cyberbotics.com>
2021-09-15 10:15:48 +0200

    Fix clang-format issues

--- tests/api/controllers/skin/skin.c
+++ tests/api/controllers/skin/skin.c
@@ -27,7 +27,6 @@ int main(int argc, char **argv) {
   const int bones_count = wb_skin_get_bone_count(graphical_skin);
   ts_assert_int_equal(bones_count, 31, "Wrong number of bones in Anthony FBX skeleton.");
 
-
   wb_supervisor_export_image("../../../../distribution/skin.jpg", 100);
 
   int spine_bone_index = 7;
