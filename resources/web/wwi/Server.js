2af78ec88c956b8c65d04e12cf7fb488b2456e22
Ayk Borstelmann <ayk.borstelmann@iais.fraunhofer.de>
2021-10-04 14:43:20 +0200

    fixup! Add error message on connection timeout

--- resources/web/wwi/Server.js
+++ resources/web/wwi/Server.js
@@ -29,7 +29,7 @@ export default class Server {
       .then(response => response.text())
       .then(function(data) {
         if (data.startsWith('Error:')) {
-          document.getElementById('webotsProgress').style.display = 'none';
+          self.onError();
           let errorMessage = data.substring(6).trim();
           errorMessage = errorMessage.charAt(0).toUpperCase() + errorMessage.substring(1);
           alert('Session server error: ' + errorMessage);
@@ -50,10 +50,15 @@ export default class Server {
         };
       })
       .catch(error => {
-        console.error(error);
-        document.getElementById('webotsProgress').style.display = 'none';
+        this.onError()
         alert('Could not connect to session server');
+        console.error(error);
       });
+  }
+
+  onError() {
+    document.getElementById('webotsProgress').style.display = 'none';
+    this._view.onclose()
   }
 
   onOpen(event) {
