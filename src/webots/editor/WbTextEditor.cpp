0469058f8786ceb17092fdcb956571f46be111dc
Olivier Michel <Olivier.Michel@cyberbotics.com>
2021-10-06 18:20:35 +0200

    reverted again

--- src/webots/editor/WbTextEditor.cpp
+++ src/webots/editor/WbTextEditor.cpp
@@ -236,6 +236,7 @@ void WbTextEditor::tabChanged(int tab) {
     connect(mCurrentBuffer, &WbTextBuffer::modificationChanged, this, &WbTextEditor::modificationChanged);
     connect(mCurrentBuffer, &WbTextBuffer::focusIn, this, &WbTextEditor::updateEditMenu);
   }
+  // cppcheck-suppress virtualCallInConstructor
   updateGui();
 }
 
@@ -298,6 +299,7 @@ void WbTextEditor::selectTab(int tab) {
   if (tab < 0 || tab >= mTabWidget->count())
     return;
   mTabWidget->setCurrentIndex(tab);
+  // cppcheck-suppress virtualCallInConstructor
   updateGui();
 }
 
