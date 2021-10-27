85343cc2105b0b9e1b752708ffc68c8de711e52f
Olivier Michel <Olivier.Michel@cyberbotics.com>
2021-10-27 12:55:53 +0200

    split module into multiple files

---
+++ lib/controller/python/webots/motor.py
@@ -0,0 +1,48 @@
+# Copyright 1996-2021 Cyberbotics Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import ctypes
+from webots.wb import wb
+from webots.constants import constant
+
+wb.wb_motor_get_target_position.restype = ctypes.c_double
+wb.wb_motor_get_velocity.restype = ctypes.c_double
+
+
+class Motor:
+    ROTATIONAL = constant('ROTATIONAL')
+    LINEAR = constant('LINEAR')
+
+    def __init__(self, name: str):
+        self._ref = wb.wb_robot_get_device(str.encode(name))
+
+    @property
+    def targetPosition(self) -> float:
+        return wb.wb_motor_get_target_position(self._ref)
+
+    @targetPosition.setter
+    def targetPosition(self, p: float):
+        wb.wb_motor_set_position(self._ref, ctypes.c_double(p))
+
+    @property
+    def targetVelocity(self) -> float:
+        return wb.wb_motor_get_velocity(self._ref)
+
+    @targetVelocity.setter
+    def targetVelocity(self, v: float):
+        wb.wb_motor_set_velocity(self._ref, ctypes.c_double(v))
+
+    @property
+    def type(self) -> int:
+        return wb.wb_motor_get_type(self._ref)
---
+++ lib/controller/python/webots/sensor.py
@@ -0,0 +1,32 @@
+# Copyright 1996-2021 Cyberbotics Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import typing
+from webots.wb import wb
+
+
+class Sensor:
+    def __init__(self, name: str, samplingPeriod: int = None):
+        self._ref = wb.wb_robot_get_device(str.encode(name))
+        self.samplingPeriod = int(wb.wb_robot_get_basic_time_step()) if samplingPeriod is None else samplingPeriod
+
+    @property
+    def samplingPeriod(self) -> int:
+        return self._get_sampling_period(self._ref)
+
+    @samplingPeriod.setter
+    def samplingPeriod(self, p: typing.Union[int, None]):
+        if p is None:
+            p = 0
+        self._enable(self._ref, p)
