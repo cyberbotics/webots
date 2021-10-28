4e651ab469bba40c253c4bac21c8cd5d027a3d1d
Olivier Michel <Olivier.Michel@cyberbotics.com>
2021-10-28 12:23:27 +0200

    Fixed missing emitter argtypes in Emitter class

--- lib/controller/python/webots/emitter.py
+++ lib/controller/python/webots/emitter.py
@@ -17,12 +17,12 @@ import sys
 import typing
 from webots.wb import wb
 
-wb.wb_emitter_send.argtypes = [ctypes.c_int, ctypes.c_char_p, ctypes.c_int]
-
 
 class Emitter:
     def __init__(self, name: str):
         self._tag = wb.wb_robot_get_device(str.encode(name))
+
+    wb.wb_emitter_send.argtypes = [ctypes.c_int, ctypes.c_char_p, ctypes.c_int]
 
     def send(self, message: typing.Union[str, bytes], length: int = None):
         if isinstance(message, str):
