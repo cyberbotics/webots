eec0b1765282321dd675de1a6910979599936e7f
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2021-11-09 09:01:52 +0100

    restore radar.c

--- projects/samples/devices/controllers/radar/radar.c
+++ projects/samples/devices/controllers/radar/radar.c
@@ -31,7 +31,7 @@
 #define TIME_STEP 64
 
 int main() {
-  WbDeviceTag ds0, ds1, radar;
+  WbDeviceTag ds0, ds1, radar = 0, left_motor, right_motor;
   int left_speed, right_speed, i;
 
   wb_robot_init();
