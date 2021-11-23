d9596d754fe3c27868e5a0126435a5e8ed553604
ad-daniel <44834743+ad-daniel@users.noreply.github.com>
2021-11-23 14:53:09 +0100

    ENU: convert worlds with roads (#3768)
    * first commit
    
    * Begin of humanoid_marathon
    
    * convert sumo world
    
    * Fix pillar
    
    * convert highways
    
    * clean + convert boomer
    
    * convert highway
    
    * convert cities + fix forest
    
    * fix crossroad
    
    * fix forest
    
    * fix forest + convert village
    
    * convert villages winter+realistic
    
    * convert village center + fix stripBuilding
    
    * fix forests + add script to convert them
    
    * fix fastFoodRestaurant
    
    * convert CH_Vens
    
    * convert ch_morges
    
    * convert humanoid marathon
    
    * fix robotis proto translation
    
    * fix humanoid marathon
    
    * clean + convert forst highway
    
    * swap y/z + protos fixes
    
    * fix forest
    
    * clean+ sumo update
    
    * convert sumo exporter
    
    * convert vehicle sensors
    
    * fix ibeoLux proto
    
    * pep8 compliance
    
    * fix highway_overtake
    
    * convert highway overtake
    
    * convert city net
    
    * convert city night
    
    * convert city traffic net
    
    * convert village net
    
    * convert village center net
    
    * convert village realistic net
    
    * fix sumo_interface_example traffic lights
    
    * convert radar target tracker controller
    
    * convert highway controller
    
    * highway
    
    * restore radar.c
    
    * fix robotbenchmark/highway_driving
    
    * fix humanoid_marathon.wbt
    
    * correct CH_Morges
    
    * fix buildings in CH_Morges
    
    * fix highway_overtake
    
    * fix building and route village_realistic
    
    * fix village_center route
    
    * convert webots vehicle
    
    * compliance
    
    * delete edges and nodes.
    
    * replace trip by flow for highway
    
    * Apply suggestions from code review
    
    Co-authored-by: Olivier Michel <Olivier.Michel@cyberbotics.com>
    
    * remove useless crossroad on highway.wbt
    
    * remove crossroads shape
    
    * correct roll and pitch
    
    * fix truck and tank simple
    
    * fixes from daniel reviews
    
    * add edge and node for sumo example
    
    * small fix ch_morges
    
    * fixes village_center
    
    * fix strange behaviour vehicle
    
    * fix turn and changeline SUMO
    
    * fix darwin
    
    * fix sumo center of rotation
    
    * pep8
    
    * Update sumo (#3876)
    
    * update makefile
    
    * adapt sumo webots for 1.10.0
    
    * add windows md5
    
    * add linux md5
    
    * add mac md5
    
    * update docs
    
    * update docs
    
    * add SUMO dependencies
    
    * update tarball installation
    
    * add SUMO dependencies to linux compilation
    
    * update Makefile for ubuntu18.04
    
    * fix change lane function
    
    * fix default translation
    
    * Update docs/automobile/sumo-exporter.md
    
    Co-authored-by: Olivier Michel <Olivier.Michel@cyberbotics.com>
    
    * Update projects/vehicles/protos/generic/TruckTrailerSimple.proto
    
    Co-authored-by: Olivier Michel <Olivier.Michel@cyberbotics.com>
    
    * Update projects/objects/road/protos/RoadPillars.proto
    
    Co-authored-by: Olivier Michel <Olivier.Michel@cyberbotics.com>
    
    * fix car_node + comment
    
    Co-authored-by: joachimhgg <joachim.honegger@gmail.com>
    Co-authored-by: Benjamin Deleze <benjamin.deleze@cyberbotics.com>
    Co-authored-by: Benjamin Délèze <benjamin.deleze@cyberbotics.ch>
    Co-authored-by: joachimhgg <48200998+joachimhgg@users.noreply.github.com>
    Co-authored-by: Olivier Michel <Olivier.Michel@cyberbotics.com>

--- resources/sumo_exporter/crossroad.py
+++ resources/sumo_exporter/crossroad.py
@@ -37,7 +37,7 @@ class Crossroad(object):
             self.rotation = [float(x) for x in re.findall(r'rotation\s*(%s\s*%s\s*%s\s*%s)' %
                                                           (floatRE, floatRE, floatRE, floatRE), wbtString)[0].split()]
         except:
-            self.rotation = [0.0, 1.0, 0.0, 0.0]
+            self.rotation = [0.0, 0.0, 1.0, 0.0]
         try:
             self.connectedRoadIDs = [x.replace('"', '') for x in re.findall(r'connectedRoadIDs\s*\[([^\]]*)\]',
                                                                             wbtString)[0].split()]
@@ -46,6 +46,13 @@ class Crossroad(object):
         if self.crossroadType == "Crossroad":
             try:
                 self.shape = grouper(3, [float(x) for x in re.findall(r'shape\s*\[([^\]]*)\]', wbtString)[0].split()])
+                correction_angle = -math.pi * 0.5
+                for i in range(len(self.shape)):
+                    shape = self.shape[i]
+                    x = - math.cos(correction_angle) * shape[0] + math.sin(correction_angle) * shape[1]
+                    y = math.cos(correction_angle) * shape[1] + math.sin(correction_angle) * shape[0]
+                    z = shape[2]
+                    self.shape[i] = [x, y, z]
             except:
                 pass
         elif self.crossroadType == "RoadIntersection":
@@ -61,25 +68,25 @@ class Crossroad(object):
             except:
                 roadsWidth = 7.0
             outerRadius = roadsWidth / (2 * math.sin(math.pi / roadNumber))
-            angle = self.rotation[3]
-            if self.rotation[1] > 0:
+            angle = -self.rotation[3]
+            if self.rotation[2] > 0:
                 angle = -angle
             for i in range(roadNumber):
                 x1 = outerRadius * math.cos(2 * math.pi * i / roadNumber)
                 y1 = outerRadius * math.sin(2 * math.pi * i / roadNumber)
                 x2 = math.cos(angle) * x1 - math.sin(angle) * y1
                 y2 = math.cos(angle) * y1 + math.sin(angle) * x1
-                self.shape.append([x2, 0, y2])
+                self.shape.append([x2, y2, 0])
 
     def create_node(self, nodes):
         """Populate the SUMO XML node."""
         node = ET.SubElement(nodes, 'node')
         node.attrib['id'] = self.id
-        node.attrib['x'] = str(- self.translation[0])
-        node.attrib['y'] = str(self.translation[2])
+        node.attrib['x'] = str(self.translation[0])
+        node.attrib['y'] = str(self.translation[1])
         if self.shape:
             shape = ""
             for wayPoint in self.shape:
-                shape += "%f,%f " % (- wayPoint[0] - self.translation[0], wayPoint[2] + self.translation[2])
-            shape += "%f,%f" % (- self.shape[0][0] - self.translation[0], self.shape[0][2] + self.translation[2])
+                shape += "%f,%f " % (wayPoint[0] + self.translation[0], wayPoint[1] + self.translation[1])
+            shape += "%f,%f" % (self.shape[0][0] + self.translation[0], self.shape[0][1] + self.translation[1])
             node.attrib['shape'] = shape
