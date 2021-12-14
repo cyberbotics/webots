5aae6e27a3f736fd4e47e23f76bff7882ab4c9e6
joachimhgg <joachim.honegger@gmail.com>
2021-12-14 10:36:56 +0100

    move PR

--- src/webots/gui/WbGuiApplication.cpp
+++ src/webots/gui/WbGuiApplication.cpp
@@ -541,16 +541,10 @@ void WbGuiApplication::loadInitialWorld() {
 }
 
 void WbGuiApplication::udpateStyleSheet() {
-QString themeToLoad;
-#ifdef __linux__
-  themeToLoad = WbPreferences::instance()->value("General/theme", "webots_night.qss").toString();
-#else
-  themeToLoad = WbPreferences::instance()->value("General/theme", "webots_classic.qss").toString();
-#endif
-
-QFile qssFile(WbStandardPaths::resourcesPath() + themeToLoad);
-qssFile.open(QFile::ReadOnly);
-QString styleSheet = QString::fromUtf8(qssFile.readAll());
+  QString themeToLoad = WbPreferences::instance()->value("General/theme", "webots_classic.qss").toString();
+  QFile qssFile(WbStandardPaths::resourcesPath() + themeToLoad);
+  qssFile.open(QFile::ReadOnly);
+  QString styleSheet = QString::fromUtf8(qssFile.readAll());
 
 #ifdef __APPLE__
   QFile macOSQssFile(WbStandardPaths::resourcesPath() + "stylesheet.macos.qss");
