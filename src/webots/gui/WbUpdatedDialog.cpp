debf4e50aeaf4ceb8d07b5d1f6f51535f2b9807f
joachimhgg <joachim.honegger@gmail.com>
2021-12-14 14:53:46 +0100

    update text from reviews

--- src/webots/gui/WbUpdatedDialog.cpp
+++ src/webots/gui/WbUpdatedDialog.cpp
@@ -24,35 +24,36 @@ WbUpdatedDialog::WbUpdatedDialog(QWidget *parent) : QDialog(parent) {
   mImage->setScaledContents(true);
 
   mLabelThanks = new QLabel(this);
-  mLabelThanks->setGeometry(QRect(70, 30, 330, 31));
-  mLabelThanks->setText(tr("<b>Thank you for updating to the last version of Webots.</b>"));
+  mLabelThanks->setGeometry(QRect(75, 30, 330, 31));
+  mLabelThanks->setText(tr("<b>Thank you for updating to Webots R2022a.</b>"));
+  mLabelThanks->setStyleSheet("font-size: 15px;");
   mLabelThanks->setAlignment(Qt::AlignLeading | Qt::AlignLeft | Qt::AlignTop);
   mLabelThanks->setWordWrap(true);
 
   mGroupBox = new QGroupBox(this);
   mGroupBox->setGeometry(QRect(35, 65, 365, 230));
-  mGroupBox->setTitle(tr("Important Note for previous users"));
+  mGroupBox->setTitle(tr("Important Note for Users of Previous Versions"));
   mGroupBox->setStyleSheet("QGroupBox {border: 1px solid gray;border-radius: 9px;margin-top: 0.5em; } "
                            "QGroupBox::title {subcontrol-origin:  margin; subcontrol-position: top center; }");
 
   mLabelNote = new QLabel(mGroupBox);
-  mLabelNote->setGeometry(QRect(15, 20, 345, 200));
+  mLabelNote->setGeometry(QRect(15, 20, 335, 200));
   mLabelNote->setText(
     tr("<p style=\"line-height:1.2\"> <br />"
-       "Webots axis systems and objects orientation conventions respectively <b>changed to ENU and FLU</b>. All the PROTOs and "
-       "worlds included in the Webots package are now in ENU and FLU. <br /> <br /> <b>If you are using your own world or "
-       "PROTO</b>, "
-       "Webots will "
-       "rotate all the elements of your world when you open it in this new version. However, you may have to rotate some "
-       "objects, fix bounding objects by yourself or adjust your controller. Refer to the <a style='color: #5DADE2;' "
-       "href='https://cyberbotics.com/doc/reference/changelog'>wiki</a> for more information. </p>"));
+       "All Webots worlds are now in the ENU axis system and all Webots devices, geometries, and PROTOs are in the FLU axis "
+       "system. <br /> <br /> "
+       "If your world or PROTO includes devices, geometries, or Webots PROTOs, "
+       "then Webots will try to preserve the old behavior by rotating them. However, you may have to rotate some objects, "
+       "fix bounding objects or adjust your controller by yourself. <br />Refer to the < a style = 'color: #5DADE2;' "
+       "href='https://github.com/cyberbotics/webots/wiki/"
+       "FLU-and-ENU-conversion-guide'>wiki</a> for more information. </p>"));
   mLabelNote->setOpenExternalLinks(true);
   mLabelNote->setAlignment(Qt::AlignLeading | Qt::AlignLeft | Qt::AlignTop);
   mLabelNote->setStyleSheet("border: none");
   mLabelNote->setWordWrap(true);
 
   mLabelChangelog = new QLabel(this);
-  mLabelChangelog->setGeometry(QRect(35, 300, 290, 41));
+  mLabelChangelog->setGeometry(QRect(35, 305, 290, 41));
   mLabelChangelog->setText(
     tr("Find out the new features, enhancements and bug fixes of Webots R2022a in the <a style='color: #5DADE2;' "
        "href='https://cyberbotics.com/doc/reference/changelog-r2022'>changelog</a>."));
