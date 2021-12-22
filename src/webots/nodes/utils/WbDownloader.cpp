acef97c7e30d7b9976bf07fee395633be3a0aebb
Olivier Michel <Olivier.Michel@cyberbotics.com>
2021-12-22 14:10:50 +0100

    Update WbDownloader.cpp

--- src/webots/nodes/utils/WbDownloader.cpp
+++ src/webots/nodes/utils/WbDownloader.cpp
@@ -100,7 +100,7 @@ void WbDownloader::download(const QUrl &url) {
   else
     request.setAttribute(QNetworkRequest::CacheLoadControlAttribute, QNetworkRequest::PreferCache);
 
-  assert(mNetworkReply);
+  assert(mNetworkReply == NULL);
   mNetworkReply = WbNetwork::instance()->networkAccessManager()->get(request);
   connect(mNetworkReply, &QNetworkReply::finished, this, &WbDownloader::finished, Qt::UniqueConnection);
   connect(WbApplication::instance(), &WbApplication::worldLoadingWasCanceled, mNetworkReply, &QNetworkReply::abort);
