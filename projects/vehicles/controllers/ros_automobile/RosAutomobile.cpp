18cde1639744158ae1ccab82152275dc8ad5adcb
Orhan G. Hafif <orhangazi4401@gmail.com>
2022-01-06 09:48:33 +0300

    Update clang-format of RosAutomobile.cpp

--- projects/vehicles/controllers/ros_automobile/RosAutomobile.cpp
+++ projects/vehicles/controllers/ros_automobile/RosAutomobile.cpp
@@ -107,37 +107,31 @@ void RosAutomobile::launchRos(int argc, char **argv) {
     nodeHandle()->advertiseService("automobile/get_gear_number", &RosAutomobile::getGearNumberCallback, this);
   mGetHazardFlashersServer =
     nodeHandle()->advertiseService("automobile/get_hazard_flashers", &RosAutomobile::getHazardFlashersCallback, this);
-  mGetIndicatorServer =
-    nodeHandle()->advertiseService("automobile/get_indicator", &RosAutomobile::getIndicatorCallback, this);
-  mGetIndicatorPeriodServer = nodeHandle()->advertiseService("automobile/get_indicator_period",
-                                                             &RosAutomobile::getIndicatorPeriodCallback, this);
+  mGetIndicatorServer = nodeHandle()->advertiseService("automobile/get_indicator", &RosAutomobile::getIndicatorCallback, this);
+  mGetIndicatorPeriodServer =
+    nodeHandle()->advertiseService("automobile/get_indicator_period", &RosAutomobile::getIndicatorPeriodCallback, this);
   mGetAutomobileTypeServer =
     nodeHandle()->advertiseService("automobile/get_type", &RosAutomobile::getAutomobileTypeCallback, this);
-  mGetWiperModeServer =
-    nodeHandle()->advertiseService("automobile/get_wiper_mode", &RosAutomobile::getWiperModeCallback, this);
+  mGetWiperModeServer = nodeHandle()->advertiseService("automobile/get_wiper_mode", &RosAutomobile::getWiperModeCallback, this);
   mGetWipersModeServer =
     nodeHandle()->advertiseService("automobile/get_wipers_mode", &RosAutomobile::getWiperModeCallback, this);
   mSetAntiFogLightServer =
     nodeHandle()->advertiseService("automobile/set_antifog_light", &RosAutomobile::setAntiFogLightCallback, this);
-  mSetBrakeServer =
-    nodeHandle()->advertiseService("automobile/set_brake_intensity", &RosAutomobile::setBrakeCallback, this);
+  mSetBrakeServer = nodeHandle()->advertiseService("automobile/set_brake_intensity", &RosAutomobile::setBrakeCallback, this);
   mSetCruisingSpeedServer =
     nodeHandle()->advertiseService("automobile/set_cruising_speed", &RosAutomobile::setCruisingSpeedCallback, this);
   mSetDippedBeamServer =
     nodeHandle()->advertiseService("automobile/set_dipped_beam", &RosAutomobile::setDippedBeamCallback, this);
   mSetGearServer = nodeHandle()->advertiseService("automobile/set_gear", &RosAutomobile::setGearCallback, this);
-  mSetIndicatorServer =
-    nodeHandle()->advertiseService("automobile/set_indicator", &RosAutomobile::setIndicatorCallback, this);
-  mSetIndicatorPeriodServer = nodeHandle()->advertiseService("automobile/set_indicator_period",
-                                                             &RosAutomobile::setIndicatorPeriodCallback, this);
+  mSetIndicatorServer = nodeHandle()->advertiseService("automobile/set_indicator", &RosAutomobile::setIndicatorCallback, this);
+  mSetIndicatorPeriodServer =
+    nodeHandle()->advertiseService("automobile/set_indicator_period", &RosAutomobile::setIndicatorPeriodCallback, this);
   mSetHazardFlashersServer =
     nodeHandle()->advertiseService("automobile/set_hazard_flashers", &RosAutomobile::setHazardFlashersCallback, this);
   mSetSteeringAngleServer =
     nodeHandle()->advertiseService("automobile/set_steering_angle", &RosAutomobile::setSteeringAngleCallback, this);
-  mSetThrottleServer =
-    nodeHandle()->advertiseService("automobile/set_throttle", &RosAutomobile::setThrottleCallback, this);
-  mSetWiperModeServer =
-    nodeHandle()->advertiseService("automobile/set_wiper_mode", &RosAutomobile::setWiperModeCallback, this);
+  mSetThrottleServer = nodeHandle()->advertiseService("automobile/set_throttle", &RosAutomobile::setThrottleCallback, this);
+  mSetWiperModeServer = nodeHandle()->advertiseService("automobile/set_wiper_mode", &RosAutomobile::setWiperModeCallback, this);
   mSetWipersModeServer =
     nodeHandle()->advertiseService("automobile/set_wipers_mode", &RosAutomobile::setWiperModeCallback, this);
 
@@ -146,27 +140,17 @@ void RosAutomobile::launchRos(int argc, char **argv) {
   mCurrentSpeedPublisher = nodeHandle()->advertise<webots_ros::Float64Stamped>("automobile/current_speed", 1);
   mRpmPublisher = nodeHandle()->advertise<webots_ros::Float64Stamped>("automobile/rpm", 1);
   mSteeringAnglePublisher = nodeHandle()->advertise<webots_ros::Float64Stamped>("automobile/steering_angle", 1);
-  mRightSteeringAnglePublisher =
-    nodeHandle()->advertise<webots_ros::Float64Stamped>("automobile/right_steering_angle", 1);
-  mLeftSteeringAnglePublisher =
-    nodeHandle()->advertise<webots_ros::Float64Stamped>("automobile/left_steering_angle", 1);
+  mRightSteeringAnglePublisher = nodeHandle()->advertise<webots_ros::Float64Stamped>("automobile/right_steering_angle", 1);
+  mLeftSteeringAnglePublisher = nodeHandle()->advertise<webots_ros::Float64Stamped>("automobile/left_steering_angle", 1);
   mThrottlePublisher = nodeHandle()->advertise<webots_ros::Float64Stamped>("automobile/throttle", 1);
-  mWheelEncoderPublisher[0] =
-    nodeHandle()->advertise<webots_ros::Float64Stamped>("automobile/front_right_wheel_encoder", 1);
-  mWheelEncoderPublisher[1] =
-    nodeHandle()->advertise<webots_ros::Float64Stamped>("automobile/front_left_wheel_encoder", 1);
-  mWheelEncoderPublisher[2] =
-    nodeHandle()->advertise<webots_ros::Float64Stamped>("automobile/rear_right_wheel_encoder", 1);
-  mWheelEncoderPublisher[3] =
-    nodeHandle()->advertise<webots_ros::Float64Stamped>("automobile/rear_left_wheel_encoder", 1);
-  mWheelSpeedPublisher[0] =
-    nodeHandle()->advertise<webots_ros::Float64Stamped>("automobile/front_right_wheel_speed", 1);
-  mWheelSpeedPublisher[1] =
-    nodeHandle()->advertise<webots_ros::Float64Stamped>("automobile/front_left_wheel_speed", 1);
-  mWheelSpeedPublisher[2] =
-    nodeHandle()->advertise<webots_ros::Float64Stamped>("automobile/rear_right_wheel_speed", 1);
-  mWheelSpeedPublisher[3] =
-    nodeHandle()->advertise<webots_ros::Float64Stamped>("automobile/rear_left_wheel_speed", 1);
+  mWheelEncoderPublisher[0] = nodeHandle()->advertise<webots_ros::Float64Stamped>("automobile/front_right_wheel_encoder", 1);
+  mWheelEncoderPublisher[1] = nodeHandle()->advertise<webots_ros::Float64Stamped>("automobile/front_left_wheel_encoder", 1);
+  mWheelEncoderPublisher[2] = nodeHandle()->advertise<webots_ros::Float64Stamped>("automobile/rear_right_wheel_encoder", 1);
+  mWheelEncoderPublisher[3] = nodeHandle()->advertise<webots_ros::Float64Stamped>("automobile/rear_left_wheel_encoder", 1);
+  mWheelSpeedPublisher[0] = nodeHandle()->advertise<webots_ros::Float64Stamped>("automobile/front_right_wheel_speed", 1);
+  mWheelSpeedPublisher[1] = nodeHandle()->advertise<webots_ros::Float64Stamped>("automobile/front_left_wheel_speed", 1);
+  mWheelSpeedPublisher[2] = nodeHandle()->advertise<webots_ros::Float64Stamped>("automobile/rear_right_wheel_speed", 1);
+  mWheelSpeedPublisher[3] = nodeHandle()->advertise<webots_ros::Float64Stamped>("automobile/rear_left_wheel_speed", 1);
 }
 
 void RosAutomobile::setRosDevices(const char **hiddenDevices, int numberHiddenDevices) {
