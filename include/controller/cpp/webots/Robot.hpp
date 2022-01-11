450005ca5ff97ca6f5b8c6163b41a6b69b9d43e1
Yannick Goumaz <yannick.goumaz@epfl.ch>
2022-01-11 11:54:00 +0100

    Update Robot.hpp

--- include/controller/cpp/webots/Robot.hpp
+++ include/controller/cpp/webots/Robot.hpp
@@ -68,6 +68,8 @@ namespace webots {
     virtual ~Robot();
 
     virtual int step(int duration);
+    virtual int step_begin(int duration);
+    virtual int step_end();
     UserInputEvent waitForUserInputEvent(UserInputEvent event_type, int timeout);
     std::string getName() const;
     std::string getUrdf(std::string prefix = "") const;
