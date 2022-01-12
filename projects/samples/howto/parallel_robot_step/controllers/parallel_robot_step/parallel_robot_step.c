d7169cab52a4e28b2be7f6d38f091dc4eea0e90c
Yannick Goumaz <yannick.goumaz@epfl.ch>
2022-01-12 12:35:37 +0100

    Update sample demo with image computation

--- projects/samples/howto/parallel_robot_step/controllers/parallel_robot_step/parallel_robot_step.c
+++ projects/samples/howto/parallel_robot_step/controllers/parallel_robot_step/parallel_robot_step.c
@@ -14,60 +14,51 @@
  * limitations under the License.
  */
 
+/*
+ * Description:  An example of the parallelization of the control loop with the Webots simulation step (main code taken from the
+ * /projects/samples/devices/controllers/camera example).
+ */
+
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-
-#include <webots/accelerometer.h>
 #include <webots/camera.h>
-#include <webots/distance_sensor.h>
-#include <webots/light_sensor.h>
 #include <webots/motor.h>
-#include <webots/position_sensor.h>
 #include <webots/robot.h>
+#include <webots/utils/system.h>
 
-#define WHEEL_RADIUS 0.02
-#define AXLE_LENGTH 0.052
-#define RANGE (1024 / 2)
+#define ANSI_COLOR_RED "\x1b[31m"
+#define ANSI_COLOR_GREEN "\x1b[32m"
+#define ANSI_COLOR_YELLOW "\x1b[33m"
+#define ANSI_COLOR_BLUE "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN "\x1b[36m"
+#define ANSI_COLOR_RESET "\x1b[0m"
 
-static void compute_odometry(WbDeviceTag left_position_sensor, WbDeviceTag right_position_sensor) {
-  double l = wb_position_sensor_get_value(left_position_sensor);
-  double r = wb_position_sensor_get_value(right_position_sensor);
-  double dl = l * WHEEL_RADIUS;         // distance covered by left wheel in meter
-  double dr = r * WHEEL_RADIUS;         // distance covered by right wheel in meter
-  double da = (dr - dl) / AXLE_LENGTH;  // delta orientation
-  printf("estimated distance covered by left wheel: %g m.\n", dl);
-  printf("estimated distance covered by right wheel: %g m.\n", dr);
-  printf("estimated change of orientation: %g rad.\n", da);
-}
+#define SPEED 4
+enum BLOB_TYPE { RED, GREEN, BLUE, NONE };
 
-int main(int argc, char *argv[]) {
-  /* define variables */
-  WbDeviceTag distance_sensor[8], left_motor, right_motor, left_position_sensor, right_position_sensor;
+int main() {
+  WbDeviceTag camera, left_motor, right_motor;
+  int width, height;
+  int pause_counter = 0;
+  int left_speed, right_speed;
   int i, j;
-  double speed[2];
-  double sensors_value[8];
-  double braitenberg_coefficients[8][2] = {{0.942, -0.22}, {0.63, -0.1}, {0.5, -0.06},  {-0.06, -0.06},
-                                           {-0.06, -0.06}, {-0.06, 0.5}, {-0.19, 0.63}, {-0.13, 0.942}};
-  int time_step;
-  int camera_time_step;
-  /* initialize Webots */
+  int red, blue, green;
+  const char *color_names[3] = {"red", "green", "blue"};
+  const char *ansi_colors[3] = {ANSI_COLOR_RED, ANSI_COLOR_GREEN, ANSI_COLOR_BLUE};
+  const char *filenames[3] = {"red_blob.png", "green_blob.png", "blue_blob.png"};
+  enum BLOB_TYPE current_blob;
+
   wb_robot_init();
 
-  if (strcmp(wb_robot_get_model(), "GCtronic e-puck2") == 0) {
-    printf("e-puck2 robot\n");
-    time_step = 64;
-    camera_time_step = 64;
-  } else {  // original e-puck
-    printf("e-puck robot\n");
-    time_step = 256;
-    camera_time_step = 1024;
-  }
+  const int time_step = wb_robot_get_basic_time_step();
 
-  /* get and enable the camera and accelerometer */
-  WbDeviceTag camera = wb_robot_get_device("camera");
-  wb_camera_enable(camera, camera_time_step);
-  WbDeviceTag accelerometer = wb_robot_get_device("accelerometer");
-  wb_accelerometer_enable(accelerometer, time_step);
+  /* Get the camera device, enable it, and store its width and height */
+  camera = wb_robot_get_device("camera");
+  wb_camera_enable(camera, time_step);
+  width = wb_camera_get_width(camera);
+  height = wb_camera_get_height(camera);
 
   /* get a handler to the motors and set target position to infinity (speed control). */
   left_motor = wb_robot_get_device("left wheel motor");
@@ -76,21 +67,6 @@ int main(int argc, char *argv[]) {
   wb_motor_set_position(right_motor, INFINITY);
   wb_motor_set_velocity(left_motor, 0.0);
   wb_motor_set_velocity(right_motor, 0.0);
-
-  /* get a handler to the position sensors and enable them. */
-  left_position_sensor = wb_robot_get_device("left wheel sensor");
-  right_position_sensor = wb_robot_get_device("right wheel sensor");
-  wb_position_sensor_enable(left_position_sensor, time_step);
-  wb_position_sensor_enable(right_position_sensor, time_step);
-
-  for (i = 0; i < 8; i++) {
-    char device_name[4];
-
-    /* get distance sensors */
-    sprintf(device_name, "ps%d", i);
-    distance_sensor[i] = wb_robot_get_device(device_name);
-    wb_distance_sensor_enable(distance_sensor[i], time_step);
-  }
 
   /* initial step to initialize sensor values */
   /* avoid NULL values on the first sensor reads */
@@ -104,23 +80,85 @@ int main(int argc, char *argv[]) {
     if (wb_robot_step_begin(time_step) == -1)
       break;
 
-    /* get sensors values */
-    for (i = 0; i < 8; i++)
-      sensors_value[i] = wb_distance_sensor_get_value(distance_sensor[i]);
-    const double *a = wb_accelerometer_get_values(accelerometer);
-    printf("accelerometer values = %0.2f %0.2f %0.2f\n", a[0], a[1], a[2]);
+    /* Get the new camera values */
+    const unsigned char *image = wb_camera_get_image(camera);
 
-    /* compute odometry and speed values*/
-    compute_odometry(left_position_sensor, right_position_sensor);
-    for (i = 0; i < 2; i++) {
-      speed[i] = 0.0;
-      for (j = 0; j < 8; j++)
-        speed[i] += braitenberg_coefficients[j][i] * (1.0 - (sensors_value[j] / RANGE));
+    /* Decrement the pause_counter */
+    if (pause_counter > 0)
+      pause_counter--;
+
+    /* Case 1 */
+    if (pause_counter > 640 / time_step) {
+      left_speed = 0;
+      right_speed = 0;
+    }
+    /* Case 2 */
+    else if (pause_counter > 0) {
+      left_speed = -SPEED;
+      right_speed = SPEED;
+    }
+    /* Case 3 */
+    else if (!image) {  // image may be NULL if Robot.synchronization is FALSE
+      left_speed = 0;
+      right_speed = 0;
+    } else {  // pause_counter == 0
+      /* Reset the sums */
+      red = 0;
+      green = 0;
+      blue = 0;
+
+      /* Intense computation on image executed in parallel with the Webots simulation step */
+      for (i = width / 3; i < 2 * width / 3; i++) {
+        for (j = height / 2; j < 3 * height / 4; j++) {
+          red += wb_camera_image_get_red(image, width, i, j);
+          blue += wb_camera_image_get_blue(image, width, i, j);
+          green += wb_camera_image_get_green(image, width, i, j);
+        }
+      }
+
+      /* Detect blob */
+      if ((red > 3 * green) && (red > 3 * blue))
+        current_blob = RED;
+      else if ((green > 3 * red) && (green > 3 * blue))
+        current_blob = GREEN;
+      else if ((blue > 3 * red) && (blue > 3 * green))
+        current_blob = BLUE;
+      else
+        current_blob = NONE;
+
+      /* Case 3a */
+      if (current_blob == NONE) {
+        left_speed = -SPEED;
+        right_speed = SPEED;
+      }
+      /* Case 3b */
+      else {
+        left_speed = 0;
+        right_speed = 0;
+        printf("Looks like I found a %s%s%s blob.\n", ansi_colors[current_blob], color_names[current_blob], ANSI_COLOR_RESET);
+        // compute the file path in the user directory
+        char *filepath;
+#ifdef _WIN32
+        const char *user_directory = wbu_system_short_path(wbu_system_getenv("USERPROFILE"));
+        filepath = (char *)malloc(strlen(user_directory) + 16);
+        strcpy(filepath, user_directory);
+        strcat(filepath, "\\");
+#else
+        const char *user_directory = wbu_system_getenv("HOME");
+        filepath = (char *)malloc(strlen(user_directory) + 16);
+        strcpy(filepath, user_directory);
+        strcat(filepath, "/");
+#endif
+        strcat(filepath, filenames[current_blob]);
+        wb_camera_save_image(camera, filepath, 100);
+        free(filepath);
+        pause_counter = 1280 / time_step;
+      }
     }
 
-    /* set speed values */
-    wb_motor_set_velocity(left_motor, speed[0]);
-    wb_motor_set_velocity(right_motor, speed[1]);
+    /* Set the motor speeds. */
+    wb_motor_set_velocity(left_motor, left_speed);
+    wb_motor_set_velocity(right_motor, right_speed);
 
     /* end simulation step computation: retrieve new sensor values from Webots */
     /* leave the loop when the simulation is over */
