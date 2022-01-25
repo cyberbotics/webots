5efb8fde34db5630a4c42f179480b0a1cbb9c24a
joachimhgg <joachim.honegger@gmail.com>
2022-01-25 16:04:25 +0100

    adapt test html robot window

--- tests/api/plugins/robot_windows/info/info.js
+++ tests/api/plugins/robot_windows/info/info.js
@@ -2,10 +2,13 @@ import RobotWindow from 'https://cyberbotics.com/wwi/R2022b/RobotWindow.js';
 
 window.robotWindow = new RobotWindow();
 
-window.robotWindow.receive = function(message, robot) {
-  if (message.startsWith('test')) {
-    document.querySelector('#text-display').innerHTML = message;
-    this.send('Answer: ' + message, robot);
-  } else
-    console.log("Received unknown message for robot '" + robot + "': '" + message + "'");
-};
+window.onload = function() {
+  window.robotWindow.receive = function(message, robot) {
+    if (message.startsWith('test')) {
+      document.querySelector('#text-display').innerHTML = message;
+      window.robotWindow.send('Answer: ' + message);
+    } else
+      console.log("Received unknown message for robot '" + robot + "': '" + message + "'");
+  };
+  window.robotWindow.send("configure");
+}
