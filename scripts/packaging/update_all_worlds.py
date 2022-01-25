d5fe812918f5a905d062e49ccfb8f14c720f15fa
joachimhgg <joachim.honegger@gmail.com>
2022-01-25 15:41:37 +0100

    add worlds test update worlds

--- scripts/packaging/update_all_worlds.py
+++ scripts/packaging/update_all_worlds.py
@@ -24,7 +24,7 @@ print("python update all worlds")
 worlds = []
 root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 for rootPath, dirNames, fileNames in os.walk(os.path.join(root, 'projects')):
-    for fileName in fnmatch.filter(fileNames, 'speaker*.wbt'):
+    for fileName in fnmatch.filter(fileNames, '*.wbt'):
         print("test")
         world = os.path.join(rootPath, fileName)
         worlds.append(world)
