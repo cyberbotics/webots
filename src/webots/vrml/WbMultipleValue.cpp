c8f13ef0c8c2612d699c7481762662c36136a776
ad-daniel <daniel.dias@epfl.ch>
2022-01-25 08:14:18 +0100

    Rename function

--- src/webots/vrml/WbMultipleValue.cpp
+++ src/webots/vrml/WbMultipleValue.cpp
@@ -56,7 +56,7 @@ QString WbMultipleValue::toString(WbPrecision::Level level) const {
 
 QString WbMultipleValue::itemToString(int index, WbPrecision::Level level) const {
   assert(index >= 0 && index < size());
-  QString value = variantValue(index).toStringRepresentation(level);
+  QString value = variantValue(index).toSimplifiedStringRepresentation(level);
   return type() == WB_MF_NODE ? value + "{}" : value;
 }
 
