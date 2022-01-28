489fbfd0050713f227e72b2f8457d4a07c90e513
Benjamin Hug <benjamin.hug1997@gmail.com>
2022-01-28 17:03:54 +0100

    fix-range-finder-max-range

--- tests/api/controllers/range_finder_checker/range_finder_checker.c
+++ tests/api/controllers/range_finder_checker/range_finder_checker.c
@@ -38,7 +38,7 @@ int main(int argc, char **argv) {
       {width / 2, height - 1}   // bottom line, central pixel
     };
     const double samples_expected_values[4] = {
-      wb_range_finder_get_max_range(range_finder),  // no collision
+      INFINITY,                                     // no collision
       2.0,                                          // collision with transparent object
       2.0,                                          // collision
       2.0                                           // collision with colored ElevationGrid
