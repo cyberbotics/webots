56fee54f54a484c8d4a8c5994e909d115d54df8a
ad-daniel <daniel.dias@epfl.ch>
2022-01-31 17:41:48 +0100

    Test

--- src/webots/wren/WbWrenCamera.cpp
+++ src/webots/wren/WbWrenCamera.cpp
@@ -387,7 +387,7 @@ void WbWrenCamera::setBackgroundColor(const WbRgb &color) {
   if (mIsColor)
     mBackgroundColor = color;
   else
-    mBackgroundColor = WbRgb(INFINITY, INFINITY, INFINITY);
+    mBackgroundColor = WbRgb(FLT_MAX, FLT_MAX, FLT_MAX);
 
   const float backgroundColor[] = {static_cast<float>(mBackgroundColor.red()), static_cast<float>(mBackgroundColor.green()),
                                    static_cast<float>(mBackgroundColor.blue())};
