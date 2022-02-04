85907a77c12a4bd625793d729ada57e03f0193f9
ad-daniel <daniel.dias@epfl.ch>
2022-02-04 09:23:53 +0100

    update script

--- tests/sources/test_line_ending.py
+++ tests/sources/test_line_ending.py
@@ -17,7 +17,17 @@
 """Test line ending"""
 import unittest
 import os
-import fnmatch
+
+SKIPPED_DIRECTORIES = [
+    'dependencies',
+    '.git',
+    'aseba'
+]
+
+EXTENSIONS_TO_CHECK = [
+    '.proto', '.wbt', '.wbo', '.forest', '.txt', '.md', '.c', '.h', '.cpp', '.hpp', '.py', '.java', '.m', '.motion', '.bvh',
+    '.obj', '.fbx', '.dae'
+]
 
 
 class TestLineEnding(unittest.TestCase):
@@ -27,17 +37,18 @@ class TestLineEnding(unittest.TestCase):
         """Get all files to be tested"""
 
         self.files = []
-        for root_path, _, file_names in os.walk(os.environ['WEBOTS_HOME']):
-            for extension in ('*.proto', '*.wbt', '*.wbo', '*.forest'):
-                for file_name in fnmatch.filter(file_names, extension):
-                    self.files.append(os.path.join(root_path, file_name))
+        for root_path, dir_names, file_names in os.walk(os.environ['WEBOTS_HOME']):
+            dir_names[:] = [d for d in dir_names if d not in SKIPPED_DIRECTORIES]
+            for file in file_names:
+                if file.endswith(tuple(EXTENSIONS_TO_CHECK)):
+                    self.files.append(os.path.join(root_path, file))
 
     def test_line_ending(self):
-        """Test that all assets have the correct line-ending"""
+        """Test that all files have the correct line-ending"""
 
         invalid_endings = [b'\r\n', b'\r']
         for file in self.files:
-            with open(file, 'rb') as f:
+            with open(file, 'r+b') as f:
                 self.assertFalse(
                     any(x in f.read() for x in invalid_endings),
                     msg='Wrong line ending in file: "%s"' % file
