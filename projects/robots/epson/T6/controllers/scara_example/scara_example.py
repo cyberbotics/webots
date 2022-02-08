d88811f828f39a4a78f01e33d97d2a85d0a28fbc
joachimhgg <joachim.honegger@gmail.com>
2022-02-08 17:29:00 +0100

    scara example

--- projects/robots/epson/T6/controllers/scara_example/scara_example.py
+++ projects/robots/epson/T6/controllers/scara_example/scara_example.py
@@ -4,23 +4,31 @@ from controller import Robot
 
 robot = Robot()
 
-timestep = int(robot.getBasicTimeStep())
+timeStep = int(robot.getBasicTimeStep())
+t = 0
+ledStatus = True
 
 base_arm = robot.getDevice("base_arm_motor")
-base_arm_pos = robot.getDevice('base_arm_position_sensor')
-base_arm_pos.enable(timestep)
+base_arm_pos = robot.getDevice('base_arm_position')
+base_arm_pos.enable(timeStep)
 
 arm = robot.getDevice("arm_motor")
-arm_pos = robot.getDevice('arm_position_sensor')
-arm_pos.enable(timestep)
+arm_pos = robot.getDevice('arm_position')
+arm_pos.enable(timeStep)
 
 shaft = robot.getDevice("shaft_linear_motor")
+led = robot.getDevice("epson_led")
 
-while robot.step(timestep) != -1:
 
-    # Read the position sensors:
-    base_arm_pos_value = base_arm_pos.getValue()
+while robot.step(timeStep) != -1:
+
+    if (robot.getTime() - t > 1):
+        # Read the position sensors:
+        base_arm_pos_value = base_arm_pos.getValue()
+        led.set(ledStatus)
+        ledStatus = not(ledStatus)
+        t = robot.getTime()
 
     arm.setPosition(0.14)
-    base_arm.setPosition(1.4)
+    base_arm.setPosition(0.5)
     shaft.setPosition(-0.1)
