c7d77968ef366e8e4f509e1e463eb0b779385307
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2022-02-09 14:52:42 +0100

    improve bounding object and controller

--- projects/robots/epson/ScaraT6/controllers/scara_food_industry/scara_food_industry.py
+++ projects/robots/epson/ScaraT6/controllers/scara_food_industry/scara_food_industry.py
@@ -24,14 +24,6 @@ arm = supervisor.getDevice("arm_motor")
 shaft_linear = supervisor.getDevice("shaft_linear_motor")
 led = supervisor.getDevice("epson_led")
 
-# sensors
-base_arm_pos = supervisor.getDevice('base_arm_position')
-base_arm_pos.enable(timestep)
-arm_pos = supervisor.getDevice('arm_position')
-arm_pos.enable(timestep)
-shaft_pos = supervisor.getDevice('shaft_linear_position')
-shaft_pos.enable(timestep)
-
 ledStatus = True
 merged_tool = False
 fruitType = 0
@@ -42,93 +34,46 @@ def merge_tool(fruit_id):
 
     if not merged_tool:
         fruit = supervisor.getFromDef("fruit" + str(fruit_id))
-        gripper = supervisor.getSelf()
+        vaccum = supervisor.getFromDef("VACCUM")
         if fruit:
-            string = fruit.exportString()
-            fruit.remove()
-            field = gripper.getField("handSlot")
-
-            newString = ""
-            for line in string.splitlines():
-                if line.startswith("  translation "):
-                    line = " translation 0 0 -0.07\n"
-                if line.startswith("  rotation "):
-                    line = "  rotation 0 0 0 0\n"
-                newString += line
-            field.importMFNodeFromString(-1, newString)
-            merged_tool = True
-
-
-def unmerge_tool(fruit_id):
-    global merged_tool
-
-    if merged_tool:
-        fruit = supervisor.getFromDef("fruit" + str(fruit_id))
-        gripper = supervisor.getSelf()
-
-        if fruit:
-            string = fruit.exportString()
-            oldPos = fruit.getPose()
-            oldPosStr = " translation " + str(oldPos[3]) + " " + str(oldPos[7]) + " " + str(oldPos[11]) + "\n"
-            newString = ""
-            for line in string.splitlines():
-                if line.startswith("  translation "):
-                    line = oldPosStr
-                if line.startswith("  rotation "):
-                    line = "  rotation 0 0 0 0\n"
-                newString += line
-            fruit.remove()
-            root = supervisor.getRoot()
-            field = root.getField("children")
-            field.importMFNodeFromString(-1, newString)
-            gripper.resetPhysics()
-            merged_tool = False
-
+            poses = vaccum.getPosition();
+            fruitTranslation = fruit.getField('translation')
+            if fruitTranslation and poses:
+              fruitTranslation.setSFVec3f([poses[0], poses[1], poses[2] - 0.07])
+              fruit.resetPhysics()
 
 def ledAnimation():
     global ledStatus
     led.set(ledStatus)
     ledStatus = not(ledStatus)
 
-
-def animation():
-    global fruitType
-
-    if supervisor.step(0) == -1:
-        return
-    arm.setPosition(0.6)
-    base_arm.setPosition(0.2)
-    ledAnimation()
-
-    if supervisor.step(400) == -1:
-        return
-    shaft_linear.setPosition(-0.148)
-    ledAnimation()
-
-    if supervisor.step(800) == -1:
-        return
-    merge_tool(fruitType)
-    shaft_linear.setPosition(0)
-    ledAnimation()
-
-    if supervisor.step(900) == -1:
-        led()
-        return
-    if fruitType:
-        base_arm.setPosition(0)
-        arm.setPosition(-0.83)
-    else:
-        base_arm.setPosition(-0.73)
-        arm.setPosition(-0.83)
-    ledAnimation()
-    if supervisor.step(1100) == -1:
-        led()
-        return
-    unmerge_tool(fruitType)
-    fruitType = randint(0, 1)
-    ledAnimation()
-
-
+i = 0
 while supervisor.step(timestep) != -1:
+    if i == 0:
+        arm.setPosition(0.6)
+        base_arm.setPosition(0.2)
+        ledAnimation()
+    elif i > 450:
+        i = -1
+    elif i > 400:
+        fruitType = randint(0, 1)
+    elif i > 180:
+        merge_tool(fruitType)
+        if i > 250:
+            if fruitType:
+                base_arm.setPosition(0)
+                arm.setPosition(-0.83)
+            else:
+                base_arm.setPosition(-0.50)
+                arm.setPosition(-0.83)
+        else :
+            shaft_linear.setPosition(0)
+            ledAnimation()
 
-    animation()
+    elif i > 150:
+            merge_tool(fruitType)
+    elif i > 110:
+        shaft_linear.setPosition(-0.148)
+        ledAnimation()
+
+    i = i + 1
