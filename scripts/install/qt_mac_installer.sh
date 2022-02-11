16818b84f76aa14e082cc4321dd875adb02164a6
Olivier Michel <Olivier.Michel@cyberbotics.com>
2022-02-11 18:13:22 +0100

    mac

--- scripts/install/qt_mac_installer.sh
+++ scripts/install/qt_mac_installer.sh
@@ -5,7 +5,7 @@ if [[ -z "${WEBOTS_HOME}" ]]; then
   exit 1
 fi
 
-QT_VERSION=5.15.2
+QT_VERSION=6.2.3
 
 # prepare Webots
 cd $WEBOTS_HOME
@@ -14,29 +14,21 @@ mkdir lib/webots/qt
 mkdir include/qt
 
 # populate webots
-cd $HOME/Qt$QT_VERSION/$QT_VERSION/clang_64
+cd $HOME/Qt/$QT_VERSION/macos
 cp bin/lrelease $WEBOTS_HOME/bin/qt
 cp bin/lupdate $WEBOTS_HOME/bin/qt
-cp bin/moc $WEBOTS_HOME/bin/qt
-cp -r include/QtPlatformHeaders $WEBOTS_HOME/include/qt/
+cp libexec/moc $WEBOTS_HOME/bin/qt
 
 declare -a qtFrameworks=( \
   "QtConcurrent" \
   "QtCore" \
   "QtDBus" \
   "QtGui" \
-  "QtMultimedia" \
-  "QtMultimediaWidgets" \
   "QtNetwork" \
   "QtOpenGL" \
-  "QtPositioning" \
+  "QtOpenGLWidgets" \
   "QtPrintSupport" \
   "QtQml" \
-  "QtQmlModels" \
-  "QtQuick" \
-  "QtQuickWidgets" \
-  "QtSensors" \
-  "QtSql" \
   "QtWebSockets" \
   "QtWidgets" \
   "QtXml" \
@@ -52,11 +44,13 @@ mkdir $WEBOTS_HOME/lib/webots/qt/plugins/imageformats
 mkdir $WEBOTS_HOME/lib/webots/qt/plugins/platforms
 mkdir $WEBOTS_HOME/lib/webots/qt/plugins/printsupport
 mkdir $WEBOTS_HOME/lib/webots/qt/plugins/styles
+mkdir $WEBOTS_HOME/lib/webots/qt/plugins/tls
 mkdir $WEBOTS_HOME/lib/webots/qt/libexec
 cp plugins/imageformats/libqjpeg.dylib $WEBOTS_HOME/lib/webots/qt/plugins/imageformats/
 cp plugins/platforms/libqcocoa.dylib $WEBOTS_HOME/lib/webots/qt/plugins/platforms/
 cp plugins/printsupport/libcocoaprintersupport.dylib $WEBOTS_HOME/lib/webots/qt/plugins/printsupport/
 cp plugins/styles/libqmacstyle.dylib $WEBOTS_HOME/lib/webots/qt/plugins/styles/
+cp plugins/tls/*.dylib $WEBOTS_HOME/lib/webots/qt/plugins/tls/
 echo $'[Paths]\nPrefix = ..\n' > $WEBOTS_HOME/lib/webots/qt/libexec/qt.conf
 
 # remove the debug frameworks
@@ -68,12 +62,12 @@ cd  $WEBOTS_HOME/Contents/Frameworks/
 
 for fA in "${qtFrameworks[@]}"
 do
-   install_name_tool -id @rpath/Contents/Frameworks/$fA.framework/Versions/5/$fA $fA.framework/Versions/5/$fA
-   install_name_tool -delete_rpath @executable_path/../Frameworks $fA.framework/Versions/5/$fA
-   install_name_tool -delete_rpath @loader_path/Frameworks $fA.framework/Versions/5/$fA
+   install_name_tool -id @rpath/Contents/Frameworks/$fA.framework/Versions/Current/$fA $fA.framework/Versions/Current/$fA
+   install_name_tool -delete_rpath @executable_path/../Frameworks $fA.framework/Versions/Current/$fA
+   install_name_tool -delete_rpath @loader_path/Frameworks $fA.framework/Versions/Current/$fA
    for fB in "${qtFrameworks[@]}"
    do
-     install_name_tool -change @rpath/$fB.framework/Versions/5/$fB @rpath/Contents/Frameworks/$fB.framework/Versions/5/$fB $fA.framework/Versions/5/$fA
+     install_name_tool -change @rpath/$fB.framework/Versions/Current/$fB @rpath/Contents/Frameworks/$fB.framework/Versions/Current/$fB $fA.framework/Versions/Current/$fA
    done
 done
 
@@ -90,18 +84,18 @@ do
   install_name_tool -add_rpath @loader_path/../../../.. $lib
   for f in "${qtFrameworks[@]}"
   do
-    install_name_tool -change @rpath/$f.framework/Versions/5/$f @rpath/Contents/Frameworks/$f.framework/Versions/5/$f $lib
+    install_name_tool -change @rpath/$f.framework/Versions/Current/$f @rpath/Contents/Frameworks/$f.framework/Versions/Current/$f $lib
   done
 done
 
 # Change the RPATH of the executables
 cd $WEBOTS_HOME/bin/qt
 install_name_tool -rpath @loader_path/../lib @loader_path/../.. lrelease
-install_name_tool -change @rpath/QtCore.framework/Versions/5/QtCore @rpath/Contents/Frameworks/QtCore.framework/Versions/5/QtCore lrelease
-install_name_tool -change @rpath/QtXml.framework/Versions/5/QtXml @rpath/Contents/Frameworks/QtXml.framework/Versions/5/QtXml lrelease
+install_name_tool -change @rpath/QtCore.framework/Versions/Current/QtCore @rpath/Contents/Frameworks/QtCore.framework/Versions/Current/QtCore lrelease
+install_name_tool -change @rpath/QtXml.framework/Versions/Current/QtXml @rpath/Contents/Frameworks/QtXml.framework/Versions/Current/QtXml lrelease
 install_name_tool -rpath @loader_path/../lib @loader_path/../.. lupdate
-install_name_tool -change @rpath/QtCore.framework/Versions/5/QtCore @rpath/Contents/Frameworks/QtCore.framework/Versions/5/QtCore lupdate
-install_name_tool -change @rpath/QtXml.framework/Versions/5/QtXml @rpath/Contents/Frameworks/QtXml.framework/Versions/5/QtXml lupdate
+install_name_tool -change @rpath/QtCore.framework/Versions/Current/QtCore @rpath/Contents/Frameworks/QtCore.framework/Versions/Current/QtCore lupdate
+install_name_tool -change @rpath/QtXml.framework/Versions/Current/QtXml @rpath/Contents/Frameworks/QtXml.framework/Versions/Current/QtXml lupdate
 install_name_tool -add_rpath @loader_path/../.. moc
 
 cd $WEBOTS_HOME
