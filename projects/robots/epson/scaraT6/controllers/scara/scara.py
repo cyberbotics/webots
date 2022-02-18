9d9fa463ced3603f0dc7a9c6a68bffd7fcbf43a9
joachimhgg <joachim.honegger@gmail.com>
2022-02-18 16:50:02 +0100

    rename scara.py

--- projects/robots/epson/scaraT6/controllers/scara/scara.py
+++ projects/robots/epson/scaraT6/controllers/scara/scara.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-""" scara_example controller."""
+""" scara example controller."""
 
 from controller import Robot
 
