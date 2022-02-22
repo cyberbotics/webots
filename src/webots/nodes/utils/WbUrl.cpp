7ea786a258dd6b6019b6aa9b77fcb26187c3a400
ad-daniel <daniel.dias@epfl.ch>
2022-02-22 14:26:34 +0100

    Fix url search in distributions

--- src/webots/nodes/utils/WbUrl.cpp
+++ src/webots/nodes/utils/WbUrl.cpp
@@ -24,6 +24,7 @@
 #include "WbProtoList.hpp"
 #include "WbProtoModel.hpp"
 #include "WbStandardPaths.hpp"
+#include "WbVersion.hpp"
 
 #include <QtCore/QDir>
 #include <QtCore/QFileInfo>
@@ -122,8 +123,10 @@ QString WbUrl::computePath(const WbNode *node, const QString &field, const QStri
 
     if (isLocalUrl(url)) {
       QString newUrl(url);
-      newUrl.replace("webots://", "https://raw.githubusercontent.com/" + WbApplicationInfo::repo() + "/" +
-                                    WbApplicationInfo::branch() + "/");
+      WbVersion version = WbApplicationInfo::version();
+      // if it's an official release, use the tag (for example R2022b), if it's a nightly use the commit
+      QString reference = version.commit().isEmpty() ? version.toString() : version.commit();
+      newUrl.replace("webots://", "https://raw.githubusercontent.com/cyberbotics/webots/" + reference + "/");
       return newUrl;
     }
 
