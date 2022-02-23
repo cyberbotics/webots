fc9e47b10cdf4fd2cfc61d7b3bbaa8dfb085fb72
ad-daniel <daniel.dias@epfl.ch>
2022-02-23 09:27:05 +0100

    const ref

--- src/webots/nodes/utils/WbUrl.cpp
+++ src/webots/nodes/utils/WbUrl.cpp
@@ -122,7 +122,7 @@ QString WbUrl::computePath(const WbNode *node, const QString &field, const QStri
 
     if (isLocalUrl(url)) {
       QString newUrl(url);
-      const WbVersion version = WbApplicationInfo::version();
+      const WbVersion &version = WbApplicationInfo::version();
       // if it's an official release, use the tag (for example R2022b), if it's a nightly use the commit
       const QString reference = version.commit().isEmpty() ? version.toString() : version.commit();
       newUrl.replace("webots://", "https://raw.githubusercontent.com/cyberbotics/webots/" + reference + "/");
