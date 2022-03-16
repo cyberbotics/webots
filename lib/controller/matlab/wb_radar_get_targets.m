c9eaee65df87f54db23fdfae4af27ace4799fc07
Olivier Michel <Olivier.Michel@cyberbotics.com>
2022-03-16 17:36:01 +0100

    Fixed stdout to 1 and stderr to 2 (standard values)

--- lib/controller/matlab/wb_radar_get_targets.m
+++ lib/controller/matlab/wb_radar_get_targets.m
@@ -6,7 +6,7 @@ function targets = wb_radar_get_targets(tag)
 rate = calllib('libController', 'wb_radar_get_sampling_period', tag);
 if rate <= 0
     targets = []
-    calllib('libController', 'wb_console_print', 'Warning: wb_radar_get_targets() called for a disabled radar! Please call wb_radar_enable() before.', 1);
+    calllib('libController', 'wb_console_print', 'Warning: wb_radar_get_targets() called for a disabled radar! Please call wb_radar_enable() before.', 2);
 else
     size = calllib('libController', 'wb_radar_get_number_of_targets', tag);
     for n = 1:size
