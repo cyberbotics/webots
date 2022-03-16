72930ded9c267712ac55a35be833f7ff3d786688
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2022-03-16 10:37:04 +0100

    clang

--- projects/samples/robotbenchmark/wall_following/controllers/wall_following_supervisor/wall_following_supervisor.c
+++ projects/samples/robotbenchmark/wall_following/controllers/wall_following_supervisor/wall_following_supervisor.c
@@ -27,8 +27,7 @@
 #define BENCHMARK_DURATION 60
 //#define ALLOW_LABELS true // uncomment to enable labels
 
-static WallFollowingMetric *
-generate_metric_from_proto(const double *robot_starting_position) {
+static WallFollowingMetric *generate_metric_from_proto(const double *robot_starting_position) {
   WallFollowingMetric *metric = NULL;
 
   const char *custom_data = wb_robot_get_custom_data();
@@ -55,26 +54,22 @@ generate_metric_from_proto(const double *robot_starting_position) {
 
     // Recovers the node corresponding to the LinkedWall PROTO.
     WbNodeRef wall = wb_supervisor_node_get_from_def("WALL");
-    WbFieldRef translation_field =
-        wb_supervisor_node_get_field(wall, "translation");
+    WbFieldRef translation_field = wb_supervisor_node_get_field(wall, "translation");
     WbFieldRef rotation_field = wb_supervisor_node_get_field(wall, "rotation");
     WbFieldRef length_field = wb_supervisor_node_get_field(wall, "length");
 
     // Recovers the position of the wall.
-    const double *translation =
-        wb_supervisor_field_get_sf_vec3f(translation_field);
+    const double *translation = wb_supervisor_field_get_sf_vec3f(translation_field);
     const double first_segment_position[2] = {translation[2], translation[0]};
 
     // Recover the orientation of the wall.
-    double first_segment_orientation =
-        wb_supervisor_field_get_sf_rotation(rotation_field)[3];
+    double first_segment_orientation = wb_supervisor_field_get_sf_rotation(rotation_field)[3];
 
     // Recovers the size of one wall segment.
     double segment_length = wb_supervisor_field_get_sf_float(length_field);
 
-    metric = create_new_wall_following_metric(
-        robot_starting_position, first_segment_position, angles, nb_angles,
-        first_segment_orientation, segment_length, 0.5);
+    metric = create_new_wall_following_metric(robot_starting_position, first_segment_position, angles, nb_angles,
+                                              first_segment_orientation, segment_length, 0.5);
     free(angles);
   }
   return metric;
@@ -101,35 +96,29 @@ int main(int argc, char **argv) {
 
     char str_buffer[512];
 
-    while (wb_robot_step(time_step) != -1 &&
-           wb_robot_get_time() < BENCHMARK_DURATION) {
+    while (wb_robot_step(time_step) != -1 && wb_robot_get_time() < BENCHMARK_DURATION) {
       position = wb_supervisor_node_get_position(pioneer);
       position2d[0] = position[2];
       position2d[1] = position[0];
 
 #ifdef ALLOW_LABELS
-      PathSegment *current_segment =
-          path_get_closest_segment(metric->path, position2d, &distance);
+      PathSegment *current_segment = path_get_closest_segment(metric->path, position2d, &distance);
 
       sprintf(str_buffer, "Distance from path: %f", distance);
-      wb_supervisor_set_label(0, str_buffer, 0.01, 0.01, 0.05, 0x0000ff, 0.0,
-                              "Lucida Console");
+      wb_supervisor_set_label(0, str_buffer, 0.01, 0.01, 0.05, 0x0000ff, 0.0, "Lucida Console");
 
       sprintf(str_buffer, "Current segment: %d", current_segment->index);
-      wb_supervisor_set_label(1, str_buffer, 0.01, 0.04, 0.05, 0x0000ff, 0.0,
-                              "Lucida Console");
+      wb_supervisor_set_label(1, str_buffer, 0.01, 0.04, 0.05, 0x0000ff, 0.0, "Lucida Console");
 #endif
 
       wall_following_metric_update(metric, position2d);
 
 #ifdef ALLOW_LABELS
       sprintf(str_buffer, "Current performance: %f", metric->performance);
-      wb_supervisor_set_label(2, str_buffer, 0.01, 0.07, 0.05, 0x0000ff, 0.0,
-                              "Lucida Console");
+      wb_supervisor_set_label(2, str_buffer, 0.01, 0.07, 0.05, 0x0000ff, 0.0, "Lucida Console");
 
       sprintf(str_buffer, "Current time: %f", wb_robot_get_time());
-      wb_supervisor_set_label(3, str_buffer, 0.01, 0.1, 0.05, 0x0000ff, 0.0,
-                              "Lucida Console");
+      wb_supervisor_set_label(3, str_buffer, 0.01, 0.1, 0.05, 0x0000ff, 0.0, "Lucida Console");
 #endif
 
       sprintf(str_buffer, "update: %.2f", 100 * metric->performance);
@@ -144,8 +133,7 @@ int main(int argc, char **argv) {
       const char *answer_message;
       while ((answer_message = wb_robot_wwi_receive_text())) {
         if (strncmp(answer_message, "record:", 7) == 0) {
-          robotbenchmark_record(answer_message, "wall_following",
-                                metric->performance);
+          robotbenchmark_record(answer_message, "wall_following", metric->performance);
           waiting_answer = 0;
         } else if (strcmp(answer_message, "exit") == 0)
           waiting_answer = 0;
