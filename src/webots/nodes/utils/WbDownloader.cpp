02265765b4b783d26a1cf372bd31dba3095376a4
ad-daniel <44834743+ad-daniel@users.noreply.github.com>
2022-03-16 10:36:54 +0100

    Update src/webots/nodes/utils/WbDownloader.cpp
    Co-authored-by: Olivier Michel <Olivier.Michel@cyberbotics.com>

--- src/webots/nodes/utils/WbDownloader.cpp
+++ src/webots/nodes/utils/WbDownloader.cpp
@@ -94,7 +94,7 @@ void WbDownloader::download(const QUrl &url) {
 void WbDownloader::finished() {
   // cache result
   if (mNetworkReply && mNetworkReply->error()) {
-    mError = tr("Cannot download %1, error code: %2: %3")
+    mError = tr("Cannot download '%1', error code: %2: %3")
                .arg(mUrl.toString())
                .arg(mNetworkReply->error())
                .arg(mNetworkReply->errorString());
