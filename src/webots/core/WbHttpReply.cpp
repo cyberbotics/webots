aedb860a9bb731563983d8a676008feeca269961
Olivier Michel <Olivier.Michel@cyberbotics.com>
2022-03-24 12:29:37 +0100

    Fixed localhost protocol

--- src/webots/core/WbHttpReply.cpp
+++ src/webots/core/WbHttpReply.cpp
@@ -39,7 +39,7 @@ QByteArray WbHttpReply::forgeFileReply(const QString &fileName, const QString &e
     return forge404Reply();
 
   QByteArray data = file.readAll();
-  QByteArray replace = "https://" + host.toUtf8() + "/";
+  QByteArray replace = "http://" + host.toUtf8() + "/";
   data.replace("webots://", replace);
 
   const QByteArray hash = QCryptographicHash::hash(data, QCryptographicHash::Md5);
