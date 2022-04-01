b22ffdf0bf39b3f28280b8d28cc25e38e838cfe4
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2022-04-01 10:24:02 +0200

    remove print

--- resources/web/wwi/Parser.js
+++ resources/web/wwi/Parser.js
@@ -60,7 +60,7 @@ export default class Parser {
       const parser = new DOMParser();
       xml = parser.parseFromString(text, 'text/xml');
     }
-    console.log(xml)
+
     if (typeof xml === 'undefined')
       console.error('File to parse not found');
     else {
