7c39be9948aba1d87191de0e270963f26919256b
ThomasOliverKimble <tokimble@gmail.com>
2022-04-04 14:26:19 +0200

    correct urls

--- resources/web/wwi/WebotsView.js
+++ resources/web/wwi/WebotsView.js
@@ -29,7 +29,7 @@ export default class WebotsView extends HTMLElement {
 
     this._initialCallbackDone = true;
     this.css = document.createElement('link');
-    this.css.href = 'https://cyberbotics.com/wwi/testing1/css/toolbar.css';
+    this.css.href = 'https://cyberbotics.com/wwi/R2022b/css/toolbar.css';
     this.css.type = 'text/css';
     this.css.rel = 'stylesheet';
     document.head.appendChild(this.css);
