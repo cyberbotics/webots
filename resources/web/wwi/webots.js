848fa17b14a3cc8e8906e5cbd46d1f10d395296e
ThomasOliverKimble <tokimble@gmail.com>
2022-04-06 15:45:08 +0200

    still not working

--- resources/web/wwi/webots.js
+++ resources/web/wwi/webots.js
@@ -125,8 +125,7 @@ webots.View = class View {
         this.view3D.appendChild(this.progress);
       }
 
-      if (document.getElementById('webots-progress'))
-        document.getElementById('webots-progress').style.display = 'block';
+      this.setProgressBar('block', 'none', 'none', 'none');
 
       if (this._isWebSocketProtocol) {
         if (this.url.endsWith('.wbt')) { // url expected form: "wss://localhost:1999/simple/worlds/simple.wbt" or
@@ -146,8 +145,7 @@ webots.View = class View {
     };
 
     const finalizeWorld = () => {
-      if (document.getElementById('webots-progress-message'))
-        document.getElementById('webots-progress-message').innerHTML = 'Loading World...';
+      this.setProgressBar('block', 'Loading World...', 0, 'Loading world...');
       if (typeof this.x3dScene !== 'undefined') {
         if (!this._isWebSocketProtocol) { // skip robot windows initialization
           if (typeof this.animation !== 'undefined')
@@ -280,8 +278,7 @@ webots.View = class View {
   }
 
   resetSimulation() {
-    if (document.getElementById('webots-progress'))
-      document.getElementById('webots-progress').style.display = 'none';
+    this.setProgressBar('none');
     this.removeLabels();
     if (document.getElementById('webotsClock'))
       document.getElementById('webotsClock').innerHTML = webots.parseMillisecondsIntoReadableTime(0);
@@ -291,13 +288,14 @@ webots.View = class View {
     if (this.broadcast)
       return;
     this.close();
+    console.log("Bye bye");
     if (document.getElementById('webots-progress')) {
-      document.getElementById('webots-progress').style.display = 'block';
-      document.getElementById('webots-progress-message').innerHTML = 'Bye bye...';
+      document.getElementById('webots-progress-message').innerHTML = "Bye bye...";
     }
     setTimeout(() => {
-      if (document.getElementById('webots-progress'))
-        document.getElementById('webots-progress').style.display = 'none';
+      if (document.getElementById('webots-progress')) {
+        document.getElementById('webots-progress').display = 'none';
+      }
     }, 1000);
     this.quitting = true;
     this.onquit();
@@ -314,6 +312,39 @@ webots.View = class View {
       Selector.reset();
     }
   }
+
+  setProgressBar(display, message, percent, info) {
+    if (document.getElementById('webots-progress')) {
+      console.log("Gone here with: ");
+      console.log("  -display: " + display);
+      console.log("  -message: " + message);
+      console.log("  -percent: " + percent);
+      console.log("  -info: " + info);
+      document.getElementById('webots-progress').display = display;
+      if (message) {
+        if (message === 'none')
+          document.getElementById('webots-progress-message').display = 'none';
+        else
+          document.getElementById('webots-progress-message').innerHTML = message;
+      }
+      if (percent) {
+        if (percent === 'none')
+          document.getElementById('webots-progress-percent').display = 'none';
+        else if (percent > 100)
+          document.getElementById('webots-progress-percent').value = 100;
+        else
+          document.getElementById('webots-progress-percent').value = percent;
+      }
+      if (info) {
+        if (info === 'none')
+          document.getElementById('webots-progress-info').display = 'none';
+        if (info.length > 40)
+          document.getElementById('webots-progress-info').innerHTML = info.substring(0, 40) + '...';
+        else
+          document.getElementById('webots-progress-info').innerHTML = info;
+      }
+    }
+  };
 
   _getHeight(el) {
     const s = window.getComputedStyle(el, null);
