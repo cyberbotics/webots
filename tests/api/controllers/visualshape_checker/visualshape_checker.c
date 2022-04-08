421a7197f563a5dadf7857249cfc5f74c2a207fc
ad-daniel <daniel.dias@epfl.ch>
2022-04-08 09:38:52 +0200

    Fix tests

--- tests/api/controllers/visualshape_checker/visualshape_checker.c
+++ tests/api/controllers/visualshape_checker/visualshape_checker.c
@@ -19,7 +19,7 @@ void test_camera_color(int i, const int expected_color[3]) {
   r = wb_camera_image_get_red(image, width, 0, 0);
   g = wb_camera_image_get_green(image, width, 0, 0);
   b = wb_camera_image_get_blue(image, width, 0, 0);
-  ts_assert_color_in_delta(r, g, b, expected_color[0], expected_color[1], expected_color[2], 2,
+  ts_assert_color_in_delta(r, g, b, expected_color[0], expected_color[1], expected_color[2], 5,
                            "Camera %d received color = (%d, %d, %d) but expected color = (%d, %d, %d)", i, r, g, b,
                            expected_color[0], expected_color[1], expected_color[2]);
 }
@@ -37,7 +37,7 @@ int main(int argc, char **argv) {
 
   wb_robot_step(TIME_STEP);
 
-  const int expected_color[3][3] = {{198, 2, 2}, {1, 15, 199}, {198, 192, 2}};
+  const int expected_color[3][3] = {{198, 2, 2}, {2, 17, 197}, {198, 192, 2}};
   for (int i = 0; i < NB_CAMERAS; ++i) {
     const int color = i < 4 ? 0 : (i < 8 ? 1 : 2);
     test_camera_color(i, expected_color[color]);
