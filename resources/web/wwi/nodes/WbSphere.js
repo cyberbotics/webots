d58deea910abf1d148a81fbb0b1c82565795ff4b
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2022-04-13 17:09:51 +0200

    sphere

--- resources/web/wwi/nodes/WbSphere.js
+++ resources/web/wwi/nodes/WbSphere.js
@@ -1,4 +1,5 @@
 import WbGeometry from './WbGeometry.js';
+import {resetIntIfNotInRangeWithIncludedBounds, resetDoubleIfNonPositive} from './utils/WbFieldChecker.js';
 
 export default class WbSphere extends WbGeometry {
   constructor(id, radius, ico, subdivision) {
@@ -15,6 +16,8 @@ export default class WbSphere extends WbGeometry {
 
   createWrenObjects() {
     super.createWrenObjects();
+
+    this._sanitizeFields();
     this._buildWrenMesh();
   }
 
@@ -34,6 +37,9 @@ export default class WbSphere extends WbGeometry {
   }
 
   updateScale() {
+    if (!this._sanitizeFields())
+      return;
+
     const scaledRadius = this.radius;
 
     _wr_transform_set_scale(this.wrenNode, _wrjs_array3(scaledRadius, scaledRadius, scaledRadius));
@@ -56,7 +62,7 @@ export default class WbSphere extends WbGeometry {
     super._computeWrenRenderable();
 
     const createOutlineMesh = super.isInBoundingObject();
-    this._wrenMesh = _wr_static_mesh_unit_sphere_new(this.subdivision, this.ico, false);
+    this._wrenMesh = _wr_static_mesh_unit_sphere_new(this.subdivision, this.ico, createOutlineMesh);
 
     // Restore pickable state
     super.setPickable(this.isPickable);
@@ -67,5 +73,22 @@ export default class WbSphere extends WbGeometry {
       this.updateLineScale();
     else
       this.updateScale();
+  }
+
+  _sanitizeFields() {
+    let newSubdivision;
+    if (this.ico)
+      newSubdivision = resetIntIfNotInRangeWithIncludedBounds(this.subdivision, 1, 5, 1);
+    else
+      newSubdivision = resetIntIfNotInRangeWithIncludedBounds(this.subdivision, 3, 32, 24);
+
+    if (newSubdivision)
+      this.subdivision = newSubdivision;
+
+    const newRadius = resetDoubleIfNonPositive(this.radius, 1.0);
+    if (newRadius)
+      this.radius = newRadius;
+
+    return !newSubdivision && !newRadius;
   }
 }
