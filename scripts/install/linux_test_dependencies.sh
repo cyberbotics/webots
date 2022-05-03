89fc21cc48ac7d9586e2d768e79ebf22a146c034
Stefania Pedrazzi <stefania.pedrazzi@cyberbotics.com>
2022-05-03 08:59:28 +0200

    Restore dependecies test workflow

--- scripts/install/linux_test_dependencies.sh
+++ scripts/install/linux_test_dependencies.sh
@@ -11,20 +11,15 @@ if [[ $@ != *"--exclude-ros"* ]]; then
   UBUNTU_VERSION=$(lsb_release -rs)
   if [[ $UBUNTU_VERSION == "20.04" ]]; then
          export ROS_DISTRO=noetic
-  elif [[ $UBUNTU_VERSION == "22.04" ]]; then
-         exit 0
-  else
+         echo "Adding ROS dependencies"
+         sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+         curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | apt-key add -
+         apt update -qq
+         apt install -y ros-$ROS_DISTRO-ros-base ros-$ROS_DISTRO-sensor-msgs ros-$ROS_DISTRO-tf ros-$ROS_DISTRO-ros-controllers ros-$ROS_DISTRO-controller-manager ros-$ROS_DISTRO-ros-control ros-$ROS_DISTRO-class-loader ros-$ROS_DISTRO-roslib liburdfdom-tools
+         apt install -y python3-rosdep
+  elif [[ $UBUNTU_VERSION != "22.04" ]]; then
          echo "Unsupported Linux version: dependencies may not be completely installed. Only the two latest Ubuntu LTS are supported."
-         exit 1
   fi
-
-  echo "Adding ROS dependencies"
-  sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-  curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | apt-key add -
-  apt update -qq
-  apt install -y ros-$ROS_DISTRO-ros-base ros-$ROS_DISTRO-sensor-msgs ros-$ROS_DISTRO-tf ros-$ROS_DISTRO-ros-controllers ros-$ROS_DISTRO-controller-manager ros-$ROS_DISTRO-ros-control ros-$ROS_DISTRO-class-loader ros-$ROS_DISTRO-roslib liburdfdom-tools
-
-  apt install -y python3-rosdep
 fi
 
 if [[ $@ != *"--norecurse"* ]]; then
