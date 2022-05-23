0e8d9befee57f810be54eb6c2df0519a4c3e5457
ThomasOliverKimble <tokimble@gmail.com>
2022-05-23 15:45:26 +0200

    clang format

--- src/webots/gui/WbMainWindow.cpp
+++ src/webots/gui/WbMainWindow.cpp
@@ -1685,7 +1685,7 @@ void WbMainWindow::uploadFinished() {
     QString error = reply->error() ? reply->errorString() : "Failed to confirm upload id";
     WbMessageBox::critical(tr("Upload failed. Error::%1").arg(error), this, tr("Webots.cloud"));
   } else {
-    QString url = jsonAnswer["url"].toString();  
+    QString url = jsonAnswer["url"].toString();
     int id = jsonAnswer["id"].toInt();
 
     QNetworkAccessManager *manager = new QNetworkAccessManager(this);
