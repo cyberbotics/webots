b478ae657e825507a5dda6dd446aa9e1180740c9
Benjamin Délèze <benjamin.deleze@cyberbotics.com>
2022-06-02 11:16:06 +0200

    Update resources/web/wwi/Server.js
    Co-authored-by: Olivier Michel <Olivier.Michel@cyberbotics.com>

--- resources/web/wwi/Server.js
+++ resources/web/wwi/Server.js
@@ -97,7 +97,7 @@ export default class Server {
       const shutdownTimeout = parseFloat(message.substring(17)) - 300; // Warning is issued five minutes before closing
       if (shutdownTimeout > 0)
         setTimeout(() => {
-          alert('Warning: the time limit is almost reached.\nThe simulation will be automatically closed in: 5 minutes');
+          alert('Warning: the time limit is almost reached.\nThe simulation will be automatically closed in 5 minutes');
         }, shutdownTimeout * 1000);
     } else
       console.log('Received an unknown message from the Webots server socket: "' + message + '"');
