fb105efeee29ff1a49e2966f6c4c81f382176fac
ThomasOliverKimble <tokimble@gmail.com>
2022-06-16 12:04:30 +0200

    restore files to R2022b

--- projects/robots/gctronic/e-puck/plugins/robot_windows/e-puck/e-puck.js
+++ projects/robots/gctronic/e-puck/plugins/robot_windows/e-puck/e-puck.js
@@ -3,7 +3,7 @@
 /* exported wifiConnect */
 /* exported wifiDisconnect */
 
-import RobotWindow from 'https://cyberbotics.com/wwi/testingR2022b/RobotWindow.js';
+import RobotWindow from 'https://cyberbotics.com/wwi/R2022b/RobotWindow.js';
 
 window.onload = function() {
   var progressBar = document.getElementById('uploadProgressBar');
