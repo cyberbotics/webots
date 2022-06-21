3c4d81b3a8c8617173e21ed71d1369d410a88e87
Stefania Pedrazzi <stefania.pedrazzi@cyberbotics.com>
2022-06-21 15:28:50 +0200

    Fix URLs reverted before creating the snap package

--- scripts/packaging/webots_distro.py
+++ scripts/packaging/webots_distro.py
@@ -33,6 +33,9 @@ webots_version = get_webots_version()
 subprocess.run(["git", "fetch", "--all", "--tags"])
 tags = subprocess.check_output(["git", "tag", "--points-at", "HEAD"]).decode()
 if webots_version not in tags:
+    commit_file_path = os.path.join(WEBOTS_HOME, 'resources', 'commit.txt')
+    if not os.path.exists(commit_file_path):
+        subprocess.run(os.path.join(WEBOTS_HOME, 'scripts', 'get_git_info', 'get_git_info.sh'))
     with open(os.path.join(WEBOTS_HOME, 'resources', 'commit.txt')) as f:
         current_tag = f.readline().strip()
 else:
@@ -77,6 +80,3 @@ else:
     webots_package = LinuxWebotsPackage(application_name)
 print('generating webots bundle')
 webots_package.create_webots_bundle()
-
-# revert changes in URLs
-replace_projects_urls(current_tag, True)
