0f4e64e272681a42863f663c22a20320704761f2
knmcguire <kimberly@bitcraze.io>
2022-06-21 16:16:07 +0200

    Update licence and description crazyflie control

--- projects/robots/bitcraze/crazyflie/controllers/crazyflie_controller/pid_controller.c
+++ projects/robots/bitcraze/crazyflie/controllers/crazyflie_controller/pid_controller.c
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2022 Bitcraze AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /**
  *  ...........       ____  _ __
  *  |  ,-^-,  |      / __ )(_) /_______________ _____  ___
@@ -7,8 +23,10 @@
  *
  *
  * @file pid_controller.c
- * A simple PID controller for attitude control of an
- * quadcopter
+ * Description: A simple PID controller for attitude,
+ *          height and velocity  control of an quadcopter
+ * Author:      Kimberly McGuire (Bitcraze AB)
+
  */
 
 #include <math.h>
--- projects/robots/bitcraze/crazyflie/controllers/crazyflie_controller/pid_controller.h
+++ projects/robots/bitcraze/crazyflie/controllers/crazyflie_controller/pid_controller.h
@@ -1,6 +1,22 @@
 #pragma once
 
 /*
+ * Copyright 2022 Bitcraze AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
  *  ...........       ____  _ __
  *  |  ,-^-,  |      / __ )(_) /_______________ _____  ___
  *  | (  O  ) |     / __  / / __/ ___/ ___/ __ `/_  / / _ \
@@ -9,8 +25,8 @@
  *
  *
  * @file pid_controller.h
- * PID controller header file
- *
+ * Description:  PID controller header file
+ * Author:       Kimberly McGuire (Bitcraze AB)
  */
 
 typedef struct motor_power_s {
