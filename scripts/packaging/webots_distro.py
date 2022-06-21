4408aba0e7f14ff2a679afc2e54a0ab601fb2102
Stefania Pedrazzi <stefania.pedrazzi@cyberbotics.com>
2022-06-21 12:00:23 +0200

    Create commit.txt if needed

--- scripts/packaging/webots_distro.py
+++ scripts/packaging/webots_distro.py
@@ -33,6 +33,10 @@ webots_version = get_webots_version()
 subprocess.run(["git", "fetch", "--all", "--tags"])
 tags = subprocess.check_output(["git", "tag", "--points-at", "HEAD"]).decode()
 if webots_version not in tags:
+    commit_file_path = os.path.join(WEBOTS_HOME, 'resources', 'commit.txt')
+    if not os.path.exists(commit_file_path):
+        subprocess.run(os.path.join(WEBOTS_HOME, 'scripts', 'get_git_info', 'get_git_info.sh'))
+        print("Compute commit id")
     with open(os.path.join(WEBOTS_HOME, 'resources', 'commit.txt')) as f:
         current_tag = f.readline().strip()
 else:
