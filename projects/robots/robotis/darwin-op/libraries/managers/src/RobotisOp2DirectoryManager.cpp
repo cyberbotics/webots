3e5582428837730b209ab3b6e9a29c61e015398f
Thomas Kimble <100125668+ThomasOliverKimble@users.noreply.github.com>
2022-06-29 10:22:13 +0200

    Update projects/robots/robotis/darwin-op/libraries/managers/src/RobotisOp2DirectoryManager.cpp
    Co-authored-by: Olivier Michel <Olivier.Michel@cyberbotics.com>

--- projects/robots/robotis/darwin-op/libraries/managers/src/RobotisOp2DirectoryManager.cpp
+++ projects/robots/robotis/darwin-op/libraries/managers/src/RobotisOp2DirectoryManager.cpp
@@ -30,10 +30,9 @@ const string &RobotisOp2DirectoryManager::getDataDirectory() {
   char *WEBOTS_HOME = wbu_system_getenv("WEBOTS_HOME");
   static string path = string(WEBOTS_HOME)
 #ifdef __APPLE__
-                       + "Contents/projects/robots/robotis/darwin-op/libraries/robotis-op2/robotis/Data/";
-#else
-                       + "/projects/robots/robotis/darwin-op/libraries/robotis-op2/robotis/Data/";
+                       + "Contents/"
 #endif
+                       + "/projects/robots/robotis/darwin-op/libraries/robotis-op2/robotis/Data/";
 #endif
   return path;
 }
