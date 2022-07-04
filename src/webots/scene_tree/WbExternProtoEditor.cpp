1bca38de7f670b300a0c8db3f8f64c9abf032a5c
Olivier Michel <Olivier.Michel@cyberbotics.com>
2022-07-04 14:02:20 +0200

    include order

--- src/webots/scene_tree/WbExternProtoEditor.cpp
+++ src/webots/scene_tree/WbExternProtoEditor.cpp
@@ -12,8 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "WbActionManager.hpp"
 #include "WbExternProtoEditor.hpp"
+
+#include "WbActionManager.hpp"
 #include "WbInsertExternProtoDialog.hpp"
 #include "WbProtoManager.hpp"
 
