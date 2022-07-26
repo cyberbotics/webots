59ca982c58614ac184f06bde1e03a8b37999e780
Stefania Pedrazzi <stefaniapedrazzi@users.noreply.github.com>
2022-07-26 11:57:47 +0200

    Add missing semicolon

--- src/webots/vrml/WbProtoModel.cpp
+++ src/webots/vrml/WbProtoModel.cpp
@@ -463,7 +463,7 @@ const QString WbProtoModel::projectPath() const {
       QString dir = protoProjectDir.path();
       // cd up (we don't use QDir::cdUp() as it doesn't cd up if the upper folder doesn't exist which may happen here)
       dir.chop(protoProjectDir.dirName().size());
-      assert(!dir.isEmpty())
+      assert(!dir.isEmpty());
       protoProjectDir.setPath(dir);
       if (protoProjectDir.isRoot())
         return QString();
