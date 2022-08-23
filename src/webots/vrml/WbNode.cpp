45339b07e0eecdf4f7f2fd897d32970e162d31ab
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2022-08-23 10:10:46 +0200

    add variable

--- src/webots/vrml/WbNode.cpp
+++ src/webots/vrml/WbNode.cpp
@@ -767,8 +767,9 @@ void WbNode::notifyFieldChanged() {
   // this is the changed field
   WbField *const field = static_cast<WbField *>(sender());
 
-  if (parentField() && isProtoParameterNode())
-    emit parentField()->parentNode()->parameterChanged(parentField());
+  WbField *const parentField = this->parentField();
+  if (parentField && isProtoParameterNode())
+    emit parentField->parentNode()->parameterChanged(parentField);
 
   if (mIsBeingDeleted || cUpdatingDictionary) {
     emit fieldChanged(field);
