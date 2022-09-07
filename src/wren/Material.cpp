ecf986ce8b999186ddae9f31242406ea9618cd66
ad-daniel <daniel.dias@epfl.ch>
2022-09-07 09:51:44 +0200

    More fixes

--- src/wren/Material.cpp
+++ src/wren/Material.cpp
@@ -149,7 +149,7 @@ namespace wren {
     }
   }
 
-  int Material::countTextureInstances(const Texture *texture) {
+  int Material::countTextureInstances(const Texture *texture) const {
     int instances = 0;
     for (const auto &textureInstance : mTextures) {
       if (textureInstance.first == texture)
