0882caacaade6269783690e3f7480e3a9adeb348
Yann LEROY <yann.leroy@manurob.com>
2022-11-15 11:51:47 +0100

    include and typo

--- tests/api/controllers/gps_speed/gps_speed.c
+++ tests/api/controllers/gps_speed/gps_speed.c
@@ -1,3 +1,4 @@
+#include <webots/device.h>
 #include <webots/gps.h>
 #include <webots/robot.h>
 
@@ -19,24 +20,24 @@ int main(int argc, char **argv) {
   ts_setup(argv[0]);
   const int time_step = wb_robot_get_basic_time_step();
 
-  WbDeviceTag gps_with_physic = wb_robot_get_device("gps_with_physic");
-  WbDeviceTag gps_without_physic = wb_robot_get_device("gps_without_physic");
+  WbDeviceTag gps_with_physics = wb_robot_get_device("gps_with_physics");
+  WbDeviceTag gps_without_physics = wb_robot_get_device("gps_without_physics");
 
-  check_speed(gps_with_physic, NAN, NAN, NAN, NAN, "before the device is enabled");
-  wb_gps_enable(gps_with_physic, time_step);
-  check_speed(gps_without_physic, NAN, NAN, NAN, NAN, "before the device is enabled");
-  wb_gps_enable(gps_without_physic, time_step);
+  check_speed(gps_with_physics, NAN, NAN, NAN, NAN, "before the device is enabled");
+  wb_gps_enable(gps_with_physics, time_step);
+  check_speed(gps_without_physics, NAN, NAN, NAN, NAN, "before the device is enabled");
+  wb_gps_enable(gps_without_physics, time_step);
 
-  check_speed(gps_with_physic, NAN, NAN, NAN, NAN, "before a wb_robot_step is performed");
-  check_speed(gps_without_physic, NAN, NAN, NAN, NAN, "before a wb_robot_step is performed");
+  check_speed(gps_with_physics, NAN, NAN, NAN, NAN, "before a wb_robot_step is performed");
+  check_speed(gps_without_physics, NAN, NAN, NAN, NAN, "before a wb_robot_step is performed");
   wb_robot_step(time_step);
 
-  check_speed(gps_with_physic, 5.0, -5.0, 0.0, 0.0, "after one step (because got a Physics node)");
-  check_speed(gps_without_physic, NAN, NAN, NAN, NAN, "after one step (because no Physics node)");
+  check_speed(gps_with_physics, 5.0, -5.0, 0.0, 0.0, "after one step (because got a Physics node)");
+  check_speed(gps_without_physics, NAN, NAN, NAN, NAN, "after one step (because no Physics node)");
   wb_robot_step(time_step);
 
-  check_speed(gps_with_physic, 5.0, -5.0, 0.0, 0.0, "after two steps");
-  check_speed(gps_without_physic, 5.0, 0.0, -5.0, 0.0, "after two steps");
+  check_speed(gps_with_physics, 5.0, -5.0, 0.0, 0.0, "after two steps");
+  check_speed(gps_without_physics, 5.0, 0.0, -5.0, 0.0, "after two steps");
 
   ts_send_success();
   return EXIT_SUCCESS;
