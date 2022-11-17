77099933e6425e207d89d6fda3d40f5e9853d52b
Yann LEROY <yann.leroy@manurob.com>
2022-11-17 10:00:12 +0100

    fix arg

--- tests/api/controllers/gps/gps.c
+++ tests/api/controllers/gps/gps.c
@@ -4,7 +4,8 @@
 #include "../../../lib/ts_assertion.h"
 #include "../../../lib/ts_utils.h"
 
-static void check_position(WbDeviceTag gps, double *expected_position, char *end_msg) {
+static void check_position(WbDeviceTag gps, double x, double y, double z, char *end_msg) {
+  double expected_position[3] = {x, y, z};
   const double *position = wb_gps_get_values(gps);
   const char *axisNames = "XYZ";
   for (int i = 0; i < 3; i++)
@@ -18,10 +19,10 @@ int main(int argc, char **argv) {
   const int time_step = wb_robot_get_basic_time_step();
   WbDeviceTag gps = wb_robot_get_device("gps");
 
-  check_position(gps, (double *){NAN, NAN, NAN}, "before the device is enabled");
+  check_position(gps, NAN, NAN, NAN, "before the device is enabled");
   wb_gps_enable(gps, time_step);
 
-  check_position(gps, (double *){NAN, NAN, NAN}, "before a wb_robot_step is performed");
+  check_position(gps, NAN, NAN, NAN, "before a wb_robot_step is performed");
 
   ts_assert_int_equal(wb_gps_get_coordinate_system(gps), WB_GPS_LOCAL_COORDINATE, "Wrong coordinate system returned");
 
@@ -29,7 +30,7 @@ int main(int argc, char **argv) {
     wb_robot_step(time_step);
     char s[64];
     snprintf(s, sizeof(s), "after %d wb_robot_step(s)", i);
-    check_position(gps, (double *){0.19996, 0.05, -0.35}, s);
+    check_position(gps, 0.19996, 0.05, -0.35, s);
   }
 
   ts_send_success();
