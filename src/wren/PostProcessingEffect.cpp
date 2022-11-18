9f8c9111121451b2b0e3661dd7e9aa92dc3ff856
Benjamin Délèze <benjamin.deleze@cyberbotics.com>
2022-11-18 15:35:42 +0100

    Backport gtao fixes (#5518)
    * Backport gtao fixes
    
    * stlye

--- src/wren/PostProcessingEffect.cpp
+++ src/wren/PostProcessingEffect.cpp
@@ -118,8 +118,14 @@ namespace wren {
           mInputTextures[i]->setTextureUnit(i);
           mInputTextures[i]->bind(mInputTextureParams[i]);
 
-          const int locationTexture =
+          int locationTexture =
             mProgram->uniformLocation(static_cast<WrGlslLayoutUniform>(WR_GLSL_LAYOUT_UNIFORM_TEXTURE0 + i));
+
+          // Special gtao case to bypass a chrome driver bug where texelFetch does not work with textures coming from array:
+          // https://community.amd.com/t5/archives-discussions/bug-report-texelfetch-shader-crash-on-msaa-fbo/td-p/87124
+
+          if (locationTexture == -1 && i == 2)
+            locationTexture = mProgram->uniformLocation(static_cast<WrGlslLayoutUniform>(WR_GLSL_LAYOUT_UNIFORM_GTAO));
           assert(locationTexture >= 0);
           glUniform1i(locationTexture, mInputTextures[i]->textureUnit());
         }
