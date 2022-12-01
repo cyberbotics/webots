8d43b632d54db7d7d3d389559b12740a1d9a8795
BenjaminDeleze <benjamin.deleze@cyberbotics.com>
2022-12-01 16:56:15 +0100

    rangefinder

--- resources/web/wwi/nodes/WbRangeFinder.js
+++ resources/web/wwi/nodes/WbRangeFinder.js
@@ -2,4 +2,38 @@ import WbAbstractCamera from './WbAbstractCamera.js';
 
 // This class is used to retrieve the type of device
 export default class WbRangeFinder extends WbAbstractCamera {
+  #maxRange;
+  #minRange;
+  constructor(id, translation, scale, rotation, name, height, width, fieldOfView, maxRange, minRange) {
+    super(id, translation, scale, rotation, name, height, width, fieldOfView);
+    this.#maxRange = maxRange;
+    console.log(minRange)
+    this.#minRange = minRange;
+  }
+
+  get maxRange() {
+    return this.#maxRange;
+  }
+
+  set maxRange(newMaxRange) {
+    this.#maxRange = newMaxRange;
+    this._update();
+  }
+
+  get minRange() {
+    return this.#minRange;
+  }
+
+  set minRange(newMinRange) {
+    this.#minRange = newMinRange;
+    this._update();
+  }
+
+  _minRange() {
+    return this.#minRange;
+  }
+
+  _maxRange() {
+    return this.#maxRange;
+  }
 }
