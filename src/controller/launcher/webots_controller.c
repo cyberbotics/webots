94c1b62e0863212797eecef0af727b3b6a706961
ygoumaz <yannick.goumaz@gmail.com>
2023-01-05 16:16:36 +0100

    fix free realloc

--- src/controller/launcher/webots_controller.c
+++ src/controller/launcher/webots_controller.c
@@ -65,7 +65,6 @@ static void remove_comment(char *string) {
     string[content_size] = '\0';
     return;
   }
-  string[strlen(string)] = '\0';
 }
 
 // Replaces all occurrences of a character in a string with a different character.
@@ -674,12 +673,20 @@ int main(int argc, char **argv) {
     // Change to controller directory and edit controller file path
     chdir(controller_path_tmp);
     const size_t new_controller_size = snprintf(NULL, 0, "%s%s", controller_path, controller_name + 1) + 1;
-    controller = realloc(controller, new_controller_size);
+    char *tmp_realloc = realloc(controller, new_controller_size);
+    if (!tmp_realloc) {
+      free(controller);
+      exit(1);
+    }
     snprintf(controller, new_controller_size, "%s%s", controller_path, controller_name + 1);
   } else {
     // Add current relative path to controller for execvp() function
     const size_t controller_size = strlen(controller);
-    controller = realloc(controller, controller_size + 3);
+    char *tmp_realloc = realloc(controller, controller_size + 3);
+    if (!tmp_realloc) {
+      free(controller);
+      exit(1);
+    }
     memmove(controller + 2, controller, controller_size + 1);
     memcpy(controller, controller_path, 2);
   }
