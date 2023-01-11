61237fab04d234f89129f91aad739fc1879f4ac3
ad-daniel <daniel.dias@epfl.ch>
2023-01-11 11:27:05 +0100

    Minor

--- resources/web/wwi/protoVisualizer/Parameter.js
+++ resources/web/wwi/protoVisualizer/Parameter.js
@@ -213,7 +213,7 @@ export default class Parameter {
         const action = {};
         action['id'] = this.node.id;
         action[this.name] = this.value.toJson();
-        // console.log('setPose', action);
+        console.log('setPose', action);
         view.x3dScene.applyPose(action);
       }
       view.x3dScene.render();
