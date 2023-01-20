969ac5f9a135153793b5a54b5200f2b28f935c14
Benjamin Délèze <benjamin.deleze@cyberbotics.com>
2023-01-20 14:39:48 +0100

    Update src/webots/nodes/utils/WbSupervisorUtilities.cpp
    Co-authored-by: Olivier Michel <Olivier.Michel@cyberbotics.com>

--- src/webots/nodes/utils/WbSupervisorUtilities.cpp
+++ src/webots/nodes/utils/WbSupervisorUtilities.cpp
@@ -684,7 +684,7 @@ void WbSupervisorUtilities::handleMessage(QDataStream &stream) {
       int labelId;
       if (id < MAX_LABELS) {
         labelId = (int)id + mRobot->uniqueId() * MAX_LABELS;  // kind of hack to avoid an id clash.
-      } else {
+      else {
         mRobot->warn(tr("wb_supervisor_set_label() is out of range. The supported range is [0, %1].").arg(MAX_LABELS - 1));
         return;
       }
