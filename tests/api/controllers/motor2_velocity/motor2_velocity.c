69ed19b91dcfc0ea739765a3e25f134f8aa92a03
Dean Brettle <dean@brettle.com>
2023-02-07 05:05:30 -0800

    Rework motor2_velocity test to use quaternions.

--- tests/api/controllers/motor2_velocity/motor2_velocity.c
+++ tests/api/controllers/motor2_velocity/motor2_velocity.c
@@ -10,13 +10,17 @@
 // copied from the .wbt file
 #define TIME_STEP 32
 
-void test_position_under_velocity_control(WbDeviceTag motor, WbDeviceTag position_sensor, int axis) {
+void test_position_under_velocity_control(const char *sensor_name, const char *motor_name, double max_velocity, int axis) {
   const WbDeviceTag inertial_unit = wb_robot_get_device("inertial unit");
   wb_inertial_unit_enable(inertial_unit, TIME_STEP);
+
+  const WbDeviceTag position_sensor = wb_robot_get_device(sensor_name);
+  const WbDeviceTag motor = wb_robot_get_device(motor_name);
+
   wb_position_sensor_enable(position_sensor, TIME_STEP);
   wb_robot_step(TIME_STEP);
 
-  const double *roll_pitch_yaw = wb_inertial_unit_get_roll_pitch_yaw(inertial_unit);
+  const double *quaternion = wb_inertial_unit_get_quaternion(inertial_unit);
   double position = wb_position_sensor_get_value(position_sensor);
   int i;
 
@@ -26,52 +30,58 @@ void test_position_under_velocity_control(WbDeviceTag motor, WbDeviceTag positio
   wb_motor_set_position(motor, INFINITY);
   wb_motor_set_velocity(motor, 0);
   for (i = 0; i < 100; ++i) {
-    wb_motor_set_velocity(motor, VELOCITY * fabs(cos(i * 2 * M_PI / 100)));
+    wb_motor_set_velocity(motor, max_velocity * fabs(cos(i * 2 * M_PI / 100)));
     wb_robot_step(TIME_STEP);
-    roll_pitch_yaw = wb_inertial_unit_get_roll_pitch_yaw(inertial_unit);
+    quaternion = wb_inertial_unit_get_quaternion(inertial_unit);
+    position = wb_position_sensor_get_value(position_sensor);
+    // printf("position = %g, quaternion = %g, %g, %g, %g\n", position, quaternion[0], quaternion[1], quaternion[2],
+    //        quaternion[3]);
   }
 
-  // The new position should match the one provided by the inertial unit
-  position = wb_position_sensor_get_value(position_sensor);
-  double normalized_position = fmod(position, 2 * M_PI);
-  if (normalized_position > M_PI)
-    normalized_position -= 2 * M_PI;
-  ts_assert_double_in_delta(roll_pitch_yaw[axis], normalized_position, 0.01,
-                            "The normalized rotation measured by position sensor (%g) and the inertial unit (%g) are different",
-                            normalized_position, roll_pitch_yaw[axis]);
+  // The new position should match the one provided by the inertial unit.
+  // That means the associated quaternions should be the same or just have opposite signs.
+  // Since they are unit quaternions, |q1*q2| should equal 1.
+  double actual_quaternion[4] = {0.0, 0.0, 0.0, 0.0};
+  actual_quaternion[3] = cos(position / 2);
+  actual_quaternion[axis] = sin(position / 2);
+  double dot_product = actual_quaternion[0] * quaternion[0] + actual_quaternion[1] * quaternion[1] +
+                       actual_quaternion[2] * quaternion[2] + actual_quaternion[3] * quaternion[3];
+  ts_assert_double_in_delta(fabs(dot_product), 1.0, 1e-6,
+                            "The orientation associated with the position returned by %s is wrong: expected quaternion = "
+                            "+/-(%g, %g, %g, %g) actual quaternion = (%g, %g, %g, %g)",
+                            sensor_name, quaternion[0], quaternion[1], quaternion[2], quaternion[3], actual_quaternion[0],
+                            actual_quaternion[1], actual_quaternion[2], actual_quaternion[3]);
 
   // The position itself should not be normalized to be between -pi and pi
-  ts_assert_double_is_bigger(position, 2 * M_PI, "The position shoud be at least %g but is only %g", 2 * M_PI, position);
+  double min_expected_position = max_velocity / 2 * TIME_STEP / 1000.0 * 100.0;
+  ts_assert_double_is_bigger(position, min_expected_position, "The position measured by %s shoud be at least %g but is only %g",
+                             sensor_name, min_expected_position, position);
 
   // Returning to original position
+  // printf("Returning to original position\n");
   wb_motor_set_position(motor, original_position);
-  wb_motor_set_velocity(motor, VELOCITY);
-  double previous_position;
+  wb_motor_set_velocity(motor, max_velocity);
+  double previous_position = 0.0, previous_position2 = 0.0;
   for (i = 0; i < 400; ++i) {
     wb_robot_step(TIME_STEP);
+    previous_position2 = previous_position;
     previous_position = position;
     position = wb_position_sensor_get_value(position_sensor);
-    roll_pitch_yaw = wb_inertial_unit_get_roll_pitch_yaw(inertial_unit);
-    if (fabs(position - previous_position) < 1e-6) {
+    if (fabs(position - previous_position) < 1e-6 && fabs(previous_position - previous_position2) < 1e-6)
       break;
-    }
   }
   ts_assert_double_in_delta(original_position, position, 0.01,
-                            "The motor's position (%g) and it's original position (%g) are different", position,
-                            original_position);
-  ts_assert_double_is_bigger(1e-6, fabs(position - previous_position), "The motor has not stopped");
+                            "The position measured by %s (%g) and it's original position (%g) are different", sensor_name,
+                            position, original_position);
+  ts_assert_double_is_bigger(1e-6, fabs(position - previous_position), "The motor associated with %s has not stopped",
+                             sensor_name);
 }
 
 int main(int argc, char **argv) {
   ts_setup(argv[0]);
 
-  const WbDeviceTag position_sensor = wb_robot_get_device("position sensor1");
-  const WbDeviceTag motor = wb_position_sensor_get_motor(position_sensor);
-  test_position_under_velocity_control(motor, position_sensor, 2);
-
-  const WbDeviceTag position_sensor2 = wb_robot_get_device("position sensor2");
-  const WbDeviceTag motor2 = wb_position_sensor_get_motor(position_sensor2);
-  test_position_under_velocity_control(motor2, position_sensor2, 1);
+  test_position_under_velocity_control("position sensor1", "rotational motor1", 10.0, 1);
+  test_position_under_velocity_control("position sensor2", "rotational motor2", 10.0, 2);
 
   ts_send_success();
   return EXIT_SUCCESS;
