360ccf65db5970844f251852e0db384d863283d0
BenjaminDeleze <benjamin.deleze@cyberbotics.com>
2023-02-09 17:01:34 +0100

    Fix delete physic

--- resources/web/wwi/X3dScene.js
+++ resources/web/wwi/X3dScene.js
@@ -136,7 +136,8 @@ export default class X3dScene {
 
   #deleteObject(id) {
     const object = WbWorld.instance.nodes.get('n' + id);
-    object.delete();
+    if (typeof object !== 'undefined')
+      object.delete();
 
     WbWorld.instance.robots.forEach((robot, i) => {
       if (robot.id === 'n' + id)
