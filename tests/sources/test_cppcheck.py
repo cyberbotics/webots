551ce267f699fd3adb769ca075153fdb09144636
Yannick Goumaz <yannick.goumaz@epfl.ch>
2023-02-10 11:48:11 +0100

    Update test source script spaces

--- tests/sources/test_cppcheck.py
+++ tests/sources/test_cppcheck.py
@@ -123,10 +123,10 @@ class TestCppCheck(unittest.TestCase):
             'src/webots/widgets',
             'src/webots/wren'
         ]
-        command = 'cppcheck --enable=warning,style,performance,portability --inconclusive --force -q --library=qt '
+        command = 'cppcheck --enable=warning,style,performance,portability --inconclusive --force -q --library=qt'
         command += ' -j %s' % str(multiprocessing.cpu_count())
-        command += ' --inline-suppr --suppress=invalidPointerCast --suppress=useStlAlgorithm --suppress=uninitMemberVar '
-        command += ' --suppress=noCopyConstructor --suppress=noOperatorEq --suppress=strdupCalled --suppress=unknownMacro '
+        command += ' --inline-suppr --suppress=invalidPointerCast --suppress=useStlAlgorithm --suppress=uninitMemberVar'
+        command += ' --suppress=noCopyConstructor --suppress=noOperatorEq --suppress=strdupCalled --suppress=unknownMacro'
         # command += ' --xml '  # Uncomment this line to get more information on the errors
         command += ' --output-file=\"' + self.reportFilename + '\"'
         for include in includeDirs:
@@ -165,11 +165,11 @@ class TestCppCheck(unittest.TestCase):
         skippedfiles = [
             'projects/robots/robotis/darwin-op/plugins/remote_controls/robotis-op2_tcpip/stb_image.h'
         ]
-        command = 'cppcheck --enable=warning,style,performance,portability --inconclusive --force -q --library=qt '
-        command += '--inline-suppr --suppress=invalidPointerCast --suppress=useStlAlgorithm -UKROS_COMPILATION '
-        command += '--suppress=strdupCalled --suppress=ctuOneDefinitionRuleViolation --suppress=unknownMacro '
+        command = 'cppcheck --enable=warning,style,performance,portability --inconclusive --force -q --library=qt'
+        command += ' --inline-suppr --suppress=invalidPointerCast --suppress=useStlAlgorithm -UKROS_COMPILATION'
+        command += ' --suppress=strdupCalled --suppress=ctuOneDefinitionRuleViolation --suppress=unknownMacro'
         # command += '--xml '  # Uncomment this line to get more information on the errors
-        command += '--std=c++03 --output-file=\"' + self.reportFilename + '\"'
+        command += ' --std=c++03 --output-file=\"' + self.reportFilename + '\"'
         sources = self.add_source_files(sourceDirs, skippedDirs, skippedfiles)
         if not sources:
             return
