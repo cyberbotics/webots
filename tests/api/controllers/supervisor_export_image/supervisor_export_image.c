62cdfa97c032855764e423b325d8cb287a42f8f1
Dean Brettle <dean@brettle.com>
2023-02-14 07:20:03 -0800

    Fix style issues.

--- tests/api/controllers/supervisor_export_image/supervisor_export_image.c
+++ tests/api/controllers/supervisor_export_image/supervisor_export_image.c
@@ -1,10 +1,11 @@
-#include <math.h>
-#include <unistd.h>
-
 #include <webots/camera.h>
 #include <webots/supervisor.h>
 #include "../../../lib/ts_assertion.h"
 #include "../../../lib/ts_utils.h"
+
+#include <math.h>
+#include <stdio.h>
+#include <unistd.h>
 
 #define FILE_NAME_PNG "image.png"
 #define FILE_NAME_JPG "image.jpg"
@@ -24,27 +25,27 @@ static void test_exported_image_correct(const char *color_def, const unsigned ch
   WbDeviceTag camera = wb_robot_get_device("camera");
   wb_camera_enable(camera, 1);
 
-  char file_name_png[BUFFER_SIZE];
-  char file_name_jpg[BUFFER_SIZE];
-  sprintf(fileNamePng, "%s.png", colorDef);
-  sprintf(fileNameJpg, "%s.jpg", colorDef);
+  char file_name_png[BUFSIZ];
+  char file_name_jpg[BUFSIZ];
+  sprintf(file_name_png, "%s.png", color_def);
+  sprintf(file_name_jpg, "%s.jpg", color_def);
 
-  remove(fileNamePng);
-  remove(fileNameJpg);
+  remove(file_name_png);
+  remove(file_name_jpg);
 
-  ts_assert_boolean_not_equal(file_exists(fileNamePng), "Impossible to remove the PNG file");
-  ts_assert_boolean_not_equal(file_exists(fileNameJpg), "Impossible to remove the JPG file");
+  ts_assert_boolean_not_equal(file_exists(file_name_png), "Impossible to remove the PNG file");
+  ts_assert_boolean_not_equal(file_exists(file_name_jpg), "Impossible to remove the JPG file");
 
   // Turn to face the requested direction
-  WbFieldRef rotationField = wb_supervisor_node_get_field(wb_supervisor_node_get_from_def("Test"), "rotation");
+  WbFieldRef rotation_field = wb_supervisor_node_get_field(wb_supervisor_node_get_from_def("Test"), "rotation");
   double rotation[4] = {0.0, 0.0, 1.0, 0.0};
-  rotation[3] = angleRadians;
-  wb_supervisor_field_set_sf_rotation(rotationField, rotation);
+  rotation[3] = angle_radians;
+  wb_supervisor_field_set_sf_rotation(rotation_field, rotation);
 
   wb_robot_step(time_step);
 
-  wb_supervisor_export_image(fileNamePng, 100);
-  wb_supervisor_export_image(fileNameJpg, 100);
+  wb_supervisor_export_image(file_name_png, 100);
+  wb_supervisor_export_image(file_name_jpg, 100);
 
   // Allow some time for the files to be written. 1 second should be plenty.
   // Why isn't this all that is needed?
@@ -53,22 +54,22 @@ static void test_exported_image_correct(const char *color_def, const unsigned ch
   // Why do we need to wait a steps for this to work in real-time or fast mode but not when stepping?
   wb_robot_step(time_step);
 
-  ts_assert_boolean_equal(file_exists(fileNamePng),
-                          "wb_supervisor_export_image() failed to create the %s file.", fileNamePng);
+  ts_assert_boolean_equal(file_exists(file_name_png),
+                          "wb_supervisor_export_image() failed to create the %s file.", file_name_png);
 
-  ts_assert_boolean_equal(file_exists(fileNameJpg),
-                          "wb_supervisor_export_image() failed to create the %s file.", fileNameJpg);
+  ts_assert_boolean_equal(file_exists(file_name_jpg),
+                          "wb_supervisor_export_image() failed to create the %s file.", file_name_jpg);
 
   // Remove the emissive color and set the texture to be the image we just exported
-  WbNodeRef appearance = wb_supervisor_node_get_from_def(colorDef);
+  WbNodeRef appearance = wb_supervisor_node_get_from_def(color_def);
   WbNodeRef material = wb_supervisor_field_get_sf_node(wb_supervisor_node_get_field(appearance, "material"));
-  WbFieldRef emissiveColor = wb_supervisor_node_get_field(material, "emissiveColor");
+  WbFieldRef emissive_color = wb_supervisor_node_get_field(material, "emissiveColor");
   const double black[3] = {0.0, 0.0, 0.0};
-  wb_supervisor_field_set_sf_color(emissiveColor, black);
+  wb_supervisor_field_set_sf_color(emissive_color, black);
   WbNodeRef texture = wb_supervisor_field_get_sf_node(wb_supervisor_node_get_field(appearance, "texture"));
-  WbFieldRef textureUrl = wb_supervisor_node_get_field(texture, "url");
-  sprintf(fileNamePng, "../controllers/supervisor_export_image/%s.png", colorDef);
-  wb_supervisor_field_insert_mf_string(textureUrl, 0, fileNamePng);
+  WbFieldRef texture_url = wb_supervisor_node_get_field(texture, "url");
+  sprintf(file_name_png, "../controllers/supervisor_export_image/%s.png", color_def);
+  wb_supervisor_field_insert_mf_string(texture_url, 0, file_name_png);
 
   wb_robot_step(time_step);
 
@@ -81,9 +82,9 @@ static void test_exported_image_correct(const char *color_def, const unsigned ch
   rgb[1] = wb_camera_image_get_green(image, width, width / 2, height / 2);
   rgb[2] = wb_camera_image_get_blue(image, width, width / 2, height / 2);
 
-  ts_assert_color_in_delta(rgb[0], rgb[1], rgb[2], expectedRgb[0], expectedRgb[1], expectedRgb[2], 1,
+  ts_assert_color_in_delta(rgb[0], rgb[1], rgb[2], expected_rgb[0], expected_rgb[1], expected_rgb[2], 1,
                            "Color (%d, %d, %d) is not close enough to expected color (%d, %d, %d)", rgb[0], rgb[1], rgb[2],
-                           expectedRgb[0], expectedRgb[1], expectedRgb[2]);
+                           expected_rgb[0], expected_rgb[1], expected_rgb[2]);
 }
 
 int main(int argc, char **argv) {
