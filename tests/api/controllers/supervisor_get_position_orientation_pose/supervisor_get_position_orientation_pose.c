9b95525fa5b649fbf417e2422c838f54bdca04ab
ad-daniel <daniel.dias@epfl.ch>
2023-03-08 13:44:14 +0100

    Fix warning

--- tests/api/controllers/supervisor_get_position_orientation_pose/supervisor_get_position_orientation_pose.c
+++ tests/api/controllers/supervisor_get_position_orientation_pose/supervisor_get_position_orientation_pose.c
@@ -70,7 +70,12 @@ int main(int argc, char **argv) {
                              "wb_supervisor_node_get_pose() did not return the expected values.");
   wb_supervisor_node_remove(node_parent);
   wb_robot_step(2 * TIME_STEP);
-  const double *tracked_pose2 = wb_supervisor_node_get_pose(node, node_parent);
+  #pragma GCC diagnostic push
+  #pragma GCC diagnostic ignored "-Wunused-variable"
+  {
+  const void *tracked_pose2 = wb_supervisor_node_get_pose(node, node_parent);
+  }
+  #pragma GCC diagnostic pop
   wb_supervisor_node_disable_pose_tracking(node, node_parent);
 
   ts_send_success();
