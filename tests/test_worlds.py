56994bb397a18acf2b79ddb0c9b510a6c96f0826
ad-daniel <daniel.dias@epfl.ch>
2023-03-13 13:46:43 +0100

    Fix

--- tests/test_worlds.py
+++ tests/test_worlds.py
@@ -19,10 +19,18 @@ import unittest
 import os
 import fnmatch
 import sys
+import platform
 from subprocess import Popen, PIPE, TimeoutExpired
 
 if sys.version_info[0] != 3 or sys.version_info[1] < 7:
     sys.exit('This script requires Python version 3.7')
+
+if platform.system() == 'Darwin':
+    CACHE_DIR = os.path.join(os.path.expanduser('~'), 'Library', 'Caches', 'Cyberbotics', 'Webots', 'assets')
+elif platform.system() == 'Linux':
+    CACHE_DIR = os.path.join(os.path.expanduser('~'), '.cache', 'Cyberbotics', 'Webots', 'assets')
+elif platform.system() == 'Windows':
+    CACHE_DIR = os.path.join(os.path.expanduser('~'), 'AppData', 'Local', 'Cyberbotics', 'Webots', 'cache', 'assets')
 
 
 class TestWorldsWarnings(unittest.TestCase):
@@ -64,10 +72,12 @@ class TestWorldsWarnings(unittest.TestCase):
                 sys.exit(1)
             self.webotsFullPath = os.path.normpath(self.webotsFullPath)
 
-    def test_worlds_warnings(self):
-        """Test all the 'projects' worlds for loading warnings."""
+    def test_worlds_warnings_and_cache(self):
+        """Test all the 'projects' worlds for loading warnings and if they reference un-cached assets."""
         problematicWorlds = []
         crashedWorlds = []
+        worldsWithNonCachedAssets = []
+        cacheSizeBefore = len(os.listdir(CACHE_DIR))
         for i in range(len(self.worlds)):
             print('Testing: %d/%d: %s' % (i + 1, len(self.worlds), self.worlds[i]))
             self.process = Popen([
@@ -93,12 +103,30 @@ class TestWorldsWarnings(unittest.TestCase):
                 problematicWorlds.append(self.worlds[i])
             if errors and self.crashError in str(errors):
                 crashedWorlds.append(self.worlds[i])
+
+            # url.wbt is an exception as it contains non-cached assets on purpose
+            if os.path.basename(self.worlds[i]) == 'url.wbt':
+                cacheSizeBefore += 22
+
+            cacheSizeAfter = len(os.listdir(CACHE_DIR))
+            if cacheSizeAfter - cacheSizeBefore > 0:
+                worldsWithNonCachedAssets.append(self.worlds[i])
+                cacheSizeBefore = cacheSizeAfter  # update the counter so it doesn't trigger again for the same reason
+
         if crashedWorlds:
             print('\n\t'.join(['Impossible to test the following worlds because they crash when loading:'] + crashedWorlds))
         self.assertTrue(
             not problematicWorlds,
             msg='\n\t'.join(['The following worlds have unwanted warnings:'] + problematicWorlds)
         )
+
+        if worldsWithNonCachedAssets:
+            print('Worlds with non-cached assets:')
+            for world in worldsWithNonCachedAssets:
+                print('- ' + world)
+
+        if not crashedWorlds or not problematicWorlds or not worldsWithNonCachedAssets:
+            sys.exit(1)
 
 
 if __name__ == '__main__':
