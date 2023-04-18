1acf64ff6338ad292342382efacca0a0219a8f2a
Stefania Pedrazzi <stefaniapedrazzi@users.noreply.github.com>
2023-04-18 12:57:07 +0200

    Fix crash when mTriangleMesh is NULL

--- src/webots/nodes/WbTriangleMeshGeometry.cpp
+++ src/webots/nodes/WbTriangleMeshGeometry.cpp
@@ -361,7 +361,7 @@ bool WbTriangleMeshGeometry::isSuitableForInsertionInBoundingObject(bool warning
 }
 
 bool WbTriangleMeshGeometry::isAValidBoundingObject(bool checkOde, bool warning) const {
-  return mTriangleMesh->isValid() && WbGeometry::isAValidBoundingObject(checkOde, warning);
+  return mTriangleMesh && mTriangleMesh->isValid() && WbGeometry::isAValidBoundingObject(checkOde, warning);
 }
 /////////////////
 // Ray tracing //
