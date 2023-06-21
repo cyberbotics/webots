1616c936662b25e8b1e21937ba9a288939ba6aeb
Benjamin Délèze <benjamin.deleze@cyberbotics.com>
2023-06-21 09:25:18 +0200

    Update src/webots/gui/WbUpdatedDialog.cpp
    Co-authored-by: Stefania Pedrazzi <stefaniapedrazzi@users.noreply.github.com>

--- src/webots/gui/WbUpdatedDialog.cpp
+++ src/webots/gui/WbUpdatedDialog.cpp
@@ -46,7 +46,7 @@ WbUpdatedDialog::WbUpdatedDialog(QWidget *parent) : QDialog(parent) {
   label->setGeometry(QRect(15, 20, 315, 70));
   label->setText(
     tr("<p style=\"line-height:1.2\">"
-       "Some features introduced in this version may break backward compatibility with your worlds and protos. <br /> <br /> "
+       "Some features introduced in this version may break backward compatibility with your worlds and PROTO models. <br /> <br /> "
        "Please refer to the <a style='color: #5DADE2;' "
        "href='https://cyberbotics.com/doc/guide/upgrading-webots'>upgrade guide</a>.</p>"));
   label->setOpenExternalLinks(true);
