81c64eaad8033b503a7546180f709d9661699650
Benjamin Deleze <benjamin.deleze@cyberbotics.com>
2023-07-05 12:12:43 +0200

    clangoformat

--- src/webots/gui/WbUpdatedDialog.cpp
+++ src/webots/gui/WbUpdatedDialog.cpp
@@ -56,8 +56,9 @@ WbUpdatedDialog::WbUpdatedDialog(QWidget *parent) : QDialog(parent) {
 
   label = new QLabel(this);
   label->setGeometry(QRect(35, 200, 283, 44));
-  label->setText(tr("Find out the new features, enhancements and bug fixes of Webots R2023b revision 1 in the <a style='color: #5DADE2;' "
-                    "href='https://cyberbotics.com/doc/reference/changelog-r2023'>changelog</a>."));
+  label->setText(
+    tr("Find out the new features, enhancements and bug fixes of Webots R2023b revision 1 in the <a style='color: #5DADE2;' "
+       "href='https://cyberbotics.com/doc/reference/changelog-r2023'>changelog</a>."));
   label->setOpenExternalLinks(true);
   label->setAlignment(Qt::AlignLeading | Qt::AlignLeft | Qt::AlignTop);
   label->setWordWrap(true);
