c8ebca7ae4c10f6980ee5fcc8f7a0480a2fc8fb1
BenjaminDeleze <benjamin.deleze@cyberbotics.com>
2023-07-06 11:35:47 +0200

    cleanup

--- resources/web/wwi/X3dScene.js
+++ resources/web/wwi/X3dScene.js
@@ -201,17 +201,19 @@ export default class X3dScene {
       if (key === 'id')
         continue;
 
+      const nodeType = object.nodeType;
+
       if (key === 'translation') {
         if (object instanceof WbPose)
           object.translation = convertStringToVec3(update[key]);
-        else if (object.nodeType === WbNodeType.WB_NODE_TEXTURE_TRANSFORM)
+        else if (nodeType === WbNodeType.WB_NODE_TEXTURE_TRANSFORM)
           object.translation = convertStringToVec2(update[key]);
       } else if (key === 'rotation') {
-        if (object.nodeType === WbNodeType.WB_NODE_TEXTURE_TRANSFORM)
+        if (nodeType === WbNodeType.WB_NODE_TEXTURE_TRANSFORM)
           object.rotation = parseFloat(update[key]);
         else {
           const quaternion = convertStringToQuaternion(update[key]);
-          if (object.nodeType === WbNodeType.WB_NODE_TRACK_WHEEL)
+          if (nodeType === WbNodeType.WB_NODE_TRACK_WHEEL)
             object.updateRotation(quaternion);
           else
             object.rotation = quaternion;
@@ -219,71 +221,76 @@ export default class X3dScene {
       } else if (key === 'scale') {
         if (object instanceof WbPose)
           object.scale = convertStringToVec3(update[key]);
-        else if (object.nodeType === WbNodeType.WB_NODE_TEXTURE_TRANSFORM)
+        else if (nodeType === WbNodeType.WB_NODE_TEXTURE_TRANSFORM)
           object.scale = convertStringToVec2(update[key]);
       } else if (key === 'center') {
-        if (object.nodeType === WbNodeType.WB_NODE_TEXTURE_TRANSFORM)
+        if (nodeType === WbNodeType.WB_NODE_TEXTURE_TRANSFORM)
           object.center = convertStringToVec2(update[key]);
       } else if (key === 'castShadows') {
-        if (object instanceof WbLight || object.nodeType === WbNodeType.WB_NODE_CAD_SHAPE ||
-          object.nodeType === WbNodeType.WB_NODE_SHAPE)
+        if (object instanceof WbLight || nodeType === WbNodeType.WB_NODE_CAD_SHAPE || nodeType === WbNodeType.WB_NODE_SHAPE)
           object.castShadows = update[key].toLowerCase() === 'true';
       } else if (key === 'isPickable') {
-        if (object.nodeType === WbNodeType.WB_NODE_CAD_SHAPE || object.nodeType === WbNodeType.WB_NODE_SHAPE)
+        if (nodeType === WbNodeType.WB_NODE_CAD_SHAPE || nodeType === WbNodeType.WB_NODE_SHAPE)
           object.isPickable = update[key].toLowerCase() === 'true';
       } else if (key === 'size') {
-        if (object.nodeType === WbNodeType.WB_NODE_BOX || object.nodeType === WbNodeType.WB_NODE_PLANE)
+        if (nodeType === WbNodeType.WB_NODE_BOX || nodeType === WbNodeType.WB_NODE_PLANE)
           object.size = convertStringToVec3(update[key]);
       } else if (key === 'radius') {
-        if (object.nodeType === WbNodeType.WB_NODE_CAPSULE || object.nodeType === WbNodeType.WB_NODE_SPHERE || object.nodeType === WbNodeType.WB_NODE_CYLINDER ||
-          object.nodeType === WbNodeType.WB_NODE_SPOT_LIGHT || object.nodeType === WbNodeType.WB_NODE_POINT_LIGHT)
+        if (nodeType === WbNodeType.WB_NODE_CAPSULE || nodeType === WbNodeType.WB_NODE_SPHERE ||
+          nodeType === WbNodeType.WB_NODE_CYLINDER || nodeType === WbNodeType.WB_NODE_SPOT_LIGHT ||
+          nodeType === WbNodeType.WB_NODE_POINT_LIGHT)
           object.radius = parseFloat(update[key]);
       } else if (key === 'subdivision') {
-        if (object.nodeType === WbNodeType.WB_NODE_SPHERE || object.nodeType === WbNodeType.WB_NODE_CAPSULE || object.nodeType === WbNodeType.WB_NODE_CONE ||
-          object.nodeType === WbNodeType.WB_NODE_CYLINDER)
+        if (nodeType === WbNodeType.WB_NODE_SPHERE || nodeType === WbNodeType.WB_NODE_CAPSULE ||
+            nodeType === WbNodeType.WB_NODE_CONE || nodeType === WbNodeType.WB_NODE_CYLINDER)
           object.subdivision = parseInt(update[key]);
       } else if (key === 'ico') {
-        if (object.nodeType === WbNodeType.WB_NODE_SPHERE)
+        if (nodeType === WbNodeType.WB_NODE_SPHERE)
           object.ico = update[key].toLowerCase() === 'true';
       } else if (key === 'height') {
-        if (object.nodeType === WbNodeType.WB_NODE_CAPSULE || object.nodeType === WbNodeType.WB_NODE_CONE || object.nodeType === WbNodeType.WB_NODE_CYLINDER)
+        if (nodeType === WbNodeType.WB_NODE_CAPSULE || nodeType === WbNodeType.WB_NODE_CONE ||
+          nodeType === WbNodeType.WB_NODE_CYLINDER)
           object.height = parseFloat(update[key]);
-        else if (object.nodeType === WbNodeType.WB_NODE_ELEVATION_GRID)
+        else if (nodeType === WbNodeType.WB_NODE_ELEVATION_GRID)
           // Filter is used to remove Nan elements
           object.height = convertStringToFloatArray(update[key]).filter(e => e);
         else if (object instanceof WbAbstractCamera)
           object.height = parseInt(update[key]);
       } else if (key === 'bottom') {
-        if (object.nodeType === WbNodeType.WB_NODE_CAPSULE || object.nodeType === WbNodeType.WB_NODE_CONE || object.nodeType === WbNodeType.WB_NODE_CYLINDER)
+        if (nodeType === WbNodeType.WB_NODE_CAPSULE || nodeType === WbNodeType.WB_NODE_CONE ||
+          nodeType === WbNodeType.WB_NODE_CYLINDER)
           object.bottom = update[key].toLowerCase() === 'true';
       } else if (key === 'top') {
-        if (object.nodeType === WbNodeType.WB_NODE_CAPSULE || object.nodeType === WbNodeType.WB_NODE_CYLINDER)
+        if (nodeType === WbNodeType.WB_NODE_CAPSULE || nodeType === WbNodeType.WB_NODE_CYLINDER)
           object.top = update[key].toLowerCase() === 'true';
       } else if (key === 'side') {
-        if (object.nodeType === WbNodeType.WB_NODE_CAPSULE || object.nodeType === WbNodeType.WB_NODE_CONE || object.nodeType === WbNodeType.WB_NODE_CYLINDER)
+        if (nodeType === WbNodeType.WB_NODE_CAPSULE || nodeType === WbNodeType.WB_NODE_CONE ||
+          nodeType === WbNodeType.WB_NODE_CYLINDER)
           object.side = update[key].toLowerCase() === 'true';
       } else if (key === 'bottomRadius') {
-        if (object.nodeType === WbNodeType.WB_NODE_CONE)
+        if (nodeType === WbNodeType.WB_NODE_CONE)
           object.bottomRadius = parseFloat(update[key]);
       } else if (key === 'ccw') {
-        if (object.nodeType === WbNodeType.WB_NODE_MESH || object.nodeType === WbNodeType.WB_NODE_INDEXED_FACE_SET || object.nodeType === WbNodeType.WB_NODE_CAD_SHAPE)
+        if (nodeType === WbNodeType.WB_NODE_MESH || nodeType === WbNodeType.WB_NODE_INDEXED_FACE_SET ||
+          nodeType === WbNodeType.WB_NODE_CAD_SHAPE)
           object.ccw = update[key].toLowerCase() === 'true';
       } else if (key === 'direction') {
-        if (object.nodeType === WbNodeType.WB_NODE_SPOT_LIGHT || WbDirectionalLight)
+        if (nodeType === WbNodeType.WB_NODE_SPOT_LIGHT || WbDirectionalLight)
           object.direction = convertStringToVec3(update[key]);
       } else if (key === 'color') {
-        if (object instanceof WbLight || object.nodeType === WbNodeType.WB_NODE_FOG)
+        if (object instanceof WbLight || nodeType === WbNodeType.WB_NODE_FOG)
           object.color = convertStringToVec3(update[key]);
-        else if (object.nodeType === WbNodeType.WB_NODE_COLOR)
+        else if (nodeType === WbNodeType.WB_NODE_COLOR)
           object.color = convertStringToVec3Array(update[key]);
       } else if (key === 'name') {
-        if (object.nodeType === WbNodeType.WB_NODE_MESH)
+        if (nodeType === WbNodeType.WB_NODE_MESH)
           object.name = update[key];
       } else if (key === 'materialIndex') {
-        if (object.nodeType === WbNodeType.WB_NODE_MESH)
+        if (nodeType === WbNodeType.WB_NODE_MESH)
           object.materialIndex = parseInt(update[key]);
       } else if (key === 'url') {
-        if (object.nodeType === WbNodeType.WB_NODE_MESH || object.nodeType === WbNodeType.WB_NODE_CAD_SHAPE || object.nodeType === WbNodeType.WB_NODE_IMAGE_TEXTURE) {
+        if (nodeType === WbNodeType.WB_NODE_MESH || nodeType === WbNodeType.WB_NODE_CAD_SHAPE ||
+          nodeType === WbNodeType.WB_NODE_IMAGE_TEXTURE) {
           let urlString = update[key];
           if (urlString === '[]') {
             object.url = undefined;
@@ -294,33 +301,33 @@ export default class X3dScene {
           object.url = urlString.split('"').filter(element => { if (element !== ' ') return element; })[0];
         }
       } else if (key === 'point') {
-        if (object.nodeType === WbNodeType.WB_NODE_COORDINATE)
+        if (nodeType === WbNodeType.WB_NODE_COORDINATE)
           object.point = convertStringToVec3Array(update[key]);
-        if (object.nodeType === WbNodeType.WB_NODE_TEXTURE_COORDINATE)
+        if (nodeType === WbNodeType.WB_NODE_TEXTURE_COORDINATE)
           object.point = convertStringToVec2Array(update[key]);
       } else if (key === 'vector') {
-        if (object.nodeType === WbNodeType.WB_NODE_NORMAL)
+        if (nodeType === WbNodeType.WB_NODE_NORMAL)
           object.vector = convertStringToVec3Array(update[key]);
       } else if (key === 'coordIndex') {
-        if (object.nodeType === WbNodeType.WB_NODE_INDEXED_LINE_SET || object.nodeType === WbNodeType.WB_NODE_INDEXED_FACE_SET)
+        if (nodeType === WbNodeType.WB_NODE_INDEXED_LINE_SET || nodeType === WbNodeType.WB_NODE_INDEXED_FACE_SET)
           object.coordIndex = update[key];
       } else if (key === 'attenuation') {
-        if (object.nodeType === WbNodeType.WB_NODE_SPOT_LIGHT || object.nodeType === WbNodeType.WB_NODE_POINT_LIGHT)
+        if (nodeType === WbNodeType.WB_NODE_SPOT_LIGHT || nodeType === WbNodeType.WB_NODE_POINT_LIGHT)
           object.attenuation = convertStringToVec3(update[key]);
       } else if (key === 'location') {
-        if (object.nodeType === WbNodeType.WB_NODE_SPOT_LIGHT || object.nodeType === WbNodeType.WB_NODE_POINT_LIGHT)
+        if (nodeType === WbNodeType.WB_NODE_SPOT_LIGHT || nodeType === WbNodeType.WB_NODE_POINT_LIGHT)
           object.location = convertStringToVec3(update[key]);
-      } else if (object.nodeType === WbNodeType.WB_NODE_FOG) {
+      } else if (nodeType === WbNodeType.WB_NODE_FOG) {
         if (key === 'visibilityRange')
           object.visibilityRange = parseFloat(update[key]);
         else if (key === 'fogType')
           object.fogType = update[key];
-      } else if (object.nodeType === WbNodeType.WB_NODE_SPOT_LIGHT) {
+      } else if (nodeType === WbNodeType.WB_NODE_SPOT_LIGHT) {
         if (key === 'beamWidth')
           object.beamWidth = parseFloat(update[key]);
         else if (key === 'cutOffAngle')
           object.cutOffAngle = parseFloat(update[key]);
-      } else if (object.nodeType === WbNodeType.WB_NODE_INDEXED_FACE_SET) {
+      } else if (nodeType === WbNodeType.WB_NODE_INDEXED_FACE_SET) {
         if (key === 'normalPerVertex')
           object.normalPerVertex = update[key].toLowerCase() === 'true';
         else if (key === 'normalIndex')
@@ -329,7 +336,7 @@ export default class X3dScene {
           object.texCoordIndex = update[key];
         else if (key === 'creaseAngle')
           object.creaseAngle = parseFloat(update[key]);
-      } else if (object.nodeType === WbNodeType.WB_NODE_ELEVATION_GRID) {
+      } else if (nodeType === WbNodeType.WB_NODE_ELEVATION_GRID) {
         if (key === 'xDimension')
           object.xDimension = update[key];
         else if (key === 'xSpacing')
@@ -340,7 +347,7 @@ export default class X3dScene {
           object.ySpacing = update[key];
         else if (key === 'thickness')
           object.thickness = update[key];
-      } else if (object.nodeType === WbNodeType.WB_NODE_PBR_APPEARANCE || object.nodeType === WbNodeType.WB_NODE_MATERIAL) {
+      } else if (nodeType === WbNodeType.WB_NODE_PBR_APPEARANCE || nodeType === WbNodeType.WB_NODE_MATERIAL) {
         if (key === 'baseColor')
           object.baseColor = convertStringToVec3(update[key]);
         else if (key === 'diffuseColor')
@@ -367,24 +374,24 @@ export default class X3dScene {
           object.shininess = parseFloat(update[key]);
         else if (key === 'specularColor')
           object.specularColor = convertStringToVec3(update[key]);
-      } else if (object.nodeType === WbNodeType.WB_NODE_IMAGE_TEXTURE) {
+      } else if (nodeType === WbNodeType.WB_NODE_IMAGE_TEXTURE) {
         if (key === 'repeatS')
           object.repeatS = update[key].toLowerCase() === 'true';
         else if (key === 'repeatT')
           object.repeatT = update[key].toLowerCase() === 'true';
         else if (key === 'filtering')
           object.filtering = parseInt(update[key]);
-      } else if (object.nodeType === WbNodeType.WB_NODE_CAMERA) {
+      } else if (nodeType === WbNodeType.WB_NODE_CAMERA) {
         if (key === 'far')
           object.far = parseFloat(update[key]);
         else if (key === 'near')
           object.near = parseFloat(update[key]);
-      } else if (object.nodeType === WbNodeType.WB_NODE_RANGE_FINDER) {
+      } else if (nodeType === WbNodeType.WB_NODE_RANGE_FINDER) {
         if (key === 'maxRange')
           object.maxRange = parseFloat(update[key]);
         else if (key === 'minRange')
           object.minRange = parseFloat(update[key]);
-      } else if (object.nodeType === WbNodeType.WB_NODE_LIDAR) {
+      } else if (nodeType === WbNodeType.WB_NODE_LIDAR) {
         if (key === 'maxRange')
           object.maxRange = parseFloat(update[key]);
         else if (key === 'minRange')
@@ -397,7 +404,7 @@ export default class X3dScene {
           object.tiltAngle = parseFloat(update[key]);
         else if (key === 'verticalFieldOfView')
           object.verticalFieldOfView = parseFloat(update[key]);
-      } else if (object.nodeType === WbNodeType.WB_NODE_RADAR) {
+      } else if (nodeType === WbNodeType.WB_NODE_RADAR) {
         if (key === 'maxRange')
           object.maxRange = parseFloat(update[key]);
         else if (key === 'minRange')
@@ -406,10 +413,10 @@ export default class X3dScene {
           object.verticalFieldOfView = parseFloat(update[key]);
         else if (key === 'horizontalFieldOfView')
           object.horizontalFieldOfView = parseFloat(update[key]);
-      } else if (object.nodeType === WbNodeType.WB_NODE_PEN) {
+      } else if (nodeType === WbNodeType.WB_NODE_PEN) {
         if (key === 'write')
           object.write = update[key].toLowerCase() === 'true';
-      } else if (object.nodeType === WbNodeType.WB_NODE_DISTANCE_SENSOR) {
+      } else if (nodeType === WbNodeType.WB_NODE_DISTANCE_SENSOR) {
         if (key === 'numberOfRays')
           object.numberOfRays = parseInt(update[key]);
         else if (key === 'aperture')
@@ -428,7 +435,7 @@ export default class X3dScene {
           }
           object.lookupTable = lookupTable;
         }
-      } else if (object.nodeType === WbNodeType.WB_NODE_CONNECTOR) {
+      } else if (nodeType === WbNodeType.WB_NODE_CONNECTOR) {
         if (key === 'numberOfRotations')
           object.numberOfRotations = parseInt(update[key]);
       }
