ebb2474ff3d746e77260b8c7d824ccda1cfb678b
Yannick Goumaz <yannick.goumaz@epfl.ch>
2023-07-13 17:31:15 +0200

    Offset rotating lidar by PI

--- src/webots/nodes/WbLidar.cpp
+++ src/webots/nodes/WbLidar.cpp
@@ -362,7 +362,7 @@ void WbLidar::copyAllLayersToMemoryMappedFile() {
     maxWidth = ((double)w / 2.0) * (1.0 + ratio);
 
     double tmpAngle =
-      (fabs(mPreviousRotatingAngle / (2.0 * M_PI)) - floor(fabs(mPreviousRotatingAngle / (2.0 * M_PI)))) * (2.0 * M_PI);
+      (fabs((mPreviousRotatingAngle + M_PI) / (2.0 * M_PI)) - floor(fabs((mPreviousRotatingAngle + M_PI) / (2.0 * M_PI)))) * (2.0 * M_PI);
     if (tmpAngle < 0)
       tmpAngle += 2.0 * M_PI;
     widthOffset = resolution * (tmpAngle / (2.0 * M_PI));
@@ -383,7 +383,7 @@ void WbLidar::copyAllLayersToMemoryMappedFile() {
         memcpy(data + (i + 1) * resolution + minWidth + widthOffset, mTemporaryImage + width() * (int)(i * skip) + minWidth,
                sizeof(float) * abs(minWidth + widthOffset));
         memcpy(data + i * resolution, mTemporaryImage + width() * (int)(i * skip) - widthOffset,
-               sizeof(float) * (maxWidth + widthOffset));
+               sizeof(float) * abs(maxWidth + widthOffset));
       }
     }
   }
@@ -426,7 +426,7 @@ void WbLidar::updatePointCloud(int minWidth, int maxWidth) {
   const double dtheta = mIsActuallyRotating ? (-2 * M_PI / (double)resolution) : (-actualFieldOfView() / (w - 1.0));
   const double cosdTheta = cos(dtheta);
   const double sindTheta = sin(dtheta);
-  const double theta0 = mIsActuallyRotating ? (minWidth * dtheta) : (actualFieldOfView() / 2 + minWidth * dtheta);
+  const double theta0 = mIsActuallyRotating ? (minWidth * dtheta) - M_PI: (actualFieldOfView() / 2 + minWidth * dtheta);
   const double cosTheta0 = cos(theta0);
   const double sinTheta0 = sin(theta0);
 
