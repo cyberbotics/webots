3d3474fa88a2f9890ee1c4cb2c0814380255c7d6
CoolSpy3 <coolspythree@gmail.com>
2024-07-09 17:39:26 -0700

    add api test

---
+++ tests/other_api/controllers/cpp_device_with_same_name/cpp_device_with_same_name.cpp
@@ -0,0 +1,22 @@
+#include <webots/Accelerometer.hpp>
+#include <webots/Robot.hpp>
+
+#include "../../../lib/TestUtils.hpp"
+
+using namespace webots;
+
+int main(int argc, char **argv) {
+  Robot *robot = new Robot();
+  TestUtils ts(robot, argv[0]);
+
+  int deviceTag1 = robot->getDeviceTagFromIndex(0);
+  int deviceTag2 = robot->getDeviceTagFromIndex(0);
+  Accelerometer *device1 = dynamic_cast<Accelerometer *>(robot->getDeviceFromTag(deviceTag1));
+  Accelerometer *device2 = dynamic_cast<Accelerometer *>(robot->getDeviceFromTag(deviceTag2));
+
+  ts.assertPointerNotNull(device1, "Accelerometer 1 not found.");
+  ts.assertPointerNotNull(device2, "Accelerometer 2 not found.");
+  ts.assertIntNotEqual(device1->getTag(), device2->getTag(), "Accelerometer 1 and 2 are the same device.");
+
+  ts.sendSuccess();
+}
