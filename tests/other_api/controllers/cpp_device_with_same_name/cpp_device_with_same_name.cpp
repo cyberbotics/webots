6b1842a555b795970d3fcd5742cc472da810d92c
CoolSpy3 <55305038+CoolSpy3@users.noreply.github.com>
2024-07-09 18:36:05 -0700

    fix warnings in test

--- tests/other_api/controllers/cpp_device_with_same_name/cpp_device_with_same_name.cpp
+++ tests/other_api/controllers/cpp_device_with_same_name/cpp_device_with_same_name.cpp
@@ -1,4 +1,4 @@
-#include <webots/Accelerometer.hpp>
+#include <webots/Brake.hpp>
 #include <webots/Robot.hpp>
 
 #include "../../../lib/TestUtils.hpp"
@@ -11,12 +11,12 @@ int main(int argc, char **argv) {
 
   int deviceTag1 = robot->getDeviceTagFromIndex(0);
   int deviceTag2 = robot->getDeviceTagFromIndex(1);
-  Accelerometer *device1 = dynamic_cast<Accelerometer *>(robot->getDeviceFromTag(deviceTag1));
-  Accelerometer *device2 = dynamic_cast<Accelerometer *>(robot->getDeviceFromTag(deviceTag2));
+  Brake *device1 = dynamic_cast<Brake *>(robot->getDeviceFromTag(deviceTag1));
+  Brake *device2 = dynamic_cast<Brake *>(robot->getDeviceFromTag(deviceTag2));
 
-  ts.assertPointerNotNull(device1, "Accelerometer 1 not found.");
-  ts.assertPointerNotNull(device2, "Accelerometer 2 not found.");
-  ts.assertIntNotEqual(device1->getTag(), device2->getTag(), "Accelerometer 1 and 2 are the same device.");
+  ts.assertPointerNotNull(device1, "Brake 1 not found.");
+  ts.assertPointerNotNull(device2, "Brake 2 not found.");
+  ts.assertIntNotEqual(device1->getTag(), device2->getTag(), "Brakes 1 and 2 are the same device.");
 
   ts.sendSuccess();
 }
