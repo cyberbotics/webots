438ad2fe020f2f9239e9d47267ad5567905ef68b
CoolSpy3 <55305038+CoolSpy3@users.noreply.github.com>
2024-07-09 23:27:12 -0700

    make test objects constant
    Co-authored-by: Olivier Michel <Olivier.Michel@cyberbotics.com>

--- tests/other_api/controllers/cpp_device_with_same_name/cpp_device_with_same_name.cpp
+++ tests/other_api/controllers/cpp_device_with_same_name/cpp_device_with_same_name.cpp
@@ -6,13 +6,13 @@
 using namespace webots;
 
 int main(int argc, char **argv) {
-  Robot *robot = new Robot();
+  const Robot *robot = new Robot();
   TestUtils ts(robot, argv[0]);
 
-  int deviceTag1 = robot->getDeviceTagFromIndex(0);
-  int deviceTag2 = robot->getDeviceTagFromIndex(1);
-  Brake *device1 = dynamic_cast<Brake *>(robot->getDeviceFromTag(deviceTag1));
-  Brake *device2 = dynamic_cast<Brake *>(robot->getDeviceFromTag(deviceTag2));
+  const int deviceTag1 = robot->getDeviceTagFromIndex(0);
+  const int deviceTag2 = robot->getDeviceTagFromIndex(1);
+  const Brake *device1 = dynamic_cast<Brake *>(robot->getDeviceFromTag(deviceTag1));
+  const Brake *device2 = dynamic_cast<Brake *>(robot->getDeviceFromTag(deviceTag2));
 
   ts.assertPointerNotNull(device1, "Brake 1 not found.");
   ts.assertPointerNotNull(device2, "Brake 2 not found.");
