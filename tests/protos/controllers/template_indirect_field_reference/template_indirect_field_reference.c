98cbe2677ce922f38e3c0e46b6e2a5a967839d4b
CoolSpy3 <coolspythree@gmail.com>
2024-08-03 17:13:47 -0700

    add test

---
+++ tests/protos/controllers/template_indirect_field_reference/template_indirect_field_reference.c
@@ -0,0 +1,18 @@
+#include <webots/motor.h>
+#include <webots/robot.h>
+#include <webots/supervisor.h>
+
+#include "../../../lib/ts_assertion.h"
+#include "../../../lib/ts_utils.h"
+
+#define TIME_STEP 32
+
+int main(int argc, char **argv) {
+  ts_setup(argv[1]);  // give the controller args
+  WbNodeRef proto = wb_supervisor_node_get_from_def("PROTO_TEMPLATE_INDIRECT_FIELD_REFERENCE");
+  WbFieldRef radarCrossSection = wb_supervisor_node_get_proto_field(proto, "radarCrossSection");
+  ts_assert_double_in_delta(wb_supervisor_field_get_sf_float(radarCrossSection), 6.0, 0.0001, "radarCrossSection should be 6.0");
+  ts_send_success();
+
+  return EXIT_SUCCESS;
+}
