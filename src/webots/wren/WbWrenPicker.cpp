2d84fab930b01482997ed5cee14427ec01b003f3
Dean Brettle <dean@brettle.com>
2024-08-12 14:45:41 -0700

    Rework approach to ensure that only offscreen framebuffers are used for the initial render.

--- src/webots/wren/WbWrenPicker.cpp
+++ src/webots/wren/WbWrenPicker.cpp
@@ -134,7 +134,7 @@ bool WbWrenPicker::pick(int x, int y) {
   wr_viewport_enable_skybox(mViewport, false);
   wr_scene_enable_translucence(scene, false);
   wr_scene_enable_depth_reset(scene, false);
-  wr_scene_render_to_viewports(scene, 1, &mViewport, "picking", true);
+  wr_scene_render_to_viewports(scene, 1, &mViewport, "picking", true, false);
   wr_scene_enable_depth_reset(scene, true);
   wr_viewport_enable_skybox(mViewport, true);
   wr_scene_enable_translucence(scene, true);
