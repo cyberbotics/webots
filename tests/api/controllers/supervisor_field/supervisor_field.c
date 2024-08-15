c4bd83d2437f0ec46a18fd5a08d73da81066161d
CoolSpy3 <coolspythree@gmail.com>
2024-08-15 11:55:59 -0700

    fix typo

--- tests/api/controllers/supervisor_field/supervisor_field.c
+++ tests/api/controllers/supervisor_field/supervisor_field.c
@@ -70,7 +70,7 @@ int main(int argc, char **argv) {
   d = wb_supervisor_field_get_sf_float(field);
   ts_assert_double_equal(d, 0.73, "Proto \"camera_fieldOfView\" SFFloat field should have value 0.73 not %f", d);
 
-  // invalid to retrive a non-PROTO field or a PROTO parameter
+  // invalid to retrieve a non-PROTO field or a PROTO parameter
   internal_field = wb_supervisor_node_get_proto_field(box, "scale");
   ts_assert_pointer_null(internal_field, "wb_supervisor_node_get_proto_field should only work for PROTO nodes.");
   internal_field = wb_supervisor_node_get_proto_field(mfTest, "mfBool");
