9c318acf7d08c627b8f2e2fdbe5e8787683651b1
CoolSpy3 <coolspythree@gmail.com>
2024-08-16 16:35:14 -0700

    update supervisor_notify_import_remove test

--- tests/api/controllers/supervisor_notify_import_remove_mf/supervisor_notify_import_remove_mf.c
+++ tests/api/controllers/supervisor_notify_import_remove_mf/supervisor_notify_import_remove_mf.c
@@ -12,19 +12,34 @@ int main(int argc, char **argv) {
 
   int i = 0;
   WbNodeRef node = wb_supervisor_node_get_from_def("NODE");
+  WbProtoRef baseProto = wb_supervisor_proto_get_parent(wb_supervisor_node_get_proto(node));
   const int FIELD_COUNT = 8;
   WbFieldRef mfField[FIELD_COUNT];
-  mfField[0] = wb_supervisor_node_get_field(node, "bool");
-  mfField[1] = wb_supervisor_node_get_field(node, "int");
-  mfField[2] = wb_supervisor_node_get_field(node, "float");
-  mfField[3] = wb_supervisor_node_get_field(node, "vec2");
-  mfField[4] = wb_supervisor_node_get_field(node, "vec3");
-  mfField[5] = wb_supervisor_node_get_field(node, "rot");
-  mfField[6] = wb_supervisor_node_get_field(node, "color");
-  mfField[7] = wb_supervisor_node_get_field(node, "string");
+  mfField[0] = wb_supervisor_node_get_parameter(node, "bool");
+  mfField[1] = wb_supervisor_node_get_parameter(node, "int");
+  mfField[2] = wb_supervisor_node_get_parameter(node, "float");
+  mfField[3] = wb_supervisor_node_get_parameter(node, "vec2");
+  mfField[4] = wb_supervisor_node_get_parameter(node, "vec3");
+  mfField[5] = wb_supervisor_node_get_parameter(node, "rot");
+  mfField[6] = wb_supervisor_node_get_parameter(node, "color");
+  mfField[7] = wb_supervisor_node_get_parameter(node, "string");
+  WbFieldRef mfProtoFields[FIELD_COUNT];
+  mfProtoFields[0] = wb_supervisor_proto_get_parameter(baseProto, "bool");
+  mfProtoFields[1] = wb_supervisor_proto_get_parameter(baseProto, "int");
+  mfProtoFields[2] = wb_supervisor_proto_get_parameter(baseProto, "float");
+  mfProtoFields[3] = wb_supervisor_proto_get_parameter(baseProto, "vec2");
+  mfProtoFields[4] = wb_supervisor_proto_get_parameter(baseProto, "vec3");
+  mfProtoFields[5] = wb_supervisor_proto_get_parameter(baseProto, "rot");
+  mfProtoFields[6] = wb_supervisor_proto_get_parameter(baseProto, "color");
+  mfProtoFields[7] = wb_supervisor_proto_get_parameter(baseProto, "string");
   int mfFieldCount[FIELD_COUNT];
-  for (i = 0; i < FIELD_COUNT; ++i)
-    mfFieldCount[i] = wb_supervisor_field_get_count(mfField[i]);
+  for (i = 0; i < FIELD_COUNT; ++i) {
+    const int count = wb_supervisor_field_get_count(mfField[i]);
+    mfFieldCount[i] = count;
+    const int protoCount = wb_supervisor_field_get_count(mfProtoFields[i]);
+    ts_assert_int_equal(protoCount, count,
+                        "Size of proto field %d not correctly initialized: found %d, expected %d", i, count, count);
+  }
 
   wb_robot_step(3 * TIME_STEP);
 
@@ -35,6 +50,10 @@ int main(int argc, char **argv) {
     ts_assert_int_equal(count, mfFieldCount[i] + increment,
                         "Size of field %d not correctly updated after item inserted: found %d, expected %d", i, count,
                         mfFieldCount[i] + increment);
+    const int protoCount = wb_supervisor_field_get_count(mfProtoFields[i]);
+    ts_assert_int_equal(protoCount, mfFieldCount[i] + increment,
+                        "Size of proto field %d not correctly updated after item inserted: found %d, expected %d", i,
+                        protoCount, mfFieldCount[i] + increment);
   }
 
   for (i = FIELD_COUNT - 1; i >= 0; --i) {
@@ -44,6 +63,10 @@ int main(int argc, char **argv) {
     ts_assert_int_equal(count, mfFieldCount[i] + increment,
                         "Size of field %d not correctly updated after item removed: found %d, expected %d", i, count,
                         mfFieldCount[i] + increment);
+    const int protoCount = wb_supervisor_field_get_count(mfProtoFields[i]);
+    ts_assert_int_equal(protoCount, mfFieldCount[i] + increment,
+                        "Size of proto field %d not correctly updated after item removed: found %d, expected %d", i,
+                        protoCount, mfFieldCount[i] + increment);
   }
 
   ts_send_success();
