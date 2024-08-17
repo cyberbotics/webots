4fcd3fc329de7b7d7681c0e7eeb4e5a146efdb47
CoolSpy3 <coolspythree@gmail.com>
2024-08-17 16:12:51 -0700

    add additional log info

--- tests/api/controllers/supervisor_proto/supervisor_proto.c
+++ tests/api/controllers/supervisor_proto/supervisor_proto.c
@@ -21,22 +21,22 @@ struct FieldDefinition {
   WbFieldType type;
 };
 
-static void assert_hierarchy_correct(WbProtoRef proto, const char **expected_hierarchy) {
+static void assert_hierarchy_correct(WbProtoRef proto, const char **expected_hierarchy, const char *node_name) {
   int i = 0;
   while (expected_hierarchy[i]) {
     const char *actual_name = wb_supervisor_proto_get_type_name(proto);
-    ts_assert_pointer_not_null(actual_name, "Hierarchy mismatch! Expected \"%s\", but got NULL", expected_hierarchy[i]);
-    ts_assert_string_equal(actual_name, expected_hierarchy[i], "Hierarchy mismatch! Expected \"%s\", but got %s", expected_hierarchy[i], actual_name);
+    ts_assert_pointer_not_null(actual_name, "(%s) Hierarchy mismatch! Expected \"%s\", but got NULL", expected_hierarchy[i]);
+    ts_assert_string_equal(actual_name, expected_hierarchy[i], "(%s) Hierarchy mismatch! Expected \"%s\", but got %s", expected_hierarchy[i], actual_name);
     proto = wb_supervisor_proto_get_parent(proto);
     i++;
   }
-  ts_assert_pointer_null(proto, "Hierarchy mismatch! Expected NULL, but got \"%s\"", wb_supervisor_proto_get_type_name(proto));
+  ts_assert_pointer_null(proto, "(%s) Hierarchy mismatch! Expected NULL, but got \"%s\"", wb_supervisor_proto_get_type_name(proto));
 }
 
-static void assert_fields_correct(WbProtoRef proto, const struct FieldDefinition **expected_fields) {
+static void assert_fields_correct(WbProtoRef proto, const struct FieldDefinition **expected_fields, const char *node_name) {
   int i = 0;
   while (expected_fields[i]) {
-    ts_assert_pointer_not_null(proto, "Expected more fields, but proto is null. The test should've failed when checking the hierarchy. This likely means that the expected hierarchy is incorrectly configured.");
+    ts_assert_pointer_not_null(proto, "(%s) Expected more fields, but proto is null. The test should've failed when checking the hierarchy. This likely means that the expected hierarchy is incorrectly configured.", node_name);
 
     int number_of_fields = 0;
     while (expected_fields[i][number_of_fields].type != WB_NO_FIELD)
@@ -45,20 +45,20 @@ static void assert_fields_correct(WbProtoRef proto, const struct FieldDefinition
     const char *proto_name = wb_supervisor_proto_get_type_name(proto);
 
     const int actual_number_of_fields = wb_supervisor_proto_get_number_of_parameters(proto);
-    ts_assert_int_equal(actual_number_of_fields, number_of_fields, "Wrong number of fields in proto \"%s\". Expected %d, but got %d", proto_name, number_of_fields, actual_number_of_fields);
+    ts_assert_int_equal(actual_number_of_fields, number_of_fields, "(%s) Wrong number of fields in proto \"%s\". Expected %d, but got %d", node_name, proto_name, number_of_fields, actual_number_of_fields);
 
     for (int j = 0; j < number_of_fields; j++) {
       const char *expected_name = expected_fields[i][j].name;
       const WbFieldType expected_type = expected_fields[i][j].type;
 
       WbFieldRef field = wb_supervisor_proto_get_parameter_by_index(proto, j);
-      ts_assert_pointer_not_null(field, "Field \"%d\" not found in proto \"%s\" despite the reported number of parameters being correct", j, proto_name);
+      ts_assert_pointer_not_null(field, "(%s) Field \"%d\" not found in proto \"%s\" despite the reported number of parameters being correct", node_name, j, proto_name);
 
       const char *actual_name = wb_supervisor_field_get_name(field);
-      ts_assert_string_equal(actual_name, expected_name, "Field mismatch in proto \"%s\"! Expected \"%s\", but got \"%s\"", proto_name, expected_name, actual_name);
+      ts_assert_string_equal(actual_name, expected_name, "(%s) Field mismatch in proto \"%s\"! Expected \"%s\", but got \"%s\"", node_name, proto_name, expected_name, actual_name);
 
       const WbFieldType actual_type = wb_supervisor_field_get_type(field);
-      ts_assert_int_equal(actual_type, expected_type, "Field \"%s\" in proto \"%s\" has wrong type! Expected %d, but got %d", expected_name, proto_name, expected_type, actual_type);
+      ts_assert_int_equal(actual_type, expected_type, "(%s) Field \"%s\" in proto \"%s\" has wrong type! Expected %d, but got %d", node_name, expected_name, proto_name, expected_type, actual_type);
     }
 
     proto = wb_supervisor_proto_get_parent(proto);
@@ -114,12 +114,12 @@ int main(int argc, char **argv) {
   WbProtoRef hierarchy_proto = wb_supervisor_node_get_proto(hierarchy);
   WbProtoRef internal_proto = wb_supervisor_node_get_proto(internal_node);
 
-  assert_hierarchy_correct(hierarchy_proto, expected_hierarchy);
-  assert_fields_correct(hierarchy_proto, expected_fields);
+  assert_hierarchy_correct(hierarchy_proto, expected_hierarchy, "HIERARCHY");
+  assert_fields_correct(hierarchy_proto, expected_fields, "HIERARCHY");
   // Because the main hierarchy node and the internal node are both of type SolidProtoInternal,
   // we can just skip the first element of both arrays when validating the internal node
-  assert_hierarchy_correct(internal_proto, expected_hierarchy + 1);
-  assert_fields_correct(internal_proto, expected_fields + 1);
+  assert_hierarchy_correct(internal_proto, expected_hierarchy + 1, "INTERNAL_HIERARCHY");
+  assert_fields_correct(internal_proto, expected_fields + 1, "INTERNAL_HIERARCHY");
 
   ts_send_success();
   return EXIT_SUCCESS;
