8293c4ca2aaf2fd75f0ceb3f10a75828b7fe8df1
Dean Brettle <dean@brettle.com>
2024-08-17 10:12:20 -0700

    Get Qt from system on ubuntu 22.04 and via aqtinstall on Ubuntu 20.04.

--- scripts/install/qt_linux_installer.sh
+++ scripts/install/qt_linux_installer.sh
@@ -3,10 +3,30 @@
 # exit when any command fails
 set -e
 
-# follow the instructions from https://github.com/cyberbotics/webots/wiki/Qt-compilation#linux to download and compile Qt before executing this script.
-
 QT_VERSION=6.5.3
-QT_INSTALLATION_PATH=~/Qt/${QT_VERSION}/gcc_64
+DISTRIBUTION=$(lsb_release -is)
+DISTRIBUTION_VERSION=$(lsb_release -rs)
+if [ $DISTRIBUTION == "Ubuntu" -a $DISTRIBUTION_VERSION == "22.04" ]; then
+       apt install --yes qt6-base-dev qt6-declarative-dev qt6-base-dev-tools qt6-l10n-tools qt6-websockets-dev qt6-wayland qt6-translations-l10n
+       QT_INSTALLATION_PATH=/
+       QT_INSTALLATION_BIN_PATH=/usr/lib/qt6/bin
+       QT_INSTALLATION_LIBEXEC_PATH=/usr/lib/qt6/libexec
+       QT_INSTALLATION_LIB_PATH=/usr/lib/x86_64-linux-gnu
+       QT_INSTALLATION_INCLUDE_PATH=/usr/include/x86_64-linux-gnu/qt6
+       QT_INSTALLATION_PLUGINS_PATH=/usr/lib/x86_64-linux-gnu/qt6/plugins
+       QT_INSTALLATION_TRANSLATIONS_PATH=/usr/share/qt6/translations
+else
+       pip install --no-input aqtinstall
+       aqt install-qt --outputdir ~/Qt linux desktop ${QT_VERSION} gcc_64 -m qtwebsockets
+       QT_INSTALLATION_PATH=~/Qt/${QT_VERSION}/gcc_64
+       QT_INSTALLATION_BIN_PATH=${QT_INSTALLATION_PATH}/bin
+       QT_INSTALLATION_LIBEXEC_PATH=${QT_INSTALLATION_PATH}/libexec
+       QT_INSTALLATION_LIB_PATH=${QT_INSTALLATION_PATH}/lib
+       QT_INSTALLATION_INCLUDE_PATH=${QT_INSTALLATION_PATH}/include
+       QT_INSTALLATION_PLUGINS_PATH=${QT_INSTALLATION_PATH}/plugins
+       QT_INSTALLATION_TRANSLATIONS_PATH=${QT_INSTALLATION_PATH}/translations
+fi
+
 WEBOTS_HOME="$(cd "$(dirname "${BASH_SOURCE[0]}" )"/../.. && pwd)"
 
 echo Installing Qt in Webots
@@ -54,57 +74,57 @@ mkdir lib/webots/qt/plugins/wayland-decoration-client
 mkdir lib/webots/qt/resources
 mkdir lib/webots/qt/translations
 
-cp $QT_INSTALLATION_PATH/bin/lrelease                              bin/qt/
-cp $QT_INSTALLATION_PATH/bin/lupdate                               bin/qt/
-cp $QT_INSTALLATION_PATH/libexec/moc                               bin/qt/
-cp -r $QT_INSTALLATION_PATH/include/QtConcurrent                   include/qt/QtConcurrent/
-cp -r $QT_INSTALLATION_PATH/include/QtCore                         include/qt/QtCore/
-cp -r $QT_INSTALLATION_PATH/include/QtGui                          include/qt/QtGui/
-cp -r $QT_INSTALLATION_PATH/include/QtNetwork                      include/qt/QtNetwork/
-cp -r $QT_INSTALLATION_PATH/include/QtOpenGL                       include/qt/QtOpenGL/
-cp -r $QT_INSTALLATION_PATH/include/QtOpenGLWidgets                include/qt/QtOpenGLWidgets/
-cp -r $QT_INSTALLATION_PATH/include/QtPrintSupport                 include/qt/QtPrintSupport/
-cp -r $QT_INSTALLATION_PATH/include/QtQml                          include/qt/QtQml/
-cp -r $QT_INSTALLATION_PATH/include/QtWebSockets                   include/qt/QtWebSockets/
-cp -r $QT_INSTALLATION_PATH/include/QtWidgets                      include/qt/QtWidgets/
-cp -r $QT_INSTALLATION_PATH/include/QtXml                          include/qt/QtXml/
+cp $QT_INSTALLATION_BIN_PATH/lrelease                              bin/qt/
+cp $QT_INSTALLATION_BIN_PATH/lupdate                               bin/qt/
+cp $QT_INSTALLATION_LIBEXEC_PATH/moc                               bin/qt/
+cp -r $QT_INSTALLATION_INCLUDE_PATH/QtConcurrent                   include/qt/QtConcurrent/
+cp -r $QT_INSTALLATION_INCLUDE_PATH/QtCore                         include/qt/QtCore/
+cp -r $QT_INSTALLATION_INCLUDE_PATH/QtGui                          include/qt/QtGui/
+cp -r $QT_INSTALLATION_INCLUDE_PATH/QtNetwork                      include/qt/QtNetwork/
+cp -r $QT_INSTALLATION_INCLUDE_PATH/QtOpenGL                       include/qt/QtOpenGL/
+cp -r $QT_INSTALLATION_INCLUDE_PATH/QtOpenGLWidgets                include/qt/QtOpenGLWidgets/
+cp -r $QT_INSTALLATION_INCLUDE_PATH/QtPrintSupport                 include/qt/QtPrintSupport/
+cp -r $QT_INSTALLATION_INCLUDE_PATH/QtQml                          include/qt/QtQml/
+cp -r $QT_INSTALLATION_INCLUDE_PATH/QtWebSockets                   include/qt/QtWebSockets/
+cp -r $QT_INSTALLATION_INCLUDE_PATH/QtWidgets                      include/qt/QtWidgets/
+cp -r $QT_INSTALLATION_INCLUDE_PATH/QtXml                          include/qt/QtXml/
 rm -rf include/qt/Qt*/*/$QT_VERSION
-cp -a $QT_INSTALLATION_PATH/lib/libQt6Concurrent.so*        lib/webots/
-cp -a $QT_INSTALLATION_PATH/lib/libQt6Core.so*              lib/webots/
-cp -a $QT_INSTALLATION_PATH/lib/libQt6DBus.so*              lib/webots/
-cp -a $QT_INSTALLATION_PATH/lib/libQt6Gui.so*               lib/webots/
-cp -a $QT_INSTALLATION_PATH/lib/libQt6Network.so*           lib/webots/
-cp -a $QT_INSTALLATION_PATH/lib/libQt6OpenGL.so*            lib/webots/
-cp -a $QT_INSTALLATION_PATH/lib/libQt6OpenGLWidgets.so*     lib/webots/
-cp -a $QT_INSTALLATION_PATH/lib/libQt6PrintSupport.so*      lib/webots/
-cp -a $QT_INSTALLATION_PATH/lib/libQt6Qml.so*               lib/webots/
-cp -a $QT_INSTALLATION_PATH/lib/libQt6WaylandClient.so*     lib/webots/
-cp -a $QT_INSTALLATION_PATH/lib/libQt6WaylandEglClientHwIntegration.so*     lib/webots/
-cp -a $QT_INSTALLATION_PATH/lib/libQt6WebSockets.so*        lib/webots/
-cp -a $QT_INSTALLATION_PATH/lib/libQt6Widgets.so*           lib/webots/
-cp -a $QT_INSTALLATION_PATH/lib/libQt6XcbQpa.so*            lib/webots/
-cp -a $QT_INSTALLATION_PATH/lib/libQt6Xml.so*               lib/webots/
-cp -a $QT_INSTALLATION_PATH/lib/libicudata.so*              lib/webots/
-cp -a $QT_INSTALLATION_PATH/lib/libicui18n.so*              lib/webots/
-cp -a $QT_INSTALLATION_PATH/lib/libicuuc.so*                lib/webots/
+cp -a $QT_INSTALLATION_LIB_PATH/libQt6Concurrent.so*        lib/webots/
+cp -a $QT_INSTALLATION_LIB_PATH/libQt6Core.so*              lib/webots/
+cp -a $QT_INSTALLATION_LIB_PATH/libQt6DBus.so*              lib/webots/
+cp -a $QT_INSTALLATION_LIB_PATH/libQt6Gui.so*               lib/webots/
+cp -a $QT_INSTALLATION_LIB_PATH/libQt6Network.so*           lib/webots/
+cp -a $QT_INSTALLATION_LIB_PATH/libQt6OpenGL.so*            lib/webots/
+cp -a $QT_INSTALLATION_LIB_PATH/libQt6OpenGLWidgets.so*     lib/webots/
+cp -a $QT_INSTALLATION_LIB_PATH/libQt6PrintSupport.so*      lib/webots/
+cp -a $QT_INSTALLATION_LIB_PATH/libQt6Qml.so*               lib/webots/
+cp -a $QT_INSTALLATION_LIB_PATH/libQt6WaylandClient.so*     lib/webots/
+cp -a $QT_INSTALLATION_LIB_PATH/libQt6WaylandEglClientHwIntegration.so*     lib/webots/
+cp -a $QT_INSTALLATION_LIB_PATH/libQt6WebSockets.so*        lib/webots/
+cp -a $QT_INSTALLATION_LIB_PATH/libQt6Widgets.so*           lib/webots/
+cp -a $QT_INSTALLATION_LIB_PATH/libQt6XcbQpa.so*            lib/webots/
+cp -a $QT_INSTALLATION_LIB_PATH/libQt6Xml.so*               lib/webots/
+cp -a $QT_INSTALLATION_LIB_PATH/libicudata.so*              lib/webots/
+cp -a $QT_INSTALLATION_LIB_PATH/libicui18n.so*              lib/webots/
+cp -a $QT_INSTALLATION_LIB_PATH/libicuuc.so*                lib/webots/
 echo $'[Paths]\nPrefix = ..\n' >                                    lib/webots/qt/libexec/qt.conf
-cp -a $QT_INSTALLATION_PATH/plugins/platforms/libqxcb.so               lib/webots/qt/plugins/platforms/
-cp -a $QT_INSTALLATION_PATH/plugins/platforms/libqwayland-egl.so       lib/webots/qt/plugins/platforms/
-cp -a $QT_INSTALLATION_PATH/plugins/platforms/libqwayland-generic.so   lib/webots/qt/plugins/platforms/
-cp -a $QT_INSTALLATION_PATH/plugins/platformthemes/*         lib/webots/qt/plugins/platformthemes/
-cp -a $QT_INSTALLATION_PATH/plugins/platforminputcontexts/libcomposeplatforminputcontextplugin.so lib/webots/qt/plugins/platforminputcontexts/
-cp -a $QT_INSTALLATION_PATH/plugins/platforminputcontexts/libibusplatforminputcontextplugin.so    lib/webots/qt/plugins/platforminputcontexts/
-cp -a $QT_INSTALLATION_PATH/plugins/printsupport/libcupsprintersupport.so lib/webots/qt/plugins/printsupport/
-cp -a $QT_INSTALLATION_PATH/plugins/tls/*.so                           lib/webots/qt/plugins/tls/
-cp -a $QT_INSTALLATION_PATH/plugins/xcbglintegrations/libqxcb-glx-integration.so lib/webots/qt/plugins/xcbglintegrations/
-cp -a $QT_INSTALLATION_PATH/plugins/wayland-graphics-integration-client/* lib/webots/qt/plugins/wayland-graphics-integration-client/
-cp -a $QT_INSTALLATION_PATH/plugins/wayland-shell-integration/*        lib/webots/qt/plugins/wayland-shell-integration/
-cp -a $QT_INSTALLATION_PATH/plugins/wayland-decoration-client/*        lib/webots/qt/plugins/wayland-decoration-client/
-cp -a $QT_INSTALLATION_PATH/plugins/imageformats/libqjpeg.so           lib/webots/qt/plugins/imageformats/
-cp -a $QT_INSTALLATION_PATH/translations/qt_*                       lib/webots/qt/translations/
-cp -a $QT_INSTALLATION_PATH/translations/qtbase_*                   lib/webots/qt/translations/
-cp -a $QT_INSTALLATION_PATH/translations/qtdeclarative_*            lib/webots/qt/translations/
-cp -a $QT_INSTALLATION_PATH/translations/qtwebsockets_*             lib/webots/qt/translations/
+cp -a $QT_INSTALLATION_PLUGINS_PATH/platforms/libqxcb.so               lib/webots/qt/plugins/platforms/
+cp -a $QT_INSTALLATION_PLUGINS_PATH/platforms/libqwayland-egl.so       lib/webots/qt/plugins/platforms/
+cp -a $QT_INSTALLATION_PLUGINS_PATH/platforms/libqwayland-generic.so   lib/webots/qt/plugins/platforms/
+cp -a $QT_INSTALLATION_PLUGINS_PATH/platformthemes/*         lib/webots/qt/plugins/platformthemes/
+cp -a $QT_INSTALLATION_PLUGINS_PATH/platforminputcontexts/libcomposeplatforminputcontextplugin.so lib/webots/qt/plugins/platforminputcontexts/
+cp -a $QT_INSTALLATION_PLUGINS_PATH/platforminputcontexts/libibusplatforminputcontextplugin.so    lib/webots/qt/plugins/platforminputcontexts/
+cp -a $QT_INSTALLATION_PLUGINS_PATH/printsupport/libcupsprintersupport.so lib/webots/qt/plugins/printsupport/
+cp -a $QT_INSTALLATION_PLUGINS_PATH/tls/*.so                           lib/webots/qt/plugins/tls/
+cp -a $QT_INSTALLATION_PLUGINS_PATH/xcbglintegrations/libqxcb-glx-integration.so lib/webots/qt/plugins/xcbglintegrations/
+cp -a $QT_INSTALLATION_PLUGINS_PATH/wayland-graphics-integration-client/* lib/webots/qt/plugins/wayland-graphics-integration-client/
+cp -a $QT_INSTALLATION_PLUGINS_PATH/wayland-shell-integration/*        lib/webots/qt/plugins/wayland-shell-integration/
+cp -a $QT_INSTALLATION_PLUGINS_PATH/wayland-decoration-client/*        lib/webots/qt/plugins/wayland-decoration-client/
+cp -a $QT_INSTALLATION_PLUGINS_PATH/imageformats/libqjpeg.so           lib/webots/qt/plugins/imageformats/
+cp -a $QT_INSTALLATION_TRANSLATIONS_PATH/qt_*                       lib/webots/qt/translations/
+cp -a $QT_INSTALLATION_TRANSLATIONS_PATH/qtbase_*                   lib/webots/qt/translations/
+cp -a $QT_INSTALLATION_TRANSLATIONS_PATH/qtdeclarative_*            lib/webots/qt/translations/
+cp -a $QT_INSTALLATION_TRANSLATIONS_PATH/qtwebsockets_*             lib/webots/qt/translations/
 
 ARCHIVE=dependencies/webots-qt-$QT_VERSION-linux64-release.tar.bz2
 echo Compressing $ARCHIVE \(please wait\)
