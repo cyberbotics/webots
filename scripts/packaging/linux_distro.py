f7a8341b47ed81e218723eb34778c30f52a536d1
Dean Brettle <dean@brettle.com>
2024-08-18 06:50:54 -0700

    Copy system openssl3 libs on Ubuntu 22.04 and use built libs on Ubuntu 20.04.
    Building libs is still TODO.

--- scripts/packaging/linux_distro.py
+++ scripts/packaging/linux_distro.py
@@ -122,17 +122,17 @@ class LinuxWebotsPackage(WebotsPackage):
             # copy OpenSSL libraries from Ubuntu 20.04 system and needed on Ubuntu 22.04
             system_lib_path = os.path.join('/usr', 'lib', 'x86_64-linux-gnu')
             package_webots_lib = os.path.join(self.package_webots_path, 'lib', 'webots')
-            if distro.version() == '22.04':
-                shutil.copy(os.path.join(self.webots_home, 'lib', 'webots', 'libcrypto.so.1.1'),
-                            os.path.join(package_webots_lib, 'libcrypto.so.1.1'))
+            if distro.version() == '20.04':
+                shutil.copy(os.path.join(self.webots_home, 'lib', 'webots', 'libcrypto.so.3'),
+                            os.path.join(package_webots_lib, 'libcrypto.so.3'))
                 shutil.copy(os.path.join(self.webots_home, 'lib', 'webots', 'libcrypto.so'),
                             os.path.join(package_webots_lib, 'libcrypto.so'))
-                shutil.copy(os.path.join(self.webots_home, 'lib', 'webots', 'libssl.so.1.1'),
-                            os.path.join(package_webots_lib, 'libssl.so.1.1'))
+                shutil.copy(os.path.join(self.webots_home, 'lib', 'webots', 'libssl.so.3'),
+                            os.path.join(package_webots_lib, 'libssl.so.3'))
                 shutil.copy(os.path.join(self.webots_home, 'lib', 'webots', 'libssl.so'),
                             os.path.join(package_webots_lib, 'libssl.so'))
-            else:  # Ubuntu 20.04
-                openssl_libs = ['libcrypto.so.1.1', 'libcrypto.so', 'libssl.so.1.1', 'libssl.so']
+            else:  # Ubuntu 22.04
+                openssl_libs = ['libcrypto.so.3', 'libcrypto.so', 'libssl.so.3', 'libssl.so']
                 for lib in openssl_libs:
                     shutil.copy(os.path.join(system_lib_path, lib), package_webots_lib)
 
