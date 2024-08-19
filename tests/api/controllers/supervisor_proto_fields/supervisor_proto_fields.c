11791840f6a2b5def6f4d0748743b1aecbb8ea37
CoolSpy3 <coolspythree@gmail.com>
2024-08-18 19:26:44 -0700

    don't expect changes in read-only fields

--- tests/api/controllers/supervisor_proto_fields/supervisor_proto_fields.c
+++ tests/api/controllers/supervisor_proto_fields/supervisor_proto_fields.c
@@ -252,7 +252,7 @@ int main(int argc, char **argv) {
       check_field(actual_main_parameters[i], actual_main_parameters[i], i, false, main_field_names[i][0], "Update field [main parameter]");
     for (int j = 0; j < NUMBER_OF_HIERARCHY_LEVELS; j++)
       if (main_fields[i][j])
-        check_field(main_fields[i][j], actual_main_parameters[i], i, false, main_field_names[i][j], "Update field [main field]");
+        check_field(main_fields[i][j], actual_main_parameters[i], actual_main_parameters[i] ? i : 0.01, false, main_field_names[i][j], "Update field [main field]");
   }
 
   for (int i = 0; i < NUMBER_OF_INTERNAL_FIELDS; i++) {
@@ -260,7 +260,7 @@ int main(int argc, char **argv) {
       check_field(actual_internal_parameters[i], actual_internal_parameters[i], i, false, internal_field_names[i][0], "Update field [internal parameter]");
     for (int j = 0; j < NUMBER_OF_HIERARCHY_LEVELS; j++)
       if (internal_fields[i][j])
-        check_field(internal_fields[i][j], actual_internal_parameters[i], i, false, internal_field_names[i][j], "Update field [internal field]");
+        check_field(internal_fields[i][j], actual_internal_parameters[i], actual_internal_parameters[i] ? i : 0.01, false, internal_field_names[i][j], "Update field [internal field]");
   }
 
   ts_send_success();
