cyberbotics/webots

commit 0ee25809535c09667f0ecd0b275645d2ab07c317

Make supervisor_export_image use FBO instead of screen framebuffer. (#6622)
* Make supervisor_export_image use FBO instead of screen framebuffer.

This makes the supervisor_export_image test pass under Wayland and has the added benefit of making that test (and the billboard test) work with "--no-rendering --minimize". As a result those tests are moved into the api group and the with_rendering group is removed.

* Add changelog entry.

src/webots/gui/WbWrenWindow.cpp
@@ -446,15 +446,5 @@ void WbWrenWindow::feedMultimediaStreamer() {
 }
 
 void WbWrenWindow::readPixels(int width, int height, unsigned int format, void *buffer) {
-#ifdef __linux__
-  if (WbSysInfo::isVirtualMachine()) {
-    // Reading the front buffer is not supported by all OpenGL implementations (especially on Linux running in a VM).
-    // In that case, to read the front buffer, we need to swap the buffers, read the back buffer and swap the buffers again.
-    // However, doing this may cause flickering on platforms where reading the front buffer is supported (including macOS).
-    WbWrenOpenGlContext::instance()->swapBuffers(this);
-    wr_scene_get_main_buffer(wr_scene_get_instance(), width, height, format, GL_UNSIGNED_BYTE, GL_BACK, buffer);
-    WbWrenOpenGlContext::instance()->swapBuffers(this);
-  } else
-#endif
-    wr_scene_get_main_buffer(wr_scene_get_instance(), width, height, format, GL_UNSIGNED_BYTE, GL_FRONT, buffer);
+  wr_scene_get_main_buffer(width, height, format, GL_UNSIGNED_BYTE, buffer);
 }

include/wren/scene.h
@@ -68,8 +68,7 @@ void wr_scene_enable_depth_reset(WrScene *scene, bool enable);
 void wr_scene_add_frame_listener(WrScene *scene, void (*listener)());
 void wr_scene_remove_frame_listener(WrScene *scene, void (*listener)());
 
-void wr_scene_get_main_buffer(WrScene *scene, int width, int height, unsigned int format, unsigned int data_type,
-                              unsigned int buffer_type, void *buffer);
+void wr_scene_get_main_buffer(int width, int height, unsigned int format, unsigned int data_type, void *buffer);
 
 void wr_scene_init_frame_capture(WrScene *scene, int pixel_buffer_count, unsigned int *pixel_buffer_ids, int frame_size);
 void wr_scene_bind_pixel_buffer(WrScene *scene, int buffer);

src/webots/gui/WbView3D.cpp
@@ -332,7 +332,7 @@ void WbView3D::refresh() {
   const WbSimulationState *const sim = WbSimulationState::instance();
   mPhysicsRefresh = true;
   if (mScreenshotRequested)
-    renderNow();
+    renderNow(true, true);
   else if (sim->isPaused())
     renderLater();
   else if (WbVideoRecorder::instance()->isRecording()) {

src/wren/Scene.cpp
@@ -101,11 +101,10 @@ namespace wren {
     assert(!Texture2d::cachedItemCount());
   }
 
-  void Scene::getMainBuffer(int width, int height, unsigned int format, unsigned int data_type, unsigned int buffer_type,
-                            void *buffer) {
-    assert(buffer_type == GL_FRONT || buffer_type == GL_BACK);
-    glstate::bindFrameBuffer(0);
-    glReadBuffer(buffer_type);
+  void Scene::getMainBuffer(int width, int height, unsigned int format, unsigned int data_type, void *buffer) {
+    glstate::bindFrameBuffer(instance()->mMainViewport->frameBuffer()->glName());
+    glReadBuffer(GL_COLOR_ATTACHMENT0);
+    glFinish();
     glReadPixels(0, 0, width, height, format, data_type, buffer);
   }
 
@@ -884,9 +883,8 @@ void wr_scene_apply_pending_updates(WrScene *scene) {
   reinterpret_cast<wren::Scene *>(scene)->applyPendingUpdates();
 }
 
-void wr_scene_get_main_buffer(WrScene *scene, int width, int height, unsigned int format, unsigned int data_type,
-                              unsigned int buffer_type, void *buffer) {
-  reinterpret_cast<wren::Scene *>(scene)->getMainBuffer(width, height, format, data_type, buffer_type, buffer);
+void wr_scene_get_main_buffer(int width, int height, unsigned int format, unsigned int data_type, void *buffer) {
+  wren::Scene::getMainBuffer(width, height, format, data_type, buffer);
 }
 
 void wr_scene_init_frame_capture(WrScene *scene, int pixel_buffer_count, unsigned int *pixel_buffer_ids, int frame_size) {

src/wren/Scene.hpp
@@ -53,8 +53,7 @@ namespace wren {
     void reset();
     static void applyPendingUpdates();
 
-    static void getMainBuffer(int width, int height, unsigned int format, unsigned int data_type, unsigned int buffer_type,
-                              void *buffer);
+    static void getMainBuffer(int width, int height, unsigned int format, unsigned int data_type, void *buffer);
     void initFrameCapture(int pixelBufferCount, unsigned int *pixelBufferIds, int frameSize);
     static void bindPixelBuffer(int buffer);
     void *mapPixelBuffer(unsigned int accessMode);

tests/Makefile
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-.PHONY: api cache other_api parser physics protos rendering with_rendering
+.PHONY: api cache other_api parser physics protos rendering
 
-release debug profile: api cache other_api parser physics protos rendering with_rendering
+release debug profile: api cache other_api parser physics protos rendering
 
 clean: api cache parser physics protos rendering
 	@-rm -rf *.txt \
@@ -56,6 +56,3 @@ protos:
 
 rendering:
 	+@make -s -C rendering $(MAKECMDGOALS)
-
-with_rendering:
-	+@make -s -C with_rendering $(MAKECMDGOALS)

commit 78cb2584e0496071451e603aa30b7dd8aba817b1

Fix video recording size bug introduced by PR #6622.

src/webots/gui/WbVideoRecorder.cpp
@@ -346,7 +346,7 @@ void WbVideoRecorder::stopRecording(bool canceled) {
 void WbVideoRecorder::writeSnapshot(unsigned char *frame) {
   QString fileName = nextFileName();
   FrameWriterThread *thread =
-    new FrameWriterThread(frame, fileName, mVideoResolution * mScreenPixelRatio, mScreenPixelRatio, mVideoQuality);
+    new FrameWriterThread(frame, fileName, mVideoResolution, 1, mVideoQuality);
   connect(thread, &QThread::finished, this, &WbVideoRecorder::terminateSnapshotWrite);
   thread->start();
 }
@@ -481,7 +481,7 @@ void WbVideoRecorder::createMpeg() {
     // bitrate range between 4 and 24000000
     // cast into 'long long int' is mandatory on 32-bit machine
     long long int bitrate = (long long int)mVideoQuality * mMovieFPS * mVideoResolution.width() * mVideoResolution.height() /
-                            256 / (mScreenPixelRatio * mScreenPixelRatio);
+                            256;
 
     QTextStream stream(&ffmpegScript);
 #ifndef _WIN32

src/webots/gui/WbWrenWindow.cpp
@@ -327,9 +327,8 @@ QImage WbWrenWindow::grabWindowBufferNow() {
 void WbWrenWindow::initVideoPBO() {
   WbWrenOpenGlContext::makeWrenCurrent();
 
-  const int ratio = (int)devicePixelRatio();
-  mVideoWidth = width() * ratio;
-  mVideoHeight = height() * ratio;
+  mVideoWidth = width();
+  mVideoHeight = height();
   const int size = 4 * mVideoWidth * mVideoHeight;
   wr_scene_init_frame_capture(wr_scene_get_instance(), PBO_COUNT, mVideoPBOIds, size);
   mVideoPBOIndex = -1;