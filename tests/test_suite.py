ee341c455de46ae24cd3e84a4f94ff8dfee10384
Dean Brettle <dean@brettle.com>
2024-08-29 07:50:30 -0700

    Skip billboard.wbt test during macos CI.
    The test passes locally, so the failure is presumably due to the CI server being headless.

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -200,12 +200,14 @@ def generateWorldsList(groupName):
         for filename in filenames:
             # speaker test not working on github action because of missing sound drivers
             # robot window and movie recording test not working on BETA Ubuntu 22.04 GitHub Action environment
+            # billboard test not working in macos GitHub Action environment
             if (not filename.endswith('_temp.wbt') and
                     not ('GITHUB_ACTIONS' in os.environ and (
                         filename.endswith('speaker.wbt') or
                         filename.endswith('local_proto_with_texture.wbt') or
                         (filename.endswith('robot_window_html.wbt') and is_ubuntu_22_04) or
-                        (filename.endswith('supervisor_start_stop_movie.wbt') and is_ubuntu_22_04)
+                        (filename.endswith('supervisor_start_stop_movie.wbt') and is_ubuntu_22_04) or
+                        (filename.endswith('billboard.wbt') and sys.platform == 'darwin')
                     ))):
                 worldsList.append(filename)
 
