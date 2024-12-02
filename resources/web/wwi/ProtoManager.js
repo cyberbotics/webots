116fc81f38ad7285c36471dc02d67589533a55fc
MNikoliCC <nikolicmilos3610@gmail.com>
2024-12-02 17:36:43 +0100

    Revert The URL-s to the 2023b Version

--- resources/web/wwi/ProtoManager.js
+++ resources/web/wwi/ProtoManager.js
@@ -150,7 +150,7 @@ export default class ProtoManager {
     appearance.setAttribute('baseColor', '0.8 0.8 0.8');
     const imageTexture = xml.createElement('ImageTexture');
     imageTexture.setAttribute('id', getAnId());
-    imageTexture.setAttribute('url', 'https://raw.githubusercontent.com/cyberbotics/webots/R2025a/projects/default/worlds/textures/grid.png');
+    imageTexture.setAttribute('url', 'https://raw.githubusercontent.com/cyberbotics/webots/R2023b/projects/default/worlds/textures/grid.png');
     imageTexture.setAttribute('role', 'baseColorMap');
     appearance.appendChild(imageTexture);
     const textureTransform = xml.createElement('TextureTransform');
