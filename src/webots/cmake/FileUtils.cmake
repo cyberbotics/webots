f7d7cbaaa0b939ac3e1e2363bf92886504194ea3
Kreijstal <rainb@tfwno.gf>
2024-12-22 17:27:59 +0100

    feat: Add CMake configuration for Wren library and FileUtils utility

---
+++ src/webots/cmake/FileUtils.cmake
@@ -0,0 +1,15 @@
+# Helper function to get source files
+function(get_source_files DIR SOURCES_OUT)
+    file(GLOB SOURCES "${DIR}/*.cpp")
+    set(${SOURCES_OUT} ${SOURCES} PARENT_SCOPE)
+endfunction()
+
+# Helper function to create a library with standard settings
+function(add_webots_library LIB_NAME)
+    get_filename_component(DIR_NAME ${CMAKE_CURRENT_SOURCE_DIR} NAME)
+    file(GLOB SOURCES "${CMAKE_CURRENT_SOURCE_DIR}/*.cpp")
+    
+    add_library(${DIR_NAME}_lib ${SOURCES})
+    target_include_directories(${DIR_NAME}_lib PUBLIC ${CMAKE_CURRENT_SOURCE_DIR})
+    target_link_libraries(${DIR_NAME}_lib PRIVATE Qt6::Core)
+endfunction()
