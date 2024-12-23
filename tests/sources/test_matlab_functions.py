4558c0f4d867adaa7ce60ba270206fb666f54511
CoolSpy3 <coolspythree@gmail.com>
2024-12-22 20:59:59 -0800

    update matlab source test to work with new def file

--- tests/sources/test_matlab_functions.py
+++ tests/sources/test_matlab_functions.py
@@ -30,17 +30,20 @@ class TestMatlabFunctions(unittest.TestCase):
         if sys.version_info[0] >= 3:
             mgenerate.UPDATE = False
             mgenerate.main()
-            """Get all the required function."""
+            """Get all the required functions."""
             skippedLines = [
-                'wbr',
-                'microphone',
-                'remote_control',
-                'robot',
-                'wb_device_get_type',
-                'wb_node_get_name',
-                'wbu_string',
-                'lookup_table_size',
-                'EXPORTS'
+                # Comments / def keywords
+                ';', 'EXPORTS',
+                'abstract_camera', # Abstract Camera functions should be called through their variant functions (e.g. wb_camera_get_*)
+                'microphone', # Microphone Node is Experimental
+                'radio', # Radio Plugin
+                'remote_control', 'wbr', # Remote Control Plugin
+                'init', 'cleanup', # These are usually called automatically
+                'robot', # Many robot functions are used internally by the C API (e.x. wb_robot_mutex_*)
+                'lookup_table_size', # Matlab lets you get the size of an array, so these functions are not needed
+                'no_mutex', # "no_mutex" function variants should only be used internally
+                'get_unique_id', # Only used internally by the C API
+                'wbu_string' # String manipulation functions are not needed
             ]
             self.functions = []
             filename = os.path.join(WEBOTS_HOME, 'src', 'controller', 'c', 'Controller.def')
@@ -50,8 +53,23 @@ class TestMatlabFunctions(unittest.TestCase):
             )
             with open(filename) as file:
                 for line in file:
-                    if not any(skippedLine in line for skippedLine in skippedLines) and not line[3:].isupper():
-                        self.functions.append(line.replace('\n', ''))
+                    if line.strip().startswith('wb') and not any(skippedLine in line for skippedLine in skippedLines) and not line[3:].isupper():
+                        line = line.strip()
+                        self.functions.append(line[:line.find(' ')]) # Line is of the form "symbol_name @ address"
+
+            # These functions are used internally by the Matlab API
+            self.functions.remove('wb_camera_recognition_get_object')
+            self.functions.remove('wb_lidar_get_point')
+            self.functions.remove('wb_mouse_get_state_pointer')
+            self.functions.remove('wb_radar_get_target')
+
+            self.functions.remove('wb_device_get_type') # Deprecated since 8.0.0
+
+            # Not Yet Implemented
+            self.functions.remove('wb_node_get_name')
+            self.functions.remove('wb_file_get_extension')
+            self.functions.remove('wbu_system_tmpdir')
+            self.functions.remove('wbu_system_webots_instance_path')
 
     @unittest.skipIf(sys.version_info[0] < 3, "not supported by Python 2.7")
     def test_matlab_function_exists(self):
