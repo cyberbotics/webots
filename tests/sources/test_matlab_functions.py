e757b559936020fffcd0ce6d1ec9149d5c9aca4e
CoolSpy3 <coolspythree@gmail.com>
2024-12-22 21:45:56 -0800

    formatting

--- tests/sources/test_matlab_functions.py
+++ tests/sources/test_matlab_functions.py
@@ -32,18 +32,17 @@ class TestMatlabFunctions(unittest.TestCase):
             mgenerate.main()
             """Get all the required functions."""
             skippedLines = [
-                # Comments / def keywords
-                ';', 'EXPORTS',
-                'abstract_camera', # Abstract Camera functions should be called through their variant functions (e.g. wb_camera_get_*)
-                'microphone', # Microphone Node is Experimental
-                'radio', # Radio Plugin
-                'remote_control', 'wbr', # Remote Control Plugin
-                'init', 'cleanup', # These are usually called automatically
-                'robot', # Many robot functions are used internally by the C API (e.x. wb_robot_mutex_*)
-                'lookup_table_size', # Matlab lets you get the size of an array, so these functions are not needed
-                'no_mutex', # "no_mutex" function variants should only be used internally
-                'get_unique_id', # Only used internally by the C API
-                'wbu_string' # String manipulation functions are not needed
+                ';', 'EXPORTS', # Comments / Keywords
+                'abstract_camera',  # Abstract Camera functions should be called through their variant functions (e.g. wb_camera_get_*)
+                'microphone',  # Microphone Node is Experimental
+                'radio',  # Radio Plugin
+                'remote_control', 'wbr',  # Remote Control Plugin
+                'init', 'cleanup',  # These are usually called automatically
+                'robot',  # Many robot functions are used internally by the C API (e.x. wb_robot_mutex_*)
+                'lookup_table_size',  # Matlab lets you get the size of an array, so these functions are not needed
+                'no_mutex',  # "no_mutex" function variants should only be used internally
+                'get_unique_id',  # Only used internally by the C API
+                'wbu_string'  # String manipulation functions are not needed
             ]
             self.functions = []
             filename = os.path.join(WEBOTS_HOME, 'src', 'controller', 'c', 'Controller.def')
@@ -55,7 +54,7 @@ class TestMatlabFunctions(unittest.TestCase):
                 for line in file:
                     if line.strip().startswith('wb') and not any(skippedLine in line for skippedLine in skippedLines) and not line[3:].isupper():
                         line = line.strip()
-                        self.functions.append(line[:line.find(' ')]) # Line is of the form "symbol_name @ address"
+                        self.functions.append(line[:line.find(' ')])  # Line is of the form "symbol_name @ address"
 
             # These functions are used internally by the Matlab API
             self.functions.remove('wb_camera_recognition_get_object')
@@ -63,8 +62,8 @@ class TestMatlabFunctions(unittest.TestCase):
             self.functions.remove('wb_mouse_get_state_pointer')
             self.functions.remove('wb_radar_get_target')
 
-            self.functions.remove('wb_device_get_type') # Deprecated since 8.0.0
-            self.functions.remove('wb_file_get_extension') # Part of the internal API
+            self.functions.remove('wb_device_get_type')  # Deprecated since 8.0.0
+            self.functions.remove('wb_file_get_extension')  # Part of the internal API
 
             # Not Yet Implemented
             self.functions.remove('wb_node_get_name')
