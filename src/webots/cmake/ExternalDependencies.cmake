d09d47bd83ffa2fa3bd7aff6c7e2eb740fe5c43c
Kreijstal <rainb@tfwno.gf>
2024-12-26 15:16:23 +0100

    Fixing pico

--- src/webots/cmake/ExternalDependencies.cmake
+++ src/webots/cmake/ExternalDependencies.cmake
@@ -1,35 +1,51 @@
 include(ExternalProject)
 
+# Determine library suffix based on platform
+if(WIN32)
+    set(PICO_LIB_NAME "libpico.dll")
+elseif(APPLE)
+    set(PICO_LIB_NAME "libpico.dylib")
+else()
+    set(PICO_LIB_NAME "libpico.so")
+endif()
+
+# Set paths
+set(PICOTTS_PREFIX ${CMAKE_BINARY_DIR}/external/picotts)
+set(PICOTTS_LIBRARY ${PICOTTS_PREFIX}/lib/${PICO_LIB_NAME})
+set(PICOTTS_INCLUDE_DIR ${PICOTTS_PREFIX}/include)
+
+# Create necessary directories
+file(MAKE_DIRECTORY ${PICOTTS_INCLUDE_DIR})
+
 # PicoTTS
 ExternalProject_Add(
-    picotts
+    picotts_external
     GIT_REPOSITORY https://github.com/Kreijstal/picotts.git
     GIT_TAG master
-    PREFIX ${CMAKE_BINARY_DIR}/external/picotts
+    PREFIX ${PICOTTS_PREFIX}
     SOURCE_SUBDIR pico
     CMAKE_ARGS
-        -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
-    BUILD_IN_SOURCE 0
+        -DCMAKE_INSTALL_PREFIX=${PICOTTS_PREFIX}
+        -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+    BUILD_BYPRODUCTS ${PICOTTS_LIBRARY}
+    # Copy header files after build
+    INSTALL_COMMAND 
+        ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR>/pico/lib ${PICOTTS_INCLUDE_DIR}
+    COMMAND
+        ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR>/pico/compat ${PICOTTS_INCLUDE_DIR}
+    COMMAND
+        ${CMAKE_MAKE_PROGRAM} install
 )
 
-# Find the installed libpico library
-find_library(PICOTTS_LIBRARY
-    NAMES pico libpico
-    PATHS ${CMAKE_BINARY_DIR}/external/picotts/lib ${CMAKE_BINARY_DIR}/external/picotts/<INSTALL_DIR>/lib
-    NO_DEFAULT_PATH
+# Create the imported library target
+add_library(pico::pico SHARED IMPORTED GLOBAL)
+
+# Set properties on the target
+set_target_properties(pico::pico PROPERTIES
+    IMPORTED_LOCATION ${PICOTTS_LIBRARY}
+    INTERFACE_INCLUDE_DIRECTORIES ${PICOTTS_INCLUDE_DIR}
 )
-message(STATUS "PICOTTS_LIBRARY is ${PICOTTS_LIBRARY}")
 
-# Create an IMPORTED target for libpico
-if(PICOTTS_LIBRARY)
-    add_library(pico::pico UNKNOWN IMPORTED)  # Use SHARED on Linux/macOS
-    set_target_properties(pico::pico PROPERTIES
-        IMPORTED_LOCATION ${PICOTTS_LIBRARY}
-        INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_BINARY_DIR}/external/picotts/src/picotts/pico/lib;${CMAKE_BINARY_DIR}/external/picotts/src/picotts/pico/compat"
-    )
-
-    # Add a dependency to ensure picotts is built before we use its library
-    add_dependencies(pico::pico picotts)
-else()
-    message(FATAL_ERROR "Could not find libpico library after building picotts")
-endif()
+# Create a custom target that depends on the external project
+add_custom_target(picotts ALL DEPENDS picotts_external)
+add_dependencies(pico::pico picotts)
