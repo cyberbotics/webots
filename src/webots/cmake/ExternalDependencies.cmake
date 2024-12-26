4a0cf2afcfec8342baf03e24bd5287228a7a0f2c
Kreijstal <rainb@tfwno.gf>
2024-12-26 14:35:44 +0100

    add cmakefiles

--- src/webots/cmake/ExternalDependencies.cmake
+++ src/webots/cmake/ExternalDependencies.cmake
@@ -7,19 +7,29 @@ ExternalProject_Add(
     GIT_TAG master
     PREFIX ${CMAKE_BINARY_DIR}/external/picotts
     SOURCE_SUBDIR pico
-    CMAKE_ARGS 
+    CMAKE_ARGS
         -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
     BUILD_IN_SOURCE 0
 )
 
-# Set variables for include and lib paths
-set(PICOTTS_INCLUDE_DIR 
-    ${CMAKE_BINARY_DIR}/external/picotts/src/picotts/pico/lib
-    ${CMAKE_BINARY_DIR}/external/picotts/src/picotts/pico/compat
+# Find the installed libpico library
+find_library(PICOTTS_LIBRARY
+    NAMES pico libpico
+    PATHS ${CMAKE_BINARY_DIR}/external/picotts/lib ${CMAKE_BINARY_DIR}/external/picotts/<INSTALL_DIR>/lib
+    NO_DEFAULT_PATH
 )
-set(PICOTTS_LIB_DIR ${CMAKE_BINARY_DIR}/external/picotts/lib)
+message(STATUS "PICOTTS_LIBRARY is ${PICOTTS_LIBRARY}")
 
-# Add compile definitions and flags for PicoTTS
-add_compile_definitions(PICO_EXTERN_C)
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fno-strict-aliasing -D__STDC_VERSION__=199901L")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-strict-aliasing")
+# Create an IMPORTED target for libpico
+if(PICOTTS_LIBRARY)
+    add_library(pico::pico UNKNOWN IMPORTED)  # Use SHARED on Linux/macOS
+    set_target_properties(pico::pico PROPERTIES
+        IMPORTED_LOCATION ${PICOTTS_LIBRARY}
+        INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_BINARY_DIR}/external/picotts/src/picotts/pico/lib;${CMAKE_BINARY_DIR}/external/picotts/src/picotts/pico/compat"
+    )
+
+    # Add a dependency to ensure picotts is built before we use its library
+    add_dependencies(pico::pico picotts)
+else()
+    message(FATAL_ERROR "Could not find libpico library after building picotts")
+endif()
