ee531290ee8d52e71312c0cefcc796b596a7b828
CoolSpy3 <coolspythree@gmail.com>
2026-06-29 23:30:35 -0700

    try rounding viewport size

--- src/wren/Scene.cpp
+++ src/wren/Scene.cpp
@@ -284,8 +284,8 @@ namespace wren {
         if (!offScreen && mCurrentViewport == mMainViewport && mCurrentViewport->frameBuffer()) {
           glstate::bindDrawFrameBuffer(0);
           mCurrentViewport->frameBuffer()->blit(0, true, false, false, 0, 0, 0, 0, 0, 0,
-                                                mCurrentViewport->width() * mCurrentViewport->pixelRatio(),
-                                                mCurrentViewport->height() * mCurrentViewport->pixelRatio());
+                                                round(mCurrentViewport->width() * mCurrentViewport->pixelRatio()),
+                                                round(mCurrentViewport->height() * mCurrentViewport->pixelRatio()));
         }
       }
 
